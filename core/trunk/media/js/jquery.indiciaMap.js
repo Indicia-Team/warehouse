bb7781a0d5144d6d850e65cd219a09ef2cce70ac
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-03-26 11:42:49 +0000

    Added new client-side map plugin for jQuery.

---
+++ core/trunk/media/js/jquery.indiciaMap.js
@@ -0,0 +1,10 @@
+/**
+* Re-implementation of the map helper as a purely client-side library, because the mixed method in use was messy and
+* going to be difficult to maintain.
+*/
+
+(function($)
+{
+  
+}
+)(jQuery);
