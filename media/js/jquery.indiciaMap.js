4bbb0c22a5b19b85502ccdb695cad65d2faaa348
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-03-26 18:15:35 +0000

    Temporarily reverted map.php, further changes.

--- media/js/jquery.indiciaMap.js
+++ media/js/jquery.indiciaMap.js
@@ -20,7 +20,7 @@
 	     initial_zoom: 7,
 	     proxy: "http://localhost/cgi-bin/proxy.cgi?url=",
 	     displayFormat: "image/png",
-	     presetLayers: ['google_physical'],
+	     presetLayers: ['google_streets'],
 	     openLayersOptions: 
 	     {
 	       projection: new OpenLayers.Projection("EPSG:900913"),
@@ -35,15 +35,15 @@
     // Potential layers to add to the map
     this.presetLayers =
     {
-      google_physical : new OpenLayers.Layer.Google('Google Physical', {type: G_PHYSICAL_MAP, 'sphericalMercator': 'true'}),
-	     google_streets : new OpenLayers.Layer.Google('Google Streets', {numZoomLevels : 20, 'sphericalMercator': true}),
-	     google_hybrid : new OpenLayers.Layer.Google('Google Hybrid', {type: G_HYBRID_MAP, numZoomLevels: 20, 'sphericalMercator': true}),
-	     google_satellite : new OpenLayers.Layer.Google('Google Satellite', {type: G_SATELLITE_MAP, numZoomLevels: 20, 'sphericalMercator': true}),
-	     openlayers_wms : new OpenLayers.Layer.WMS('OpenLayers WMS', 'http://labs.metacarta.com/wms/vmap0', {layers: 'basic', 'sphericalMercator': true}),
-	     nasa_mosaic : new OpenLayers.Layer.WMS('NASA Global Mosaic', 'http://t1.hypercube.telascience.org/cgi-bin/landsat7', {layers: 'landsat7', 'sphericalMercator': true}),
-	     virtual_earth : new OpenLayers.Layer.VirtualEarth('Virtual Earth', {'type': VEMapStyle.Aerial, 'sphericalMercator': true}),
-	     multimap_default : new OpenLayers.Layer.MultiMap('MultiMap', {sphericalMercator: true}),
-	     multimap_landranger : new OpenLayers.Layer.MultiMap('OS Landranger', {sphericalMercator: true, dataSource: 904})
+      google_physical : "new OpenLayers.Layer.Google('Google Physical', {type: G_PHYSICAL_MAP, 'sphericalMercator': 'true'})",
+	     google_streets : "new OpenLayers.Layer.Google('Google Streets', {numZoomLevels : 20, 'sphericalMercator': true})",
+	     google_hybrid : "new OpenLayers.Layer.Google('Google Hybrid', {type: G_HYBRID_MAP, numZoomLevels: 20, 'sphericalMercator': true})",
+	     google_satellite : "new OpenLayers.Layer.Google('Google Satellite', {type: G_SATELLITE_MAP, numZoomLevels: 20, 'sphericalMercator': true})",
+	     openlayers_wms : "new OpenLayers.Layer.WMS('OpenLayers WMS', 'http://labs.metacarta.com/wms/vmap0', {layers: 'basic', 'sphericalMercator': true})",
+	     nasa_mosaic : "new OpenLayers.Layer.WMS('NASA Global Mosaic', 'http://t1.hypercube.telascience.org/cgi-bin/landsat7', {layers: 'landsat7', 'sphericalMercator': true})",
+	     virtual_earth : "new OpenLayers.Layer.VirtualEarth('Virtual Earth', {'type': VEMapStyle.Aerial, 'sphericalMercator': true})",
+	     multimap_default : "new OpenLayers.Layer.MultiMap('MultiMap', {sphericalMercator: true})",
+	     multimap_landranger : "new OpenLayers.Layer.MultiMap('OS Landranger', {sphericalMercator: true, dataSource: 904})"
     };
     
     this.construct = function(options)
@@ -55,17 +55,22 @@
       {
 	this.settings = settings;
 	// Constructs the map
-	this.map = new OpenLayers.map(this.attr('id'), this.settings.openLayersOptions);
+	this.map = new OpenLayers.Map($(this).attr('id'), this.settings.openLayersOptions);
 	
 	// Iterate over the preset layers, adding them to the map
 	$.each(this.settings['presetLayers'], function(i, item)
 	{
+	  alert(item);
 	  // Check whether this is a defined layer
 	  if (this.presetLayers.hasOwnProperty(item))
 	  {
-	    this.addLayer(this.presetLayers['item']);
+	    this.addLayer(eval(this.presetLayers['item']));
 	  }
 	});
+	if (this.settings.presetLayers.size > 1)
+	{
+	  this.map.addControl(new OpenLayers.Control.LayerSwitcher());
+	}
       });
     };
     
--- modules/demo/map2.php
+++ modules/demo/map2.php
@@ -2,12 +2,18 @@
 <head>
 <title>Map helper test</title>
 <script type='text/javascript' src='../../media/js/jquery.js' ></script>
+<script type='text/javascript' src='../../media/js/json2.js' ></script>
 <script type='text/javascript' src='../../media/js/OpenLayers.js' ></script>
 <script type='text/javascript' src='../../media/js/jquery.indiciaMap.js' ></script>
 <script src='http://dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6.1'></script>
-<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=null" type="text/javascript"></script>
+<script src="http://maps.google.com/maps?file=api&v=2&key=null" type="text/javascript"></script>
 <script type='text/javascript'>
-(function($){ $('#map').indiciaMap(); })(jQuery);
+(function($){ 
+$(document).ready(function()
+{
+$('#map').indiciaMap(); 
+});
+})(jQuery);
 </script>
 </head>
 <body>
