ca70cd62e75702b190064e6988534e3cebc6f511
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-03-27 15:53:24 +0000

    Further changes to map helpers

--- core/trunk/media/js/jquery.indiciaMap.edit.js
+++ core/trunk/media/js/jquery.indiciaMap.edit.js
@@ -11,13 +11,21 @@
 
 (function($)
 {
-  $.extend( indiciaMapEdit : function()
+  $.extend({indiciaMapEdit : new function()
   {
     this.defaults = 
     {
       wkt : null,
-      input_field_name : 'entered_sref',
-	    geom_field_name : 'geom'
+	    layerName : "Current location boundary",
+	    input_field_name : 'entered_sref',
+	    geom_field_name : 'geom',
+	    systems_field_name : 'entered_sref_systems',
+	    systems : {WGS84 : "Lat/Long on th WGS84 Datum", OSGB : "Ordnance Survey"}
+	    placeControls : true,
+	    controlPosition : 0,
+	    boundaryStyle: new OpenLayers.Util.applyDefaults({ strokeWidth: 1, strokeColor: "#ff0000", fillOpacity: 0.3,
+							       fillColor:"#ff0000" },
+							       OpenLayers.Feature.Vector.style['default']) 
     };
     
     this.construct = function(options)
@@ -27,10 +35,60 @@
       return this.each(function()
       {
 	this.settings = settings;
-	var div = this;
+	
+	// Add an editable layer to the map
+	var editLayer = new OpenLayers.Layer.Vector(this.settings.layerName, {style: this.settings.boundaryStyle, 'sphericalMercator': true});
+	this.map.editLayer = editLayer;
+	this.map.addLayers([this.map.editLayer]);
+	
+	if (this.settings.wkt != null)
+	{
+	  showWktFeature(this);
+	}
+	
+	if (this.settings.placeControls)
+	{
+	  placeControls(this);
+	}
+	
       });
+    };
+    
+    // Private functions
+    
+    /**
+    * Adds controls into the div in the specified position.
+    */
+    function placeControls(div)
+    {
+      var pos = div.settings.controlPosition;
     }
-  };
+    
+    function showWktFeature(div) {
+      var editlayer = div.map.editLayer;
+      var wkt = div.settings.wkt;
+      var parser = new OpenLayers.Format.WKT();
+      var feature = parser.read(wkt);
+      editlayer.destroyFeatures();
+      editlayer.addFeatures([feature]);
+      var bounds=feature.geometry.getBounds();
+      // extend the boundary to include a buffer, so the map does not zoom too tight.
+      dy = (bounds.top-bounds.bottom)/1.5;
+      dx = (bounds.right-bounds.left)/1.5;
+      bounds.top = bounds.top + dy;
+      bounds.bottom = bounds.bottom - dy;
+      bounds.right = bounds.right + dx;
+      bounds.left = bounds.left - dx;
+      // Set the default view to show something triple the size of the grid square
+      div.map.zoomToExtent(bounds);
+      // if showing a point, don't zoom in too far
+      if (dy==0 && dx==0) {
+	div.map.zoomTo(11);
+      }
+      
+    }
+  }
+  });
   
-  $.fn.extend( indiciaMapEdit : $.indiciaMapEdit.construct );
+  $.fn.extend({ indiciaMapEdit : $.indiciaMapEdit.construct });
 })(jQuery);
--- core/trunk/modules/demo/map2.php
+++ core/trunk/modules/demo/map2.php
@@ -5,13 +5,15 @@
 <script type='text/javascript' src='../../media/js/json2.js' ></script>
 <script type='text/javascript' src='../../media/js/OpenLayers.js' ></script>
 <script type='text/javascript' src='../../media/js/jquery.indiciaMap.js' ></script>
+<script type='text/javascript' src='../../media/js/jquery.indiciaMap.js' ></script>
+<script type='text/javascript' src='../../media/js/jquery.indiciaMap.edit.js' ></script>
 <script src='http://dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6.1'></script>
 <script src="http://maps.google.com/maps?file=api&v=2&key=null" type="text/javascript"></script>
 <script type='text/javascript'>
 (function($){ 
 $(document).ready(function()
 {
-$('#map').indiciaMap();
+$('#map').indiciaMap().indiciaMapEdit();
 });
 })(jQuery);
 </script>
