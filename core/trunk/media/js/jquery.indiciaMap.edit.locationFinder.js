f13d250c748c5a0fad2d854d9c065b11d12987eb
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-03-31 12:00:36 +0000

    Registering components.

--- core/trunk/media/js/jquery.indiciaMap.edit.locationFinder.js
+++ core/trunk/media/js/jquery.indiciaMap.edit.locationFinder.js
@@ -19,6 +19,7 @@
       placeControls : true,
 	    controlPosition : 0,
 	    search_box_name : 'place_search_box',
+	    search_button_name : 'place_search_button',
 	    search_output_name : 'place_search_output',
 	    preferedArea : 'gb',
 	    country : 'United Kingdom',
@@ -45,11 +46,40 @@
     function placeControls(div)
     {
       var pos = div.settings.controlPosition;
+      var inputId = div.settings.search_input_name;
+      var buttonId = div.settings.search_button_name;
+      var searchDivId = div.settings.search_box_name;
+      var outputDivId = div.settings.search_output_name;
+      var closeId = div.settings.search_close_name;
       
-      var html = "<div>";
-      html += "</div>";
+      var html = "<div class='locationFinderControls'>";
+      html += "<label for='"+inputId+"'>Search for place on map:</label>\n";
+      html += "<input type='text' name='"+inputId+"' id='"+inputId+"' />\n";
+      html += "<input type='button' name='"+buttonId+"' id='"+buttonId+"' style='margin-top: -2px' />\n";
+      html += "<div id='"+searchDivId+"' style='display: none'><div id='"+outputDivId+"' />\n";
+      html += "<a href='#' id='"+closeId+"'>Close</a>\n";
+      html += "</div></div>";
+      
+      if (pos == 0)
+      {
+	$(div).before(html);
+      }
+      else
+      {
+	$(div).after(html);
+      }
     }
     
-  }
+    function registerControls(div)
+    {
+      var inputId = '#'+div.settings.search_input_name;
+      var buttonId = '#'+div.settings.search_button_name;
+      var searchDivId = '#'+div.settings.search_box_name;
+      var outputDivId = '#'+div.settings.search_output_name;
+      var closeId = '#'+div.settings.search_close_name;
+      
+      
+    };
+    
   }});
-  })(jQuery);
+})(jQuery);
