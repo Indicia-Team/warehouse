0c06ec337eca1880ba6e102734b8c681e99ae858
nicholas.clarke <nicholas.clarke@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-03-31 12:00:48 +0000

    Location finder now works.

--- modules/demo/map2.php
+++ modules/demo/map2.php
@@ -1,3 +1,6 @@
+<?php include '../../client_helpers/helper_config.php'; 
+$apiKey = helper_config::$geoplanet_api_key;
+?>
 <html>
 <head>
 <title>Map helper test</title>
@@ -7,13 +10,14 @@
 <script type='text/javascript' src='../../media/js/jquery.indiciaMap.js' ></script>
 <script type='text/javascript' src='../../media/js/jquery.indiciaMap.js' ></script>
 <script type='text/javascript' src='../../media/js/jquery.indiciaMap.edit.js' ></script>
+<script type='text/javascript' src='../../media/js/jquery.indiciaMap.edit.locationFinder.js' ></script>
 <script src='http://dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6.1'></script>
 <script src="http://maps.google.com/maps?file=api&v=2&key=null" type="text/javascript"></script>
 <script type='text/javascript'>
 (function($){ 
 $(document).ready(function()
 {
-$('#map').indiciaMap().indiciaMapEdit();
+$('#map').indiciaMap().indiciaMapEdit().locationFinder({apiKey : '<?php echo $apiKey; ?>'});
 });
 })(jQuery);
 </script>
