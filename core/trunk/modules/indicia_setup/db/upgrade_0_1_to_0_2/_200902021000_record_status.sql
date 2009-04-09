3f51cc2e49626a7b04ec67fe6326a5c5d40c1143
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-04-09 12:17:14 +0000

    Removed unnecessary files.

--- core/trunk/modules/indicia_setup/db/upgrade_0_1_to_0_2/_200902021000_record_status.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE occurrences
-ADD COLUMN record_status char(1) DEFAULT 'I' CHECK (record_status IN ('I', 'C', 'V'));
-COMMENT ON COLUMN occurrences.record_status IS 'Progress of this record. I - in progress, C - completed, V - verified.';
