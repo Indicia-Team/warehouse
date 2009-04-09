1c1145294f016fb7c53e2ae05005f216a706d3a7
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-04-09 12:45:03 +0000

    More demo module tidying

--- core/trunk/modules/demo/map_data_entry_helper.php
+++ core/trunk/modules/demo/map_data_entry_helper.php
@@ -2,11 +2,15 @@
 <html>
 <head>
 <title>Map helper test</title>
+<link rel="stylesheet" href="demo.css" type="text/css" media="screen">
 </head>
 <body>
-<?php 
+<div id="wrap">
+<h1>Data Entry Helper Map</h1>
+<?php
 echo data_entry_helper::map('map', array('google_physical', 'virtual_earth'), true, true, null, true);
 echo data_entry_helper::dump_javascript();
 ?>
+</div>
 </body>
 </html>
--- core/trunk/modules/demo/viewsource.php
+++ core/trunk/modules/demo/viewsource.php
@@ -1,20 +1,21 @@
 <?php
 $allowed = array(
-	'data_entry/test_data_entry.php',
-	'data_entry/species_checklist.php',
-	'map_data_entry_helper.php',
-	'map_javascript_classes.php',
-	'occurrence_grid.php',
-	'occurrence.php',
-	'map_polygon_capture.php',
-	'valid.php'
+  'data_entry/test_data_entry.php',
+  'data_entry/species_checklist.php',
+  'map_data_entry_helper.php',
+  'map_javascript_classes.php',
+  'occurrence_grid.php',
+  'occurrence.php',
+  'map_polygon_capture.php',
+  'valid.php',
+  'advanced_data_entry/header.php'
 );
 
 if ($_GET['file']) {
-	if (in_array($_GET['file'], $allowed)) {
-		highlight_file($_GET['file']);
-	} else {
-		echo "file not in list of allowed source code files";
-	}
+  if (in_array($_GET['file'], $allowed)) {
+    highlight_file($_GET['file']);
+  } else {
+    echo "file not in list of allowed source code files";
+  }
 }
 ?>
