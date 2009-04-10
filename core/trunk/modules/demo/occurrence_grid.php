287e3023b967abaabcd893e08cf4544b7e765df5
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-04-10 09:10:05 +0000

    Another configuration fix.

--- core/trunk/modules/demo/occurrence_grid.php
+++ core/trunk/modules/demo/occurrence_grid.php
@@ -2,6 +2,7 @@
 include '../../client_helpers/data_entry_helper.php';
 include 'data_entry_config.php';
 $readAuth = data_entry_helper::get_read_auth($config['website_id'], $config['password']);
+$svcUrl = data_entry_helper::$base_url.'/index.php/services';
 ?>
 <html>
 <head>
@@ -12,7 +13,11 @@ $readAuth = data_entry_helper::get_read_auth($config['website_id'], $config['pas
 <script type='text/javascript'>
 (function($) {
 $(document).ready(function(){
-$('div#grid').indiciaDataGrid('occurrence', {actionColumns: {view : "occurrence.php?id=£id£", edit : "data_entry/test_data_entry.php?id=£id£"}, auth : { nonce : "<?php echo $readAuth['nonce']; ?>", auth_token : "<?php echo $readAuth['auth_token']; ?>"}});
+  $('div#grid').indiciaDataGrid('occurrence', {
+    indiciaSvc: "<?php echo $svcUrl; ?>",
+    actionColumns: {view : "occurrence.php?id=£id£", edit : "data_entry/test_data_entry.php?id=£id£"},
+    auth : { nonce : "<?php echo $readAuth['nonce']; ?>", auth_token : "<?php echo $readAuth['auth_token']; ?>"}
+  });
 });
 })(jQuery);
 </script>
