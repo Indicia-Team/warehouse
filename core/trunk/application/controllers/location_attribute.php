2c8ab7d6b0b0763c2c9b56fce18f5474837b51ab
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-04-13 07:39:00 +0000

    Custom attribute page titles.

--- core/trunk/application/controllers/location_attribute.php
+++ core/trunk/application/controllers/location_attribute.php
@@ -2,22 +2,22 @@
 
 class Location_attribute_Controller extends Attr_Gridview_Base_Controller {
 
-	public function __construct() {
-		parent::__construct('location_attribute',		// modelname
-							'Location',				// name
-							'location_attributes_website',	// website table modelname
-							'gv_location_attribute',	// gridmodelname
-							'custom_attribute/index',	// viewname
-							NULL);						// controllerpath
-		$this->columns = array(
-			'website'=>'',
-			'survey'=>'',
-			'caption'=>'',
-			'data_type'=>'');
-		$this->pagetitle = "Custom Location Attribute";
-		$this->model = ORM::factory('location_attribute');
-		$this->auth_filter = $this->gen_auth_filter;
-	}
-	
+  public function __construct() {
+    parent::__construct('location_attribute',		// modelname
+              'Location',				// name
+              'location_attributes_website',	// website table modelname
+              'gv_location_attribute',	// gridmodelname
+              'custom_attribute/index',	// viewname
+              NULL);						// controllerpath
+    $this->columns = array(
+      'website'=>'',
+      'survey'=>'',
+      'caption'=>'',
+      'data_type'=>'');
+    $this->pagetitle = "Location Attribute";
+    $this->model = ORM::factory('location_attribute');
+    $this->auth_filter = $this->gen_auth_filter;
+  }
+
 }
 ?>
--- core/trunk/application/controllers/occurrence_attribute.php
+++ core/trunk/application/controllers/occurrence_attribute.php
@@ -2,22 +2,22 @@
 
 class Occurrence_attribute_Controller extends Attr_Gridview_Base_Controller {
 
-	public function __construct() {
-		parent::__construct('occurrence_attribute',		// modelname
-							'Occurrence',				// name
-							'occurrence_attributes_website',	// website table modelname
-							'gv_occurrence_attribute',	// gridmodelname
-							'custom_attribute/index',	// viewname
-							NULL);						// controllerpath
-		$this->columns = array(
-			'website'=>'',
-			'survey'=>'',
-			'caption'=>'',
-			'data_type'=>'');
-		$this->pagetitle = "Custom Occurrence Attribute";
-		$this->model = ORM::factory('occurrence_attribute');
-		$this->auth_filter = $this->gen_auth_filter;
-	}
-	 
+  public function __construct() {
+    parent::__construct('occurrence_attribute',		// modelname
+              'Occurrence',				// name
+              'occurrence_attributes_website',	// website table modelname
+              'gv_occurrence_attribute',	// gridmodelname
+              'custom_attribute/index',	// viewname
+              NULL);						// controllerpath
+    $this->columns = array(
+      'website'=>'',
+      'survey'=>'',
+      'caption'=>'',
+      'data_type'=>'');
+    $this->pagetitle = "Occurrence Attribute";
+    $this->model = ORM::factory('occurrence_attribute');
+    $this->auth_filter = $this->gen_auth_filter;
+  }
+
 }
 ?>
