c8c96d34c6994855764f2194b646b38fecf337f7
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-04-20 11:04:07 +0000

    Tidying

--- core/trunk/modules/demo/advanced_data_entry/header.php
+++ core/trunk/modules/demo/advanced_data_entry/header.php
@@ -17,12 +17,12 @@ data_entry_helper::clear_session();
 <link rel="stylesheet" href="advanced.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="../../../media/css/default_site.css" type="text/css" media="screen">
 <script type='text/javascript' src='<?php echo $baseUrl ?>/media/js/jquery.js' ></script>
-<script type='text/javascript' src='../../../media/js/json2.js' ></script>
-<script type='text/javascript' src='../../../media/js/OpenLayers.js' ></script>
-<script type='text/javascript' src='../../../media/js/jquery.indiciaMap.js' ></script>
-<script type='text/javascript' src='../../../media/js/jquery.indiciaMap.js' ></script>
-<script type='text/javascript' src='../../../media/js/jquery.indiciaMap.edit.js' ></script>
-<script type='text/javascript' src='../../../media/js/jquery.indiciaMap.edit.locationFinder.js' ></script>
+<script type='text/javascript' src='<?php echo $baseUrl ?>/media/js/json2.js' ></script>
+<script type='text/javascript' src='<?php echo $baseUrl ?>/media/js/OpenLayers.js' ></script>
+<script type='text/javascript' src='<?php echo $baseUrl ?>/media/js/jquery.indiciaMap.js' ></script>
+<script type='text/javascript' src='<?php echo $baseUrl ?>/media/js/jquery.indiciaMap.js' ></script>
+<script type='text/javascript' src='<?php echo $baseUrl ?>/media/js/jquery.indiciaMap.edit.js' ></script>
+<script type='text/javascript' src='<?php echo $baseUrl ?>/media/js/jquery.indiciaMap.edit.locationFinder.js' ></script>
 <script src="http://maps.google.com/maps?file=api&v=2&key=<?php echo $googleApiKey; ?>" type="text/javascript"></script>
 <script type="text/javascript" src="http://developer.multimap.com/API/maps/1.2/<?php echo $multimapApiKey; ?>" ></script>
 <script type="text/javascript" src="http://dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=6.1"></script>
