4a34fddeb8a9c7a357b8edb861924c57ec43d670
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2009-05-02 13:35:11 +0000

    Just some indentation.

--- media/js/jquery.indiciaMap.edit.js
+++ media/js/jquery.indiciaMap.edit.js
@@ -27,41 +27,41 @@
       controlPosition : 0,
       boundaryStyle: new OpenLayers.Util.applyDefaults({ strokeWidth: 1, strokeColor: "#ff0000", fillOpacity: 0.3, fillColor:"#ff0000" }, OpenLayers.Feature.Vector.style['default'])
     };
-    
+
     this.construct = function(options)
     {
       return this.each(function()
       {
-	var settings = {};
-	$.extend(settings, $.indiciaMapEdit.defaults, this.settings, options);
-	this.settings = settings;
-	
-	// Add an editable layer to the map
-	var editLayer = new OpenLayers.Layer.Vector(this.settings.layerName, {style: this.settings.boundaryStyle, 'sphericalMercator': true});
-	this.map.editLayer = editLayer;
-	this.map.addLayers([this.map.editLayer]);
-	
-	if (this.settings.wkt != null)
-	{
-	  showWktFeature(this, this.settings.wkt);
-	}
-	
-	if (this.settings.placeControls)
-	{
-	  placeControls(this);
-	}
-	
-	registerControls(this);
-	
+        var settings = {};
+        $.extend(settings, $.indiciaMapEdit.defaults, this.settings, options);
+        this.settings = settings;
+
+        // Add an editable layer to the map
+        var editLayer = new OpenLayers.Layer.Vector(this.settings.layerName, {style: this.settings.boundaryStyle, 'sphericalMercator': true});
+        this.map.editLayer = editLayer;
+        this.map.addLayers([this.map.editLayer]);
+
+        if (this.settings.wkt != null)
+        {
+          showWktFeature(this, this.settings.wkt);
+        }
+
+        if (this.settings.placeControls)
+        {
+          placeControls(this);
+        }
+
+        registerControls(this);
+
       });
     };
-    
+
     this.showWktFeature = function(div, wkt) {
       var editlayer = div.map.editLayer;
       var parser = new OpenLayers.Format.WKT();
       var feature = parser.read(wkt);
       var bounds=feature.geometry.getBounds();
-      
+
       editlayer.destroyFeatures();
       editlayer.addFeatures([feature]);
       // extend the boundary to include a buffer, so the map does not zoom too tight.
@@ -75,12 +75,12 @@
       div.map.zoomToExtent(bounds);
       // if showing a point, don't zoom in too far
       if (dy==0 && dx==0) {
-	div.map.zoomTo(11);
+        div.map.zoomTo(11);
       }
     };
-    
+
     // Private functions
-    
+
     /**
     * Adds controls into the div in the specified position.
     */
@@ -88,34 +88,34 @@
     {
       var pos = div.settings.controlPosition;
       var systems = div.settings.systems;
-      
-      var html = "<span>";
+
+      var html = "<div>";
       html += "<label for='"+div.settings.input_field_name+"'>"+div.settings.label_spatial_ref+":</label>\n";
       html += "<input type='text' id='" + div.settings.input_field_name + "' name='" + div.settings.input_field_name + "' />\n";
       // hidden field for the geom
       html += "<input type='hidden' class='hidden' id='" + div.settings.geom_field_name + "' name='" + div.settings.geom_field_name + "' />\n";
       if (systems.length == 1)
       {
-	// Hidden field for the system
-	html += "<input type='hidden' id='" + div.settings.systems_field_name + "' name='" + div.settings.systems_field_name + "'/>\n";
+        // Hidden field for the system
+        html += "<input type='hidden' id='" + div.settings.systems_field_name + "' name='" + div.settings.systems_field_name + "'/>\n";
       }
       else
       {
-	html += "<label style=\"width: auto\" for='"+div.settings.systems_field_name+"'>"+div.settings.label_system+":</label>";
-	html += "<select id='" + div.settings.systems_field_name + "' name='" + div.settings.systems_field_name + "' >\n";
-	$.each(systems, function(key, val) { html += "<option value='" + key + "'>" + val + "</option>\n" });
-	html += "</select>\n";
+        html += "<label style=\"width: auto\" for='"+div.settings.systems_field_name+"'>"+div.settings.label_system+":</label>";
+        html += "<select id='" + div.settings.systems_field_name + "' name='" + div.settings.systems_field_name + "' >\n";
+        $.each(systems, function(key, val) { html += "<option value='" + key + "'>" + val + "</option>\n" });
+        html += "</select>\n";
       }
-      html += "</span>";
-      
+      html += "</div>";
+
       if (pos == 0) {
-	$(div).before(html);
+        $(div).before(html);
       }
       else {
-	$(div).after(html);
+        $(div).after(html);
       }
     }
-    
+
     /**
     * Registers controls with the map - binds functions to them and places correct data in if wkt has been supplied.
     */
@@ -125,67 +125,66 @@
       var geomFld = '#' + div.settings.geom_field_name;
       var systemsFld = '#' +div.settings.systems_field_name;
       var map = div.map;
-      
+
       OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
-	defaultHandlerOptions: { 'single': true, 'double': false, 'pixelTolerance': 0, 'stopSingle': false, 'stopDouble': false },
-	  initialize: function(options)
-	  {
-	    this.handlerOptions = OpenLayers.Util.extend({}, this.defaultHandlerOptions);
-	    OpenLayers.Control.prototype.initialize.apply(this, arguments);
-	    this.handler = new OpenLayers.Handler.Click( this, {'click': this.trigger}, this.handlerOptions );
-	  },
-	  trigger: function(e)
-	  {
-	    var lonlat = map.getLonLatFromViewPortPx(e.xy);
-	    // get approx metres accuracy we can expect from the mouse click - about 5mm accuracy.
-	    var precision = map.getScale()/200;
-	    // now round to find appropriate square size
-	    if (precision<30) {
-	      precision=8;
-	    } else if (precision<300) {
-	      precision=6;
-	    } else if (precision<3000) {
-	      precision=4;
-	    } else {
-	      precision=2;
-	    }
-	    $.getJSON(div.settings.indiciaSvc + "/index.php/services/spatial/wkt_to_sref"+
-	    "?wkt=POINT(" + lonlat.lon + "  " + lonlat.lat + ")"+
-	    "&system=" + $(systemsFld).val() +
-	    "&precision=" + precision +
-	    "&callback=?", function(data)
-	    {
-	      $(inputFld).attr('value', data.sref);
-	      map.editLayer.destroyFeatures();
-	      $(geomFld).attr('value', data.wkt);
-	      var parser = new OpenLayers.Format.WKT();
-	      var feature = parser.read(data.wkt);
-	      map.editLayer.addFeatures([feature]);
-	    }
-	    );
-	  }
+  defaultHandlerOptions: { 'single': true, 'double': false, 'pixelTolerance': 0, 'stopSingle': false, 'stopDouble': false },
+    initialize: function(options)
+    {
+      this.handlerOptions = OpenLayers.Util.extend({}, this.defaultHandlerOptions);
+      OpenLayers.Control.prototype.initialize.apply(this, arguments);
+      this.handler = new OpenLayers.Handler.Click( this, {'click': this.trigger}, this.handlerOptions );
+    },
+    trigger: function(e)
+    {
+      var lonlat = map.getLonLatFromViewPortPx(e.xy);
+      // get approx metres accuracy we can expect from the mouse click - about 5mm accuracy.
+      var precision = map.getScale()/200;
+      // now round to find appropriate square size
+      if (precision<30) {
+        precision=8;
+      } else if (precision<300) {
+        precision=6;
+      } else if (precision<3000) {
+        precision=4;
+      } else {
+        precision=2;
+      }
+      $.getJSON(div.settings.indiciaSvc + "/index.php/services/spatial/wkt_to_sref"+
+        "?wkt=POINT(" + lonlat.lon + "  " + lonlat.lat + ")"+
+        "&system=" + $(systemsFld).val() +
+        "&precision=" + precision +
+        "&callback=?", function(data)
+        {
+          $(inputFld).attr('value', data.sref);
+          map.editLayer.destroyFeatures();
+          $(geomFld).attr('value', data.wkt);
+          var parser = new OpenLayers.Format.WKT();
+          var feature = parser.read(data.wkt);
+          map.editLayer.addFeatures([feature]);
+        }
+      );
+    }
       });
-      
+
       // Add the click control to the map.
       var click = new OpenLayers.Control.Click();
       map.addControl(click);
       click.activate();
-      
+
       // Bind functions to the input field.
-      $(inputFld).change(function()
-      {
-	$.getJSON(div.settings.indiciaSvc + "/index.php/services/spatial/sref_to_wkt"+
-	"?sref=" + $(this).val() +
-	"&system=" + $(systemsFld).val() +
-	"&callback=?", function(data){
-	  $(geomFld).attr('value', data.wkt);
-	  showWktFeature(div, data.wkt);
-	}
-	);
+      $(inputFld).change(function() {
+        $.getJSON(div.settings.indiciaSvc + "/index.php/services/spatial/sref_to_wkt"+
+          "?sref=" + $(this).val() +
+          "&system=" + $(systemsFld).val() +
+          "&callback=?", function(data){
+            $(geomFld).attr('value', data.wkt);
+            showWktFeature(div, data.wkt);
+          }
+        );
       });
     }
   }
   });
-  
+
   $.fn.extend({ indiciaMapEdit : $.indiciaMapEdit.construct });
 })(jQuery);
