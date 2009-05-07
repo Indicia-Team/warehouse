210f6160032a9845a27e4a6ee478df06e75e2e55
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-05-07 14:37:49 +0000

    Started work on a port of grab a brid ref. Lots to do yet...

--- core/trunk/media/js/jquery.indiciaMap.edit.js
+++ core/trunk/media/js/jquery.indiciaMap.edit.js
@@ -1,5 +1,21 @@
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ */
+
 /*
-* Editable plugin for jQuery.indiciaMap.
+* Plugin for jQuery.indiciaMap allowing it to be used to grab spatial references
+* for data entry forms.
 * @requires jquery
 * @requires jquery.indiciamap
 *
