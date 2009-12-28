329a911404b3f9d67aead80aec8898954ad9a22e
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2009-12-28 20:37:02 +0000

    Fixes for the treeview control.

--- core/trunk/media/js/jquery.treeview.async.js
+++ core/trunk/media/js/jquery.treeview.async.js
@@ -30,12 +30,17 @@ function load(settings, qfield, qvalue, child, container) {
 	$.getJSON(settings.url+"?callback=?&view="+settings.view+filter+extras,			
 			null,
 		function(response) {
-		  function createNode(parent) {
-			var parsed = settings.parse(this);
-			var current = $("<li/>").attr("id", parsed.value || "").html("<span>" + parsed.caption + "</span>").appendTo(parent);
+		  function createNode(parent) {	
+			// build the node from the content, and copy over the database values for the node
+			var node = settings.nodeTmpl;
+			this.caption = this[settings.captionField];
+			for (var item in this) {
+			  node = node.replace('{'+item+ '}', this[item])
+			}
+			var current = $("<li/>").attr("id", this[settings.valueField] || "").html(node).appendTo(parent);
 			if (this.classes) {
 				current.children("span").addClass(this.classes);
-			}
+			}			
 			if (! this.nochildren ) {				
 				var branch = $("<ul/>").appendTo(current);
 				current.addClass("hasChildren");
@@ -46,24 +51,26 @@ function load(settings, qfield, qvalue, child, container) {
 				}, branch);*/
 			} 
 		  }
-		child.empty();
-		$.each(response, createNode, [child]);
-		if (child.children().length)
-			$(container).treeview({add: child}); 
-		else {
-			/* There are no children for this node, so remove the hitarea (which holds the expand/collapse image)
-			 * for the node ie the <LI> parent of the <UL> identified by child. Also remove/alter the classes
-			 * for the parent, and remove the now used and forever empty <UL> identified by child.
-			 * Have to do this here due to async nature.
-			 */ 
-			if (hitarea.length)
-				hitarea.remove();
-			parent
-				.replaceClass(CLASSES.lastCollapsable, CLASSES.last)
-				.removeClass(CLASSES.collapsable);
-			child.remove();
-		}
-	});
+		  child.empty();
+		  $.each(response, createNode, [child]);		  
+		  if (child.children().length) {
+		    $(container).treeview({add: child});		    		    
+		  }
+          else {
+		    /* There are no children for this node, so remove the hitarea (which holds the expand/collapse image)
+             * for the node ie the <LI> parent of the <UL> identified by child. Also remove/alter the classes
+             * for the parent, and remove the now used and forever empty <UL> identified by child.
+             * Have to do this here due to async nature.
+             */ 
+             if (hitarea.length)
+               hitarea.remove();
+             parent
+		         .replaceClass(CLASSES.lastCollapsable, CLASSES.last)
+			     .removeClass(CLASSES.collapsable);
+		     child.remove();
+		  }
+	    }  
+	);
 }
 
 var proxied = $.fn.treeview;
