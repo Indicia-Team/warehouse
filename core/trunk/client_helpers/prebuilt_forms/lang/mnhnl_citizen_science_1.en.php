f2ddae474ac1aa118cb4f7812183463801634a6e
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2010-01-08 13:11:44 +0000

    Internationalisation jQuery valdiation plugin

---
+++ core/trunk/client_helpers/prebuilt_forms/lang/mnhnl_citizen_science_1.en.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package	Client
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL 3.0
+ * @link 	http://code.google.com/p/indicia/
+ */
+
+global $custom_terms;
+
+/**
+ * Language terms for the mnhnl_citizen_science_1 form.
+ *
+ * @package	Client
+ */
+$custom_terms = array(
+  'happy for contact'=>'Please tick this box if you are happy for us to contact you about this record',
+
+  // Tab instructions
+  'place tab instructions'=>'Step 2 - Place Selection<br/>Please either enter the spatial reference of the observation if you know it, or click on the map to specify the place as accurately as you can.',
+  'species tab instructions'=>'Step 1 - Species Selection<br/>Please click on the species which you observed.',
+);
