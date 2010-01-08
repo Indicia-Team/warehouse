43c121116f76923ee72608a4001f17ce071ab6ed
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2010-01-08 13:10:35 +0000

    Lots of updates to integrate jQuery validation plugin

--- core/trunk/media/js/jquery.treebrowser.js
+++ core/trunk/media/js/jquery.treebrowser.js
@@ -12,10 +12,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
  *
- * @package	Media
- * @author	Indicia Team
- * @license	http://www.gnu.org/licenses/gpl.html GPL 3.0
- * @link 	http://code.google.com/p/indicia/
+ * @package Media
+ * @author  Indicia Team
+ * @license http://www.gnu.org/licenses/gpl.html GPL 3.0
+ * @link    http://code.google.com/p/indicia/
  */
 
 /**
@@ -23,82 +23,78 @@
  */
 
 (function($) {
-  $.extend({
-    indiciaTreeBrowser: new function() {
 
-      this.defaults = {
-        panelClass: "ui-widget ui-widget-content ui-corner-all",
-        headerClass: ""        
-      };
-
-      /**
-      * Constructor. 
-      */
-      this.construct = function(options) {
-    	this.settings = {};
-    	// Extend with defaults and options
-        $.extend(this.settings, $.indiciaTreeBrowser.defaults, options);        
-        load(this);
-      }
-      
-      /**
-       * Load a level into the control 
-       */
-      function load(container, id) {
-    	// Store the clicked on id in the hidden form field.
-    	$("#"+container.settings.valueControl).val(id);    	    	
-    	if (id==undefined) {
-    	  id='NULL';
-    	}    		  
-    	var extras = '';
-    	for (var p in container.settings.extraParams) {
-    	  extras += '&' + p + '=' + container.settings.extraParams[p];
-    	}
-    	$.getJSON(container.settings.url+"?callback=?&"+container.settings.parentField+"="+id+"&view="+container.settings.view+extras,			
-    	    null,
-    	    function(response) {
-    		  if (response.length) {    			
-    			// Build a new list at the bottom of the container, hidden for now
-                var list = $("<ul/>").appendTo(container);
-                list.hide();    			  
-                // Create entries in the list
-    		    $.each(response, function(idx, record) {    			
-                  var node = container.settings.nodeTmpl;
-                  record.caption = record[container.settings.captionField];
-                  for (var item in record) {
-                    node = node.replace('{'+item+ '}', record[item])
-                  }    			
-    			  var current = $('<li class="ui-state-default"/>').attr("id", record[container.settings.valueField] || "").html(node).appendTo(list);
-    			  current.click(function() {
-    			    load(container, this.id);
-    			  });    			  
-    		    });
-    		    // If in single layer mode, hide the previous level lists. Add a go back button. 
-    		    if (container.settings.singleLayer && id!='NULL') {
-    		      container.children().slideUp('fast');
-    		      var back = $('<li class="ui-state-default">'+container.settings.backCaption+"</li>").click(function() {
-    		        list.slideUp('fast', function() {list.remove();} );    		        
-    		        container.children().show('fast');
-    		      }).appendTo(list);
-    		    }
-    		    if (id!='NULL')
-  			      list.slideDown('fast');    		    
-    		    else
-    		      list.fadeIn('slow');    		    
-    		  }
-    		  container.find("li").removeClass('ui-state-highlight');
-    		  $("li#"+id).addClass('ui-state-highlight');    		  
-    	    }
-    	);
-      }     
-    }
-  });
-  
   /**
-   * Extend the function object.
+   * Constructor. 
    */
-  $.fn.extend({
-	indiciaTreeBrowser: $.indiciaTreeBrowser.construct
-  });
+  $.fn.indiciaTreeBrowser = function(options) {
+    this.settings = {};
+    // Extend with defaults and options
+    $.extend(this.settings, $.fn.indiciaTreeBrowser.defaults, options);
+    load(this);
+  };
+      
+  /**
+   * Load a level into the control 
+   */
+  function load(container, id) {
+    // Store the clicked on id in the hidden form field.        
+    $("#"+container.settings.valueControl.replace(':','\\:')).val(id);
+    // Ensure the Warehouse services understand an empty value
+    if (id===undefined) {
+      id='NULL';
+    }
+    var extras = '';
+    for (var p in container.settings.extraParams) {
+      extras += '&' + p + '=' + container.settings.extraParams[p];
+    }
+    container.find("li").removeClass('ui-state-highlight');
+    $("li#"+id).addClass('ui-state-highlight');
+    $.getJSON(container.settings.url+"?callback=?&"+container.settings.parentField+"="+id+"&view="+container.settings.view+extras,      
+      null,
+      function(response) {
+      if (response.length) {
+        var itemClass;
+        // Build a new list at the bottom of the container, hidden for now
+        var list = $("<ul/>").appendTo(container);
+        list.hide();            
+        // Create entries in the list
+        $.each(response, function(idx, record) {          
+          var node = container.settings.nodeTmpl;
+          record.caption = record[container.settings.captionField];
+          for (var item in record) {
+            node = node.replace(new RegExp('{'+item+'}','g'), record[item]);
+          }          
+          itemClass=container.settings.listItemClass;
+          if (record[container.settings.valueField]==container.settings.defaultValue) {
+            itemClass += ' ui-state-highlight';
+          }
+          var current = $('<li class="'+itemClass+'"/>').attr("id", record[container.settings.valueField] || "").html(node).appendTo(list);
+          current.click(function() {
+            load(container, this.id);
+          });            
+        });
+        // If in single layer mode, hide the previous level lists. Add a go back button. 
+        if (container.settings.singleLayer && id!='NULL') {
+          container.children().slideUp('fast');
+          $('<li class="ui-state-default">'+container.settings.backCaption+"</li>").click(function() {
+            list.slideUp('fast', function() {list.remove();} );                
+            container.children().show('fast');
+          }).appendTo(list);
+        }
+        if (id!='NULL') {
+          list.slideDown('fast');            
+        } else {
+          list.fadeIn('slow');
+        }
+      }                    
+    });
+  }
+
 })(jQuery);
-     
+
+$.fn.indiciaTreeBrowser.defaults = {
+    panelClass: "ui-widget ui-widget-content ui-corner-all",
+    headerClass: "",
+    listItemClass: "ui-state-default ui-corner-all"
+};
