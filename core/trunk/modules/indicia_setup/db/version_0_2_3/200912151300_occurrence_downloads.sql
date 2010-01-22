d6669f5d0a37beaed9d8eeb3567adcf485c588c2
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2010-01-22 11:38:58 +0000

    Fixed an SQL syntax error.

--- core/trunk/modules/indicia_setup/db/version_0_2_3/200912151300_occurrence_downloads.sql
+++ core/trunk/modules/indicia_setup/db/version_0_2_3/200912151300_occurrence_downloads.sql
@@ -4,4 +4,4 @@ ADD COLUMN downloaded_on timestamp, --Date occurrence downloaded out of system;
 ADD CONSTRAINT occurrences_downloaded_flag_check CHECK ((downloaded_flag = ANY (ARRAY['N'::bpchar, 'I'::bpchar, 'F'::bpchar])));
 
 COMMENT ON COLUMN occurrences.downloaded_on IS 'Date occurrence downloaded out of system'; 
-COMMENT ON COLUMN occurrences.downloaded_flag IS 'Downloaded status flag: 'N' - not downloaded, 'I' - Initial download, 'F' - Final download'; 
+COMMENT ON COLUMN occurrences.downloaded_flag IS 'Downloaded status flag: N - not downloaded, I - Initial download, F - Final download'; 
