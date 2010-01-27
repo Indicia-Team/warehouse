482d8645d29952808a37cd26e18ff916721bbd52
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2010-01-27 09:08:53 +0000

    Internationalised MY_html.php

--- application/i18n/en_GB/misc.php
+++ application/i18n/en_GB/misc.php
@@ -2,10 +2,18 @@
 
 $lang = array
 (
+  // separators
 	'x_y_separator' => ',',
 	'd_m_s_separator' => ':', // degrees, minutes, seconds for Lat/Long
+  // other language strings
+  'cancel'=>'Cancel',
+  'confirm_delete'=>'Are you sure you want to delete this record?',
+  'delete'=>'Delete',
   'indicia_version' => 'Indicia Version',
+  'lookup_existing_record'=>'lookup existing record',
   'new_image'=>'New Image',
+  'save'=>'Save'
+  
 );
 
 ?>
