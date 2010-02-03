24bf3e04dbb30fabcfba66e94b15227ca5d4eba2
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2010-02-03 11:50:10 +0000

    First drop of new language files for citizen science form. Not tested!

---
+++ client_helpers/prebuilt_forms/lang/mnhnl_citizen_science_1.lu.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package	Client
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL 3.0
+ * @link 	http://code.google.com/p/indicia/
+ */
+
+global $custom_terms;
+
+/**
+ * Language terms for the mnhnl_citizen_science_1 form.
+ *
+ * @package	Client
+ */
+$custom_terms = array(
+  'abundance' => 'Zuel',
+  'close'=>'Zoumaachen',
+  'email'=>'Email',
+  'first name'=>'Virnumm',
+  'happy for contact'=>'Ech sinn averstan eventuell zu dëser Beobachtung kontaktéiert ze ginn',
+  'next step'=>'Weider',
+  'occurrence:taxa_taxon_list_id'=>'Art',
+  'phone number'=>'Telefonsnummer',
+  'prev step'=>'Zréck',
+  'save'=>'Späicheren',
+  'sample:comment'=>'Bemierkung',
+  'sample:date' => 'Datum',
+  'sample:entered_sref'=>'Koordinaten',
+  'search'=>'Sichen',
+  'search for place on map'=>'Plaatz op der Kaart sichen',
+  'surname'=>'Nonumm',
+  
+  // Tab titles (only visible if interface tabbed
+  'about you'=>'Ween ech sinn',
+  'what did you see'=>'Wat ech gesinn hunn',
+  'where was it'=>'Wou et war',
+  'other information'=>'Aner Informatiounen',
+  // Tab instructions
+  'about you tab instructions'=>'<strong>Ween ech sinn</strong><br/>Sot eis wann ech gelift ween Dir sidd. Dëst kéint wichteg sinn, fir dass mir Iech kontaktéiere kënnen, wann Är Beobachtung besonnesch interessant ass.',
+  'species tab instructions'=>'<strong>Wat ech gesinn hunn</strong><br/>Klickt op déi Blumm, déi Dir gesinn hutt, dono op Weider.',
+  'place tab instructions'=>'<strong>Wou et war</strong><br/>Sicht op der Kaart d'Plaatz, wou Dir déi Blumm gesinn hutt, sou genau wéi méiglech. Wann Dir d'Koordinate kennt, kënnt Dir déi och direkt aginn.',
+  'other tab instructions'=>'<strong>Aner Informatioune</strong><br/>Sot eis wann ech gelift wéini Dir déi Blumm gesinn hutt, wéi vill et der ongeféier waren a wann néideg weider Bemierkungen.',
+
+
+);
