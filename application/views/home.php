85871b6bb1b15abf9c0b0303029b554fcccb2c52
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2010-04-12 10:29:48 +0000

    Minor introduction improvements to reference the NBN.

--- application/views/home.php
+++ application/views/home.php
@@ -53,6 +53,6 @@ foreach($problems as $problem) {
 </div>
 <?php endif; ?>
 <p>Indicia is a toolkit that simplifies the construction of new websites which allow data entry, mapping and reporting
-of wildlife records. Indicia is an Open Source project funded by the Open Air Laboratories Network and managed
-by the Centre for Ecology and Hydrology.</p>
+of wildlife records. Indicia is an Open Source project funded by the Open Air Laboratories Network (of which the NBN is a partner) and managed by the 
+Centre for Ecology and Hydrology.</p>
 <p>You can see Indicia in action on the <a href="<?php echo url::base();?>modules/demo/index.php">website demonstration pages</a>.</p>
