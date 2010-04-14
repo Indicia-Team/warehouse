087cbde5dc333aaf239e5d3e23c07af2915e65e1
jame2 <jame2@ceh.ac.uk>
2010-04-14 07:14:18 +0000

    Update to preSubmit to support verification via web services.

--- application/models/occurrence.php
+++ application/models/occurrence.php
@@ -83,21 +83,25 @@ class Occurrence_Model extends ORM
     return parent::validate($array, $save);
   }
 
- // Override preSubmit to add in the verifier (verified_by_id) and verification date (verified_on) if the 
+ // Override preSubmit to add in the verifier (verified_by_id) and verification date (verified_on) if the
  // occurrence is being set to status=V(erified).
  protected function preSubmit()
  {
    if (array_key_exists('record_status', $this->submission['fields']))
    {
      $rs = $this->submission['fields']['record_status']['value'];
-     // If we are making it verified in the submitted data, but we don't already have a verifier in the database
+     // If we are making it verified in the submitted data, but we don't already have a verifier in
+     // the database
      if ($rs == 'V' && !$this->verified_by_id)
      {
        $defaultUserId = Kohana::config('indicia.defaultPersonId');
-       // Set the verifier to the logged in user, or the default user ID from config if not logged into Warehouse
-       $this->submission['fields']['verified_by_id']['value'] = $_SESSION['auth_user'] || $defaultUserId;
-       // and store the date of the verification event.
-       $this->submission['fields']['verified_on']['value'] = date("Ymd H:i:s");
+       // Set the verifier to the logged in user, or the default user ID from config if not logged
+       // into Warehouse, if it is not in the submission
+       if (!array_key_exists('verified_by_id', $this->submission['fields']))
+         $this->submission['fields']['verified_by_id']['value'] = isset($_SESSION['auth_user']) ? $_SESSION['auth_user'] : $defaultUserId;
+       // and store the date of the verification event if not specified.
+       if (!array_key_exists('verified_on', $this->submission['fields']))
+         $this->submission['fields']['verified_on']['value'] = date("Ymd H:i:s");
      }
    }
    parent::preSubmit();
