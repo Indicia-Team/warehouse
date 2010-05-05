166e8f05b0b46b47c15cf26d60b171e8b265ed65
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2010-05-05 10:06:16 +0000

    Specified projections for OSGB36 and LUREF so they don't need to be looked up.

--- media/js/proj4defs.js
+++ media/js/proj4defs.js
@@ -1,1 +1,2 @@
-Proj4js.defs["EPSG:900913"]= "+title= Google Mercator EPSG:900913 +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs";
+Proj4js.defs["EPSG:2169"] = "+proj=tmerc +lat_0=49.83333333333334 +lon_0=6.166666666666667 +k=1 +x_0=80000 +y_0=100000 +ellps=intl +towgs84=-193,13.7,-39.3,-0.41,-2.933,2.688,0.43 +units=m +no_defs";
+Proj4js.defs["EPSG:27700"] = "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs";
