e9130b4ddfa80ad978d948fda7406c047497e482
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2010-06-10 12:59:14 +0000

    Made remembering methods public static.

--- core/trunk/client_helpers/prebuilt_forms/mnhnl_citizen_science_1.php
+++ core/trunk/client_helpers/prebuilt_forms/mnhnl_citizen_science_1.php
@@ -422,12 +422,12 @@ class iform_mnhnl_citizen_science_1 {
    * @param $args
    * @return unknown_type
    */
-  function indicia_define_remembered_fields($args) {
+  public static function indicia_define_remembered_fields($args) {
     data_entry_helper::set_remembered_fields(array(
       'smpAttr:'.$args['first_name_attr_id'],
       'smpAttr:'.$args['surname_attr_id'],
       'smpAttr:'.$args['email_attr_id'],
-      'smpAttr:'.$args['phone_attr_id'],
+      'smpAttr:'.$args['phone_attr_id']
     ));
   }
 
