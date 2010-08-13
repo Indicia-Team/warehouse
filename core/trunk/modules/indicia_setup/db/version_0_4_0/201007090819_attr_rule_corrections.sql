b94a01ec548c11337d90fb18823f2848c852cbd9
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2010-08-13 15:21:21 +0000

    References to sample attributes cannot be by ID.

--- core/trunk/modules/indicia_setup/db/version_0_4_0/201007090819_attr_rule_corrections.sql
+++ core/trunk/modules/indicia_setup/db/version_0_4_0/201007090819_attr_rule_corrections.sql
@@ -1,7 +1,7 @@
 UPDATE sample_attributes SET validation_rules='numeric
 minimum[-20]
-maximum[45]' WHERE id=2;
+maximum[45]' WHERE caption='Temperature (Celsius)';
 
 
 
-UPDATE sample_attributes SET validation_rules='numeric' WHERE id=12;
+UPDATE sample_attributes SET validation_rules='numeric' WHERE caption='Altitude (m)'
