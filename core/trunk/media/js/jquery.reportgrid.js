b6152af5703c0ec9952213088b60c70e44bf09f2
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2010-09-03 09:34:07 +0000

    Pagination now works with actions that are just simple links.

--- core/trunk/media/js/jquery.reportgrid.js
+++ core/trunk/media/js/jquery.reportgrid.js
@@ -147,17 +147,28 @@ var report_grid_sortdir = '';
     };
     
     getActions = function(row, actions) {
-      var result = '', onclick;
+      var result='', onclick='', href='', link;
       $.each(actions, function(idx, action) {
         if (typeof action.javascript != "undefined") {
           onclick=' onclick="' + mergeParamsIntoTemplate(row, action.javascript) + '"';
-        } else {
-          onclick = '';
+        }
+        if (typeof action.url != "undefined") {
+          link = action.url.replace('{currentUrl}', window.location.href);
+          if (typeof action.urlParams != "undefined") {
+            if (link.indexOf('?')==-1) { link += '?'; }
+            else { link += '&'; }
+            $.each(action.urlParams, function(name, value) {
+              value = value.replace('{id}', row.id);
+              link += name + '=' + value;
+              link += '&';
+            });
+          }
+          href=' href="' + link + '"';
         }
         if (result !== '') {
           result += '<br/>';
         }
-        result += '<a class="indicia-button action-button ui-state-default ui-corner-all"'+onclick+'>'+action.caption+'</a>';
+        result += '<a class="indicia-button action-button ui-state-default ui-corner-all"'+onclick+href+'>'+action.caption+'</a>';
       });
       return result;
     }
