93e216025320bcd122b3aea195131787637e6ba6
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2010-09-15 13:42:41 +0000

    Bug fixes. When loading a page with a location select control on it, does not try to load a blank geom if the initial value is blank.

--- media/js/jquery.autocomplete.js
+++ media/js/jquery.autocomplete.js
@@ -407,7 +407,9 @@ $.Autocompleter.defaults = {
   multiple: false,
   multipleSeparator: ", ",
   highlight: function(value, term) {
-    return value.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + term.replace(/([\^\$\(\)\[\]\{\}\*\.\+\?\|\\])/gi, "\\$1") + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>");
+    return value ? 
+        value.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + term.replace(/([\^\$\(\)\[\]\{\}\*\.\+\?\|\\])/gi, "\\$1") + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>")
+        : '';
   },
   scroll: true,
   scrollHeight: 180
