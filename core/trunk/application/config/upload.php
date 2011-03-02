9fb9ddca19ee9c3eecd23038903a1d674e197837
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2011-03-02 09:05:27 +0000

    Moved a core Kohana change to our application.

--- core/trunk/application/config/upload.php
+++ core/trunk/application/config/upload.php
@@ -6,6 +6,11 @@
  */
 $config['directory'] = DOCROOT.'upload';
 
+/*
+ * This path is relative to your index file. Absolute paths are also supported.
+ */
+$config['zip_extract_directory'] = DOCROOT.'extract';
+
 /**
  * Enable or disable directory creation.
  */
