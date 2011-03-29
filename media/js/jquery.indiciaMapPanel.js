6155ed89ac64a7ae92918db439a64b9f92235f5a
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2011-03-29 19:15:23 +0000

    Bing shaded fix.

--- media/js/jquery.indiciaMapPanel.js
+++ media/js/jquery.indiciaMapPanel.js
@@ -335,7 +335,7 @@ mapInitialisationHooks = [];
         virtual_earth : function() { return new OpenLayers.Layer.VirtualEarth('Virtual Earth', {'type': VEMapStyle.Aerial, 'sphericalMercator': true}); },
         bing_aerial : function() { return new OpenLayers.Layer.VirtualEarth('Bing Aerial', {'type': VEMapStyle.Aerial, 'sphericalMercator': true}); },
         bing_hybrid : function() { return new OpenLayers.Layer.VirtualEarth('Bing Hybrid', {'type': VEMapStyle.Hybrid, 'sphericalMercator': true}); },
-        bing_shaded : function() { return new OpenLayers.Layer.VirtualEarth('Bing Shaded', {'type': VEMapStyle.Aerial, 'sphericalMercator': true}); },
+        bing_shaded : function() { return new OpenLayers.Layer.VirtualEarth('Bing Shaded', {'type': VEMapStyle.Shaded, 'sphericalMercator': true}); },
         multimap_default : function() { return new OpenLayers.Layer.MultiMap('MultiMap', {sphericalMercator: true}); },
         multimap_landranger : function() { return new OpenLayers.Layer.MultiMap('Multimap OS Landranger', {sphericalMercator: true}); }
       };
