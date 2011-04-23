ccc429428e2c3b888ba1cad38004019e56fbfd6e
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2011-04-23 21:16:17 +0000

    Fixes issues 300 Deleted people are not returned by people views.

---
+++ core/trunk/modules/indicia_setup/db/version_0_7_0/201104232153_people_views.sql
@@ -0,0 +1,24 @@
+﻿-- View: detail_people
+
+CREATE OR REPLACE VIEW detail_people AS 
+   SELECT p.id, p.first_name, p.surname, p.initials, p.email_address, p.website_url, p.created_by_id, c.username AS created_by, p.updated_by_id, u.username AS updated_by, uw.website_id
+   FROM people p
+   JOIN users c ON c.id = p.created_by_id
+   JOIN users u ON u.id = p.updated_by_id
+   LEFT JOIN users us ON us.person_id = p.id AND us.deleted=false
+   LEFT JOIN users_websites uw ON uw.user_id = us.id AND uw.site_role_id IS NOT NULL
+   WHERE p.deleted=false;
+
+-- View: list_people
+
+CREATE OR REPLACE VIEW list_people AS 
+   SELECT p.id, p.first_name, p.surname, p.initials, (p.first_name::text || ' '::text) || p.surname::text AS caption, uw.website_id
+   FROM people p
+   LEFT JOIN users us ON us.person_id = p.id AND us.deleted=false
+   LEFT JOIN users_websites uw ON uw.user_id = us.id AND uw.site_role_id IS NOT NULL
+   WHERE p.deleted=false;
+
+
+
+
+
