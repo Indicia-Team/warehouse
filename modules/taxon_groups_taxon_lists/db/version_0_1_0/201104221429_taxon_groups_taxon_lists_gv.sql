9c651a4b17a05e334a04be12b323735e3b75590d
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2011-05-06 15:22:05 +0000

    Email address added back to index page for people. Fixed a hard coded reference to a nbn web services registration key in nbn species dict sync module. NBN species dict sync module now only imports taxa into the list for the selected taxon groups associated with the checklist if the taxon groups taxon lists module is installed.

--- modules/taxon_groups_taxon_lists/db/version_0_1_0/201104221429_taxon_groups_taxon_lists_gv.sql
+++ modules/taxon_groups_taxon_lists/db/version_0_1_0/201104221429_taxon_groups_taxon_lists_gv.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE VIEW gv_taxon_groups_taxon_lists AS
-SELECT tgtl.id, tg.id as taxon_group_id, tg.title, tgtl.deleted, tgtl.taxon_list_id
+SELECT tgtl.id, tg.id as taxon_group_id, tg.title, tgtl.taxon_list_id
 FROM taxon_groups_taxon_lists tgtl
 JOIN taxon_groups tg ON tg.id=tgtl.taxon_group_id AND tg.deleted=false
+AND tgtl.deleted=false;
