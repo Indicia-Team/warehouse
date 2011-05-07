e3d716e92c77bc63cd344b9208761ad68bc4424c
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2011-05-07 20:14:00 +0000

    Fixed report download link.

--- core/trunk/client_helpers/prebuilt_forms/report_grid.php
+++ core/trunk/client_helpers/prebuilt_forms/report_grid.php
@@ -104,7 +104,7 @@ class iform_report_grid {
     $auth = report_helper::get_read_write_auth($args['website_id'], $args['password']);
     $reportOptions = iform_report_get_report_options($args, $auth);
     // Add a download link - get_report_data does not use paramDefaults but needs all settings in the extraParams
-    if (!isset($args['download_link']) || $args['download_link']===1)
+    if (!isset($args['download_link']) || $args['download_link'])
       $r .= '<br/>'.report_helper::report_download_link($reportOptions);
     // now the grid
     $r .= '<br/>'.report_helper::report_grid($reportOptions);
