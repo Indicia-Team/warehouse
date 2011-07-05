2b93a57bdaaa3a057b12c9dae263e6f63cfcc785
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2011-07-05 13:32:27 +0000

    Fix to proxy large request caching

--- client_helpers/prebuilt_forms/includes/map.php
+++ client_helpers/prebuilt_forms/includes/map.php
@@ -255,7 +255,7 @@ function iform_map_get_map_options($args, $readAuth) {
   // if in Drupal, and IForm proxy is installed, then use this path as OpenLayers proxy
   if (defined('DRUPAL_BOOTSTRAP_CONFIGURATION') && module_exists('iform_proxy')) {
     global $base_url;
-    $options['proxy'] = $base_url . '?q=' . variable_get('iform_proxy_path', 'proxy') . '&url=';
+    $options['proxy'] = $base_url . '/?q=' . variable_get('iform_proxy_path', 'proxy') . '&url=';
   }
   return $options;
 }
