3d938b5f372f991070d98ce89dd2e839d853bfe1
jame2 <jame2@ceh.ac.uk>
2011-07-09 11:18:53 +0000

    Tidy up indenting

--- modules/sref_osie/helpers/osie.php
+++ modules/sref_osie/helpers/osie.php
@@ -28,158 +28,159 @@
  */
 class osie {
 
-	/**
-	 * Returns true if the spatial reference is a recognised Irish Grid square.
-	 *
-	 * @param $sref string Spatial reference to validate
-	 */
-	public static function is_valid($sref)
-	{
+  /**
+   * Returns true if the spatial reference is a recognised Irish Grid square.
+   *
+   * @param $sref string Spatial reference to validate
+   */
+  public static function is_valid($sref)
+  {
     // ignore any spaces in the grid ref
     $sref = str_replace(' ','',$sref);
-		$sq100 = strtoupper(substr($sref, 0, 1));
-		if (!preg_match('([A-HJ-Z])', $sq100))
-			return FALSE;
-		$eastnorth=substr($sref, 1);
-		// 2 cases - either remaining chars must be all numeric and an equal number, up to 10 digits
-		// OR for DINTY Tetrads, 2 numbers followed by a letter (Excluding O, including I)
-		if ((!preg_match('/^[0-9]*$/', $eastnorth) || strlen($eastnorth) % 2 != 0 || strlen($eastnorth)>10) AND
-				(!preg_match('/^[0-9][0-9][A-NP-Z]$/', $eastnorth)))
-			return FALSE;
-		return TRUE;
-	}
+    $sq100 = strtoupper(substr($sref, 0, 1));
+    if (!preg_match('([A-HJ-Z])', $sq100))
+      return FALSE;
+    $eastnorth=substr($sref, 1);
+    // 2 cases - either remaining chars must be all numeric and an equal number, up to 10 digits
+    // OR for DINTY Tetrads, 2 numbers followed by a letter (Excluding O, including I)
+    if ((!preg_match('/^[0-9]*$/', $eastnorth) || strlen($eastnorth) % 2 != 0 || strlen($eastnorth)>10) AND
+                    (!preg_match('/^[0-9][0-9][A-NP-Z]$/', $eastnorth)))
+      return FALSE;
+    return TRUE;
+  }
 
-	/**
-	 * Converts a grid reference in OSI notation into the WKT text for the polygon, in
-	 * easting and northings from the zero reference.
-	 *
-	 * @param string $sref The grid reference
-	 * @return string String containing the well known text.
-	 */
-	public static function sref_to_wkt($sref)
-	{
+  /**
+   * Converts a grid reference in OSI notation into the WKT text for the polygon, in
+   * easting and northings from the zero reference.
+   *
+   * @param string $sref The grid reference
+   * @return string String containing the well known text.
+   */
+  public static function sref_to_wkt($sref)
+  {
     // ignore any spaces in the grid ref
     $sref = str_replace(' ','',$sref);
-		if (!self::is_valid($sref))
-			throw new Exception('Spatial reference is not a recognisable grid square.');
-		$sq_100 = self::get_100k_square($sref);
-		if (strlen($sref)==4) {
-			// Assume DINTY Tetrad format 2km squares
-			// extract the easting and northing
-			$east  = substr($sref, 1, 1);
-			$north = substr($sref, 2, 1);
- 			$sq_code_letter_ord = ord(substr($sref, 3, 1));
-			if ($sq_code_letter_ord > 79) $sq_code_letter_ord--; // Adjust for no O
- 			$sq_size = 2000;
-			$east = $east * 10000 + floor(($sq_code_letter_ord - 65) / 5) * 2000;
-			$north = $north * 10000 + (($sq_code_letter_ord - 65) % 5) * 2000;
-  	} else {
-			// Normal Numeric Format
-			$coordLen = (strlen($sref)-1)/2;
-			// extract the easting and northing
-			$east  = substr($sref, 1, $coordLen);
-			$north = substr($sref, 1+$coordLen);
-			// if < 10 figure the easting and northing need to be multiplied up to the power of 10
-			$sq_size = pow(10, 5-$coordLen);
-			$east = $east * $sq_size;
-			$north = $north * $sq_size;
-		}
-		$westEdge=$east + $sq_100['x'];
-		$southEdge=$north + $sq_100['y'];
-		$eastEdge=$westEdge+$sq_size;
-		$northEdge=$southEdge+$sq_size;
-		return 	"POLYGON(($westEdge $southEdge,$westEdge $northEdge,".
-			 "$eastEdge $northEdge,$eastEdge $southEdge,$westEdge $southEdge))";
-	}
+    if (!self::is_valid($sref))
+      throw new Exception('Spatial reference is not a recognisable grid square.');
+    $sq_100 = self::get_100k_square($sref);
+    if (strlen($sref)==4) {
+      // Assume DINTY Tetrad format 2km squares
+      // extract the easting and northing
+      $east  = substr($sref, 1, 1);
+      $north = substr($sref, 2, 1);
+      $sq_code_letter_ord = ord(substr($sref, 3, 1));
+      if ($sq_code_letter_ord > 79) $sq_code_letter_ord--; // Adjust for no O
+      $sq_size = 2000;
+      $east = $east * 10000 + floor(($sq_code_letter_ord - 65) / 5) * 2000;
+      $north = $north * 10000 + (($sq_code_letter_ord - 65) % 5) * 2000;
+    } else {
+      // Normal Numeric Format
+      $coordLen = (strlen($sref)-1)/2;
+      // extract the easting and northing
+      $east  = substr($sref, 1, $coordLen);
+      $north = substr($sref, 1+$coordLen);
+      // if < 10 figure the easting and northing need to be multiplied up to the power of 10
+      $sq_size = pow(10, 5-$coordLen);
+      $east = $east * $sq_size;
+      $north = $north * $sq_size;
+    }
+    $westEdge=$east + $sq_100['x'];
+    $southEdge=$north + $sq_100['y'];
+    $eastEdge=$westEdge+$sq_size;
+    $northEdge=$southEdge+$sq_size;
+    return 	"POLYGON(($westEdge $southEdge,$westEdge $northEdge,".
+             "$eastEdge $northEdge,$eastEdge $southEdge,$westEdge $southEdge))";
+  }
 
-	/**
-	 * Converts a WKT to a grid square in the OSI grid
-	 * reference notation. Only accepts POINT & POLYGON WKT at the moment.
-	 *
-	 * @param string $wkt The well known text
+  /**
+   * Converts a WKT to a grid square in the OSI grid
+   * reference notation. Only accepts POINT & POLYGON WKT at the moment.
+   *
+   * @param string $wkt The well known text
    * @param integer $precision The number of digits to include in the return value.
    * For a polygon, omit the parameter and the precision is inferred from the
    * size of the polygon. To return a grid reference in tetrad form, set this to 3.
-	 * @return string String containing OSI grid reference.
-	 */
-	public static function wkt_to_sref($wkt, $precision=null)
-	{
-		if (substr($wkt, 0, 7) == 'POLYGON')
-			$points = substr($wkt, 9, -2);
-		elseif (substr($wkt, 0, 5) == 'POINT') {
-			$points = substr($wkt, 6, -1);
-			if ($precision===null)
-				throw new Exception('wkt_to_sref translation for POINTs requires an accuracy.');
-		}
-		else
-			throw new Exception('wkt_to_sref translation only works for POINT or POLYGON wkt.');
-		$points = explode(',',$points);
-		// use the first point to do the conversion
-		$point = explode(' ',$points[0]);
-		$easting = $point[0];
-		$northing = $point[1];
+   * @return string String containing OSI grid reference.
+   */
+  public static function wkt_to_sref($wkt, $precision=null)
+  {
+    if (substr($wkt, 0, 7) == 'POLYGON')
+      $points = substr($wkt, 9, -2);
+    elseif (substr($wkt, 0, 5) == 'POINT') {
+      $points = substr($wkt, 6, -1);
+      if ($precision===null)
+        throw new Exception('wkt_to_sref translation for POINTs requires an accuracy.');
+    }
+    else
+      throw new Exception('wkt_to_sref translation only works for POINT or POLYGON wkt.');
+
+    $points = explode(',',$points);
+    // use the first point to do the conversion
+    $point = explode(' ',$points[0]);
+    $easting = $point[0];
+    $northing = $point[1];
     // ensure the point is within the range of the grid
     if ($easting < 0 || $easting > 500000 || $northing < 0 || $northing > 500000)
-				throw new Exception('wkt_to_sref translation is outside range of grid.');
-		if ($precision===null) {
-			// find the distance in metres from point 2 to point 1 (assuming a square is passed).
-			// This is the accuracy of the polygon.
-			$point_2 = explode(' ',$points[1]);
-			$accuracy = abs(($point_2[0]-$point[0]) + ($point_2[1]-$point[1]));
-			$precision = 12 - strlen($accuracy)*2;
-		} else if ($precision==3) {
-			// DINTY TETRADS
-			// no action as all fixed.
-		} else
-		  $accuracy = pow(10, (10-$precision)/2);
+      throw new Exception('wkt_to_sref translation is outside range of grid.');
+    if ($precision===null) {
+      // find the distance in metres from point 2 to point 1 (assuming a square is passed).
+      // This is the accuracy of the polygon.
+      $point_2 = explode(' ',$points[1]);
+      $accuracy = abs(($point_2[0]-$point[0]) + ($point_2[1]-$point[1]));
+      $precision = 12 - strlen($accuracy)*2;
+    } else if ($precision==3) {
+      // DINTY TETRADS
+      // no action as all fixed.
+    } else
+      $accuracy = pow(10, (10-$precision)/2);
 
-		$hundredKmE = floor($easting / 100000);
-  	$hundredKmN = floor($northing / 100000);
+    $hundredKmE = floor($easting / 100000);
+    $hundredKmN = floor($northing / 100000);
     $index = 65 + ((4 - ($hundredKmN % 5)) * 5) + ($hundredKmE % 5);
     // Shift index along if letter is greater than I, since I is skipped
     if ($index >= 73) $index++;
     $firstLetter = chr($index);
     if ($precision == 3) {
-		  // DINTY TETRADS
-    	// 2 numbers at start equivalent to precision = 2
-    	$e = floor(($easting - (100000 * $hundredKmE)) / 10000);
-    	$n = floor(($northing - (100000 * $hundredKmN)) / 10000);
-    	$letter = 65 + floor(($northing - (100000 * $hundredKmN) - ($n * 10000)) / 2000) + 5 * floor(($easting - (100000 * $hundredKmE) - ($e * 10000)) / 2000);
-			if ($letter >= 79) $letter++; // Adjust for no O
-    	return $firstLetter.str_pad($e, 1, '0', STR_PAD_LEFT).str_pad($n, 1, '0', STR_PAD_LEFT).chr($letter);
+      // DINTY TETRADS
+      // 2 numbers at start equivalent to precision = 2
+      $e = floor(($easting - (100000 * $hundredKmE)) / 10000);
+      $n = floor(($northing - (100000 * $hundredKmN)) / 10000);
+      $letter = 65 + floor(($northing - (100000 * $hundredKmN) - ($n * 10000)) / 2000) + 5 * floor(($easting - (100000 * $hundredKmE) - ($e * 10000)) / 2000);
+      if ($letter >= 79) $letter++; // Adjust for no O
+      return $firstLetter.str_pad($e, 1, '0', STR_PAD_LEFT).str_pad($n, 1, '0', STR_PAD_LEFT).chr($letter);
     }
     $e = floor(($easting - (100000 * $hundredKmE)) / $accuracy);
     $n = floor(($northing - (100000 * $hundredKmN)) / $accuracy);
     return $firstLetter.str_pad($e, $precision/2, '0', STR_PAD_LEFT).str_pad($n, $precision/2, '0', STR_PAD_LEFT);
-	}
+  }
 
-	/**
-	 * Return the underying EPSG code for the datum this notation is based on.
+  /**
+   * Return the underying EPSG code for the datum this notation is based on.
    * See http://www.dnf.org/registry/coordinate_reference_systems/#crs120
-	 */
-	public static function get_srid()
-	{
-		return 29903;
-	}
+   */
+  public static function get_srid()
+  {
+    return 29903;
+  }
 
-	/** Retrieve the easting and northing of the sw corner of a
-	 * 100km square, indicated by the first character of the grid ref.
-	 *
-	 * @param string $sref Spatial reference string to parse (OSI)
-	 * @return array Array containing (x, y)
-	 */
-	protected static function get_100k_square($sref)
-	{
-		$north = 0;
-		$east = 0;
-  	$char1ord = ord(substr($sref, 0, 1));
-		if ($char1ord > 73) $char1ord--; // Adjust for no I
-		$east = (($char1ord - 65) % 5) * 100000;
-  	$north = (4 - floor(($char1ord - 65) / 5)) * 100000;
-  	$output['x']=$east;
-  	$output['y']=$north;
-		return $output;
-	}
+  /** Retrieve the easting and northing of the sw corner of a
+   * 100km square, indicated by the first character of the grid ref.
+   *
+   * @param string $sref Spatial reference string to parse (OSI)
+   * @return array Array containing (x, y)
+   */
+  protected static function get_100k_square($sref)
+  {
+    $north = 0;
+    $east = 0;
+    $char1ord = ord(substr($sref, 0, 1));
+    if ($char1ord > 73) $char1ord--; // Adjust for no I
+    $east = (($char1ord - 65) % 5) * 100000;
+    $north = (4 - floor(($char1ord - 65) / 5)) * 100000;
+    $output['x']=$east;
+    $output['y']=$north;
+    return $output;
+  }
 
 }
 ?>
