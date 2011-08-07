84c6bfd44418210f0755278c09bc519a3d38a19d
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2011-08-07 20:29:03 +0000

    Significant new feature to support attaching custom attributes to surveys only when the sample is a certain sample method, or a location is of a certain type. To use this you need to pass the sample method or location type id to the getAttributes call which will then load just the appropriate attributes for the selected type/method. This allows attributes to be used which are specific to certain sampling methods, for example.

--- application/models/sample_attributes_website.php
+++ application/models/sample_attributes_website.php
@@ -52,7 +52,8 @@ class Sample_attributes_website_Model extends Valid_ORM
         'default_date_start_value',
         'default_date_end_value',
         'default_date_type_value',
-	      'control_type_id');
+	      'control_type_id',
+        'restrict_to_sample_method_id');
     return parent::validate($array, $save);
   }
   
