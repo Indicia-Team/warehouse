08866808d3b9156978a466a5efa898f81dc60dda
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2011-08-31 15:57:36 +0000

    Some tests for saving via the data services.

--- modules/indicia_svc_data/tests/controllers/services/dataTest.php
+++ modules/indicia_svc_data/tests/controllers/services/dataTest.php
@@ -1,15 +1,20 @@
 <?php
 
 require_once 'client_helpers/data_entry_helper.php';
+require_once 'client_helpers/submission_builder.php';
 
 class Controllers_Services_Data_Test extends PHPUnit_Framework_TestCase {
 
-  private $readAuth;
-  private $locationId;
-  private $locationWebsiteId;
+  protected $auth;
+  protected $locationId;
+  protected $locationTypeId;
+  protected $locationAttributeId;
+  protected $locationAttrWebsiteId;
 
   public function setup() {
-    $this->readAuth = data_entry_helper::get_read_auth(1, 'password');
+    $this->auth = data_entry_helper::get_read_write_auth(1, 'password');
+    // make the tokens re-usable
+    $this->auth['write_tokens']['persist_auth']=true;
     // set up a tiny bit of data. First pick a termlists_term_id to use which is never going to be a valid location type,
     // so we can filter for just our test location.
     $this->db = new Database();
@@ -27,26 +32,35 @@ class Controllers_Services_Data_Test extends PHPUnit_Framework_TestCase {
     $loc->set_metadata();
     $loc->save();
     $this->locationId=$loc->id;
-    $locwebsite = ORM::Factory('locations_website');
+    $locattr = ORM::Factory('location_attribute');
+    $locattr->caption='UnitTest';
+    $locattr->data_type='T';
+    $locattr->public='f';
+    $locattr->set_metadata();
+    $locattr->save();
+    $this->locationAttributeId=$locattr->id;
+    $locwebsite = ORM::Factory('location_attributes_website');
     $locwebsite->website_id=1;
-    $locwebsite->location_id=$this->locationId;
+    $locwebsite->location_attribute_id=$this->locationAttributeId;
     $locwebsite->set_metadata();
     $locwebsite->save();
-    $this->locationWebsiteId=$locwebsite->id;
+    $this->locationAttrWebsiteId=$locwebsite->id;
   }
   
   public function tearDown() {
-    $locwebsite = ORM::Factory('locations_website', $this->locationWebsiteId);
-    $locwebsite->delete();
     $loc = ORM::Factory('location', $this->locationId);
     $loc->delete();
+    $locwebsite = ORM::Factory('location_attributes_website', $this->locationAttrWebsiteId);
+    $locwebsite->delete();
+    $locattr = ORM::Factory('location_attribute', $this->locationAttributeId);
+    $locattr->delete();
   }
   
   public function testRequestDataGetRecordByDirectId() {
     $params = array(
       'mode'=>'json',
-      'auth_token'=>$this->readAuth['auth_token'],
-      'nonce'=>$this->readAuth['nonce']
+      'auth_token'=>$this->auth['read']['auth_token'],
+      'nonce'=>$this->auth['read']['nonce']
     );
     $url = data_entry_helper::$base_url.'index.php/services/data/location/'.$this->locationId.'?'.http_build_query($params, '', '&');
     $response = self::getResponse($url);
@@ -57,8 +71,8 @@ class Controllers_Services_Data_Test extends PHPUnit_Framework_TestCase {
   public function testRequestDataGetRecordByIndirectId() {
     $params = array(
       'mode'=>'json',
-      'auth_token'=>$this->readAuth['auth_token'],
-      'nonce'=>$this->readAuth['nonce'],
+      'auth_token'=>$this->auth['read']['auth_token'],
+      'nonce'=>$this->auth['read']['nonce'],
       'id'=>$this->locationId
     );
     $url = data_entry_helper::$base_url.'index.php/services/data/location?'.http_build_query($params, '', '&');
@@ -70,8 +84,8 @@ class Controllers_Services_Data_Test extends PHPUnit_Framework_TestCase {
   public function testRequestDataGetRecordByQueryIn() {
     $params = array(
       'mode'=>'json',
-      'auth_token'=>$this->readAuth['auth_token'],
-      'nonce'=>$this->readAuth['nonce'],
+      'auth_token'=>$this->auth['read']['auth_token'],
+      'nonce'=>$this->auth['read']['nonce'],
       'query'=>json_encode(array('in'=>array('id', array($this->locationId))))
     );
     $url = data_entry_helper::$base_url.'index.php/services/data/location?'.http_build_query($params, '', '&');
@@ -81,8 +95,8 @@ class Controllers_Services_Data_Test extends PHPUnit_Framework_TestCase {
     // repeat test, for alternative format of in clause
     $params = array(
       'mode'=>'json',
-      'auth_token'=>$this->readAuth['auth_token'],
-      'nonce'=>$this->readAuth['nonce'],
+      'auth_token'=>$this->auth['read']['auth_token'],
+      'nonce'=>$this->auth['read']['nonce'],
       'query'=>json_encode(array(
         'in'=>array('id'=>array($this->locationId), 'name'=>array('UnitTest')),
       ))
@@ -94,8 +108,8 @@ class Controllers_Services_Data_Test extends PHPUnit_Framework_TestCase {
     // Another test- which should return 0 records because we look for the wrong name
     $params = array(
       'mode'=>'json',
-      'auth_token'=>$this->readAuth['auth_token'],
-      'nonce'=>$this->readAuth['nonce'],
+      'auth_token'=>$this->auth['read']['auth_token'],
+      'nonce'=>$this->auth['read']['nonce'],
       'query'=>json_encode(array(
         'in'=>array('id'=>array($this->locationId), 'name'=>array('UnitTests')),
       ))
@@ -127,6 +141,54 @@ class Controllers_Services_Data_Test extends PHPUnit_Framework_TestCase {
     return $response;
   }
   
+  public function testSave() {
+    // post a location with an attribute value.
+    $array = array(
+      'location:name' => 'UnitTest2',
+      'location:centroid_sref'=>'SU0101',
+      'location:centroid_sref_system'=>'osgb',
+      'locAttr:'.$this->locationAttributeId=>'saveTestAttr'
+    );
+    $s = submission_builder::build_submission($array, array('model'=>'location'));
+    $r = data_entry_helper::forward_post_to('location', $s, $this->auth['write_tokens']);
+    // check we got the required output
+    $this->assertTrue(isset($r['success']), 'Submitting a location did not return success response');
+    $locId = $r['success'];
+    $loc = ORM::Factory('location', $locId);
+    $this->assertEquals('UnitTest2', $loc->name, 'Saved location is not as expected');
+    $locAttr = ORM::Factory('location_attribute_value')->where(array('location_id'=>$locId))->find();
+    $this->assertEquals('saveTestAttr', $locAttr->text_value, 'Saved attribute value is not as expected');
+    // repost the same location, with a new name and attribute value
+    $array = array(
+      'location:id' => $locId,
+      'location:name' => 'UnitTest2-update',
+      'location:centroid_sref'=>'SU0101',
+      'location:centroid_sref_system'=>'osgb',
+      'locAttr:'.$this->locationAttributeId.':'.$locAttr->id=>'saveTestAttr-update'
+    );
+    $s = submission_builder::build_submission($array, array('model'=>'location'));
+    $r = data_entry_helper::forward_post_to('location', $s, $this->auth['write_tokens']);
+    $this->assertTrue(isset($r['success']), 'Re-submitting a location did not return success response');
+    $loc->reload();
+    $this->assertEquals('UnitTest2-update', $loc->name, 'Re-submitted location is not as expected');
+    $locAttr->reload();
+    $this->assertEquals('saveTestAttr-update', $locAttr->text_value, 'Saved attribute value is not as expected');
+    // repost the same location, with a deleted attribute value
+    $array = array(
+      'location:id' => $locId,
+      'location:name' => 'UnitTest2',
+      'location:centroid_sref'=>'SU0101',
+      'location:centroid_sref_system'=>'osgb',
+      'locAttr:'.$this->locationAttributeId.':'.$locAttr->id=>''
+    );
+    $s = submission_builder::build_submission($array, array('model'=>'location'));
+    $r = data_entry_helper::forward_post_to('location', $s, $this->auth['write_tokens']);
+    $locAttr->reload();
+    $this->assertEquals(true, $locAttr->deleted, 'Submitting a blank attribute value did not delete it');
+    // now cleanup
+    $locAttr->delete();
+    ORM::Factory('location', $locId)->delete();
+  }  
  
 }
 ?>
