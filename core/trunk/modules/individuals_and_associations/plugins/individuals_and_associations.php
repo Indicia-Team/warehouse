e7cb70111452551a8fb0062eab99d2efb7b3bd18
don@applerow.co.uk <don@applerow.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-03-12 01:12:52 +0000

    First release of individuals and associations optional module. This supports the recording of groups or identifiable groups of interest and the tracking of marked subjects. Currently under development for WWT. This release includes basic CRUD functions for known subjects and subject observations but does not included the required entity for identifying marks. This will be added soon and will change the module substantially. This module is not stable and is for development only at present.

---
+++ core/trunk/modules/individuals_and_associations/plugins/individuals_and_associations.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package	Groups and Individuals
+ * @subpackage Plugins
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL
+ * @link 	http://code.google.com/p/indicia/
+ */
+
+/**
+ * Adds a tab for viewing designations for the selected taxon.
+ * @todo Complete this tab
+ * @return <type>
+ */
+function individuals_and_associations_extend_ui() {
+  return array(array(
+    'view'=>'sample/sample_edit', 
+    'type'=>'tab',
+    'controller'=>'subject_observation/index',
+    'title'=>'Subject Observations',
+    'allowForNew'=>false,
+  ));
+}
+
+/**
+ * Create a menu item for the list of known subjects.
+ */
+function individuals_and_associations_alter_menu ($menu, $auth) {
+  if ($auth->logged_in('CoreAdmin') || $auth->has_any_website_access('editor')) {
+    $menu['Entered Data']['Known Subjects'] = 'known_subject';
+    $menu['Entered Data']['Subject Observations'] = 'subject_observation';
+    $menu['Custom Attributes']['Known Subject Attributes'] = 'known_subject_attribute';
+    $menu['Custom Attributes']['Subject Observation Attributes'] = 'subject_observation_attribute';
+  }
+  return $menu;
+}
+
+/**
+ * Hook to ORM enable the relationship between known subjects and: 
+ * 1) taxa_taxon_lists from the taxon end.
+ * 2) websites from the website end.
+ */
+function individuals_and_associations_extend_orm() {
+  return array(
+    'taxa_taxon_list'=>array(
+      'has_many'=>array('known_subjects_taxa_taxon_lists',),
+    ),
+    'occurrence'=>array(
+      'has_many'=>array('occurrences_subject_observations',),
+    ),
+    'sample'=>array(
+      'has_many'=>array('subject_observations',),
+    ),
+    'website'=>array(
+      'has_many'=>array('known_subjects', 'subject_observations',),
+    ),
+  );
+}
+
+/**
+ * Expose the Groups and Individuals entities through the data services.
+ */
+function individuals_and_associations_extend_data_services() {
+  return array(
+    'known_subjects'=>array(),
+    'subject_observations'=>array(),
+    'known_subject_attributes'=>array(),
+    'subject_observation_attributes'=>array(),
+    'known_subject_attribute_values'=>array(),
+    'subject_observation_attribute_values'=>array(),
+    'known_subject_comments'=>array(),
+  );
+}
+
+?>
