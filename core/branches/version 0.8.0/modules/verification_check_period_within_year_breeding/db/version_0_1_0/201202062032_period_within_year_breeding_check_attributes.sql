7bfda043ab9da09f565ab9baf8875fc62e3c713f
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-03-18 07:43:03 +0000

    Some modules not ready - held back from 0.8 release candidate

--- core/branches/version 0.8.0/modules/verification_check_period_within_year_breeding/db/version_0_1_0/201202062032_period_within_year_breeding_check_attributes.sql
+++
@@ -1,5 +0,0 @@
-﻿insert into taxa_taxon_list_attributes (caption, data_type, created_on, created_by_id, updated_on, updated_by_id, multi_value, public, for_verification_check)
-values ('Breeding period start date', 'D', now(), 1, now(), 1, false, true, true);
-
-insert into taxa_taxon_list_attributes (caption, data_type, created_on, created_by_id, updated_on, updated_by_id, multi_value, public, for_verification_check)
-values ('Breeding period end date', 'D', now(), 1, now(), 1, false, true, true);
--- core/branches/version 0.8.0/modules/verification_check_polygon/db/version_0_1_0/201112211613_taxon_range_attributes.sql
+++
@@ -1,2 +0,0 @@
-insert into taxa_taxon_list_attributes (caption, data_type, created_on, created_by_id, updated_on, updated_by_id, multi_value, public, for_verification_check)
-values ('Species range', 'G', now(), 1, now(), 1, false, true, true);
