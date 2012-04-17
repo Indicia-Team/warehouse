af75999ec648f0dea6cbd2688f911696d35b4ea8
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-04-17 18:58:10 +0000

    Javascript to reload report grid does not pass through unnecessary parameters (e.g. the id being loaded when displaying records after a save).

--- core/trunk/media/js/jquery.reportgrid.js
+++ core/trunk/media/js/jquery.reportgrid.js
@@ -92,9 +92,9 @@ function simple_tooltip(target_items, name){
     
     function getUrlParamsForAllRecords(div) {
       request = {};
-      // Extract any parameters from the attached form
+      // Extract any parameters from the attached form as long as they are report parameters
       $('form#'+div.settings.reportGroup+'-params input, form#'+div.settings.reportGroup+'-params select').each(function(idx, input) {
-        if (input.type!=='submit') {
+        if (input.type!=='submit' && $(input).attr('name').indexOf(div.settings.reportGroup+'-')===0) {
           paramName = $(input).attr('name').replace(div.settings.reportGroup+'-', '');
           request[paramName] = $(input).attr('value');
         }
