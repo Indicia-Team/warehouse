51326261a4708009fd6b18f81ec66138099b5435
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2012-05-21 19:30:23 +0000

    Bug fixes in the taxon searchlist generation.

--- modules/cache_builder/db/version_0_1_1/201205201958_create_abbreviation_function.sql
+++ modules/cache_builder/db/version_0_1_1/201205201958_create_abbreviation_function.sql
@@ -2,8 +2,12 @@
 CREATE OR REPLACE FUNCTION taxon_abbreviation(taxon character varying)
   RETURNS character varying AS
 $BODY$
+DECLARE 
+simplified character varying;
 BEGIN
-RETURN lower(substring(split_part(taxon, ' ', 1) from 1 for 2) || substring(split_part(taxon, ' ', 2) from 1 for 3));
+-- remove the subgenera in brackets if they exist
+simplified = regexp_replace(taxon, E'\\(.+\\) ', '', 'g');
+RETURN lower(substring(split_part(simplified, ' ', 1) from 1 for 2) || substring(split_part(simplified, ' ', 2) from 1 for 3));
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
