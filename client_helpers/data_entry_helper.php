9194d3e8c4d3070d6d45d8fc033d829db3f560fa
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2012-06-12 07:24:22 +0000

    Linked lists use query to allow null values through, e.g. so you can select master list to import against.

--- client_helpers/data_entry_helper.php
+++ client_helpers/data_entry_helper.php
@@ -3326,10 +3326,10 @@ $('div#$escaped_divId').indiciaTreeBrowser({
       $request .= '&'.self::array_to_query_string($options['extraParams']);
     }
     $query = urlencode(json_encode(array('in'=>array($options['filterField'], array(null, 'val')))));
-    $query = str_replace('%22val%22', "'+$(this).val()+'", $query);
+    $query = str_replace('%22val%22', '"+$(this).val()+"', $query);
     self::$javascript .= str_replace(
-        array('{fn}','{escapedId}','{request}','{query}','{valueField}','{captionField}','{parentControlId}', '{instruct}'),
-        array($fn, $escapedId, $request,$query,$options['valueField'],$options['captionField'],$parentControlId, $instruct),
+        array('{fn}','{escapedId}','{request}','{query}','{valueField}','{captionField}','{filterField}','{parentControlId}', '{instruct}'),
+        array($fn, $escapedId, $request,$query,$options['valueField'],$options['captionField'],$options['filterField'],$parentControlId, $instruct),
         $indicia_templates['linked_list_javascript']
     );
   }
