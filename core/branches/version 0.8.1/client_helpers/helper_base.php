aa7db1e49686d485bbddb6975796e2e8d27f31f2
jame2@ceh.ac.uk <jame2@ceh.ac.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-06-19 16:50:51 +0000

    Branch 0.8.1. Copy r4239 to fix client-side validation so messages only shown if requested by validation mode.

--- core/branches/version 0.8.1/client_helpers/helper_base.php
+++ core/branches/version 0.8.1/client_helpers/helper_base.php
@@ -1245,8 +1245,8 @@ indiciaData.windowLoaded=false;
       global $indicia_templates;
       self::$javascript .= "$('#".self::$validated_form_id."').validate({
         errorClass: \"".$indicia_templates['error_class']."\",
-        ". (in_array('inline', self::$validation_mode) ? "\n      " : "errorElement: 'p',\n      ").
-        "highlight: function(element, errorClass) {
+        ". (in_array('inline', self::$validation_mode) ? "" : "errorElement: 'p',") ."
+        highlight: function(element, errorClass) {
           $(element).addClass('ui-state-error');
         },
         unhighlight: function(element, errorClass) {
@@ -1265,7 +1265,10 @@ indiciaData.windowLoaded=false;
             ctrl.parents('.fieldset-wrapper').show();
           });
         },
-        messages: ".json_encode(self::$validation_messages)."
+        messages: ".json_encode(self::$validation_messages) .
+        // Do not place errors if 'message' not in validation_mode
+        (in_array('message', self::$validation_mode) ? "" : ",
+        errorPlacement: function(error, element) {}") ."
       });\n";
     }
   }
