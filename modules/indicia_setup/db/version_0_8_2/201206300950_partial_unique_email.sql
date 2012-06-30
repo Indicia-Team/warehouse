f6071343cea16d99c39156caa205afa3abb755cf
jame2 <jame2@ceh.ac.uk>
2012-06-30 09:01:31 +0000

    Fix to database constraint on unique email address so it ignores deleted people.

---
+++ modules/indicia_setup/db/version_0_8_2/201206300950_partial_unique_email.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE people DROP CONSTRAINT unique_email;
+
+CREATE UNIQUE INDEX unique_undeleted_email_constraint
+  ON people
+  USING btree
+  (email_address)
+  WHERE deleted = false;
+COMMENT ON INDEX unique_undeleted_email_constraint IS 'A partial unique index to ensure that emails are unique across people, excluding those deleted.';
