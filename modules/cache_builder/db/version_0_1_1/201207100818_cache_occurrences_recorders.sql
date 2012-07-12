414134bebe1a1f7a4be02fbeee4dfe9619f1adb7
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2012-07-12 09:11:59 +0000

    script fixed

--- modules/cache_builder/db/version_0_1_1/201207100818_cache_occurrences_recorders.sql
+++ modules/cache_builder/db/version_0_1_1/201207100818_cache_occurrences_recorders.sql
@@ -6,7 +6,7 @@ COMMENT ON COLUMN cache_occurrences.recorders IS 'Recorder username, name or nam
 update cache_occurrences co
 set recorders=s.recorder_names
 from samples s
-where s.sample_id=co.sample_id and s.deleted=false
+where s.id=co.sample_id and s.deleted=false
 and co.recorders is null;
 
 update cache_occurrences co
