b5028ad04f21b6b807688413c6fe850bc7444b95
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-07-27 12:17:08 +0000

    Quick species maps prebuilt form now ready for wider testing.

--- core/trunk/client_helpers/prebuilt_forms/quick_species_maps.php
+++ core/trunk/client_helpers/prebuilt_forms/quick_species_maps.php
@@ -60,7 +60,7 @@ class iform_quick_species_maps {
           'description'=>'Set to the name of a feature type on GeoServer that will be loaded to display the Indicia species data for the selected record. '.
               'Leave empty for no layer. Normally this should be set to a feature type that exposes the cache_occurrences view.',
           'type'=>'text_input',
-          'required'=>false,
+          'required'=>true,
           'default'=>'indicia:cache_occurrences',
           'group'=>'Other Map Settings'
         ), array(
@@ -69,22 +69,27 @@ class iform_quick_species_maps {
           'description'=>'Set to the name of a field exposed by the feature type which can be used to filter for the species data to display. Examples include '.
               'taxon_external_key, taxon_meaning_id.',
           'type'=>'text_input',
-          'required'=>false,
+          'required'=>true,
           'group'=>'Other Map Settings'
-        ),array(
-          'name'=>'indicia_species_layer_sld',
-          'caption'=>'SLD file from GeoServer for Indicia species layer',
-          'description'=>'Set to the name of an SLD file available on the GeoServer for the rendering of the Indicia species layer, or leave blank for default.',
-          'type'=>'text_input',
+        ), array(
+          'name'=>'indicia_species_layer_slds',
+          'caption'=>'SLD filse from GeoServer for Indicia species layer',
+          'description'=>'Set to the names of SLD files available on the GeoServer for the rendering of the Indicia species layer, or leave blank for default. '.
+              'Provide one per species layer you are going to allow on the map. Layer styles will be cycled through.',
+          'type'=>'textarea',
           'required'=>false,
+          'default'=>"dist_point_red\ndist_point_blue",
           'group'=>'Other Map Settings'
         )
       )
     );
     foreach ($r as &$param) {
-      if ($param['name']==='report_name') {
+      if ($param['name']==='report_name')
         $param['default'] = 'library/taxa/occurrence_counts_summary_by_external_key';
-      }
+      elseif ($param['name']==='indicia_species_layer_filter_field')
+        $param['default'] = 'taxa_taxon_list_external_key';
+      elseif ($param['name']==='param_presets')
+        $param['default'] = "date_from=\ndate_to=\nsurvey_id=\nquality=!Rlocation_id={profile_location}\ntaxon_groups={profile_taxon_groups}\ncurrentUser={profile_indicia_user_id}";
     }
     return $r;
     
@@ -102,16 +107,43 @@ class iform_quick_species_maps {
    */
   public static function get_form($args, $node, $response=null) {
     iform_load_helpers(array('report_helper', 'map_helper'));
+    global $user;
+    if (!isset($user->profile_indicia_user_id))
+      profile_load_profile($user);
     $conn = iform_get_connection_details($node);
     $readAuth = report_helper::get_read_auth($conn['website_id'], $conn['password']);
-    $r = '<div style="width: 50%; float: left;">';
+    $r = '<div id="leftcol">';
     $reportOptions = iform_report_get_report_options($args, $readAuth);
+    iform_report_apply_explore_user_own_preferences($reportOptions);
+    $reportOptions = array_merge(array(
+      'rowId' => 'external_key',
+      'columns' => array(),
+      'callback' => 'grid_load',
+      'rememberParamsReportGroup' => 'explore',
+      'paramsFormButtonCaption'=>lang::get('Filter')        
+    ), $reportOptions);
     $reportOptions['rowId']='external_key';
+    $reportOptions['columns'][] = array(
+      'actions'=>array(
+        array('img'=>report_helper::client_helper_path()."../media/images/add.png",'caption'=>'Click to add this species to the map')
+      )        
+    );
     $r .= report_helper::report_grid($reportOptions);
     $r .= '</div>';
-    $r .= '<div style="width: 50%; float: right;">';
+    $args['indicia_species_layer_slds']=report_helper::explode_lines($args['indicia_species_layer_slds']);
+    $r .= '<div id="rightcol">';
+    $r .= '<div id="layerbox">';
+    $r .= '<p id="instruct">'.lang::get('Click on the + buttons in the grid to add species layers to the map. You can add up to {1} layers at a time.',
+        count($args['indicia_species_layer_slds']));
+    $r .= '<p id="instruct2" style="display: none">'.lang::get('Use the - buttons to permanently remove layers, or untick the box in the legend to temporarily hide them.');
     $mapOptions = iform_map_get_map_options($args, $readAuth);
     $olOptions = iform_map_get_ol_options($args, $readAuth);
+    $r .= map_helper::layer_list(array(
+      'layerTypes'=>array('overlay'),
+      'includeSwitchers' => true,
+      'includeHiddenLayers' => true
+    ));
+    $r .= '</div>';
     $r .= map_helper::map_panel($mapOptions, $olOptions);
     $r .= '</div>';
     $websites = data_entry_helper::get_population_data(array(
@@ -121,16 +153,17 @@ class iform_quick_species_maps {
     $websiteIds = array();
     foreach ($websites as $website) 
       $websiteIds[] = $website['to_website_id'];
-    drupal_set_message(print_r($websiteIds, true));
     if (!empty($args['indicia_species_layer_feature_type']) && !empty(report_helper::$geoserver_url)) {
       $cql='website_id IN ('.implode(',',$websiteIds).') AND '.$args['indicia_species_layer_filter_field']."='{filterValue}'";
       report_helper::$javascript .= "indiciaData.indiciaSpeciesLayer = {\n".
-          '  "title":"'.lang::get('Online recording data for this species')."\",\n".
+          '  "title":"'.lang::get('{1}')."\",\n".
+          '  "myRecords":"'.lang::get('my records')."\",\n".
+          '  "userId":"'.$user->profile_indicia_user_id."\",\n".
           '  "featureType":"'.$args['indicia_species_layer_feature_type']."\",\n".
           '  "wmsUrl":"'.data_entry_helper::$geoserver_url."wms\",\n".
           "  \"cqlFilter\":\"$cql\",\n".
           "  \"filterField\":\"taxon_meaning_id\",\n".
-          '  "sld":"'.(isset($args['indicia_species_layer_sld']) ? $args['indicia_species_layer_sld'] : '')."\"\n".
+          '  "slds":'.json_encode($args['indicia_species_layer_slds'])."\n".
           "};\n";
     }
     return $r;
