33d4c29006af68d4add979d445185d1c3f6c96aa
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-08-15 13:04:00 +0000

    A data cleaner plugin to detect sample attribute values that have changed since a previous sample at the same site.

---
+++ core/trunk/modules/data_cleaner_sample_attribute_changes_for_site/plugins/data_cleaner_sample_attribute_changes_for_site.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package	Data Cleaner
+ * @subpackage Plugins
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL
+ * @link 	http://code.google.com/p/indicia/
+ */
+
+/**
+ * Hook into the data cleaner to declare checks for a sample attribute which describes something about a site that should not really 
+ * change. If there are 2 samples with the tested attribute but different values, then the 2nd will fail.
+ * @return type array of rules.
+ */
+function data_cleaner_sample_attribute_changes_for_site_data_cleaner_rules() {
+  return array(
+    'testType' => 'SampleAttributeChangesForSite',
+    'required' => array('Metadata'=>array('SampleAttr')),
+    'optional' => array('Metadata'=>array('SurveyId')),
+    'queries' => array(
+      array(
+        'joins' => 
+            "join sample_attribute_values sav on sav.sample_id=co.sample_id and sav.deleted=false
+            join samples s on s.id=sav.sample_id and s.deleted=false
+            join verification_rule_metadata vrm on vrm.value = cast(sav.sample_attribute_id as character varying) and vrm.key='SampleAttr' 
+                and vrm.deleted=false
+            join sample_attribute_values savprev on savprev.sample_attribute_id=sav.sample_attribute_id and savprev.deleted=false
+                and (coalesce(savprev.text_value, savprev.float_value::bpchar, savprev.int_value::bpchar) <> coalesce(sav.text_value, sav.float_value::bpchar, sav.int_value::bpchar) or
+                  coalesce(savprev.date_start_value, '1000-01-01')<>coalesce(sav.date_start_value, '1000-01-01') or
+                  coalesce(savprev.date_end_value, '1000-01-01')<>coalesce(sav.date_end_value, '1000-01-01') or
+                  coalesce(savprev.date_type_value, '')<>coalesce(sav.date_type_value, ''))
+            join samples sprev on sprev.id=savprev.sample_id and sprev.survey_id=s.survey_id and sprev.location_id=s.location_id
+            join verification_rules vr on vr.id=vrm.verification_rule_id and vr.test_type='SampleAttributeChangesForSite' 
+            left join verification_rule_metadata vrms on vrms.verification_rule_id=vr.id and vrms.key='SurveyId' and vrms.deleted=false ",
+        'where' =>
+            "((vrms.value=cast(co.survey_id as character varying) and vrms.value=cast(sprev.survey_id as character varying)) or vrms.id is null)"
+      )
+    )
+  );
+}
+
+?>
