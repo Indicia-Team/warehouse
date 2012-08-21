e3c73d6aee0b6328aad8c8c9b576badd62739fd0
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-08-21 08:44:41 +0000

    Case sensitivity issues fixed in data cleaner rules

--- core/trunk/modules/data_cleaner_period/plugins/data_cleaner_period.php
+++ core/trunk/modules/data_cleaner_period/plugins/data_cleaner_period.php
@@ -28,18 +28,19 @@
 function data_cleaner_period_data_cleaner_rules() {
   return array(
     'testType' => 'period',
-    'required' => array('Metadata'=>array('Tvk')),
-    'optional' => array('Metadata'=>array('StartDate','EndDate')),
+    'optional' => array('Metadata'=>array('StartDate','EndDate','Tvk','Taxon','TaxonMeaningId')),
     'queries' => array(
       // Slightly convoluted logic required in this test to get it to work with ranges in middle of year as well as ranges that span the end of the year.
       // Also note in these queries we use 2012 as the year for expanding dates that have just a month and day, as it is a leap
       // year so all dates are covered.
       array(
         'joins' => 
-            "join verification_rule_metadata vrm on (vrm.value=co.taxa_taxon_list_external_key and vrm.key='Tvk') or (vrm.value=co.preferred_taxon and vrm.key='Taxon') ".
+            "join verification_rule_metadata vrm on (upper(vrm.value)=upper(co.taxa_taxon_list_external_key) and upper(vrm.key)='TVK') ".
+            "or (upper(vrm.value)=upper(co.preferred_taxon) and upper(vrm.key)='TAXON') ".
+            "or (vrm.value=cast(co.taxon_meaning_id as character varying) and vrm.key='TaxonMeaningId') ".
             "join verification_rules vr on vr.id=vrm.verification_rule_id and vr.test_type='Period' ".
-            "left join verification_rule_metadata vrmstart on vrmstart.verification_rule_id=vr.id and vrmstart.key='StartDate' ".
-            "left join verification_rule_metadata vrmend on vrmend.verification_rule_id=vr.id and vrmend.key='EndDate' ",
+            "left join verification_rule_metadata vrmstart on vrmstart.verification_rule_id=vr.id and vrmstart.key='StartDate' and length(vrmstart.value)=8 ".
+            "left join verification_rule_metadata vrmend on vrmend.verification_rule_id=vr.id and vrmend.key='EndDate' and length(vrmend.value)=8 ",
         'where' =>
             "vr.reverse_rule<>((vrmstart is null or vrmend.value is null or vrmstart.value <= vrmend.value) ".
             "and ((vrmstart.value is not null and co.date_start < cast(vrmstart.value as date)) ".
