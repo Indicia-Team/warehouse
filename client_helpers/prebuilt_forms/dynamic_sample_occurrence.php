6564d25cc6ade3480e0cd08c680b10881a6fa150
jame2 <jame2@ceh.ac.uk>
2012-08-25 09:03:52 +0000

    Fix to prevent problems with double url-encoding

--- client_helpers/prebuilt_forms/dynamic_sample_occurrence.php
+++ client_helpers/prebuilt_forms/dynamic_sample_occurrence.php
@@ -591,16 +591,6 @@ class iform_dynamic_sample_occurrence extends iform_dynamic {
     return $attributes;
   }
   
-  protected static function getReloadPath () {
-    $reload = data_entry_helper::get_reload_link_parts();
-    unset($reload['params']['sample_id']);
-    unset($reload['params']['occurrence_id']);
-    unset($reload['params']['new']);
-    $reloadPath = $reload['path'];
-    if(count($reload['params'])) $reloadPath .= '?'.http_build_query($reload['params']);
-    return $reloadPath;
-  }
-  
   protected static function getHidden ($args) {
     $hiddens = '';
     if (!empty($args['sample_method_id'])) {
