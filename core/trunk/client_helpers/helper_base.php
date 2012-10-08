be51e7da081aab994c2a228fb3e857c121cdd22e
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-10-08 11:14:29 +0000

    Species checklist grid formatting.

--- core/trunk/client_helpers/helper_base.php
+++ core/trunk/client_helpers/helper_base.php
@@ -238,7 +238,7 @@ $("#{parentControlId}").trigger("change.indicia");'."\n",
         '<input type="hidden" id="imp-geom" name="geomFieldname" value="{defaultGeom}" />'.
         '<input type="hidden" id="{id}" name="{fieldname}" value="{default}" />',
   'attribute_cell' => "\n<td class=\"scOccAttrCell ui-widget-content {class}\" headers=\"{headers}\">{content}</td>",
-  'taxon_label_cell' => "\n<td class=\"scTaxonCell ui-state-default\" headers=\"{tableId}-species-{idx}\" {colspan}>{content}</td>",
+  'taxon_label_cell' => "\n<td class=\"scTaxonCell\" headers=\"{tableId}-species-{idx}\" {colspan}>{content}</td>",
   'helpText' => "\n<p class=\"{helpTextClass}\">{helpText}</p>",
   'file_box' => '',                   // the JQuery plugin default will apply, this is just a placeholder for template overrides.
   'file_box_initial_file_info' => '', // the JQuery plugin default will apply, this is just a placeholder for template overrides.
