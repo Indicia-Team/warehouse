9ef7d89ab930811ea751442d117da1727aa41925
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-10-10 13:05:48 +0000

    Fix to the quality check function when certainty is null.

---
+++ core/trunk/modules/indicia_setup/db/version_0_9_0/201210101403_quality_check.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION quality_check(quality character varying, record_status character, certainty character)
+  RETURNS boolean AS
+$BODY$
+DECLARE r boolean;
+BEGIN
+r=
+  -- always include verified
+  ((record_status='V') OR
+  -- include certain data if requested, unless expert marked as dubious
+  (certainty IS NOT NULL AND quality='C' AND certainty='C' AND record_status <> 'D') OR
+  -- include certain or likely data if requested, unless expert marked as dubious. Certainty not indicated treated as likely
+  (quality='L' AND (certainty in ('C', 'L') OR (certainty IS NULL)) AND record_status <> 'D') OR
+  -- include anything not dubious or worse, if requested
+  (quality='!D' AND record_status NOT IN ('D')) OR
+  -- or just include anything not rejected
+  (quality='!R')) AND 
+  -- always exclude rejected, in progress and test records
+  record_status NOT IN ('R', 'I', 'T');
+RETURN r;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
