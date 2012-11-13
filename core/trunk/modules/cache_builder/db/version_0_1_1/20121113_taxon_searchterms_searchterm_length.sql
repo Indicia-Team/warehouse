a40a99a3d14054493a1a496541bfffb93165168b
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-11-13 14:45:11 +0000

    Removed indicia schema and added query which updates new searchterm length field

--- core/trunk/modules/cache_builder/db/version_0_1_1/20121113_taxon_searchterms_searchterm_length.sql
+++ core/trunk/modules/cache_builder/db/version_0_1_1/20121113_taxon_searchterms_searchterm_length.sql
@@ -1,2 +1,5 @@
-ALTER TABLE indicia.cache_taxon_searchterms ADD COLUMN searchterm_length integer;
-COMMENT ON COLUMN indicia.cache_taxon_searchterms.searchterm_length IS 'Contains the length of the searchterm field, useful for taxon name searches. Putting shorter searchterms at the top of a list brings the "nearest" matches to the top.';
+ALTER TABLE cache_taxon_searchterms ADD COLUMN searchterm_length integer;
+COMMENT ON COLUMN cache_taxon_searchterms.searchterm_length IS 'Contains the length of the searchterm field, useful for taxon name searches. Putting shorter searchterms at the top of a list brings the "nearest" matches to the top.';
+
+UPDATE cache_taxon_searchterms SET searchterm_length=LENGTH(searchterm);
+
