3c2430fa18dee32e3eb0644ae3ba9db41284e353
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2012-11-22 16:13:11 +0000

    Minor comment tweak

--- modules/cache_builder/plugins/cache_builder.php
+++ modules/cache_builder/plugins/cache_builder.php
@@ -13,8 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
  *
- * @package	Verification Check
- * @subpackage Plugins
+ * @package	Modules
+ * @subpackage Cache builder
  * @author	Indicia Team
  * @license	http://www.gnu.org/licenses/gpl.html GPL
  * @link 	http://code.google.com/p/indicia/
