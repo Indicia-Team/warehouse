a7b7de283eae5f527174fee21df117b15a47e17b
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2012-11-27 20:56:35 +0000

    Added cache_occurrences to the tables with taxonominc_sort_order updated to bigint

--- core/trunk/modules/cache_builder/db/version_0_1_1/201211271906_cache_ttl_sort_order_size.sql
+++ core/trunk/modules/cache_builder/db/version_0_1_1/201211271906_cache_ttl_sort_order_size.sql
@@ -1,2 +1,3 @@
 -- update field size to reflect previous change to taxa_taxon_lists table
-ALTER TABLE cache_taxa_taxon_lists ALTER taxonomic_sort_order TYPE bigint
+ALTER TABLE cache_taxa_taxon_lists ALTER taxonomic_sort_order TYPE bigint;
+ALTER TABLE cache_occurrences ALTER taxonomic_sort_order TYPE bigint;
