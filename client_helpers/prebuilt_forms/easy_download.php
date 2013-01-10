6eadd4a9f29e15e45b657424c962397cd968aadb
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-01-10 10:25:57 +0000

    Updates to the Easy Download prebuilt form, which allow an All Records download. This can of course be pared back with some report filters...

--- client_helpers/prebuilt_forms/easy_download.php
+++ client_helpers/prebuilt_forms/easy_download.php
@@ -64,6 +64,48 @@ class iform_easy_download {
         'default'=>1
       ),
       array(
+        'name'=>'allow_experts_data',
+        'caption'=>'Allow expert\'s data download?',
+        'description'=>'Does this page include an option to download data that the user is an expert for, if they are an expert?',
+        'type'=>'checkbox',
+        'required'=>false,
+        'default'=>1
+      ),
+      array(
+        'name'=>'allow_all_data',
+        'caption'=>'Allow all data download?',
+        'description'=>'Does this page include an option to download all data that matches the filter?',
+        'type'=>'checkbox',
+        'required'=>false,
+        'default'=>0
+      ),
+      array(
+        'name'=>'csv_format',
+        'caption'=>'CSV format download?',
+        'description'=>'Is CSV format available as a download option?',
+        'type'=>'select',
+        'options'=>array(
+          'no'=>'No',
+          'yes'=>'Yes',
+          'expert'=>'Yes, but only for experts'
+        ),
+        'required'=>true,
+        'default'=>'yes'
+      ),
+      array(
+        'name'=>'nbn_format',
+        'caption'=>'NBN format download?',
+        'description'=>'Is NBN format available as a download option?',
+        'type'=>'select',
+        'options'=>array(
+          'no'=>'No',
+          'yes'=>'Yes',
+          'expert'=>'Yes, but only for experts'
+        ),
+        'required'=>true,
+        'default'=>'expert'
+      ),
+      array(
         'name'=>'survey_id',
         'caption'=>'Survey for download',
         'description'=>'Select the survey to download data for, or leave blank to allow user selection.',
@@ -107,6 +149,15 @@ class iform_easy_download {
         'type'=>'textarea',
         'required'=>false,
         'default'=>"smpattrs=\noccattrs=\nsearchArea=\nidlist=\nquality=V\n"
+      ),
+      array(
+        'name'=>'limit',
+        'caption'=>'Limit to number of records',
+        'description'=>'For performance reasons, unlimited downloads are not recommended. Set this to control the number of records '.
+            'that can be downloaded at one time, or set to 0 for no limit.',
+        'type'=>'text_input',
+        'required'=>false,
+        'default'=>20000
       )
     ); 
   }
@@ -119,11 +170,34 @@ class iform_easy_download {
    * @param array $response When this form is reloading after saving a submission, contains the response from the service call.
    * Note this does not apply when redirecting (in this case the details of the saved object are in the $_GET data).
    * @return Form HTML.
-   * @todo: Implement this method 
    */
   public static function get_form($args, $node, $response=null) {
+    // apply defaults to options that have been added since the original version of this form
+    $args = array_merge(array(
+      'allow_experts_data'=>true,
+      'allow_all_data'=>false,
+      'csv_format'=>'yes',
+      'nbn_format'=>'expert',
+      'limit'=>10000
+    ), $args);
+    
+    // Do they have expert access?
+    $expert = (function_exists('user_access') && user_access($args['permission']));
+    
+    // Find out which types of filters and formats are available to the user
+    $filters = self::get_filters($args);
+    $formats = array();
+    if ($args['csv_format']==='yes' || ($args['csv_format']==='expert' && $expert))
+      array_push($formats, 'csv');
+    if ($args['nbn_format']==='yes' || ($args['nbn_format']==='expert' && $expert))
+      array_push($formats, 'nbn');
+    if (count($filters)===0)
+      return 'This download page is configured so that no filter options are available.';
+    if (count($formats)===0)
+      return 'This download page is configured so that no download format options are available.';
+    
     if (!empty($_POST))
-      self::do_download($args);
+      self::do_download($args, $filters);
     $conn = iform_get_connection_details($node);
     $readAuth = data_entry_helper::get_read_auth($conn['website_id'], $conn['password']);
     iform_load_helpers(array('data_entry_helper'));
@@ -132,27 +206,28 @@ class iform_easy_download {
     if(count($reload['params'])) $reloadPath .= '?'.helper_base::array_to_query_string($reload['params']);
     $r = '<form method="POST" action="'.$reloadPath.'">';
     $r .= '<fieldset><legend>'.lang::get('Filters').'</legend>';
-    $expert = (function_exists('user_access') && user_access($args['permission']));
-    if ($expert && $args['allow_my_data']) {
+    if (count($filters)===0)
+      return 'This download page is configured so that no filter options are available.';
+    elseif (count($filters)===1) {
+      $r .= '<input type="hidden" name="user-filter" value="'.implode('', array_keys($filters)).'"/>';
+      // Since there is only one option, we may as well tell the user what it is.
+      drupal_set_title(implode('', array_values($filters)));
+      if (implode('', array_keys($filters))==='mine')
+        $r .= '<p>'.lang::get('Use this form to download your own records.').'</p>';
+    }
+    else {
       $r .= data_entry_helper::radio_group(array(
         'label' => lang::get('User filter'),
         'fieldname'=>'user-filter',
-        'lookupValues' => array(
-          'mine'=>'Download my data only',
-          'all'=>'Download all data I have access to as an expert'
-        ),
+        'lookupValues' => $filters,
         'default'=>(empty($_POST['user-filter']) ? 'mine' : $_POST['user-filter'])
       ));
-    } elseif (function_exists('drupal_set_title') && $args['allow_my_data']) 
-      // Only allowed to download your own data, so subtly tell them 
-      drupal_set_title(lang::get('Download my records'));
-    else
-      $r .= '<p>'.lang::get('Use this form to download your own records.').'</p>';
+    } 
     if (empty($args['survey_id'])) {
       // A survey picker when downloading my data
-      $r .= '<div id="survey_mine">';
+      $r .= '<div id="survey_all">';
       $r .= data_entry_helper::select(array(
-        'fieldname' => 'survey_id_mine',
+        'fieldname' => 'survey_id_all',
         'label' => lang::get('Survey to include'),
         'table' => 'survey',
         'valueField' => 'id',
@@ -174,7 +249,7 @@ class iform_easy_download {
       }
       $r .= '<div id="survey_expertise">';
       $r .= data_entry_helper::select(array(
-        'fieldname' => 'survey_id_expertise',
+        'fieldname' => 'survey_id_expert',
         'label' => lang::get('Survey to include'),
         'table' => 'survey',
         'valueField' => 'id',
@@ -210,6 +285,22 @@ class iform_easy_download {
     return $r;
   } 
   
+  /**
+   * Returns an array of the available types of filter.
+   * @param array $args Form arguments
+   * @return array Associative array of filter type 
+   */
+  private static function get_filters($args) {
+    $filters = array();
+    if ($args['allow_my_data'])
+      $filters['mine']=lang::get('Download my records');
+    if ($args['allow_experts_data'])
+      $filters['expert']=lang::get('Download all records I have access to as an expert');
+    if ($args['allow_all_data'])
+      $filters['all']=lang::get('Download all records');
+    return $filters;
+  }
+  
   /** 
    * Handles a request for download. Works out which type of request it is and calls the appropriate function.
    */
@@ -228,15 +319,17 @@ class iform_easy_download {
     global $indicia_templates;
     // let's just get the URL, not the whole anchor element
     $indicia_templates['report_download_link'] = '{link}';
+    $limit = ($args['limit']==0 ? '' : $args['limit']); // unlimited or limited
     $url = report_helper::report_download_link(array(
       'readAuth'=>$readAuth,
       'dataSource'=>$args["report_$format"],
       'extraParams'=>$filter,
       'format'=>$format,
-      'sharing'=>'data_flow'
+      'sharing'=>'data_flow',
+      'itemsPerPage'=>$limit
     ));
-    //drupal_set_message($url);
-    header("Location: $url");
+    drupal_set_message($url);
+    //header("Location: $url");
   }
   
   private static function build_filter($args, $readAuth, $format) {
@@ -244,26 +337,38 @@ class iform_easy_download {
     $location_expertise = hostsite_get_user_field('location_expertise');
     $taxon_groups_expertise = hostsite_get_user_field('taxon_groups_expertise');
     $taxon_groups_expertise = $taxon_groups_expertise ? unserialize($taxon_groups_expertise) : null;
-    $ownData = (isset($_POST['user-filter']) && $_POST['user-filter'] === 'all') || !$args['allow_my_data'] ? 0 : 1;
-    $surveys_expertise = hostsite_get_user_field('surveys_expertise');
-    $surveys_expertise = $surveys_expertise ? unserialize($surveys_expertise) : null;
-    // either a selected survey, or the surveys the user can see. The field name used will depend on which of the 2 survey selects were active.
+    $filters = self::get_filters($args);
+    $filterToApply = $_POST['user-filter'];
+    if (!array_key_exists($filterToApply, $filters))
+      throw new exception('Selected filter type not authorised');
+    // get the surveys the user could have picked from
+    if ($filterToApply==='expert') {
+      $surveys_expertise = hostsite_get_user_field('surveys_expertise');
+      $available_surveys = $surveys_expertise ? unserialize($surveys_expertise) : null;
+    } else {
+      // set to blank - in the report filter this will act as not filtered
+      $available_surveys = '';
+    }
+    // We are downloading either a configured survey, a selected single survey, or the surveys the 
+    // user can see. The field name used will depend on which of the survey selects were active - 
+    // either we are selecting from a list of surveys the user is an expert for, or a list of
+    // all surveys.
+    $surveyFieldName='survey_id_'.($filterToApply==='expert' ? 'expert' : 'all');
     if (empty($args['survey_id'])) {
-      $surveyFieldName=$ownData ? 'survey_id_mine' : 'survey_id_expertise';
-      $surveys = empty($_POST[$surveyFieldName]) ? implode(',', $surveys_expertise) : $_POST[$surveyFieldName];
+      $surveys = empty($_POST[$surveyFieldName]) ? implode(',', $available_surveys) : $_POST[$surveyFieldName];
     } else 
       // survey to load is preconfigured for the form
       $surveys = $args['survey_id'];
     $filters = array_merge(
       array(
         'currentUser'=>hostsite_get_user_field('indicia_user_id'),
-        'ownData'=>$ownData,
+        'ownData'=>$filterToApply==='mine' ? 1 : 0,
         'location_id'=>hostsite_get_user_field('location_expertise'),
         'ownLocality'=>!empty($location_expertise) && !$ownData ? 1 : 0,
         'taxon_groups'=>!empty($taxon_groups_expertise) ? implode(',', $taxon_groups_expertise) : '',
         'ownGroups'=>!empty($taxon_groups_expertise) && $taxon_groups_expertise && !$ownData ? 1 : 0,
         'surveys'=>$surveys,
-        'ownSurveys'=>((empty($surveys_expertise) || $ownData) && empty($_POST[$surveyFieldName])) && empty($args['survey_id']) ? 0 : 1
+        'ownSurveys'=>empty($surveys) ? 0 : 1
       ), get_options_array_with_user_data($args["report_params_$format"])
     );
     if (!empty($_POST['date_from']) && $_POST['date_from']!==lang::get('Click here'))
