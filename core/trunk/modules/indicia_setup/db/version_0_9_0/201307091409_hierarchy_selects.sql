30241fa4a8a6175c33da82802f20a6bdb5dcfc10
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2013-07-09 13:46:25 +0000

    Minor correction.

--- core/trunk/modules/indicia_setup/db/version_0_9_0/201307091409_hierarchy_selects.sql
+++ core/trunk/modules/indicia_setup/db/version_0_9_0/201307091409_hierarchy_selects.sql
@@ -1,3 +1,3 @@
 insert into control_types (control, for_data_type, multi_value)
 select 'hierarchical_select', 'L', 'f'
-where not exists(select 1 from control_types where control='hierarchy_select' and for_data_type='L')
+where not exists(select 1 from control_types where control='hierarchical_select' and for_data_type='L')
