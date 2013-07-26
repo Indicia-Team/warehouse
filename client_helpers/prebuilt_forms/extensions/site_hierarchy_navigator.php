2bcd0275c4cee54f6eae30159b7542a615f72628
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-07-26 09:54:27 +0000

    Comment improvements and bomb-proofing.

--- client_helpers/prebuilt_forms/extensions/site_hierarchy_navigator.php
+++ client_helpers/prebuilt_forms/extensions/site_hierarchy_navigator.php
@@ -26,16 +26,26 @@
  */
 class extension_site_hierarchy_navigator {
   
+  /**
+   * Hierarchy map control, which can be added to user interface form configurations using [site_hierarchy_navigator.map].
+   *
+   * Display a map with polygons loaded onto it of a particular location type. When the user clicks on one, reloads the map layer
+   * to show the intersecting polygons from the next location type. Continues down the locations hierarchy in a supplied sequence of 
+   * location types (e.g. you might set the location type sequence to Country, County, Parish, Site). 
+   *
+   * Supply an option @layerLocationTypes with a comma separated array of the location types ID to load in top down order.
+   */
   public function map($auth, $args, $tabalias, $options, $path) {
     if (empty($options['layerLocationTypes']))
-      return '<label>Please provide a layerLocationTypes attribute for the [site_hierarchy_navigator.map] map control on the edit tab</label>';
+      return '<p>Please provide an @layerLocationTypes option for the [site_hierarchy_navigator.map] map control on the edit tab</p>';
+    if (!preg_match('/^([0-9]*,\s*)*[0-9]*\s*$/', $options['layerLocationTypes']))
+      return '<p>The supplied @layerLocationTypes is not of the required format, a comma separated list of location type ids (from the termlists_terms table).</p>';
     iform_load_helpers(array('map_helper','report_helper'));
-    $jsPath = iform_client_helpers_path().'prebuilt_forms/extensions/site_hierarchy_navigator.js';
-    drupal_add_js($jsPath);
+    drupal_add_js(iform_client_helpers_path().'prebuilt_forms/extensions/site_hierarchy_navigator.js');
     //The location types are supplied by the user in a comma seperated list.
     //The first number is used as the initial location type to display.
     //The second number is used after the user clicks the first time on a feature and so on
-    $layerLocationTypes =  explode(',',$options['layerLocationTypes']);  
+    $layerLocationTypes = explode(',', $options['layerLocationTypes']);  
     $options = iform_map_get_map_options($args, $auth);
     $olOptions = iform_map_get_ol_options($args);
     $options['readAuth'] = $options['readAuth']['read'];
@@ -44,8 +54,7 @@ class extension_site_hierarchy_navigator {
     $options['clickableLayersOutputMode'] = 'customFunction';
     $options['customClickFn']='reload_map_with_sub_sites_for_clicked_feature';
     $options['clickableLayersOutputDiv'] = '';
-    //Tell the system which layers we to be clickable. As we initially only use
-    //one layer to start with, we only supply one item.
+    //Tell the system which layers we to be clickable.
     $options['clickableLayers']=array('indiciaData.reportlayer');
     $r .= '<div id="map-breadcrumb"></div>';
     $r .= map_helper::map_panel(
