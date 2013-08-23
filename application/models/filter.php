5e5eea2fe3140c219575ac771d402a8ed7d907b4
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-08-23 09:53:42 +0000

    Major upgrade to reporting back end to support: 1) A standardised report filtering system, so the same set of filters can be applied to a broad range of reports. 2) Map reporting based on 1k and 10k grid squares with auto-selection of the correct scale as you zoom in. 3) Auto-selection of the blurred vs precise version of a record when verifying or viewing own records vs other reporting scenarios. This commit is reasonably tested but not yet ready for live use.

---
+++ application/models/filter.php
@@ -0,0 +1,46 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package	Core
+ * @subpackage Models
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL
+ * @link 	http://code.google.com/p/indicia/
+ */
+
+/**
+ * Model class for the Filters table.
+ *
+ * @package	Core
+ * @subpackage Models
+ * @link	http://code.google.com/p/indicia/wiki/DataModel
+ */
+class Filter_Model extends ORM {
+  public $search_field='title';
+
+  protected $belongs_to = array('created_by'=>'user', 'updated_by'=>'user');
+
+  public function validate(Validation $array, $save = FALSE) {
+    // uses PHP trim() to remove whitespace from beginning and end of all fields before validation
+    $array->pre_filter('trim');
+    $array->add_rules('title', 'required');
+    $array->add_rules('definition', 'required');
+    $array->add_rules('sharing', 'required', 'chars[R,V,D]');
+    $this->unvalidatedFields = array('description', 'public, defines_permissions');
+    return parent::validate($array, $save);
+  }
+
+}
