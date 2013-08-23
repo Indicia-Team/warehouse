bf903be554a1020d0044ff8cdfebd20175c39e61
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2013-08-23 16:05:14 +0000

    More efficient building of the map squares table.

--- core/trunk/modules/cache_builder/db/version_0_1_2/201308161717_map_squares.sql
+++ core/trunk/modules/cache_builder/db/version_0_1_2/201308161717_map_squares.sql
@@ -47,7 +47,6 @@ SELECT DISTINCT on (round(st_x(st_centroid(reduce_precision(s.geom, o.confidenti
   GREATEST(o.sensitivity_precision, 10000)
 FROM samples s
 JOIN occurrences o ON o.sample_id=s.id
--- left join to ensure no duplicates, as 1km index includes 10km squares for sensitive records
 LEFT JOIN map_squares msq 
 	ON msq.x=round(st_x(st_centroid(reduce_precision(s.geom, o.confidential, GREATEST(o.sensitivity_precision, 10000), s.entered_sref_system))))
 	AND msq.y=round(st_y(st_centroid(reduce_precision(s.geom, o.confidential, GREATEST(o.sensitivity_precision, 10000), s.entered_sref_system))))
@@ -58,26 +57,47 @@ AND msq.id IS NULL;
 CREATE UNIQUE INDEX ix_map_squares_unique
    ON map_squares (x ASC NULLS LAST, y ASC NULLS LAST, size ASC NULLS LAST);
 
+SELECT DISTINCT ON (o.confidential, o.sensitivity_precision, s.entered_sref, s.entered_sref_system) 
+    s.geom, o.confidential, o.sensitivity_precision, s.entered_sref, s.entered_sref_system, 
+    round(st_x(st_centroid(reduce_precision(s.geom, o.confidential, greatest(o.sensitivity_precision, 1000), s.entered_sref_system)))) as x1k,
+    round(st_y(st_centroid(reduce_precision(s.geom, o.confidential, greatest(o.sensitivity_precision, 1000), s.entered_sref_system)))) as y1k,
+    greatest(o.sensitivity_precision, 1000) as size1k,
+    round(st_x(st_centroid(reduce_precision(s.geom, o.confidential, greatest(o.sensitivity_precision, 10000), s.entered_sref_system)))) as x10k,
+    round(st_y(st_centroid(reduce_precision(s.geom, o.confidential, greatest(o.sensitivity_precision, 10000), s.entered_sref_system)))) as y10k,
+    greatest(o.sensitivity_precision, 10000) as size10k,
+    cast(null as integer) as msq_id1k, cast(null as integer) as msq_id10k
+INTO temporary interim
+FROM occurrences o
+JOIN samples s on s.id=o.sample_id AND s.deleted=false
+WHERE o.deleted=false;
+
+UPDATE interim t SET msq_id1k=msq.id
+FROM map_squares msq
+WHERE msq.x=t.x1k
+AND msq.y=t.y1k
+AND msq.size=t.size1k;
+
+UPDATE interim t SET msq_id10k=msq.id
+FROM map_squares msq
+WHERE msq.x=t.x10k
+AND msq.y=t.y10k
+AND msq.size=t.size10k;
+
 UPDATE cache_occurrences co
-SET map_sq_1km_id=msq.id
-FROM map_squares msq, samples s, occurrences o
-WHERE s.id=co.sample_id AND o.id=co.id
-AND msq.x=round(st_x(st_centroid(reduce_precision(s.geom, o.confidential, greatest(o.sensitivity_precision, 1000), s.entered_sref_system))))
-AND msq.y=round(st_y(st_centroid(reduce_precision(s.geom, o.confidential, greatest(o.sensitivity_precision, 1000), s.entered_sref_system))))
-AND msq.size=greatest(o.sensitivity_precision, 1000);
+SET map_sq_1km_id=t.msq_id1k, map_sq_10km_id=t.msq_id10k
+FROM interim t, occurrences o, samples s
+WHERE o.id=co.id and s.id=co.sample_id
+and t.confidential=o.confidential
+and coalesce(t.sensitivity_precision, 0)=coalesce(o.sensitivity_precision, 0)
+and t.entered_sref=s.entered_sref
+and t.entered_sref_system=s.entered_sref_system;
+
+DROP TABLE interim;
 
 CREATE INDEX ix_cache_occurrences_map_sq_1km_id
   ON cache_occurrences
   USING btree
   (map_sq_1km_id);
-
-UPDATE cache_occurrences co
-SET map_sq_10km_id=msq.id
-FROM map_squares msq, samples s, occurrences o
-WHERE s.id=co.sample_id AND o.id=co.id
-AND msq.x=round(st_x(st_centroid(reduce_precision(s.geom, o.confidential, greatest(o.sensitivity_precision, 10000), s.entered_sref_system))))
-AND msq.y=round(st_y(st_centroid(reduce_precision(s.geom, o.confidential, greatest(o.sensitivity_precision, 10000), s.entered_sref_system))))
-AND msq.size=greatest(o.sensitivity_precision, 10000);
 
 CREATE INDEX ix_cache_occurrences_map_sq_10km_id
   ON cache_occurrences
