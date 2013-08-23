1404c88aa5b54e392c12cc93863705d39f852bf8
vanbreda <vanbreda@btinternet.com>
2013-08-23 12:13:02 +0000

    remove duplicate closing div when clickForSpatialRef is false

--- client_helpers/map_helper.php
+++ client_helpers/map_helper.php
@@ -419,7 +419,7 @@ class map_helper extends helper_base {
                   '<span class="label"></span><span class="data"></span><span>('.lang::get('hold +').')</span></div>';
         else
           $r .= $div . '<h3>' . lang::get('Map ref at pointer') . '</h3>' .
-              '<div class="grid-ref-hint hint-normal"><span class="label"></span><span class="data"></span></div></div>';
+              '<div class="grid-ref-hint hint-normal"><span class="label"></span><span class="data"></span></div>';
         $r .= '</div>';
       }
       return $r;
