b0330e687b5518d82f76653071adf3d56581b02c
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-08-28 14:36:16 +0000

    New functionality to support recording groups. This is the warehouse end of things - work still in progress.

---
+++ client_helpers/prebuilt_forms/group_edit.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package Client
+ * @subpackage PrebuiltForms
+ * @author  Indicia Team
+ * @license http://www.gnu.org/licenses/gpl.html GPL 3.0
+ * @link  http://code.google.com/p/indicia/
+ */
+
+/**
+ * 
+ * 
+ * @package Client
+ * @subpackage PrebuiltForms
+ * A page for editing or creating a user group.
+ */
+class iform_group_edit {
+  
+  /** 
+   * Return the form metadata.
+   * @return array The definition of the form.
+   */
+  public static function get_group_edit_definition() {
+    return array(
+      'title'=>'Create or edit a group',
+      'category' => 'Utilities',
+      'description'=>'A form for creating or editing groups of recorders.'
+    );
+  }
+  
+  /**
+   * Get the list of parameters for this form.
+   * @return array List of parameters that this form requires.
+   * @todo: Implement this method
+   */
+  public static function get_parameters() {   
+    return array();
+  }
+  
+  /**
+   * Return the generated form output.
+   * @param array $args List of parameter values passed through to the form depending on how the form has been configured.
+   * This array always contains a value for language.
+   * @param object $node The Drupal node object.
+   * @param array $response When this form is reloading after saving a submission, contains the response from the service call.
+   * Note this does not apply when redirecting (in this case the details of the saved object are in the $_GET data).
+   * @return Form HTML.
+   * @todo: Implement this method 
+   */
+  public static function get_form($args, $node, $response=null) {
+    return data_entry_helper::date_picker(array(
+      'label'=>'My date' 
+    ));
+  }
+
+}
