702b932a00735e69789edbf65f1397cc8162f07d
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2013-09-12 07:56:15 +0000

    Updates to the groups edit page, with support for different types of groups etc.

--- core/trunk/client_helpers/prebuilt_forms/group_edit.php
+++ core/trunk/client_helpers/prebuilt_forms/group_edit.php
@@ -23,12 +23,14 @@
 require_once('includes/report_filters.php');
 
 /**
- * A page for editing or creating a recording group.
+ * A page for editing or creating a group of people, such as a recording group, organisation or project.
  * 
  * @package Client
  * @subpackage PrebuiltForms
  */
 class iform_group_edit {
+  
+  private static $groupType='group';
   
   /** 
    * Return the form metadata.
@@ -124,6 +126,17 @@ class iform_group_edit {
         'type'=>'checkbox',
         'default'=>false,
         'required'=>false
+      ),
+      array(
+        'name'=>'include_members',
+        'caption'=>'Include members control',
+        'description'=>'Include a control for setting up a list of the members for this group? If not set, then the group '.
+            'creator automatically gets assigned as the administrator. Do not use this option for group joining methods that '.
+            'involve the members requesting or being invited - this is only appropriate when the group admin explicitly controls '.
+            'the group membership.',
+        'type'=>'checkbox',
+        'default'=>false,
+        'required'=>false
       )
     );
   }
@@ -144,7 +157,8 @@ class iform_group_edit {
       'include_dates'=>false,
       'include_report_filter'=>true,
       'include_private_records'=>false,
-      'include_administrators'=>false
+      'include_administrators'=>false,
+      'include_members'=>false
     ), $args);
     $optionalFields=report_helper::explode_lines($args['optional_fields']);
     $reloadPath = self::getReloadPath();   
@@ -166,25 +180,23 @@ class iform_group_edit {
         'table'=>'termlists_term',
         'extraParams'=>$auth['read'] + array('id'=>$args['group_type'])
       ));
-      $groupType=strtolower($response[0]['term']);
-    } else {
-      $groupType=lang::get('group');
+      self::$groupType=strtolower($response[0]['term']);
     }
     if (!empty(data_entry_helper::$entity_to_load['group:title']) && function_exists('drupal_set_title'))
       drupal_set_title(lang::get('Edit {1}', data_entry_helper::$entity_to_load['group:title']));
     $r .= data_entry_helper::text_input(array(
-      'label' => lang::get('{1} name', ucfirst($groupType)),
+      'label' => lang::get('{1} name', ucfirst(self::$groupType)),
       'fieldname'=>'group:title',
       'validation'=>array('required'),
       'class'=>'control-width-5',
-      'helpText'=>lang::get('The full title of the {1}', $groupType)
+      'helpText'=>lang::get('The full title of the {1}', self::$groupType)
     ));
     if ($args['include_code'])
       $r .= data_entry_helper::text_input(array(
         'label' => lang::get('Code'),
         'fieldname'=>'group:code',
         'class'=>'control-width-4',
-        'helpText'=>lang::get('A code or abbreviation identifying the {1}', $groupType)
+        'helpText'=>lang::get('A code or abbreviation identifying the {1}', self::$groupType)
       ));
     if (empty($args['group_type'])) {
       $r .= data_entry_helper::select(array(
@@ -198,14 +210,58 @@ class iform_group_edit {
         'class'=>'control-width-4'
       ));
     }
+    $r .= self::joinMethodsControl($args);
+    $r .= data_entry_helper::textarea(array(
+      'label' => ucfirst(lang::get('{1} description', self::$groupType)),
+      'fieldname' => 'group:description',
+      'helpText' => lang::get('Description and notes about the {1}.', self::$groupType)
+    ));
+    $r .= self::dateControls($args);
+    if ($args['include_private_records']) 
+      $r .= data_entry_helper::checkbox(array(
+        'label' => lang::get('Records are private'),
+        'fieldname'=>'group:private_records',
+        'helpText'=>lang::get('Tick this box if you want to withold the release of the records from this {1} until a '.
+          'later point in time, e.g. when a project is completed.', self::$groupType)
+      ));
+    $r .= self::memberControls($args, $auth);
+    $r .= self::reportFilterBlock($args, $auth, $hiddenPopupDivs);
+    // auto-insert the creator as an admin of the new group, unless the admins are manually specified
+    if (!$args['include_administrators'])
+      $r .= '<input type="hidden" name="groups_user:admin_user_id" value="' .hostsite_get_user_field('indicia_user_id'). '"/>';
+    $r .= '<input type="hidden" name="groups_user:administrator" value="t"/>';
+    $r .= '<input type="submit" class="indicia-button" id="save-button" value="'.
+        (empty(data_entry_helper::$entity_to_load['group:id']) ? 
+        lang::get('Create {1}', self::$groupType) : lang::get('Update {1} settings', self::$groupType))
+        ."\" />\n";    
+    $r .= '</form>';
+    $r .= $hiddenPopupDivs;
     
+    data_entry_helper::enable_validation('entry_form');
+    // JavaScript to grab the filter definition and store in the form for posting when the form is submitted
+    data_entry_helper::$javascript .= "
+$('#entry_form').submit(function() {
+  $('#filter-title-val').val('" . lang::get('Filter for user group') . " ' + $('#group\\\\:title').val());
+  $('#filter-def-val').val(JSON.stringify(indiciaData.filter.def));
+});\n";
+    return $r;
+  }
+  
+  /**
+   * Returns a control for picking one of the allowed joining methods. If there is only one, 
+   * then this is output as a single hidden input.
+   * @param array $args Form configuration arguments
+   * @return string HTML to output
+   */
+  private static function joinMethodsControl($args) {
+    $r = '';
     $joinMethods=data_entry_helper::explode_lines_key_value_pairs($args['join_methods']);
     if (count($joinMethods)===1) {
       $methods=array_keys($joinMethods);
       $r .= '<input type="hidden" name="group:joining_method" value="'.$methods[0].'"/>';
     } else {
       $r .= data_entry_helper::radio_group(array(
-        'label' => ucfirst(lang::get('{1} membership', $groupType)),
+        'label' => ucfirst(lang::get('{1} membership', self::$groupType)),
         'fieldname' => 'group:joining_method',
         'lookupValues' => $joinMethods,
         'helpText' => lang::get('How can users join this group?'),
@@ -213,14 +269,19 @@ class iform_group_edit {
         'validation'=>array('required')
       ));
     }
-    $r .= data_entry_helper::textarea(array(
-      'label' => ucfirst(lang::get('{1} description', $groupType)),
-      'fieldname' => 'group:description',
-      'helpText' => lang::get('Description and notes about the {1}.', $groupType)
-    ));
+    return $r;
+  }
+  
+  /**
+   * Returns controls for defining the date range of a group if this option is enabled. 
+   * @param array $args Form configuration arguments
+   * @return string HTML to output
+   */
+  private static function dateControls($args) {
+    $r = '';
     if ($args['include_dates']) {
       $r .= data_entry_helper::date_picker(array(
-        'label' => ucfirst(lang::get('{1} active from', $groupType)),
+        'label' => ucfirst(lang::get('{1} active from', self::$groupType)),
         'fieldname' => 'group:from_Date',
         'suffixTemplate' => 'nosuffix'
       ));
@@ -228,26 +289,50 @@ class iform_group_edit {
         'label' => lang::get('to'),
         'fieldname' => 'group:to_Date',
         'labelClass' => 'auto',
-        'helpText' => lang::get('Specify the period during which the {1} was active.', $groupType)
+        'helpText' => lang::get('Specify the period during which the {1} was active.', self::$groupType)
       ));
     }
-    if ($args['include_private_records']) 
-      $r .= data_entry_helper::checkbox(array(
-        'label' => lang::get('Records are private'),
-        'fieldname'=>'group:private_records',
-        'helpText'=>lang::get('Tick this box if you want to withold the release of the records from this {1} until a '.
-          'later point in time, e.g. when a project is completed.', $groupType)
-      ));
+    return '';
+  }
+  
+  /**
+   * Returns controls for defining the list of group members and administrators if this option is enabled. 
+   * @param array $args Form configuration arguments
+   * @return string HTML to output
+   */
+  private static function memberControls($args, $auth) {
+    $r = '';
     if ($args['include_administrators']) {
       $r .= data_entry_helper::sub_list(array(
-        'fieldname'=>'groups_user:user_id',
-        'label' => ucfirst(lang::get('{1} administrators', $groupType)),
+        'fieldname'=>'groups_user:admin_user_id',
+        'label' => ucfirst(lang::get('{1} administrators', self::$groupType)),
         'table'=>'user',
         'captionField'=>'person_name',
         'valueField'=>'id',
         'extraParams'=>$auth['read']+array('view'=>'detail')
       ));
     }
+    if ($args['include_members']) {
+      $r .= data_entry_helper::sub_list(array(
+        'fieldname'=>'groups_user:user_id',
+        'label' => lang::get('Other {1} members', self::$groupType),
+        'table'=>'user',
+        'captionField'=>'person_name',
+        'valueField'=>'id',
+        'extraParams'=>$auth['read']+array('view'=>'detail')
+      ));
+    }
+    return $r;
+  }
+  
+  /**
+   * Returns controls allowing a records filter to be defined and associated with the group. 
+   * @param array $args Form configuration arguments
+   * @return string HTML to output
+   */
+  private static function reportFilterBlock($args, $auth, &$hiddenPopupDivs) {
+    $r = '';
+    $hiddenPopupDivs='';
     if ($args['include_report_filter']) {
       $r .= '<p>' . lang::get('LANG_Filter_Instruct') . '</p>';
       $r .= '<label>' . lang::get('Group parameters') . ':</label>';
@@ -263,25 +348,6 @@ class iform_group_edit {
       $r .= '<input type="hidden" name="filter:definition" id="filter-def-val"/>';
       $r .= '<input type="hidden" name="filter:sharing" value="R"/>';
     }
-    // auto-insert the creator as an admin of the new group, unless the admins are manually specified
-    if (!$args['include_administrators'])
-      $r .= '<input type="hidden" name="groups_user:user_id" value="' .hostsite_get_user_field('indicia_user_id'). '"/>';
-    $r .= '<input type="hidden" name="groups_user:administrator" value="t"/>';
-    $r .= '<input type="submit" class="indicia-button" id="save-button" value="'.
-        (empty(data_entry_helper::$entity_to_load['group:id']) ? 
-        lang::get('Create {1}', $groupType) : lang::get('Update {1} settings', $groupType))
-        ."\" />\n";    
-    $r .= '</form>';
-    if (isset($hiddenStuff))
-      $r .= $hiddenStuff;
-    
-    data_entry_helper::enable_validation('entry_form');
-    // JavaScript to grab the filter definition and store in the form for posting when the form is submitted
-    data_entry_helper::$javascript .= "
-$('#entry_form').submit(function() {
-  $('#filter-title-val').val('" . lang::get('Filter for user group') . " ' + $('#group\\\\:title').val());
-  $('#filter-def-val').val(JSON.stringify(indiciaData.filter.def));
-});\n";
     return $r;
   }
   
@@ -309,13 +375,19 @@ $('#entry_form').submit(function() {
     $s = submission_builder::build_submission($values, $struct);
     // need to manually build the submission for the admins sub_list, since we are hijacking what is 
     // intended to be a custom attribute control
-    $existingAdmins=preg_grep('/^groups_user\:user_id\:[0-9]+$/', array_keys($values));
+    self::extractUserInfoFromFormValues($s, $values, 'admin_user_id', 't');
+    self::extractUserInfoFromFormValues($s, $values, 'user_id', 'f');
+    return $s;
+  }  
+  
+  private static function extractUserInfoFromFormValues(&$s, $values, $fieldname, $isAdmin) {
+    $existingAdmins=preg_grep("/^groups_user\:$fieldname\:[0-9]+$/", array_keys($values));
     if (!empty($values['groups_user:user_id']) || !empty($existingAdmins)) {
       $s['subModels']=array();
-      if (!empty($values['groups_user:user_id'])) {
-        foreach($values['groups_user:user_id'] as $userId) {
+      if (!empty($values["groups_user:$fieldname"])) {
+        foreach($values["groups_user:$fieldname"] as $userId) {
           $s['subModels'][]=array('fkId' => 'group_id', 
-            'model' => submission_builder::wrap(array('user_id'=>$userId, 'administrator'=>'t'), 'groups_user'));
+            'model' => submission_builder::wrap(array('user_id'=>$userId, 'administrator'=>$isAdmin), 'groups_user'));
         }
       }
       // for existing, we just need to look for deletions which will have an empty value
@@ -328,7 +400,6 @@ $('#entry_form').submit(function() {
       }
     }
     return $s;
-    
   }
   
   /** 
@@ -360,21 +431,23 @@ $('#entry_form').submit(function() {
       'nocache'=>true
     ));
     if ($group[0]['created_by_id']!==hostsite_get_user_field('indicia_user_id')) {
-      // user did not create group. So, check they are an admin
-      $admins = data_entry_helper::get_population_data(array(
-        'table'=>'groups_user',
-        'extraParams'=>$auth['read']+array('group_id'=>$_GET['group_id'], 'administrator'=>'t'),
-        'nocache'=>true
-      ));
-      $found=false;
-      foreach($admins as $admin) {
-        if ($admin['user_id']===hostsite_get_user_field('indicia_user_id')) {
-          $found=true;
-          break;
+      if (!function_exists('user_access') || !user_access('Iform groups admin')) {
+        // user did not create group. So, check they are an admin
+        $admins = data_entry_helper::get_population_data(array(
+          'table'=>'groups_user',
+          'extraParams'=>$auth['read']+array('group_id'=>$_GET['group_id'], 'administrator'=>'t'),
+          'nocache'=>true
+        ));
+        $found=false;
+        foreach($admins as $admin) {
+          if ($admin['user_id']===hostsite_get_user_field('indicia_user_id')) {
+            $found=true;
+            break;
+          }
         }
+        if (!$found)
+          throw new exception(lang::get('You are trying to edit a group you don\'t have admin rights to.'));
       }
-      if (!$found)
-        throw new exception(lang::get('You are trying to edit a group you don\'t have admin rights to.'));
     }
       
     data_entry_helper::$entity_to_load = array(
@@ -395,22 +468,35 @@ $('#entry_form').submit(function() {
       data_entry_helper::$javascript .= 
           "indiciaData.filter.def=$def;\n";
     }
-    if ($args['include_administrators']) {
-      $admins = data_entry_helper::get_population_data(array(
+    if ($args['include_administrators'] || $args['include_members']) {
+      $members = data_entry_helper::get_population_data(array(
         'table'=>'groups_user',
-        'extraParams'=>$auth['read']+array('view'=>'detail', 'group_id'=>$_GET['group_id'], 'administrator'=>'t'),
+        'extraParams'=>$auth['read']+array('view'=>'detail', 'group_id'=>$_GET['group_id']),
         'nocache'=>true
       ));
-      $arr = array();
-      foreach($admins as $user) {
-        $arr[]=array(
-            'fieldname'=>'groups_user:user_id:'.$user['id'],
-            'caption'=>$user['person_name'],
-            'default'=>$user['user_id']
-        );
+      $admins = array();
+      $others = array();
+      foreach($members as $member) {
+        if ($member['administrator']==='t')
+          $admins[]=array(
+              'fieldname'=>'groups_user:user_id:'.$member['id'],
+              'caption'=>$member['person_name'],
+              'default'=>$member['user_id']
+          );
+        else
+          $others[]=array(
+              'fieldname'=>'groups_user:user_id:'.$member['id'],
+              'caption'=>$member['person_name'],
+              'default'=>$member['user_id']
+          );
       }
-      data_entry_helper::$entity_to_load['groups_user:user_id']=$arr;
+      data_entry_helper::$entity_to_load['groups_user:admin_user_id']=$admins;
+      data_entry_helper::$entity_to_load['groups_user:user_id']=$others;
     }
+  }
+  
+  public static function get_perms($nid) {
+    return array('IForm groups admin');
   }
 
 }
