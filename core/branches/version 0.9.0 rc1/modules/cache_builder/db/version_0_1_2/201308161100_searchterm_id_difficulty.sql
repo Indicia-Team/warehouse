3e1c2f0fa2bc30bbc995b81567c3cb2deb7fdf19
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2013-09-23 12:37:53 +0000

    Copied to 0.9 branch - Fix dependency on data_cleaner module.

--- core/branches/version 0.9.0 rc1/modules/cache_builder/db/version_0_1_2/201308161100_searchterm_id_difficulty.sql
+++ core/branches/version 0.9.0 rc1/modules/cache_builder/db/version_0_1_2/201308161100_searchterm_id_difficulty.sql
@@ -8,6 +8,13 @@ ALTER TABLE cache_taxon_searchterms
 
 COMMENT ON COLUMN cache_taxon_searchterms.id_diff_verification_rule_id  IS 'Verification rule that is associated with the identification difficulty.';
 
+CREATE OR REPLACE function f_update_cts (OUT success bool)
+    LANGUAGE plpgsql AS
+$func$
+BEGIN 
+
+IF EXISTS (SELECT * FROM pg_class WHERE relname='verification_rule_data') THEN
+
 -- Update the existing searchterm entries with their ID difficulty rating.
 update cache_taxon_searchterms cts 
   set identification_difficulty=vrd.value::integer, id_diff_verification_rule_id=vrd.verification_rule_id 
@@ -15,4 +22,13 @@ from cache_taxa_taxon_lists cttl
 join verification_rule_data vrd on vrd.header_name='Data' and upper(vrd.key)=cttl.external_key and vrd.deleted=false
 join verification_rules vr on vr.id=vrd.verification_rule_id and vr.deleted=false
     and vr.test_type='IdentificationDifficulty'
-where cttl.id=cts.preferred_taxa_taxon_list_id 
+where cttl.id=cts.preferred_taxa_taxon_list_id;
+
+END IF; 
+
+END
+$func$;
+
+SELECT f_update_cts();
+
+DROP FUNCTION f_update_cts();
