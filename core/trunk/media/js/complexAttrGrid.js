adfef517e65455f4fff285bf79238283ac3588ec
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2013-10-09 08:08:14 +0000

    JS to support the new complex attribute grid.

---
+++ core/trunk/media/js/complexAttrGrid.js
@@ -0,0 +1,37 @@
+$(document).ready(function($) {
+  "use strict"; 
+  
+  $('table.complex-attr-grid .add-btn').click(function(e) {
+    var table=$(e.currentTarget).closest('table')[0],
+      attrName=table.id.replace('complex-attr-grid-', '').split('-'),
+      attrTypeTag=attrName[0], attrId=attrName[1],
+      row='<tr>',
+      gridDef=indiciaData['complexAttrGrid-'+attrTypeTag+'-'+attrId],
+      fieldname;
+    
+    gridDef.rowCount++; 
+    $.each(gridDef.cols, function(name, def) {
+      fieldname = attrTypeTag+"+:"+attrId+"::"+gridDef.rowCount+":"+name;
+      row += '<td><input type="text" name="'+fieldname+'" id="'+fieldname+'"/>';
+      if (typeof def.unit!=="undefined" && def.unit!=="") {
+        row += '<span class="unit">'+def.unit+'</span>';
+      }
+      row += '</td>';
+    });
+    fieldname = attrTypeTag+"+:"+attrId+":"+gridDef.rowCount+":deleted";
+    row += '<td><input type="hidden" name="'+fieldname+'" value="f" class="delete-flag"/><span class="ind-delete-icon"/></td></tr>';
+    $(table).find('tbody').append(row);
+  });
+  
+  $('table.complex-attr-grid tbody').click(function(e) {
+    // e.target is the actual thing clicked on inside the tbody
+    if ($(e.target).hasClass('ind-delete-icon')) {
+      var row=$(e.target).closest('tr')[0];
+      $(row).css('opacity', 0.4);
+      $(row).find('input').css('text-decoration', 'line-through');
+      $(row).find('.delete-flag').val('t');
+      $(row).find('input').not(':hidden').attr('disabled', true);
+    }
+  });
+
+});
