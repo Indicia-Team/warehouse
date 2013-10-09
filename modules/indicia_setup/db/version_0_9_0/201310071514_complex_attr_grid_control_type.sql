fb5b8e10aecd8a6cf8de80ff0230186d845adb1b
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-10-09 07:45:40 +0000

    Adds a new complex attribute grid control type for custom attributes.

---
+++ modules/indicia_setup/db/version_0_9_0/201310071514_complex_attr_grid_control_type.sql
@@ -0,0 +1,2 @@
+INSERT INTO control_types (control, for_data_type, multi_value)
+SELECT distinct 'complex_attr_grid', 'T', true FROM control_types WHERE NOT EXISTS(SELECT id FROM control_types WHERE control='complex_attr_grid')
