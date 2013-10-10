a81c6840b62a3ef8d5d0ab24a4284bb62cfba834
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-10-10 14:30:23 +0000

    JS to support - Complex_attr_grid now supports using a separate control to set the row count.

--- media/js/complexAttrGrid.js
+++ media/js/complexAttrGrid.js
@@ -1,8 +1,10 @@
+var changeComplexGridRowCount;
+
 $(document).ready(function($) {
   "use strict"; 
   
-  $('table.complex-attr-grid .add-btn').click(function(e) {
-    var table=$(e.currentTarget).closest('table')[0],
+  function addRowToTable(table) {
+    var
       attrName=table.id.replace('complex-attr-grid-', '').split('-'),
       attrTypeTag=attrName[0], attrId=attrName[1],
       row='<tr>',
@@ -14,7 +16,7 @@ $(document).ready(function($) {
       fieldname = attrTypeTag+"+:"+attrId+"::"+gridDef.rowCount+":"+name;
       row += '<td>';
       if (def.datatype==='lookup') {
-        row += '<select name="'+fieldname+'"><option value="">'+indiciaData.langPleaseSelect+'</option>';
+        row += '<select name="'+fieldname+'"><option value="">&lt;'+indiciaData.langPleaseSelect+'&gt;</option>';
         $.each(indiciaData['tl'+def.termlist_id], function(idx, term) {
           row += '<option value="'+term[0]+':' + term[1] + '">'+term[1]+'</option>';
         });
@@ -30,6 +32,32 @@ $(document).ready(function($) {
     fieldname = attrTypeTag+"+:"+attrId+"::"+gridDef.rowCount+":deleted";
     row += '<td><input type="hidden" name="'+fieldname+'" value="f" class="delete-flag"/><span class="ind-delete-icon"/></td></tr>';
     $(table).find('tbody').append(row);
+  }
+  
+  changeComplexGridRowCount = function(countCtrlId, attrTypeTag, attrId) {
+    var rowCount=$('#'+countCtrlId).val();
+    if ($('#complex-attr-grid-'+attrTypeTag+'-'+attrId+' tbody tr').length>rowCount) {
+      $.each($('#complex-attr-grid-'+attrTypeTag+'-'+attrId+' tbody tr'), function(idx, row) {
+        // remove only empty rows
+        if ($(row).find(":input:visible[value!='']").length===0) {
+          $(row).remove();
+        }
+      });
+      if ($('#complex-attr-grid-'+attrTypeTag+'-'+attrId+' tbody tr').length>rowCount) {
+        $('#'+countCtrlId).val($('#complex-attr-grid-'+attrTypeTag+'-'+attrId+' tbody tr').length);
+        alert(indiciaData.langCantRemoveEnoughRows);
+      }
+    }
+    if ($('#complex-attr-grid-'+attrTypeTag+'-'+attrId+' tbody tr').length<rowCount) {
+      while ($('#complex-attr-grid-'+attrTypeTag+'-'+attrId+' tbody tr').length<rowCount) {
+        addRowToTable($('#complex-attr-grid-'+attrTypeTag+'-'+attrId)[0]);
+      }
+    }
+  };
+  
+  $('table.complex-attr-grid .add-btn').click(function(e) {
+    var table=$(e.currentTarget).closest('table')[0];
+    addRowToTable(table);
   });
   
   $('table.complex-attr-grid tbody').click(function(e) {
@@ -42,5 +70,7 @@ $(document).ready(function($) {
       $(row).find('input').not(':hidden').attr('disabled', true);
     }
   });
+  
+  
 
 });
