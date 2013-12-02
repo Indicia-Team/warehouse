962eaad15c747649c8beb60a4dbd2fe32db414cc
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2013-12-02 12:08:38 +0000

    Update to groups functionality, in particular the ability for a group (e.g. a project) to have it's records private until release at project completion.

--- core/trunk/modules/groups/models/groups_user.php
+++ core/trunk/modules/groups/models/groups_user.php
@@ -36,7 +36,7 @@ class Groups_user_Model extends ORM {
     $array->pre_filter('trim');
     $array->add_rules('group_id', 'required');
     $array->add_rules('user_id', 'required');
-    $this->unvalidatedFields = array('administrator', 'deleted');
+    $this->unvalidatedFields = array('administrator', 'deleted', 'pending');
     return parent::validate($array, $save);
   }
 
