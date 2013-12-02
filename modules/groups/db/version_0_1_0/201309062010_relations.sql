ce13cc4fc38e1b9ed1d043cff8ce80e4637d33f2
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-12-02 12:14:57 +0000

    Groups functionality db updates.

--- modules/groups/db/version_0_1_0/201309062010_relations.sql
+++ modules/groups/db/version_0_1_0/201309062010_relations.sql
@@ -95,37 +95,3 @@ AND t.term='Recording group';
 
 ALTER TABLE groups
    ALTER COLUMN group_type_id SET NOT NULL;
-
-CREATE OR REPLACE VIEW list_groups AS 
- SELECT g.id, g.title, g.code, g.group_type_id, g.description, g.from_date, g.to_date, g.private_records, g.website_id, g.filter_id
-   FROM groups g
-  WHERE g.deleted = false;
-
-CREATE OR REPLACE VIEW detail_groups AS 
- SELECT g.id, g.title, g.code, g.group_type_id, g.description, g.from_date, g.to_date, g.private_records, 
-     g.website_id, g.joining_method, g.filter_id, f.definition as filter_definition, 
-     g.created_by_id, c.username AS created_by, g.updated_by_id, u.username AS updated_by
-   FROM groups g
-   LEFT JOIN filters f ON f.id=g.filter_id AND f.deleted=false
-   JOIN indicia.users c ON c.id = g.created_by_id
-   JOIN indicia.users u ON u.id = g.updated_by_id
-  WHERE g.deleted = false;
-
-CREATE OR REPLACE VIEW list_groups_users AS 
- SELECT gu.id, gu.group_id, gu.user_id, gu.administrator, g.website_id, u.username
-   FROM groups_users gu
-   JOIN groups g on g.id=gu.group_id AND g.deleted=false
-   JOIN users u on u.id=gu.user_id
-  WHERE gu.deleted = false;
-
-CREATE OR REPLACE VIEW detail_groups_users AS 
- SELECT gu.id, gu.group_id, gu.user_id, gu.administrator, g.website_id,
-    u.username, p.first_name, p.surname, p.surname || COALESCE(', ' || p.first_name, '') as person_name,
-    gu.created_by_id, c.username AS created_by, gu.updated_by_id, up.username AS updated_by
-   FROM groups_users gu
-   JOIN groups g on g.id=gu.group_id AND g.deleted=false
-   JOIN users u ON u.id=gu.user_id AND u.deleted=false
-   JOIN people p ON p.id=u.person_id AND p.deleted=false
-   JOIN users c ON c.id = gu.created_by_id
-   JOIN users up ON up.id = gu.updated_by_id
-  WHERE gu.deleted = false;
