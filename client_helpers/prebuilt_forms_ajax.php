fa504483dbe16a54fef5977fc0778f93a9c051a8
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2013-12-17 11:45:19 +0000

    Prebuilt form config ajax fix for D7 by adding closure

--- client_helpers/prebuilt_forms_ajax.php
+++ client_helpers/prebuilt_forms_ajax.php
@@ -44,8 +44,6 @@ echo form_helper::prebuilt_form_params_form(array(
   'readAuth' => $readAuth,
   'expandFirst' => true
 ));
-echo "(function ($) {\n";
-echo data_entry_helper::get_scripts(data_entry_helper::$javascript, data_entry_helper::$late_javascript, data_entry_helper::$onload_javascript);
-echo "\n})(jQuery);\n";
+echo form_helper::dump_javascript(true);
 
 ?>
