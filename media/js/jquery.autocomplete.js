8e4e6aadaeb011d9e6c49a9d9474da92de41e385
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-01-13 20:44:10 +0000

    Small tweak to the select option layout for autocompletes.

--- media/js/jquery.autocomplete.js
+++ media/js/jquery.autocomplete.js
@@ -90,12 +90,17 @@ $.Autocompleter = function(input, options) {
   if (options.selectMode) {
     $input.after('<span class="autocomplete-select"></span>');
     var btn=$input.next('.autocomplete-select');
-    // set height, and adjust for borders
-    btn.css('height', $input.height()+($input.outerHeight()-$input.innerHeight())-(btn.outerHeight()-btn.innerHeight()));
+    // set height, allowing for fact it might start hidden and therefore we can't measure the input height, and adjust for borders
+    if ($input.is(':visible')) {
+      btn.css('height', $input.height()+($input.outerHeight()-$input.innerHeight())-(btn.outerHeight()-btn.innerHeight()));
+    } else {
+      btn.css('height', 17); // a guess
+    }
     btn.css('padding-top', $input.css('padding-top'));
     btn.css('padding-bottom', $input.css('padding-bottom'));
     btn.css('margin-top', $input.css('margin-top'));
     btn.css('margin-bottom', $input.css('margin-bottom'));
+    btn.css('margin-left', 0);
     btn.css('vertical-align', $input.css('vertical-align'));
     if (btn.parents('.scTaxonCell').length) {
       btn.css('margin-left', '-'+(btn.outerWidth()+1)+'px');
