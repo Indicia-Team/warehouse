f28f3e808a15b93df4c812227c062fed27e91a65
andrewvanbreda2@gmail.com <andrewvanbreda2@gmail.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-02-07 13:19:40 +0000

    Improvements to the creating of plots on the PSS site, also cleaned up code to move some of the Javascript out into another file.

--- core/trunk/client_helpers/prebuilt_forms/extensions/splash_extensions.php
+++ core/trunk/client_helpers/prebuilt_forms/extensions/splash_extensions.php
@@ -206,8 +206,9 @@ class extension_splash_extensions {
           'view'=>'detail'
       ), $options);
       $r .= data_entry_helper::location_select($location_list_args);
-      //Create the mini report
-      $r .= self::plot_report_panel($auth,$options);
+      //Create the mini report, not currently required on PSS site
+      if (empty($options['pssMode']))
+        $r .= self::plot_report_panel($auth,$options);
       return $r;
     }
   }
@@ -412,7 +413,8 @@ class extension_splash_extensions {
   }
   
   /*
-   * When the user clicks on the map on the plot details page, we calculate a plot square on the map where the south-west corner is the clicked point.
+   * For the PSS project "Vertical" and "Linear" plot types, the user just uses the line drawing tool to draw plots on the map.
+   * For the Splash project and PSS project Plot Squares/Rectangles plot types, when the user clicks on the map on the plot details page, we calculate a plot square on the map where the south-west corner is the clicked point.
    * The size of the plot square depends on the plot type but it extends north/east along the lat long grid system (as opposed to British National Grid which is at a slight angle).
    * However we cannot calculate points a certain number of metres apart using lat/long because the unit is degrees, so to make the square calculation we need to use the British National Grid 
    * to help as this is in metres. However the BNG is also at a slight angle which makes the situation complicated, the high level algorithm for calculating a grid square is as follows,
@@ -439,6 +441,7 @@ class extension_splash_extensions {
    * 
    */
   public static function draw_map_plot($auth, $args, $tabalias, $options, $path) {
+    drupal_add_js(iform_client_helpers_path().'prebuilt_forms/extensions/splash_extensions.js');
     if (empty($options['squareSizes'])) {
       drupal_set_message('Please fill in the @squareSizes option for the draw_map_plot control');
       return '';
@@ -460,104 +463,34 @@ class extension_splash_extensions {
      else
         $squareSizesArray[$squareSizeSingleOptionSplit[0]]=array($squareSizeSingleOptionSplit[1],$squareSizeSingleOptionSplit[2]);
     }
-    $squareSizesJavascriptArray=json_encode($squareSizesArray);
-    iform_load_helpers(array('map_helper'));    
-    //When the user clicks on the map we need to draw the map square. Initialise the map and then add a trigger to it to allow the user
-    //to click on the map and then we automatically draw the plot square.
-    map_helper::$javascript .= "
-    mapInitialisationHooks.push(function (div) {
-      indiciaData.mapdiv = div;
-      OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
-      
-        defaultHandlerOptions: {
-          'single': true,
-          'double': false,
-          'pixelTolerance': 0,
-          'stopSingle': false,
-          'stopDouble': false
-        },
-
-        initialize: function(options) {
-          this.handlerOptions = OpenLayers.Util.extend(
-            {}, this.defaultHandlerOptions
-          );
-          OpenLayers.Control.prototype.initialize.apply(
-            this, arguments
-          );
-          this.handler = new OpenLayers.Handler.Click(
-            this, {
-              'click': this.trigger
-            }, this.handlerOptions
-          );
-        },
-      
-        trigger: function(e) {    
-          if ($('#location\\\\:location_type_id').val()) {
-            //When map is clicked on then remove previous plot square.
-            plotSquareLayer.removeAllFeatures();
-            var attributes = {name: 'plot_map'};
-            
-            var polygon=square_calculator(e.xy);
-            //Save the plot square to a hidden field for saving in the database
-            $('#imp-boundary-geom').val(polygon);
-            if (indiciaData.mapdiv.map.projection.getCode() != indiciaData.mapdiv.indiciaProjection.getCode()) {
-              polygon.transform(indiciaData.mapdiv.indiciaProjection, indiciaData.mapdiv.map.projection);
-            }
-            var feature = new OpenLayers.Feature.Vector(polygon, attributes);
-            feature.geometry=polygon;
-            plotSquareLayer.addFeatures([feature]);
-
-            var bounds = new OpenLayers.Bounds();
-            bounds = feature.geometry.getBounds();
-            zoom = indiciaData.mapdiv.map.getZoomForExtent(bounds);
-            indiciaData.mapdiv.map.setCenter(bounds.getCenterLonLat(), zoom); 
-          } else {
-            alert('Please select a plot type before selecting the plot location.');
-          }
-        }
-      });
-      
-      plotSquareLayer = new OpenLayers.Layer.Vector('Plot Square Layer');
-      indiciaData.mapdiv.map.addLayer(plotSquareLayer); 
-      var click = new OpenLayers.Control.Click();
-      indiciaData.mapdiv.map.addControl(click);";
-      //This code is a workaround for an issue where the Plot's square draws correctly but
-      //the grid reference click point appears in the wrong place when my custom code to automatically 
-      //zoom into the Plot Square is present.
-      //The click point would display in the same pixel position as it was clicked before the zoom occurred.
-      //In the code below we force the custom trigger click.activate() to be activated before the standard
-      //olControlClickSref control as items which are activated last are peformed first, this forces
-      //the standard click code to run before my custom code which performs the zoom. The click point then works with the zoom.
-      map_helper::$javascript .= "
-      $.each(indiciaData.mapdiv.map.controls, function(idx, ctrl) {
-        if (ctrl.displayClass==='olControlClickSref') {
-          ctrl.deactivate();
-        }
-      });
-      click.activate();
-      $.each(indiciaData.mapdiv.map.controls, function(idx, ctrl) {
-        if (ctrl.displayClass==='olControlClickSref') {
-          ctrl.activate();
-        }
-      });
-    });";
+    iform_load_helpers(array('map_helper')); 
+    $squareSizesForJavascript=json_encode($squareSizesArray);
+    map_helper::$javascript .= "indiciaData.squareSizes=$squareSizesForJavascript;\n";
     if (empty($options['pssMode'])) {
       //If you change the location type then clear the features already on the map
       map_helper::$javascript .= "
       $('#location\\\\:location_type_id').change(function() {
-        plotSquareLayer.removeAllFeatures();
+        var mapLayers = indiciaData.mapdiv.map.layers;
+        for(var a = 0; a < mapLayers.length; a++ ){
+          if (mapLayers[a].CLASS_NAME=='OpenLayers.Layer.Vector') {
+            mapLayers[a].removeAllFeatures();
+          }
+        };
         $('#imp-boundary-geom').val('');
       });";
     } else {
+      map_helper::$javascript .= "indiciaData.pssMode='".$options['pssMode']."';\n";
       //In PSS mode, the user supplies the ids of the attributes that will hold a Plot Square/Rectangle's
       //width and length in a comma seperated list e.g. @pssMode=24,25
       //We need to split these options
       $pssMode = "'".$options["pssMode"]."'";
-      $widthLength = explode(',',$options["pssMode"]);  
+      $widthLength = explode(',',$options["pssMode"]);
+      $widthLengthForJavascript = json_encode($widthLength);
+      map_helper::$javascript .= "indiciaData.widthLength=$widthLengthForJavascript;\n";
       //When Plot Details page opens, never show the controls where the user can change the plot width and length
       //as they won't have selected a plot type yet.
-      map_helper::$javascript .= "
-      var squareSizesJavascriptArray=$squareSizesJavascriptArray;  
+      map_helper::$javascript .= " 
+      var squareSizes=$squareSizesForJavascript; 
       $('[for=\"locAttr\\\\:$widthLength[0]\"]').hide();
       $('#locAttr\\\\:$widthLength[0]').hide();
       $('[for=\"locAttr\\\\:$widthLength[1]\"]').hide();
@@ -565,15 +498,26 @@ class extension_splash_extensions {
       //When the user changes the plot type, clear the features off the map.
       map_helper::$javascript .= "
       $('#location\\\\:location_type_id').change(function() {
-        plotSquareLayer.removeAllFeatures();
+        var mapLayers = indiciaData.mapdiv.map.layers;
+        for(var a = 0; a < mapLayers.length; a++ ){
+          if (mapLayers[a].CLASS_NAME=='OpenLayers.Layer.Vector') {
+            mapLayers[a].removeAllFeatures();
+          }
+        };
         $('#imp-boundary-geom').val('');";
       //If the user selects an option that is a non location type from the location type drop-down (e.g "Please select")
       //then always hide the plot width and height selection textboxes if they are displayed and also remove the line draw tool
       //from the map.
       map_helper::$javascript .= " 
         if (!$('#location\\\\:location_type_id option:selected').val()) {         
-          $('.olControlDrawFeaturePathItemActive').hide();
+          $('.olControlDrawFeaturePathItemActive').addClass('olControlDrawFeaturePathItemInactive');
+          $('.olControlDrawFeaturePathItemInactive').removeClass('olControlDrawFeaturePathItemActive');
           $('.olControlDrawFeaturePathItemInactive').hide();
+          $('.olControlNavigationItemActive').addClass('olControlNavigationItemInactive');
+          $('.olControlNavigationItemInactive').removeClass('olControlNavigationItemActive');
+          $('.olControlNavigationItemInactive').hide();
+          $('.olControlClickSrefItemInactive').addClass('olControlClickSrefItemActive');
+          $('.olControlClickSrefItemActive').removeClass('olControlClickSrefItemInactive');
           $('[for=\"locAttr\\\\:$widthLength[0]\"]').hide();
           $('#locAttr\\\\:$widthLength[0]').hide();
           $('[for=\"locAttr\\\\:$widthLength[1]\"]').hide();
@@ -594,109 +538,29 @@ class extension_splash_extensions {
       //options supplied by the user.
       map_helper::$javascript .= "
         } else {
+          $('.olControlDrawFeaturePathItemActive').addClass('olControlDrawFeaturePathItemInactive');
+          $('.olControlDrawFeaturePathItemInactive').removeClass('olControlDrawFeaturePathItemActive');
           $('.olControlDrawFeaturePathItemInactive').hide();
+          $('.olControlNavigationItemActive').addClass('olControlNavigationItemInactive');
+          $('.olControlNavigationItemInactive').removeClass('olControlNavigationItemActive');
+          $('.olControlNavigationItemInactive').hide();
+          $('.olControlClickSrefItemInactive').addClass('olControlClickSrefItemActive');
+          $('.olControlClickSrefItemActive').removeClass('olControlClickSrefItemInactive');
           $('[for=\"locAttr\\\\:$widthLength[0]\"]').show();
           $('#locAttr\\\\:$widthLength[0]').show();
           $('[for=\"locAttr\\\\:$widthLength[1]\"]').show();
           $('#locAttr\\\\:$widthLength[1]').show();
           if (!$('#locAttr\\\\:$widthLength[0]').val()) {
-            $('#locAttr\\\\:$widthLength[0]').val(squareSizesJavascriptArray[$('#location\\\\:location_type_id').val()][0]);
+            $('#locAttr\\\\:$widthLength[0]').val(squareSizes[$('#location\\\\:location_type_id').val()][0]);
           }
           if (!$('#locAttr\\\\:$widthLength[1]').val()) {
-            $('#locAttr\\\\:$widthLength[1]').val(squareSizesJavascriptArray[$('#location\\\\:location_type_id').val()][1]);
+            $('#locAttr\\\\:$widthLength[1]').val(squareSizes[$('#location\\\\:location_type_id').val()][1]);
           }
         }
       });\n";
     }
     //Do not allow submission if there is no plot set
     data_entry_helper::$javascript .= "$('#entry_form').submit(function() { if (!$('#imp-boundary-geom').val()) {alert('Please use the map control to create a plot before continuing.'); return false; }});\n";
-  
-    //This is the code that creates the plot square. It is called by the trigger when the user clicks on the map.
-    //Firstly get the initial south-west point in the various grid reference formats (4326=lat long, 27700 = British National Grid)
-    map_helper::$javascript .= "
-    function square_calculator(eventXY) {
-      var squareSizesJavascriptArray=$squareSizesJavascriptArray;
-      var squareWidth;
-      var squareLength;";
-      if (!empty($options['pssMode'])) {
-        //In PSS mode, the user can change the length of the square's sizes to make it a rectangle
-        map_helper::$javascript .= "
-        squareWidth=$('#locAttr\\\\:$widthLength[0]').val();
-        squareLength=$('#locAttr\\\\:$widthLength[1]').val();";
-      } else {
-        map_helper::$javascript .= "
-        squareWidth=squareSizesJavascriptArray[$('#location\\\\:location_type_id').val()][0];
-        squareLength=squareSizesJavascriptArray[$('#location\\\\:location_type_id').val()][1];";
-      }
-    map_helper::$javascript .= "
-      var xy3857 = indiciaData.mapdiv.map.getLonLatFromPixel(eventXY),
-      pt3857 = new OpenLayers.Geometry.Point(xy3857.lon, xy3857.lat),
-      InitialClickPoint4326 = pt3857.clone().transform(indiciaData.mapdiv.map.projection, new OpenLayers.Projection('epsg:4326')),
-      InitialClickPoint27700 = pt3857.clone().transform(indiciaData.mapdiv.map.projection, new OpenLayers.Projection('epsg:27700'));";
-    
-    //Get an arbitrary point north of the original long, lat position. In our case this is 1 degree north but the amount doesn't really matter. Then convert to British National Grid
-    map_helper::$javascript .= "
-      northTestPointLatLon = InitialClickPoint4326.clone();
-      northTestPointLatLon.y = northTestPointLatLon.y+1;
-      northTestPoint27700 = northTestPointLatLon.clone().transform('epsg:4326', new OpenLayers.Projection('epsg:27700'));";
-      
-    //Get a point the is at right angles to the original point and the arbitrary point north.
-    //We can do this by taking the british national grid x value of the south point and combining it with the 
-    //the y value of the north point. This will then create a right-angle triangle as the British National Grid is at an angle
-    //compared to long lat.
-    map_helper::$javascript .= "
-      northRightAnglePoint27700 = northTestPoint27700.clone();
-      northRightAnglePoint27700.x = InitialClickPoint27700.x;
-    
-      //We then work out the side lengths and angle of the right-angled triangle
-      var opposite = northTestPoint27700.x - northRightAnglePoint27700.x;
-      var adj = northRightAnglePoint27700.y - InitialClickPoint27700.y;
-      var gridAngle = Math.atan(opposite/adj);
-      //The hypotenuse is the distance north along the longitude line to our test point but in British National Grid 27700 metres.
-      var hyp = adj/Math.cos(gridAngle);";
-      
-    //As we now know the length in metres between the south point and our arbitrary north point (the hypotenuse), 
-    //we can now use the percent value to work out the Y distance in Lat Long 4326 format for the corner of the square above the original click point.
-    //This is because we know the distance in 4326 degrees, but now we also know the percentage the square length is along the line.
-    map_helper::$javascript .= "
-      var hypmetrePercent = squareLength/hyp;
-      var actualSquareNorthWestPoint4326= InitialClickPoint4326.clone();
-      actualSquareNorthWestPoint4326.y = InitialClickPoint4326.y+((northTestPointLatLon.y-InitialClickPoint4326.y)*hypmetrePercent);";
-    
-    //Next we need to use the same technique along the side of the square. We just need to use X values rather than Y values.
-    map_helper::$javascript .= "
-      eastTestPointLatLon = InitialClickPoint4326.clone();
-      eastTestPointLatLon.x = eastTestPointLatLon.x+1;
-      eastTestPoint27700 = eastTestPointLatLon.clone().transform('epsg:4326', new OpenLayers.Projection('epsg:27700'));
-
-      eastRightAnglePoint27700 = eastTestPoint27700.clone();
-      eastRightAnglePoint27700.y = InitialClickPoint27700.y;
-    
-      var opposite =  eastRightAnglePoint27700.y-eastTestPoint27700.y;
-      var adj = eastRightAnglePoint27700.x - InitialClickPoint27700.x;
-      var gridAngle = Math.atan(opposite/adj);
-      //The hypotenuse is the distance north along the latitude line to our east test point but in British National Grid 27700 metres.
-      var hyp = adj/Math.cos(gridAngle);
-   
-      var hypmetrePercent = squareWidth/hyp;
-    
-      var actualSquareSouthEastPoint4326= InitialClickPoint4326.clone();
-      actualSquareSouthEastPoint4326.x = InitialClickPoint4326.x+((eastTestPointLatLon.x-InitialClickPoint4326.x)*hypmetrePercent);";
-    
-    //As we know 3 of the plot corners, we can work out the 4th and then convert the plot square back into a form the map can understand  
-    map_helper::$javascript .= "   
-      actualSquareNorthEastPoint4326 = actualSquareSouthEastPoint4326.clone();
-      actualSquareNorthEastPoint4326.y = actualSquareNorthWestPoint4326.y;
-
-      mercOriginal = OpenLayers.Layer.SphericalMercator.forwardMercator(InitialClickPoint4326.x,InitialClickPoint4326.y);
-      mercNorth = OpenLayers.Layer.SphericalMercator.forwardMercator(actualSquareNorthWestPoint4326.x,actualSquareNorthWestPoint4326.y);
-      mercEast = OpenLayers.Layer.SphericalMercator.forwardMercator(actualSquareSouthEastPoint4326.x,actualSquareSouthEastPoint4326.y);  
-      mercNorthEast = OpenLayers.Layer.SphericalMercator.forwardMercator(actualSquareNorthEastPoint4326.x,actualSquareNorthEastPoint4326.y);
-
-      var polygonMetadata = 'POLYGON(('+mercOriginal.lon+' '+mercOriginal.lat+','+mercNorth.lon+' '+mercNorth.lat+','+mercNorthEast.lon+' '+mercNorthEast.lat+','+mercEast.lon+' '+mercEast.lat+'))';
-      var polygon=OpenLayers.Geometry.fromWKT(polygonMetadata);
-      return polygon;
-    }";
   }
 
 }
