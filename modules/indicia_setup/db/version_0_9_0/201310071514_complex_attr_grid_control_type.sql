9daab11cdcc40fe6ff6514a0df0877a06862caaa
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-03-04 12:13:03 +0000

    No need for ddl function on this script.

--- modules/indicia_setup/db/version_0_9_0/201310071514_complex_attr_grid_control_type.sql
+++ modules/indicia_setup/db/version_0_9_0/201310071514_complex_attr_grid_control_type.sql
@@ -1,14 +1,2 @@
-CREATE OR REPLACE function f_add_ct (OUT success bool)
-    LANGUAGE plpgsql AS
-$func$
-BEGIN 
-
 INSERT INTO control_types (control, for_data_type, multi_value)
-SELECT distinct 'complex_attr_grid2', 'T', true FROM control_types WHERE NOT EXISTS(SELECT id FROM control_types WHERE control='complex_attr_grid2');
-
-END
-$func$;
-
-SELECT f_add_ct();
-
-DROP FUNCTION f_add_ct();
+SELECT distinct 'complex_attr_grid', 'T', true FROM control_types WHERE NOT EXISTS(SELECT id FROM control_types WHERE control='complex_attr_grid')
