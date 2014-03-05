b9e966d1a17765ecc894b83330944101c210548f
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-03-05 10:29:53 +0000

    Added to 0.9 branch - Triggers used to keep delete flags tidy for occurrences, samples and locations. Contributed by Daniel Vergien

---
+++ core/branches/version 0.9.0 rc1/modules/indicia_setup/db/version_0_9_0/201403051014_cascade_on_delete.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE FUNCTION cascade_sample_delete() RETURNS TRIGGER AS $$
+  BEGIN
+    IF (OLD.deleted = false AND NEW.deleted = true) THEN
+      UPDATE occurrences SET deleted = true  WHERE sample_id  = OLD.id;
+      UPDATE sample_attribute_values SET deleted = true  WHERE sample_id  = OLD.id;
+      UPDATE sample_comments SET deleted = true  WHERE sample_id  = OLD.id;
+      UPDATE sample_media SET deleted = true  WHERE sample_id  = OLD.id;
+    END IF;
+  RETURN OLD;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION cascade_occurrence_delete() RETURNS TRIGGER AS $$
+  BEGIN
+    IF (OLD.deleted = false AND NEW.deleted = true) THEN
+      UPDATE occurrence_attribute_values SET deleted = true  WHERE occurrence_id  = OLD.id;
+      UPDATE occurrence_comments SET deleted = true  WHERE occurrence_id  = OLD.id;
+      UPDATE occurrence_media SET deleted = true  WHERE occurrence_id  = OLD.id;
+    END IF;
+  RETURN OLD;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION cascade_location_delete() RETURNS TRIGGER AS $$
+  BEGIN
+    IF (OLD.deleted = false AND NEW.deleted = true) THEN
+      UPDATE location_attribute_values SET deleted = true  WHERE location_id  = OLD.id;
+      UPDATE location_media SET deleted = true  WHERE location_id  = OLD.id;
+    END IF;
+  RETURN OLD;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER sample_delete_trigger AFTER UPDATE ON samples FOR EACH ROW EXECUTE PROCEDURE cascade_sample_delete();
+
+CREATE TRIGGER occurrence_delete_trigger AFTER UPDATE ON occurrences FOR EACH ROW EXECUTE PROCEDURE cascade_occurrence_delete();
+
+CREATE TRIGGER location_delete_trigger AFTER UPDATE ON locations FOR EACH ROW EXECUTE PROCEDURE cascade_location_delete();
+
