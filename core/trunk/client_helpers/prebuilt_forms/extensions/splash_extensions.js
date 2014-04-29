e939fa5f65e3b0ff557f54fbcd5c3c0337402d5a
andrewvanbreda2@gmail.com <andrewvanbreda2@gmail.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-04-29 20:31:39 +0000

    Alteration allows PSS Linear plots to be draw freehand (drawPolygon).
    Also updated comments

--- core/trunk/client_helpers/prebuilt_forms/extensions/splash_extensions.js
+++ core/trunk/client_helpers/prebuilt_forms/extensions/splash_extensions.js
@@ -13,40 +13,51 @@ var clear_map_features, plot_type_dropdown_change;
   
   plot_type_dropdown_change = function plot_type_dropdown_change() {
     indiciaData.clickMiddleOfPlot=false;
-    if (indiciaData.pssMode) {
-      //Rectangular PSS plots have the grid reference in the middle of the plot
-      if ($('#location\\:location_type_id option:selected').text()!='Linear'&&$('#location\\:location_type_id option:selected').text()!='Vertical') {
-        indiciaData.clickMiddleOfPlot=true;
+    //Some plot types use a free drawn polygon as the plot.
+    if (inArray($('#location\\:location_type_id option:selected').text(),indiciaData.freeDrawPlotTypeNames)) {
+      $('.olControlDrawFeaturePolygonItemActive').show();
+      $('.olControlDrawFeaturePolygonItemInactive').show();
+      //if using drawPolygon then we never need the length and width attributes on the screen
+      if ($('#locAttr\\:'+indiciaData.plotWidthAttrId).length) {
+        $('#locAttr\\:'+indiciaData.plotWidthAttrId).hide();
       }
-      //In PSS mode, the plot side lengths are placed into fields on the screen,
-      //for Splash we look up the sizes from the form structure options.
-      //The plot itself is then drawn by indiciaMapPanel
-      $('#locAttr\\:'+indiciaData.plotWidthAttrId).val(indiciaData.squareSizes[$('#location\\:location_type_id').val()][0]);
-      $('#locAttr\\:'+indiciaData.plotLengthAttrId).val(indiciaData.squareSizes[$('#location\\:location_type_id').val()][1]);
+      if ($('#locAttr\\:'+indiciaData.plotLengthAttrId).length) {
+        $('#locAttr\\:'+indiciaData.plotLengthAttrId).hide();
+      }
+      //If using drawPolygon then we don't draw a plot automatically
+      indiciaData.mapdiv.settings.clickForPlot=false;
+      indiciaData.mapdiv.settings.click_zoom=false;  
     } else {
-      //Some Splash plot types use a free drawn polygon as the plot.
-      if (inArray($('#location\\:location_type_id option:selected').text(),indiciaData.freeDrawPlotTypeNames)) {     
-        $('.olControlDrawFeaturePolygonItemActive').show();
-        $('.olControlDrawFeaturePolygonItemInactive').show();
-        indiciaData.mapdiv.settings.clickForPlot=false;
-        indiciaData.mapdiv.settings.click_zoom=false;
-      } else {
-        $('.olControlDrawFeaturePolygonItemActive').hide();
-        $('.olControlDrawFeaturePolygonItemInactive').hide();
-        //Otherwise for Splash, the plot is drawn as a rectangle.
-        //Need to select the click control by default, hide the draw free polygon tool
-        indiciaData.mapdiv.settings.clickForPlot=true;
-        indiciaData.mapdiv.settings.click_zoom=true;
-        $.each(indiciaData.mapdiv.map.controls, function(idx, control) {
-          if (control.CLASS_NAME==='OpenLayers.Control.DrawFeature'||control.CLASS_NAME==='OpenLayers.Control.Navigation') {
-            control.deactivate();
-          }
-          if (control.CLASS_NAME==='OpenLayers.Control') {
-            control.activate();
-          }
-        });   
-        indiciaData.plotWidthLength = indiciaData.squareSizes[$('#location\\:location_type_id').val()][0]+ ',' + indiciaData.squareSizes[$('#location\\:location_type_id').val()][1];
+      //Otherwise we auto generate the plot rectangle/square, remove the drawPolygon tool
+      $('.olControlDrawFeaturePolygonItemActive').hide();
+      $('.olControlDrawFeaturePolygonItemInactive').hide();
+      //if for some plot types (currently PSS, the plot length/width can be adjusted on screen, show and fill in these fields if they exist
+      if ($('#locAttr\\:'+indiciaData.plotWidthAttrId).length) {
+        $('#locAttr\\:'+indiciaData.plotWidthAttrId).show();
+        $('#locAttr\\:'+indiciaData.plotWidthAttrId).val(indiciaData.squareSizes[$('#location\\:location_type_id').val()][0]);
       }
+      if ($('#locAttr\\:'+indiciaData.plotLengthAttrId).length) {
+        $('#locAttr\\:'+indiciaData.plotLengthAttrId).show();
+        $('#locAttr\\:'+indiciaData.plotLengthAttrId).val(indiciaData.squareSizes[$('#location\\:location_type_id').val()][1]);
+
+      }
+      //Need to select the click control by default, hide the draw free polygon tool
+      indiciaData.mapdiv.settings.clickForPlot=true;
+      indiciaData.mapdiv.settings.click_zoom=true;
+      $.each(indiciaData.mapdiv.map.controls, function(idx, control) {
+        if (control.CLASS_NAME==='OpenLayers.Control.DrawFeature'||control.CLASS_NAME==='OpenLayers.Control.Navigation') {
+          control.deactivate();
+        }
+        if (control.CLASS_NAME==='OpenLayers.Control') {
+          control.activate();
+        }
+      }); 
+      if (indiciaData.pssMode && $('#location\\:location_type_id option:selected').text()!='linear' && $('#location\\:location_type_id option:selected').text()!='Vertical') {
+        //Rectangular PSS plots have the grid reference in the middle of the plot
+        indiciaData.clickMiddleOfPlot=true;
+      } else {
+        indiciaData.plotWidthLength = indiciaData.squareSizes[$('#location\\:location_type_id').val()][0]+ ',' + indiciaData.squareSizes[$('#location\\:location_type_id').val()][1];
+      }  
     }
   }
   
