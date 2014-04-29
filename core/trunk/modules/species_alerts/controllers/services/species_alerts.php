b1f41cf43f24ce2192299c1470edb9aae6248c2e
andrewvanbreda2@gmail.com <andrewvanbreda2@gmail.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-04-29 10:23:56 +0000

    Service calls can now be made to species alerts to allow the system to create species alert records, note the code that actually creates the species alert notifications themselves is not committed yet.

---
+++ core/trunk/modules/species_alerts/controllers/services/species_alerts.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package Services
+ * @subpackage Data
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL 3.0
+ * @link 	http://code.google.com/p/indicia/
+ */
+ 
+/**
+ * Class providing species_alerts web services.
+ */
+class Species_alerts_Controller extends Data_Service_Base_Controller {
+  /*
+   * Web service function that gets called and then passes onto a function to store the species_alert in the database.
+   */
+  public function register() {
+    try {
+      $this->authenticate('write');
+      //The decision the system makes on whether to return an existing user id or create a new user is based on the user's email address
+      //so pass this though as the identifier field
+      $emailIdentifierObject = new stdClass();
+      $emailIdentifierObject->type="email";
+      self::string_validate_mandatory('email');
+      $emailIdentifierObject->identifier=$_GET["email"];
+      $userIdentificationData['identifiers']=json_encode(array($emailIdentifierObject));
+      //Also pass through these fields so if a new user is required then the system can fill in the database details
+      self::string_validate_mandatory('surname');
+      $userIdentificationData['surname']=$_GET["surname"];
+      $userIdentificationData['first_name']=$_GET["first_name"];       
+      self::int_key_validate_mandatory('cms_user_id');
+      $userIdentificationData['cms_user_id']=$_GET["cms_user_id"];
+      self::int_key_validate_mandatory('website_id');
+      //Call existing user identifier code that will tell us whether a user with the same email address already exists in the database
+      $userDetails=user_identifier::get_user_id($userIdentificationData, $_GET["website_id"]);  
+      //Store the species alert for the user (which is either a new or existing user as determined by get_user_id)
+      self::store_species_alert($userDetails);
+    } 
+    catch (Exception $e) {
+      $this->handle_error($e);
+    }
+  }
+  
+  /*
+   * Create the Species Alert record to submit and save it to the database
+   */
+  private function store_species_alert($userDetails) {
+    $alertRecordSubmissionObj = ORM::factory('species_alert');
+    //The user id can be either a new user or exsting user, this has already been sorted out by the get_user_id function, so 
+    //by this point we don't care about whether the user is new or existing, we are just dealing with a user id given to us by that function.
+    //No need to validate as the user_id comes from get_user_id
+    $alertRecordSubmissionObj->user_id=$userDetails['userId'];   
+    //Region to receive alerts for.
+    //Need to validate as value is mandatory and comes directly from the get.
+    self::int_key_validate_mandatory('location_id');
+    $alertRecordSubmissionObj->location_id=$_GET['location_id'];
+    //Already checked this has been filled in so don't need to do this again
+    $alertRecordSubmissionObj->website_id=$_GET['website_id'];
+    //At least one of this must be supplied to identifiy the taxon
+    self::either_field_required('taxon_meaning_id','external_key');
+    if (!empty($_GET['taxon_meaning_id']))
+      $alertRecordSubmissionObj->taxon_meaning_id=$_GET['taxon_meaning_id'];
+    if (!empty($_GET['external_key']))
+      $alertRecordSubmissionObj->external_key=$_GET['external_key'];
+    self::boolean_validate('alert_on_entry');
+    //If boolean isn't supplied just assume as false
+    if (!empty($_GET['alert_on_entry']))
+      $alertRecordSubmissionObj->alert_on_entry=$_GET['alert_on_entry'];
+    else 
+      $alertRecordSubmissionObj->alert_on_entry="false";
+    self::boolean_validate('alert_on_verify');
+    if (!empty($_GET['alert_on_verify']))
+      $alertRecordSubmissionObj->alert_on_verify=$_GET['alert_on_verify'];
+    else 
+      $alertRecordSubmissionObj->alert_on_verify="false";
+    //Fill in the Created/Updated data fields in the record row
+    $alertRecordSubmissionObj->set_metadata($alertRecordSubmissionObj);
+    $alertRecordSubmissionObj->save();
+  }
+  
+  /*
+   * Check that a supplied table key is at least 1 and is a number. Key can't be empty
+   */
+  private function int_key_validate_mandatory($keyToValidate) {
+    if (!array_key_exists($keyToValidate, $_GET) || empty($_GET[$keyToValidate]) || !ctype_digit($_GET[$keyToValidate]) || intval($_GET[$keyToValidate])<1) {
+      throw new Exception($keyToValidate.' has not been supplied to the Species Alert service or it has not been supplied as an integer greater than 0.');
+    }
+  }
+  
+  /*
+   * If a boolean is supplied, we make sure it is of the correct type (if it isn't supplied we don't need to throw an exception 
+   * as the system will assume it is false)
+   */
+  private function boolean_validate($keyToValidate) {     
+    if (array_key_exists($keyToValidate, $_GET) && (!isset($_GET[$keyToValidate]) || !in_array($_GET[$keyToValidate], array(1, 0)))) {
+      throw new Exception($keyToValidate.' has been supplied to the Species Alert service and it has not been supplied as a boolean.');
+    }
+  }
+  
+  /*
+   * Validate strings that are required
+   */
+  private function string_validate_mandatory($keyToValidate) {
+    if (!array_key_exists($keyToValidate, $_GET) || empty($_GET[$keyToValidate])) {
+      throw new Exception($keyToValidate.' has not been supplied to the Species Alert service or is missing a data value.');
+    }
+  }
+  
+  /*
+   * Validate an "either" scenerio where is at least one of two fields must be filled in
+   */
+  private function either_field_required($field1,$field2) {
+    if (empty($_GET[$field1])&&empty($_GET[$field2])) {
+      throw new Exception($field1.' or '.$field2.' must be supplied to the Species Alert service.');
+    }
+  }
+}
+ 
+ 
+ 
