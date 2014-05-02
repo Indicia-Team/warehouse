eb2371169b6bcdbef3cd7b3e34d88d4aac922585
andrewvanbreda2 <andrewvanbreda2@gmail.com>
2014-05-02 07:59:44 +0000

    Checking in changes to the verifier email generation code so that a new "VT" (verifier task) notification type is user. The verification screen will now automatically clear any VT notifications for that user when they open the screen providing the option checkbox on the edit tab is selected
    Commit 1 of 3 Details
    
    New unified script to cover the new species alert table. Also covers all the new notification types needed for Species Alerts and verifier emails.
    
    Old script no longer needed so deleted.

--- modules/indicia_setup/db/version_0_9_1/201405011620_species_alerts_and_new_notification_types.sql
+++ modules/indicia_setup/db/version_0_9_1/201405011620_species_alerts_and_new_notification_types.sql
@@ -60,12 +60,18 @@ COMMENT ON COLUMN species_alerts.updated_on IS 'Date this record was last update
 COMMENT ON COLUMN species_alerts.updated_by_id IS 'Foreign key to the users table (last updater).';
 COMMENT ON COLUMN species_alerts.deleted IS 'Has this record been deleted?';
 
+DROP VIEW list_notifications;
+
+ALTER TABLE notifications ALTER source_type TYPE character varying(2);
 ALTER TABLE notifications DROP CONSTRAINT chk_notification_source_type;
+ALTER TABLE notifications 
+  ADD CONSTRAINT chk_notification_source_type CHECK (source_type::text = 'T'::bpchar::text OR source_type::text = 'V'::bpchar::text OR source_type::text = 'C'::bpchar::text OR source_type::text = 'A'::bpchar::text OR source_type::text = 'S'::bpchar::text OR source_type::text = 'VT'::bpchar::text OR source_type::text = 'AC'::bpchar::text);
 
-ALTER TABLE notifications
-ADD CONSTRAINT chk_notification_source_type CHECK (source_type::text = 'T'::bpchar::text OR source_type::text = 'V'::bpchar::text OR source_type::text = 'C'::bpchar::text OR source_type::text = 'S'::bpchar::text);
+COMMENT ON COLUMN notifications.source_type IS 'Defines the type of source of this notification, as described in the source. Value can be T (= trigger), C (= comment), V (= verification), A (= automated record check), S (= species alert), VT (= verifier task), AC (= achievement)';
 
-COMMENT ON COLUMN notifications.source_type IS 'Defines the type of source of this notification, as described in the source. Value can be T (= trigger), C (= comment), V (= verification), S (= species alert).';
+CREATE OR REPLACE VIEW list_notifications as
+SELECT n.id, n.source, n.source_type, n.data, n.acknowledged, n.user_id, n.triggered_on, n.digest_mode, n.cc
+FROM notifications n;
 END;
 
 END;
