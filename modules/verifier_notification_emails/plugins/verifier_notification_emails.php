1ee058f90cf7aa432e5daf7b4ab155888fa6ac44
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-05-02 09:17:42 +0000

    Added comment header.

--- modules/verifier_notification_emails/plugins/verifier_notification_emails.php
+++ modules/verifier_notification_emails/plugins/verifier_notification_emails.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package	Verifier notification emails
+ * @subpackage Plugins
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL
+ * @link 	http://code.google.com/p/indicia/
+ */
+ 
 /*
  * Scheduled task that is called to create a new 'you have new records to verify' notification for every user who doesn't already have
  * an outstanding notification and who is associated with a filter that has returned some new occurrences
