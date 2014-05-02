09dc15c7589f490defed82c20016c4314f04baf1
andrewvanbreda2@gmail.com <andrewvanbreda2@gmail.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-05-02 09:58:06 +0000

    Improved efficiency as was previously collecting all the usernames from the users table.

--- core/trunk/modules/verifier_notification_emails/plugins/verifier_notification_emails.php
+++ core/trunk/modules/verifier_notification_emails/plugins/verifier_notification_emails.php
@@ -27,11 +27,7 @@
 function verifier_notification_emails_scheduled_task($last_run_date, $db) {
   //Get all filters where the user for the filter does not already have an unacknowledged 'you have new records to verify' notification 
   $filters = get_filters_without_existing_notification($db);
-  //Collect all usernames from the users table, this is used in the notifications data column. We don't collect the username from the 
-  //table at the time we need it because the sql would be in the middle of a large loop so it would be slow to keep collecting from database,
-  //quicker to collect from array.
-  $userDataWithIdKey = get_usernames_from_users_table($db);
-  loop_through_filters_and_create_notifications($filters,$userDataWithIdKey);
+  loop_through_filters_and_create_notifications($filters);
 }
 
 /*
@@ -39,39 +35,22 @@ function verifier_notification_emails_scheduled_task($last_run_date, $db) {
  */
 function get_filters_without_existing_notification($db) {
   $filters = $db
-    ->select('f.id,f.definition,fu.user_id')
+    ->select('f.id,f.definition,fu.user_id,u.username')
     ->from('filters f')
     ->join('filters_users as fu','fu.filter_id','f.id')
+    ->join('users as u','u.id','fu.user_id')
     ->join('notifications as n', "(n.user_id=fu.user_id and n.source_type='VT' and n.acknowledged=false)", '', 'LEFT')
     ->where(array('f.sharing'=>'V', 'f.defines_permissions'=>'t','n.id'=>null,
-                  'f.deleted'=>'f','fu.deleted'=>'f'))
+                  'f.deleted'=>'f','fu.deleted'=>'f','u.deleted'=>'f'))
     ->get()->result_array(FALSE);
   return $filters;
-}
-
-/*
- * For efficiency we collect all the usernames we need before looping through the filters as we don't want to run the
- * SQL for each loop (there could be in the region of 500 filters)
- */
-function get_usernames_from_users_table($db) {
-  $userData = $db
-    ->select('u.id,u.username')
-    ->from('users u')
-    ->where(array('u.deleted'=>'f'))
-    ->get()->result_array(FALSE);
-  //Setup the data so the user id is the key and the username is the data item, this allows us to immediately return the username for any
-  //given ID.
-  foreach($userData as $userDataItem) {
-    $userDataWithIdKey[$userDataItem['id']]=$userDataItem['username'];         
-  }
-  return $userDataWithIdKey;
 }
 
 /*
  * For each filter that returns at least one new occurrence and doesn't already have an outstanding verification notification for a user,
  * create a new notification
  */
-function loop_through_filters_and_create_notifications($filters,$userDataWithIdKey) {
+function loop_through_filters_and_create_notifications($filters) {
   $report = 'library/occdelta/filterable_occdelta_count';
   //Supply a config of which websites to take into account. Supply 1 as the user
   //id to give the code maximum privileges
@@ -102,7 +81,7 @@ function loop_through_filters_and_create_notifications($filters,$userDataWithIdK
           //Use VT "Verifier Task" notification typeas we are informing the verifier that they need to perform a task.
           $notificationObj->source_type='VT'; 
           $notificationObj->data=json_encode(
-            array('username'=>$userDataWithIdKey[$filter['user_id']],
+            array('username'=>$filter['username'],
                   'comment'=>'You have records to verify.',
                   'auto_generated'=>'t'));
           $notificationObj->save();
