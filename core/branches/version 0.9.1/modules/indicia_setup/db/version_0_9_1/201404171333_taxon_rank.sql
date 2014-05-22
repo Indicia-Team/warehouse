e0392866dc4b7128560822359f068d693327a17e
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-05-22 08:50:13 +0000

    Copied to 0.9.1 - made script safe to rerun

--- core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201404171333_taxon_rank.sql
+++ core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201404171333_taxon_rank.sql
@@ -1,7 +1,48 @@
-ALTER TABLE cache_taxa_taxon_lists ADD COLUMN taxon_rank_id integer;
-ALTER TABLE cache_taxa_taxon_lists ADD COLUMN taxon_rank_sort_order integer;
-ALTER TABLE cache_taxa_taxon_lists ADD COLUMN taxon_rank varchar;
-ALTER TABLE cache_taxon_searchterms ADD COLUMN taxon_rank_sort_order integer;
+CREATE OR REPLACE function f_add_ddl (OUT success bool)
+    LANGUAGE plpgsql AS
+$func$
+BEGIN 
+  
+success := TRUE;
+
+BEGIN
+	ALTER TABLE cache_taxa_taxon_lists ADD COLUMN taxon_rank_id integer;
+
+EXCEPTION
+    WHEN duplicate_column THEN 
+      RAISE NOTICE 'column exists.';
+      success := FALSE;
+END;
+BEGIN
+	ALTER TABLE cache_taxa_taxon_lists ADD COLUMN taxon_rank_sort_order integer;
+EXCEPTION
+    WHEN duplicate_column THEN 
+      RAISE NOTICE 'column exists.';
+      success := FALSE;
+END;
+
+BEGIN
+	ALTER TABLE cache_taxa_taxon_lists ADD COLUMN taxon_rank varchar;
+EXCEPTION
+    WHEN duplicate_column THEN 
+      RAISE NOTICE 'column exists.';
+      success := FALSE;
+END;
+
+BEGIN
+	ALTER TABLE cache_taxon_searchterms ADD COLUMN taxon_rank_sort_order integer;
+EXCEPTION
+    WHEN duplicate_column THEN 
+      RAISE NOTICE 'column exists.';
+      success := FALSE;
+END;
+
+END
+$func$;
+
+SELECT f_add_ddl();
+
+DROP FUNCTION f_add_ddl();
 
 CREATE OR REPLACE VIEW gv_taxon_ranks AS 
  SELECT id, rank, sort_order
