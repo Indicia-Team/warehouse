90ab7ff5f3195c04edc6cccb6fbfc6c0cd0ac5c1
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-05-22 12:23:40 +0000

    Changes to allow privacy blurring of samples copied to 0.9.1 branch.

---
+++ modules/indicia_setup/db/version_0_9_1/201405221117_list_samples.sql
@@ -0,0 +1,6 @@
+﻿CREATE OR REPLACE VIEW list_samples AS 
+ SELECT s.id, su.title AS survey, l.name AS location, s.date_start, s.date_end, s.date_type, s.entered_sref, s.entered_sref_system, su.website_id, s.survey_id
+   FROM indicia.samples s
+   LEFT JOIN indicia.locations l ON s.location_id = l.id AND l.deleted = false
+   JOIN indicia.surveys su ON s.survey_id = su.id AND su.deleted = false
+  WHERE s.deleted = false;
