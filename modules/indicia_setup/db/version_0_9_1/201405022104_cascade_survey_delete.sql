87105d46bb0955b068139da26cb54b6759d40c9b
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-05-22 10:05:30 +0000

    Species alerts DDL copied to 0.9.1 branch

---
+++ modules/indicia_setup/db/version_0_9_1/201405022104_cascade_survey_delete.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE FUNCTION cascade_survey_delete()
+   RETURNS trigger AS
+$BODY$
+   BEGIN
+     IF (OLD.deleted = false AND NEW.deleted = true) THEN
+       UPDATE samples SET deleted = true, updated_on=now() WHERE survey_id = OLD.id AND deleted=false;
+     END IF;
+   RETURN OLD;
+END;
+$BODY$
+   LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS survey_delete_trigger ON surveys;
+
+CREATE TRIGGER survey_delete_trigger
+   AFTER UPDATE
+   ON surveys
+   FOR EACH ROW
+   EXECUTE PROCEDURE cascade_survey_delete();
+
+CREATE OR REPLACE FUNCTION cascade_sample_delete()
+  RETURNS trigger AS
+$BODY$
+  BEGIN
+    IF (OLD.deleted = false AND NEW.deleted = true) THEN
+      UPDATE occurrences SET deleted = true, updated_on=now() WHERE sample_id = OLD.id AND deleted=false;
+      UPDATE sample_attribute_values SET deleted = true, updated_on=now() WHERE sample_id = OLD.id AND deleted=false;
+      UPDATE sample_comments SET deleted = true, updated_on=now() WHERE sample_id = OLD.id AND deleted=false;
+      UPDATE sample_media SET deleted = true, updated_on=now() WHERE sample_id = OLD.id AND deleted=false;
+    END IF;
+  RETURN OLD;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+CREATE OR REPLACE FUNCTION cascade_occurrence_delete()
+  RETURNS trigger AS
+$BODY$
+  BEGIN
+    IF (OLD.deleted = false AND NEW.deleted = true) THEN
+      UPDATE occurrence_attribute_values SET deleted = true, updated_on=now() WHERE occurrence_id = OLD.id AND deleted=false;
+      UPDATE occurrence_comments SET deleted = true, updated_on=now() WHERE occurrence_id = OLD.id AND deleted=false;
+      UPDATE occurrence_media SET deleted = true, updated_on=now() WHERE occurrence_id = OLD.id AND deleted=false;
+    END IF;
+  RETURN OLD;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
