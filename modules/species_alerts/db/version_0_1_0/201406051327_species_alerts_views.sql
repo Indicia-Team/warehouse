2f5cbf4d3b87a8d158e91c8fdd513fdab7a75dd1
andrewvanbreda2 <andrewvanbreda2@gmail.com>
2014-06-05 18:07:33 +0000

    Changed one of the views I submitted to being a grid view rather than detail view as I think it is more appropriate

--- modules/species_alerts/db/version_0_1_0/201406051327_species_alerts_views.sql
+++ modules/species_alerts/db/version_0_1_0/201406051327_species_alerts_views.sql
@@ -13,7 +13,7 @@ BEGIN
 END;
 
 BEGIN
-  CREATE OR REPLACE VIEW detail_species_alerts AS 
+  CREATE OR REPLACE VIEW gv_species_alerts AS 
     SELECT sp.id as "ID", u.username as "Username", sp.alert_on_entry as "Alert On Entry", sp.alert_on_verify as "Alert On Verify", l.name as "Location Name", w.title AS "Website Title", sp.external_key as "External Key", sp.taxon_meaning_id as "Taxon Meaning Id"
       FROM species_alerts sp
       JOIN users u on u.id=sp.user_id
