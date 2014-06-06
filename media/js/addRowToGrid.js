d3d4c12f96bcdfed4050b490079c47d5b22f12c0
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-06-06 13:36:51 +0000

    Tidied the extraction of the taxon name from the label when editing in the species checklist grid.

--- media/js/addRowToGrid.js
+++ media/js/addRowToGrid.js
@@ -373,7 +373,7 @@ var addRowToGrid, keyHandler, ConvertControlsToPopup, hook_species_checklist_new
       $(e.target).parent().remove();
       taxonNameBeforeUserEdit = $(taxonCell).html();
       // first span should contain the name as it was entered
-      taxonTextBeforeUserEdit = $(taxonCell).text().split(' - ')[0];
+      taxonTextBeforeUserEdit = $(taxonCell).find('.taxon-name').text();
       //add the autocomplete cell
       $(taxonCell).append(speciesAutocomplete);
       //Adjust the size of the taxon cell to take up its full allocation of space
