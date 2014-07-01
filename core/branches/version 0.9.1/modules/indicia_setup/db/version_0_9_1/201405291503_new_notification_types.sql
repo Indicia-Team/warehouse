809f392122369d4e4c6e7b9b494ac7cf720ea6a8
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-07-01 08:31:50 +0000

    Copied to 0.9.1 - Removed defunct references to AC notification type.

--- core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201405291503_new_notification_types.sql
+++ core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201405291503_new_notification_types.sql
@@ -3,9 +3,9 @@ DROP VIEW list_notifications;
 ALTER TABLE notifications ALTER source_type TYPE character varying(2);
 ALTER TABLE notifications DROP CONSTRAINT chk_notification_source_type;
 ALTER TABLE notifications 
-  ADD CONSTRAINT chk_notification_source_type CHECK (source_type::text = 'T'::bpchar::text OR source_type::text = 'V'::bpchar::text OR source_type::text = 'C'::bpchar::text OR source_type::text = 'A'::bpchar::text OR source_type::text = 'S'::bpchar::text OR source_type::text = 'VT'::bpchar::text OR source_type::text = 'AC'::bpchar::text OR source_type::text = 'M'::bpchar::text);
+  ADD CONSTRAINT chk_notification_source_type CHECK (source_type::text = 'T'::bpchar::text OR source_type::text = 'V'::bpchar::text OR source_type::text = 'C'::bpchar::text OR source_type::text = 'A'::bpchar::text OR source_type::text = 'S'::bpchar::text OR source_type::text = 'VT'::bpchar::text OR source_type::text = 'M'::bpchar::text);
 
-COMMENT ON COLUMN notifications.source_type IS 'Defines the type of source of this notification, as described in the source. Value can be T (= trigger), C (= comment), V (= verification), A (= automated record check), S (= species alert), VT (= verifier task), AC (= achievement), M = milestone';
+COMMENT ON COLUMN notifications.source_type IS 'Defines the type of source of this notification, as described in the source. Value can be T (= trigger), C (= comment), V (= verification), A (= automated record check), S (= species alert), VT (= verifier task), M = milestone';
 
 CREATE OR REPLACE VIEW list_notifications as
 SELECT n.id, n.source, n.source_type, n.data, n.acknowledged, n.user_id, n.triggered_on, n.digest_mode, n.cc
