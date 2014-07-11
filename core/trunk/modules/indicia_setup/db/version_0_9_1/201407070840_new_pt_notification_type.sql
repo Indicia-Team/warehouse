b256c3f9f4283958a84b7579720f6c34fd178a8c
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-07-11 13:10:41 +0000

    Moved script into module which owns the email notification settings table, fixing an install issue.

--- core/trunk/modules/indicia_setup/db/version_0_9_1/201407070840_new_pt_notification_type.sql
+++ core/trunk/modules/indicia_setup/db/version_0_9_1/201407070840_new_pt_notification_type.sql
@@ -3,10 +3,3 @@ ALTER TABLE notifications
   ADD CONSTRAINT chk_notification_source_type CHECK (source_type::text = 'T'::bpchar::text OR source_type::text = 'V'::bpchar::text OR source_type::text = 'C'::bpchar::text OR source_type::text = 'A'::bpchar::text OR source_type::text = 'S'::bpchar::text OR source_type::text = 'VT'::bpchar::text OR source_type::text = 'M'::bpchar::text OR source_type::text = 'PT'::bpchar::text);
 
 COMMENT ON COLUMN notifications.source_type IS 'Defines the type of source of this notification, as described in the source. Value can be T (= trigger), C (= comment), V (= verification), A (= automated record check), S (= species alert), VT (= verifier task), M (= milestone), PT (= pending record task). When this column is altered, then the user_email_notifications_settings.notification_source_type needs updating.';
-
-
-ALTER TABLE user_email_notification_settings DROP CONSTRAINT chk_notification_source_type;
-ALTER TABLE user_email_notification_settings
-  ADD CONSTRAINT chk_notification_source_type CHECK (notification_source_type::text = 'T'::bpchar::text OR notification_source_type::text = 'V'::bpchar::text OR notification_source_type::text = 'C'::bpchar::text OR notification_source_type::text = 'A'::bpchar::text OR notification_source_type::text = 'S'::bpchar::text OR notification_source_type::text = 'VT'::bpchar::text OR notification_source_type::text = 'M'::bpchar::text OR notification_source_type::text = 'PT'::bpchar::text);
-
-COMMENT ON COLUMN user_email_notification_settings.notification_source_type IS 'The notification type the setting relates to, as described in the notification Source Type. Value can be T (= trigger), C (= comment), V (= verification), A (= automated record check), S (= species alert), VT (= verifier task), M (= milestone), PT (= pending record task). Needs updating when notification.source_type constraint is altered.';
