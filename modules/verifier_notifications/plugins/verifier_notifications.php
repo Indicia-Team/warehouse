69f639e327d0d3476a1b1b19a050aa6be18d1618
andrewvanbreda2 <andrewvanbreda2@gmail.com>
2014-07-15 08:22:37 +0000

    General clean up of the code removes global variables and adds support for VT/PT notifications at the same time.

--- modules/verifier_notifications/plugins/verifier_notifications.php
+++ modules/verifier_notifications/plugins/verifier_notifications.php
@@ -23,82 +23,66 @@ global $notificationSourceType;
 global $notificationSource;
 global $notificationComment;
 global $sharingFilter;
+global $sharingFilterFullName;
 global $noNotificationsCreatedMessage;
 global $oneNotificationCreatedMessage;
 global $multipleNotificationsCreatedMessage;
 
-//Module sends out VT (verifier tasks) by default, but can also be setup to send PT (Pending Record Tasks)
-//notifications which alert users when they have records at release_status "P" (pending) to check. Example use - 
+//Module sends out VT (verifier tasks) and PT (Pending Record Tasks) notifications which alert users when they have records to verify or
+//records at release_status "P" (pending) to check. Example use - 
 //If a mentor needs to check the records of a student then that student's records can be set to "P" release status.
 //This module can then pick up these records and automatically send the notifications to the mentor.
-try {
-  $ptModeConfig = kohana::config('verifier_notifications.use_pending_check_mode');
-} catch (Exception $e) {
-}
-if (isset($ptModeConfig) && $ptModeConfig===true) 
-  $mode = 'PT';
-else 
-  $mode = 'VT';
-
-if ($mode==='PT') {
-  $notificationSourceType = kohana::config('verifier_notifications.use_pending_check_mode');
-  $notificationSourceType = 'PT';
-  $notificationSource = 'pending_record_check_notifications';
-  $notificationComment='You have pending records to check.';
-  $sharingFilter='M';
-  $noNotificationsCreatedMessage='No new pending record check notifications have been created.';
-  $oneNotificationCreatedMessage='new pending record check notification has been created.';
-  $multipleNotificationsCreatedMessage='new pending record check notifications have been created.';
-} else {
-  $notificationSourceType = 'VT';
-  $notificationSource = 'verifier_notifications';
-  $notificationComment= 'You have records to verify.';
-  $sharingFilter='V';
-  $noNotificationsCreatedMessage='No new verification notifications have been created.';
-  $oneNotificationCreatedMessage='new verification notification has been created.';
-  $multipleNotificationsCreatedMessage='new verification notifications have been created.';
-}
 
 /*
  * Scheduled task that is called to create a new VT/PT notification for every user who doesn't already have
  * an outstanding notification of that type and who is associated with a filter that has returned some new occurrences
  */
 function verifier_notifications_scheduled_task($last_run_date, $db) {
-  //Get all filters where the user for the filter does not already have an unacknowledged VT/PT notification 
-  $filters = get_filters_without_existing_notification($db);
-  loop_through_filters_and_create_notifications($db, $filters);
+  $ptVariables['notificationSourceType'] = 'PT';
+  $ptVariables['notificationSource'] = 'pending_record_check_notifications';
+  $ptVariables['notificationComment'] = 'You have pending records to check.';
+  $ptVariables['sharingFilter'] = 'M';
+  $ptVariables['sharingFilterFullName'] = 'moderation';
+  $ptVariables['noNotificationsCreatedMessage'] = 'No new pending record check notifications have been created.';
+  $ptVariables['oneNotificationCreatedMessage'] = 'new pending record check notification has been created.';
+  $ptVariables['multipleNotificationsCreatedMessage'] = 'new pending record check notifications have been created.';
+  //Get all filters where the user for the filter does not already have an unacknowledged PT notification 
+  $filters = get_filters_without_existing_notification($db, $ptVariables);
+  loop_through_filters_and_create_notifications($db, $filters, $ptVariables);
+  $vtVariables['notificationSourceType'] = 'VT';
+  $vtVariables['notificationSource'] = 'verifier_notifications';
+  $vtVariables['notificationComment'] = 'You have records to verify.';
+  $vtVariables['sharingFilter'] = 'V';
+  $vtVariables['sharingFilterFullName'] = 'verification';
+  $vtVariables['noNotificationsCreatedMessage'] = 'No new verification notifications have been created.';
+  $vtVariables['oneNotificationCreatedMessage'] = 'new verification notification has been created.';
+  $vtVariables['multipleNotificationsCreatedMessage'] = 'new verification notifications have been created.';
+  //Get all filters where the user for the filter does not already have an unacknowledged VT notification 
+  $filters = get_filters_without_existing_notification($db, $vtVariables);
+  loop_through_filters_and_create_notifications($db, $filters, $vtVariables);
 }
 
 /*
  * Get all filters where the user for the filter does not already have an unacknowledged VT/PT notification 
  */
-function get_filters_without_existing_notification($db) {
-  global $notificationSourceType;
-  global $sharingFilter;
+function get_filters_without_existing_notification($db, $variables) {
   $filters = $db
     ->select('f.id,f.definition,fu.user_id,u.username')
     ->from('filters f')
     ->join('filters_users as fu','fu.filter_id','f.id')
     ->join('users as u','u.id','fu.user_id')
-    ->join('notifications as n', "(n.user_id=fu.user_id and n.source_type='".$notificationSourceType."' and n.acknowledged=false)", '', 'LEFT')
-    ->where(array('f.sharing'=>$sharingFilter, 'f.defines_permissions'=>'t','n.id'=>null,
+    ->join('notifications as n', "(n.user_id=fu.user_id and n.source_type='".$variables['notificationSourceType']."' and n.acknowledged=false)", '', 'LEFT')
+    ->where(array('f.sharing'=>$variables['sharingFilter'], 'f.defines_permissions'=>'t','n.id'=>null,
                   'f.deleted'=>'f','fu.deleted'=>'f','u.deleted'=>'f'))
     ->get()->result_array(FALSE);
   return $filters;
 }
 
 /*
- * For each filter that returns at least one new occurrence and doesn't already have an outstanding verification notification for a user,
+ * For each filter that returns at least one new occurrence and doesn't already have an outstanding VT/PT notification for a user,
  * create a new notification
  */
-function loop_through_filters_and_create_notifications($db, $filters) {
-  global $noNotificationsCreatedMessage;
-  global $oneNotificationCreatedMessage;
-  global $multipleNotificationsCreatedMessage;
-  global $notificationSourceType;
-  global $notificationSource;
-  global $notificationComment;
-
+function loop_through_filters_and_create_notifications($db, $filters, $variables) {
   $report = 'library/occdelta/filterable_occdelta_count';
   //Supply a config of which websites to take into account.
   try {
@@ -118,10 +102,15 @@ function loop_through_filters_and_create_notifications($db, $filters) {
   //When creating notifications keep a track of user's we have created notifications for, this allows us to 
   //avoid creating multiple notifications per user without having to check the database.
   $alreadyCreatedNotification=array();
-  //Go through each filter for users who don't have an outstanding verification notification.
+  //Go through each filter for users who don't have an outstanding VT/PT notification.
   $notificationCounter=0;
   foreach ($filters as $filterIdx=>$filter) {  
-    $params = json_decode($filter['definition'],true) + array('sharing'=>'verification');
+    $extraParams = array('sharing'=>$variables['sharingFilterFullName']);
+    //If we are only interested in detecting Pending records then provide a release_status P parameter, this will
+    //override the release_status R parameter that automatically appears in the report.
+    if ($variables['notificationSourceType']==='PT')
+      $extraParams = array_merge($extraParams,array('release_status'=>'P'));   
+    $params = json_decode($filter['definition'],true) + $extraParams;
     try {
       // don't run the filter unless we we haven't already created a notification for that user 
       if (!in_array($filter['user_id'],$alreadyCreatedNotification)) {
@@ -132,15 +121,15 @@ function loop_through_filters_and_create_notifications($db, $filters) {
         if ($output['content']['records'][0]['count'] > 0) {
           //Save the new notification
           $notificationObj = ORM::factory('notification');
-          $notificationObj->source=$notificationSource;
+          $notificationObj->source=$variables['notificationSource'];
           $notificationObj->acknowledged='false';
           $notificationObj->triggered_on=date("Ymd H:i:s");
           $notificationObj->user_id=$filter['user_id'];
           //Use VT "Verifier Task" or PT "Pending Record Task" notification type as we are informing the verifier that they need to perform a task.
-          $notificationObj->source_type=$notificationSourceType; 
+          $notificationObj->source_type=$variables['notificationSourceType']; 
           $notificationObj->data=json_encode(
             array('username'=>$from,
-                  'comment'=>$notificationComment,
+                  'comment'=>$variables['notificationComment'],
                   'auto_generated'=>'t'));
           $notificationObj->save();
           $notificationCounter++;
@@ -154,11 +143,11 @@ function loop_through_filters_and_create_notifications($db, $filters) {
   }
   //Display message to show how many notifications were created.
   if ($notificationCounter==0)
-    echo $noNotificationsCreatedMessage.'</br>';
+    echo $variables['noNotificationsCreatedMessage'].'</br>';
   elseif ($notificationCounter==1)
-    echo $notificationCounter.' '.$oneNotificationCreatedMessage.'</br>';
+    echo $notificationCounter.' '.$variables['oneNotificationCreatedMessage'].'</br>';
   else 
-    echo $notificationCounter.' '.$multipleNotificationsCreatedMessage.'</br>';
+    echo $notificationCounter.' '.$variables['multipleNotificationsCreatedMessage'].'</br>';
 }
 
 /*
