d6748975269a715b9340a1c45be26a8510683605
jame2@ceh.ac.uk <jame2@ceh.ac.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-07-24 17:39:30 +0000

    File updated based on latest example at https://github.com/moxiecode/plupload/blob/master/examples/upload.php In particular, the tidying up of partially uploaded files now makes some sort of sense.

--- core/trunk/client_helpers/upload.php
+++ core/trunk/client_helpers/upload.php
@@ -2,12 +2,13 @@
 /**
  * upload.php
  *
- * Copyright 2009, Moxiecode Systems AB
- * Released under GPL License.
- *
- * License: http://www.plupload.com/license
- * Contributing: http://www.plupload.com/contributing
- */
+ * Copyright 2013, Moxiecode Systems AB
+* Released under GPL License.
+*
+* License: http://www.plupload.com/license
+* Contributing: http://www.plupload.com/contributing
+*/
+
   // Only output real errors. We don't want warnings to break the JSON
   error_reporting(E_ERROR);
 
@@ -19,115 +20,112 @@
   header("Cache-Control: post-check=0, pre-check=0", false);
   header("Pragma: no-cache");
   
+  // 5 minutes execution time
+  @set_time_limit(5 * 60);
+
   require('helper_config.php');
   require('data_entry_helper.php');
 
-  // Settings. Note the interim image folder may not be in helper_config in which case use a default
+  // Settings. 
+  // Note the interim image folder may not be in helper_config in which case use a default
   $interim_image_folder = isset(helper_config::$interim_image_folder) ? helper_config::$interim_image_folder : 'upload/';
   $targetDir = dirname(__FILE__) . '/' . $interim_image_folder;
-  $cleanupTargetDir = false; // Remove old files
-  $maxFileAge = 60 * 60; // Temp file age in seconds
-  // 5 minutes execution time
-  @set_time_limit(5 * 60);
-  // usleep(5000);
+  // Clenaup old .part upload files
+  $cleanupTargetDir = true; 
+  // Max .part file age in seconds
+  $maxFileAge = 5 * 3600; 
 
-  // Get parameters
-  $chunk = isset($_REQUEST["chunk"]) ? $_REQUEST["chunk"] : 0;
-  $chunks = isset($_REQUEST["chunks"]) ? $_REQUEST["chunks"] : 0;
-  $fileName = isset($_REQUEST["name"]) ? $_REQUEST["name"] : '';  
-
-  // Clean the fileName for security reasons
-  $fileName = preg_replace('/[^\w\._]+/', '', $fileName);
   // Create target dir
   if (!file_exists($targetDir))
     @mkdir($targetDir);
-  $targetPath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+  
+  // Get a file name
+  if (isset($_REQUEST["name"])) {
+    $fileName = $_REQUEST["name"];
+  } elseif (!empty($_FILES)) {
+    $fileName = $_FILES["file"]["name"];
+  } else {
+    $fileName = uniqid("file_");
+  }
+  // Clean the fileName for security reasons
+  $fileName = preg_replace('/[^\w\._]+/', '', $fileName);
+
+  // Chunking might be enabled
+  $chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
+  $chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
+
+  $filePath  = $targetDir . DIRECTORY_SEPARATOR . $fileName;
 
   if (!file_exists($targetDir)) {
     echo '{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to create upload directory."}, "id" : "id"}';
     return;
   }
   
-  // Remove old temp files
-  if (is_dir($targetDir) && ($dir = opendir($targetDir))) {
-    while (($file = readdir($dir)) !== false) {
-      $filePath = $targetDir . DIRECTORY_SEPARATOR . $file;
-
-      // Remove temp files if they are older than the max age
-      if (preg_match('/\\.tmp$/', $file) && (filemtime($filePath) < time() - $maxFileAge))
-        @unlink($filePath);
-    }
-
-    closedir($dir);
-  } else {
-    echo '{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}';
-    return;
+// Remove old temp files
+if ($cleanupTargetDir) {
+  if (!is_dir($targetDir) || !$dir = opendir($targetDir)) {
+    die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
   }
-  
-  // Look for the content type header
-  if (isset($_SERVER["HTTP_CONTENT_TYPE"]))
-    $contentType = $_SERVER["HTTP_CONTENT_TYPE"];
 
-  if (isset($_SERVER["CONTENT_TYPE"]))
-    $contentType = $_SERVER["CONTENT_TYPE"];
+  while (($file = readdir($dir)) !== false) {
+    $tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
 
-  if (strpos($contentType, "multipart") !== false) {
-    if (isset($_FILES['file']['tmp_name']) && is_uploaded_file($_FILES['file']['tmp_name'])) {
-      // Open temp file
-      $out = fopen($targetPath, $chunk == 0 ? "wb" : "ab");
-      if ($out) {
-        // Read binary input stream and append it to temp file
-        $in = fopen($_FILES['file']['tmp_name'], "rb");
-        if ($in) {
-          while ($buff = fread($in, 4096))
-            fwrite($out, $buff);
-        } else {
-          echo '{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}';
-          return;
-        }
-        fclose($in);
-        fclose($out);
-        unlink($_FILES['file']['tmp_name']);
-      } else {
-        echo '{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to save the uploaded file."}, "id" : "id"}';
-        return;
-      }
-    } else {
-      echo '{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file. '.$_FILES['file']['tmp_name'].'"}, "id" : "id"}';
-      return;
+    // If .part file is current file proceed to the next
+    if ($tmpfilePath == "{$filePath}.part") {
+      continue;
     }
-  } else {
-    // Open temp file
-    $out = fopen($targetPath, $chunk == 0 ? "wb" : "ab");
-    if ($out) {
-      // Read binary input stream and append it to temp file
-      $in = fopen("php://input", "rb");
 
-      if ($in) {
-        while ($buff = fread($in, 4096))
-          fwrite($out, $buff);
-      } else {
-        echo '{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}';
-        return;
-      }
+    // Remove .part file if it is older than the max age 
+    if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge)) {
+      @unlink($tmpfilePath);
+    }
+  }
+  closedir($dir);
+}	
 
-      fclose($out);
-    } else {
-      echo '{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to save the uploaded file."}, "id" : "id"}'; 
-      return;
+  // Open .part file for output
+  if (!$out = @fopen("{$filePath}.part", $chunks ? "ab" : "wb")) {
+    die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+  }
+
+  if (!empty($_FILES)) {
+    if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
+      die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+    }
+
+    // Read binary input stream and append it to .psrt file
+    if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
+      die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+    }
+  } else {	
+    if (!$in = @fopen("php://input", "rb")) {
+      die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
     }
   }
 
-  //test uploaded file size
+  while ($buff = fread($in, 4096)) {
+    fwrite($out, $buff);
+  }
+
+  @fclose($out);
+  @fclose($in);
+
+  // Test file size after each chunk in case hacker has 
+  // circumvented client-side check to send something huge.
   clearstatcache();
-  $file['size'] = filesize($targetPath);
+  $file['size'] = filesize("{$filePath}.part");
   $file['error'] = '';
   if (!data_entry_helper::check_upload_size($file)) {
-    unlink($targetPath);
-      echo '{"jsonrpc" : "2.0", "error" : {"code": 104, "message": "Uploaded file too big."}, "id" : "id"}'; 
-      return;
+    unlink("{$filePath}.part");
+    die('{"jsonrpc" : "2.0", "error" : {"code": 104, "message": "Uploaded file too big."}, "id" : "id"}'); 
   }
   
+// Check if file has been uploaded
+  if (!$chunks || $chunk == $chunks - 1) {
+    // Strip the temp .part suffix off
+    rename("{$filePath}.part", $filePath);
+  }
+
   // Return JSON-RPC success response
   echo '{"jsonrpc" : "2.0", "result" : null, "id" : "id"}';
 ?>
