836e06ef3a2d72142c1bbfd0709d0d461614eca6
andrewvanbreda2 <andrewvanbreda2@gmail.com>
2014-07-28 13:18:55 +0000

    Fixed a problem where the occurrence comment popup was displaying with a small scrollbar. Also took the opportunity to add a validation to make sure the comment cannot be empty.

--- client_helpers/prebuilt_forms/js/group_home.js
+++ client_helpers/prebuilt_forms/js/group_home.js
@@ -59,49 +59,60 @@ jQuery(document).ready(function($) {
   //Can be called from the action column configuration on the edit tab.
   add_comment = function(id,date,entered_sref,record_status,created_by_id) {
     $.fancybox(
-      '<form>' +
-        '<label>Comment:</label>'+
-        '<textarea id="occurrence-comment" rows="4"></textarea><br>'+
-        '<input onclick="occurrence_comment_submit('+id+','+ "'" + date + "'" +','+ "'" + entered_sref + "'" +','+ "'" + record_status + "'" +','+created_by_id+')" type="button" value="Submit">' +
+      '<form id="occurrence-comment-form">' +
+        '<fieldset class="popup-form">' +
+          '<legend>Occurrence Comment</legend>' +
+          '<textarea id="occurrence-comment" rows="4" class="required"></textarea>'+
+          '<input type="button" id="occurrence-comment-save-button" value="Submit">' +
+        '</fieldset>' +
       '</form>'
     );
+    $('#occurrence-comment-save-button').click(function(){ 
+      //Trim white space
+      if (!$('#occurrence-comment').val().trim()) {
+        alert('Please fill an occurrence comment before saving.'); 
+        return false;
+      } else {
+        $('#occurrence-comment-form').submit(occurrence_comment_submit(id, "'" + date + "'" , "'" + entered_sref + "'" , "'" + record_status + "'",created_by_id));
+      }
+    });
   }
   
   //Submit an occurrence comment to the database
   occurrence_comment_submit = function(id,date,entered_sref,record_status,created_by_id) {
-    var dateObj = new Date();
-    var dateTimeTodaySlashed = dateObj.getFullYear() + '-' + (dateObj.getMonth()+1)+ '-' + dateObj.getDate() + ' ' + dateObj.getHours() + ':' + dateObj.getMinutes() + ':' + dateObj.getSeconds();
-    var dateTimeTodayHyphened = dateObj.getDate() + '/' + (dateObj.getMonth()+1) + '/' + dateObj.getFullYear()+ ' ' + dateObj.getHours() + ':' + dateObj.getMinutes() + ':' + dateObj.getSeconds();
-    //Post the occurrence comment to the database.
-    var occurrenceComment = {
-      "website_id":indiciaData.website_id,
-      "occurrence_comment:occurrence_id":id,
-      "occurrence_comment:comment": $('#occurrence-comment').val()
-    };
-    postToServer(occurrenceComment, 'occurrence_comment');
-    //The data column on the notification includes lots of information about the occurrence, collect this information to put into the database.
-    var dataJson = {
-      'occurrence_id':id,
-      'comment':$('#occurrence-comment').val(),
-      'date':date,
-      'entered_sref':entered_sref,
-      'record_status':record_status,
-      'updated_on':dateTimeTodaySlashed,
-      'auto_generated':'t'
-    };
-    //Post a notification to the database to say the occurrence comment has been created.
-    var notificationData = {
-      "website_id":indiciaData.website_id,
-      "notification:triggered_on": dateTimeTodaySlashed,
-      "notification:acknowledged": "f",
-      "notification:user_id": created_by_id,
-      "notification:source": "occurrence_comments",
-      "notification:source_type": "C",
-      "notification:data": JSON.stringify(dataJson), 
-      "notification:linked_id": id
+      var dateObj = new Date();
+      var dateTimeTodaySlashed = dateObj.getFullYear() + '-' + (dateObj.getMonth()+1)+ '-' + dateObj.getDate() + ' ' + dateObj.getHours() + ':' + dateObj.getMinutes() + ':' + dateObj.getSeconds();
+      var dateTimeTodayHyphened = dateObj.getDate() + '/' + (dateObj.getMonth()+1) + '/' + dateObj.getFullYear()+ ' ' + dateObj.getHours() + ':' + dateObj.getMinutes() + ':' + dateObj.getSeconds();
+      //Post the occurrence comment to the database.
+      var occurrenceComment = {
+        "website_id":indiciaData.website_id,
+        "occurrence_comment:occurrence_id":id,
+        "occurrence_comment:comment": $('#occurrence-comment').val()
+      };
+      postToServer(occurrenceComment, 'occurrence_comment');
+      //The data column on the notification includes lots of information about the occurrence, collect this information to put into the database.
+      var dataJson = {
+        'occurrence_id':id,
+        'comment':$('#occurrence-comment').val(),
+        'date':date,
+        'entered_sref':entered_sref,
+        'record_status':record_status,
+        'updated_on':dateTimeTodaySlashed,
+        'auto_generated':'t'
+      };
+      //Post a notification to the database to say the occurrence comment has been created.
+      var notificationData = {
+        "website_id":indiciaData.website_id,
+        "notification:triggered_on": dateTimeTodaySlashed,
+        "notification:acknowledged": "f",
+        "notification:user_id": created_by_id,
+        "notification:source": "occurrence_comments",
+        "notification:source_type": "C",
+        "notification:data": JSON.stringify(dataJson), 
+        "notification:linked_id": id
+      }
+      postToServer(notificationData, 'notification');
+      $.fancybox.close();
+      alert('Comment added');
     }
-    postToServer(notificationData, 'notification');
-    $.fancybox.close();
-    alert('Comment added');
-	}
 });
