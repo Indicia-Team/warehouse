0ea68a06ae0a1740d76bb6416fda767f3a5535e5
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-08-21 10:27:17 +0000

    Copied change to 0.9.1 to support pg 8.4

--- core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201404210906_constraint_unique_filter_name.sql
+++ core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201404210906_constraint_unique_filter_name.sql
@@ -1,3 +1,21 @@
-ALTER TABLE filters DROP CONSTRAINT IF EXISTS uc_filter_name;
+CREATE OR REPLACE function f_wrap_ddl (OUT success bool)
+    LANGUAGE plpgsql AS
+$func$
+BEGIN 
+  
+success := TRUE;
+
+BEGIN
+  ALTER TABLE filters DROP CONSTRAINT uc_filter_name;
+EXCEPTION    
+  WHEN undefined_object THEN
+    RAISE NOTICE 'constraint does not exist';
+END;
+
+END
+$func$;
+
+SELECT f_wrap_ddl();
+
 DROP INDEX IF EXISTS ix_filter_name_unique;
 CREATE UNIQUE INDEX ix_filter_name_unique ON filters (title , sharing , created_by_id) WHERE deleted=false;
