e2867b254126397f2e6e2cbd628f0b3779622ac1
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-08-25 16:24:39 +0000

    Slight UI tweaks for the new survey structure export module.

--- modules/survey_structure_export/views/survey_structure_export/index.php
+++ modules/survey_structure_export/views/survey_structure_export/index.php
@@ -1,9 +1,14 @@
+<p>This tab allows you to generate a piece of text which describes the structure of the custom attributes associated with 
+a survey. The text generated can be used to recreate the same attribute structure on another survey on this warehouse
+or on another warehouse. It is therefore ideal for migrating development or test versions of your surveys to the live
+warehouse server.</p>
 <?php
 require_once(DOCROOT.'client_helpers/data_entry_helper.php');
 echo data_entry_helper::textarea(array(
   'label'=>'Exported survey structure',
   'fieldname'=>'export survey structure',
-  'class'=>'control-width-6',
+  'cols'=>100,
+  'rows'=>8,
   'helpText' => 'Copy this text to the clipboard. You can then paste it into another survey on this or another warehouse to clone the attributes.',
   'default' => $export
 ));
@@ -14,8 +19,9 @@ echo data_entry_helper::textarea(array(
 echo data_entry_helper::textarea(array(
   'label'=>'Import survey structure',
   'fieldname'=>'import survey structure',
-  'class'=>'control-width-6',
-  'helpText' => 'Paste in the export of another survey to import its attributes'
+  'cols'=>100,
+  'rows'=>8,
+  'helpText' => 'Paste in the export of another survey to import its attributes.'
 ));
 echo data_entry_helper::hidden_text(array(
   'fieldname'=>'survey_id',
