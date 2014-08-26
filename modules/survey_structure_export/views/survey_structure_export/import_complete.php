3294b8392f56bdde6aca3e0ae93cacfc15bdbdd4
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-08-26 09:12:56 +0000

    Minor tidying

--- modules/survey_structure_export/views/survey_structure_export/import_complete.php
+++ modules/survey_structure_export/views/survey_structure_export/import_complete.php
@@ -1,7 +1,7 @@
 
 <p>The import has completed successfully.</p>
 <label>Output log:
-<textarea style="width: 100%">
+<textarea style="width: 100%" rows="15">
 <?php 
   if (count($log)>0)
     echo implode("\n", $log); 
