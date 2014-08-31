3ffb761dcad15283fefa1919e69a701a3492ea56
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-08-31 18:59:12 +0000

    Copied to 0.9.1 - Fix to loading of different spatial reference systems available for import.

--- application/views/location/upload_shp.php
+++ application/views/location/upload_shp.php
@@ -30,7 +30,7 @@
 <select id='SRID' name='srid' >
   <option value="27700">EPSG:27700 British National Grid</option>
   <option value="4326">EPSG:4326 WGS 84</option>
-  <option value="900913">EPSG:900913: Google Projection</option>
+  <option value="900913">EPSG:900913 Web Mercator (Google)</option>
   <option value="2169">EPSG:2169 Luxembourg 1930</option>
 </select><br/>
 <input type='checkbox' class="vnarrow" name="use_sref_system"/>Select this checkbox when using the boundary above and you wish the centroid to be generated using this sref. If not checked, the centroid will be generated in EPSG:4326 (Lat Long)<br/>
