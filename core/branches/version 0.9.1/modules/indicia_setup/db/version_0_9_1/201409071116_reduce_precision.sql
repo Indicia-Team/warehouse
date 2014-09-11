6e681b18d0d75409c34cab016e07cd81032cdbb8
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-09-11 11:16:37 +0000

    Copied to 0.9.1 - Map squares table tidy up

--- core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201409071116_reduce_precision.sql
+++ core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201409071116_reduce_precision.sql
@@ -17,29 +17,30 @@ BEGIN
 
   -- look for some preferred grids to see if we are in range.
   sys=null;
-  IF (sys IS NULL) AND (st_x(st_centroid(geom)) BETWEEN -1196000 AND -599200) AND (st_y(st_centroid(geom)) BETWEEN 6687800 AND 7442470) THEN -- rough check for OSIE
+  IF (st_x(st_centroid(geom_in)) BETWEEN -1196000 AND -599200) AND (st_y(st_centroid(geom_in)) BETWEEN 6687800 AND 7442470) THEN -- rough check for OSIE
     geom = st_transform(st_centroid(geom_in), 29901); 
     IF (st_x(geom) BETWEEN 10000 AND 367300) AND (st_y(geom) BETWEEN 10000 AND 468100) AND (st_x(geom)<332000 OR st_y(geom)<445900) THEN -- exact check for OSIE. Cut out top right corner.
       sys = 'OSIE'; -- 29901
     END IF;
   END IF;
-  IF (sys IS NULL) AND (st_x(st_centroid(geom)) BETWEEN -1081873 AND 422933) AND (st_y(st_centroid(geom)) BETWEEN 6405988 AND 8944478) THEN -- rough check for OSGB
+  IF (sys IS NULL) AND (st_x(st_centroid(geom_in)) BETWEEN -1081873 AND 422933) AND (st_y(st_centroid(geom_in)) BETWEEN 6405988 AND 8944478) THEN -- rough check for OSGB
     geom = st_transform(st_centroid(geom_in), 27700);
     IF st_x(geom) BETWEEN 0 AND 700000 AND st_y(geom) BETWEEN 0 AND 14000000 THEN -- exact check for OSGB
       sys = 'OSGB'; -- 27700
     END IF;
   END IF;
-  IF (sys IS NULL) AND (st_x(st_centroid(geom)) BETWEEN 634030 AND 729730) AND (st_y(st_centroid(geom)) BETWEEN 6348260 AND 6484930) THEN -- rough check for LUGR
+  IF (sys IS NULL) AND (st_x(st_centroid(geom_in)) BETWEEN 634030 AND 729730) AND (st_y(st_centroid(geom_in)) BETWEEN 6348260 AND 6484930) THEN -- rough check for LUGR
     geom = st_transform(st_centroid(geom_in), 2169);
     IF (st_x(geom) BETWEEN 46000 AND 108000) AND (st_y(geom) BETWEEN 55000 AND 141000) THEN -- exact check for OSGB
       sys = 'LUGR'; -- 2169
     END IF;
   END IF;
   IF sys IS NULL THEN
-    SELECT INTO sref_metadata srid, treat_srid_as_x_y_metres FROM spatial_systems WHERE code=lower(sref_system);
+    SELECT INTO sref_metadata srid, treat_srid_as_x_y_metres FROM spatial_systems WHERE code ilike sref_system OR code ilike 'EPSG:' || sref_system;
     IF COALESCE(sref_metadata.treat_srid_as_x_y_metres, false) THEN
       sys = sref_metadata.srid::varchar;
     ELSE
+      -- revert to the web-mercator grid for unknown parts of the world.
       sys = '900913';
     END IF;
   END IF;
@@ -80,8 +81,7 @@ BEGIN
         ELSE sys::integer
       END;
       IF srid<>900913 THEN
-        SELECT INTO sref_metadata spatial_systems.srid, treat_srid_as_x_y_metres FROM spatial_systems WHERE code=lower(sref_system);
-        geom = st_transform(st_centroid(geom_in), sref_metadata.srid);
+        geom = st_transform(st_centroid(geom_in), srid);
       ELSE
         geom = st_centroid(geom_in);
       END IF;
---
+++ core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201409111134_fix_map_squares.sql
@@ -0,0 +1,116 @@
+-- #slow script#
+
+-- Find all the broken occurrence records where the map square links don't match what we now expect
+select distinct o.id, cast(null as integer) as msq_1k_id, cast(null as integer) as msq_2k_id, cast(null as integer) as msq_10k_id,
+  round(st_x(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 1000), s.entered_sref_system)))) as x1000,
+  round(st_y(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 1000), s.entered_sref_system)))) as y1000,
+  GREATEST(o.sensitivity_precision, s.privacy_precision, 1000) as size1000,
+  round(st_x(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 2000), s.entered_sref_system)))) as x2000,
+  round(st_y(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 2000), s.entered_sref_system)))) as y2000,
+  GREATEST(o.sensitivity_precision, s.privacy_precision, 2000) as size2000,
+  round(st_x(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 10000), s.entered_sref_system)))) as x10000,
+  round(st_y(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 10000), s.entered_sref_system)))) as y10000,
+  GREATEST(o.sensitivity_precision, s.privacy_precision, 10000) as size10000
+into temporary tofix
+from cache_occurrences o
+join occurrences occ on occ.id=o.id
+join samples s on s.id=o.sample_id
+left join locations l on l.id=s.location_id
+join map_squares msq1 on msq1.id=map_sq_1km_id
+join map_squares msq2 on msq2.id=map_sq_2km_id
+join map_squares msq10 on msq10.id=map_sq_10km_id
+where msq1.x <> round(st_x(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 1000), s.entered_sref_system))))
+OR msq1.y <> round(st_y(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 1000), s.entered_sref_system))))
+OR msq2.x <> round(st_x(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 2000), s.entered_sref_system))))
+OR msq2.y <> round(st_y(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 2000), s.entered_sref_system))))
+OR msq10.x <> round(st_x(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 10000), s.entered_sref_system))))
+OR msq10.y <> round(st_y(st_centroid(reduce_precision(coalesce(s.geom, l.centroid_geom), occ.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 10000), s.entered_sref_system))));
+
+-- fill in the map square IDs for any where the squares already exist
+update tofix
+set msq_1k_id=msq.id
+from map_squares msq 
+where msq.x=tofix.x1000 and msq.y=tofix.y1000 and msq.size=tofix.size1000;
+
+update tofix
+set msq_2k_id=msq.id
+from map_squares msq 
+where msq.x=tofix.x2000 and msq.y=tofix.y2000 and msq.size=tofix.size2000;
+
+update tofix
+set msq_10k_id=msq.id
+from map_squares msq 
+where msq.x=tofix.x10000 and msq.y=tofix.y10000 and msq.size=tofix.size10000;
+
+-- THE FOLLOWING VIOLATES UNIQUE INDEXES BUT IT SHOULDN'T Probably because the square size is set to 1000 in one case, but GREATES(...) in another?
+/*
+Thw square size for msq_1km_id might point to a map square for 10000 size if it is sensitive etc. 
+*/
+
+-- create the missing squares
+INSERT INTO map_squares (geom, x, y, size)
+SELECT distinct reduce_precision(s.geom, o.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 1000), s.entered_sref_system), 
+    tofix.x1000, tofix.y1000, GREATEST(o.sensitivity_precision, s.privacy_precision, 1000)
+FROM tofix
+join occurrences o on o.id=tofix.id
+join samples s on s.id=o.sample_id
+where tofix.msq_1k_id is null is not null;
+
+INSERT INTO map_squares (geom, x, y, size)
+SELECT distinct reduce_precision(s.geom, o.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 2000), s.entered_sref_system), 
+    tofix.x2000, tofix.y2000, GREATEST(o.sensitivity_precision, s.privacy_precision, 2000)
+FROM tofix
+join occurrences o on o.id=tofix.id
+join samples s on s.id=o.sample_id
+left join map_squares msq on msq.x=tofix.x2000 and msq.y=tofix.y2000 and msq.size=GREATEST(o.sensitivity_precision, s.privacy_precision, 2000)
+where tofix.msq_2k_id is null and msq.id is null and s.geom is not null;
+
+INSERT INTO map_squares (geom, x, y, size)
+SELECT distinct reduce_precision(s.geom, o.confidential, GREATEST(o.sensitivity_precision, s.privacy_precision, 10000), s.entered_sref_system), 
+    tofix.x10000, tofix.y10000, GREATEST(o.sensitivity_precision, s.privacy_precision, 10000)
+FROM tofix
+join occurrences o on o.id=tofix.id
+join samples s on s.id=o.sample_id
+left join map_squares msq on msq.x=tofix.x10000 and msq.y=tofix.y10000 and msq.size=GREATEST(o.sensitivity_precision, s.privacy_precision, 10000)
+where tofix.msq_10k_id is null and msq.id is null and s.geom is not null;
+
+-- now link up the missing squares
+update tofix
+set msq_1k_id=msq.id
+from map_squares msq 
+where msq.x=tofix.x1000 and msq.y=tofix.y1000 and msq.size=tofix.size1000
+and msq_1k_id is null;
+
+update tofix
+set msq_2k_id=msq.id
+from map_squares msq 
+where msq.x=tofix.x2000 and msq.y=tofix.y2000 and msq.size=tofix.size2000
+and msq_2k_id is null;
+
+update tofix
+set msq_10k_id=msq.id
+from map_squares msq 
+where msq.x=tofix.x10000 and msq.y=tofix.y10000 and msq.size=tofix.size10000
+and msq_10k_id is null;
+
+-- copy the fixes across to cache_occurrences
+update cache_occurrences o
+set map_sq_1km_id=tofix.msq_1k_id, map_sq_2km_id=tofix.msq_2k_id, map_sq_10km_id=tofix.msq_10k_id
+from tofix
+where tofix.id=o.id;
+
+-- remove any unwanted map square records
+delete from map_squares 
+where id in (
+select msq.id from map_squares msq
+left join cache_occurrences o on o.map_sq_1km_id=msq.id
+where o.id is null
+) and id in (
+select msq.id from map_squares msq
+left join cache_occurrences o on o.map_sq_2km_id=msq.id
+where o.id is null
+) and id in (
+select msq.id from map_squares msq
+left join cache_occurrences o on o.map_sq_10km_id=msq.id
+where o.id is null
+);
