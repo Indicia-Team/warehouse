d1c25f55866bc01170b1a3a4c66895446e9f5008
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-09-17 08:31:58 +0000

    Copied to 0.9.1 - Final refinements to setting of location name in cache_occurrences when parent samples are in play.

--- modules/cache_builder/db/version_0_1_2/201409170851_location_name_parent_samples.sql
+++ modules/cache_builder/db/version_0_1_2/201409170851_location_name_parent_samples.sql
@@ -1,7 +1,10 @@
 update cache_occurrences co
-set location_name=coalesce(s.location_name, sp.location_name)
+set location_name=coalesce(l.name, s.location_name, lp.name, sp.location_name)
 from samples s
-join samples sp on sp.id=s.parent_id and sp.deleted=false
+left join samples sp on sp.id=s.parent_id and sp.deleted=false
+left join locations l on l.id=s.location_id and l.deleted=false
+left join locations lp on lp.id=sp.location_id and lp.deleted=false
 where s.deleted=false
 and co.sample_id=s.id
-and co.location_name is null;
+and co.location_name is null
+and coalesce(l.name, s.location_name, lp.name, sp.location_name) is not null;
