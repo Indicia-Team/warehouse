47eca7905b0cc08488a2227c909ac56f326b24a7
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-09-30 09:01:53 +0000

    Copied to 0.9.1 - Warehouse support for associating recording groups with locations/sites they record at.

--- application/models/location.php
+++ application/models/location.php
@@ -33,7 +33,7 @@ class Location_Model extends ORM_Tree {
   public $search_field='name';
 
   protected $ORM_Tree_children = "locations";
-  protected $has_and_belongs_to_many = array('websites');
+  protected $has_and_belongs_to_many = array('websites', 'groups');
   protected $has_many = array('samples', 'location_attribute_values', 'location_images');
   protected $belongs_to = array('created_by'=>'user', 'updated_by'=>'user', 'location_type'=>'termlists_term');
 
