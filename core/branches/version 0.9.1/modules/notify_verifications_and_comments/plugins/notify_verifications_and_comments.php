0063b82ea88df925b32cf357e63fad4721780d58
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-10-23 08:21:38 +0000

    Copied to 0.9.1 - Performance improvement to query for generating verifier notifications If notifications fail, system table does not update timestamp (so notifications not lost). Also, when updating created_by_id after installing easy login, does not affect existing records already allocated to a user_id (safety check)

--- core/branches/version 0.9.1/modules/notify_verifications_and_comments/plugins/notify_verifications_and_comments.php
+++ core/branches/version 0.9.1/modules/notify_verifications_and_comments/plugins/notify_verifications_and_comments.php
@@ -28,75 +28,71 @@
 function notify_verifications_and_comments_scheduled_task($last_run_date) { 
   if (!$last_run_date)
     // first run, so get all records changed in last day. Query will automatically gradually pick up the rest.
-    $last_run_date=date('Y-m-d', time()-60*60*24*50);  
-  try {
-    $db = new Database();
-    $notifications = postgreSQL::selectVerificationAndCommentNotifications($last_run_date, $db);
-    foreach ($notifications as $notification) {
-      $vd = array($notification->date_start, $notification->date_end, $notification->date_type);
-      $date = vague_date::vague_date_to_string($vd);
-      if (empty($notification->comment)) {
-        switch ($notification->record_status) {
-          case 'V': 
-            $action='verified'; 
-            break;
-          case 'R': 
-            $action='rejected'; 
-            break;
-          case 'D': 
-            $action='marked dubious'; 
-            break;
-          case 'S': 
-            $action='emailed for checking'; 
-            break;
-        }
-        $comment = 'The record of '.$notification->taxon.' at '.$notification->public_entered_sref." on $date was $action.";
-      } else {
-        if ($notification->auto_generated==='t' && substr($notification->generated_by, 0, 12)==='data_cleaner' && $notification->record_owner==='t') {          
-          $comment = 'An automated check using the <a target="_blank" href="http://www.nbn.org.uk/Tools-Resources/Recording-Resources/NBN-Record-Cleaner.aspx" target="_blank">'.
-              'NBN Record Cleaner</a> rules has highlighted your record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date;
-          $comment .= ($notification->generated_by==='data_cleaner_identification_difficulty') 
-            ? ' as being of a species for which identification is not always trivial. <br/><em>'
-            : '. The following information was given: <br/><em>';
-        }
-        elseif ($notification->verified_on>$last_run_date && $notification->record_status!=='I' && $notification->record_status!=='T' && $notification->record_status!=='C') {
-          if ($notification->record_owner==='t')
-            $comment = 'Your record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.' was examined by an expert.<br/>"';
-          else
-            $comment = 'A record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.' which you\'d previously commented on was examined by an expert.<br/>"';
-        }
-        elseif ($notification->record_owner==='t')
-          $comment = 'A comment was added to your record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.'.<br/>"';          
-        else
-          $comment = 'A reply was added to the record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.' which you\'ve previously commented on.<br/>"';          
-        $comment .= $notification->comment;
-        if ($notification->auto_generated==='t') {
-          // a difficult ID record is not necessarily important...
-          $thing = ($notification->generated_by==='data_cleaner_identification_difficulty') ? 'identification' : 'important record';
-          $comment .= "</em><br/>You may be contacted by an expert to confirm this $thing so if you can supply any more information or photographs it would be useful.";
-        } else 
-          $comment .= '"<br/>';
+    $last_run_date=date('Y-m-d', time()-60*60*24*50);
+  $db = new Database();
+  $notifications = postgreSQL::selectVerificationAndCommentNotifications($last_run_date, $db);
+  foreach ($notifications as $notification) {
+    $vd = array($notification->date_start, $notification->date_end, $notification->date_type);
+    $date = vague_date::vague_date_to_string($vd);
+    if (empty($notification->comment)) {
+      switch ($notification->record_status) {
+        case 'V': 
+          $action='verified'; 
+          break;
+        case 'R': 
+          $action='rejected'; 
+          break;
+        case 'D': 
+          $action='marked dubious'; 
+          break;
+        case 'S': 
+          $action='emailed for checking'; 
+          break;
       }
-      $theNotificationToInsert = array(
-        'source' => 'Verifications and comments',
-        'source_type' => $notification->source_type,
-        'data' => json_encode(array(
-            'username'=>$notification->username,'occurrence_id'=>$notification->id,'comment'=>$comment,
-            'taxon'=>$notification->taxon,'date'=>$date,'entered_sref'=>$notification->public_entered_sref,
-            'auto_generated'=>$notification->auto_generated, 'record_status'=>$notification->record_status, 'updated_on'=>$notification->updated_on
-        )),
-        'linked_id' => $notification->id,
-        'user_id' => $notification->notify_user_id,
-        // use digest mode the user selected for this notification, or their default if not specific
-        'digest_mode' => 'N',
-        'source_detail' => $notification->source_detail
-      );
-      $db->insert('notifications', $theNotificationToInsert);
+      $comment = 'The record of '.$notification->taxon.' at '.$notification->public_entered_sref." on $date was $action.";
+    } else {
+      if ($notification->auto_generated==='t' && substr($notification->generated_by, 0, 12)==='data_cleaner' && $notification->record_owner==='t') {          
+        $comment = 'An automated check using the <a target="_blank" href="http://www.nbn.org.uk/Tools-Resources/Recording-Resources/NBN-Record-Cleaner.aspx" target="_blank">'.
+            'NBN Record Cleaner</a> rules has highlighted your record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date;
+        $comment .= ($notification->generated_by==='data_cleaner_identification_difficulty') 
+          ? ' as being of a species for which identification is not always trivial. <br/><em>'
+          : '. The following information was given: <br/><em>';
+      }
+      elseif ($notification->verified_on>$last_run_date && $notification->record_status!=='I' && $notification->record_status!=='T' && $notification->record_status!=='C') {
+        if ($notification->record_owner==='t')
+          $comment = 'Your record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.' was examined by an expert.<br/>"';
+        else
+          $comment = 'A record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.' which you\'d previously commented on was examined by an expert.<br/>"';
+      }
+      elseif ($notification->record_owner==='t')
+        $comment = 'A comment was added to your record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.'.<br/>"';          
+      else
+        $comment = 'A reply was added to the record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.' which you\'ve previously commented on.<br/>"';          
+      $comment .= $notification->comment;
+      if ($notification->auto_generated==='t') {
+        // a difficult ID record is not necessarily important...
+        $thing = ($notification->generated_by==='data_cleaner_identification_difficulty') ? 'identification' : 'important record';
+        $comment .= "</em><br/>You may be contacted by an expert to confirm this $thing so if you can supply any more information or photographs it would be useful.";
+      } else 
+        $comment .= '"<br/>';
     }
-    echo count($notifications) . ' notifications generated<br/>';
-  } catch (Exception $e) {
-    echo $e->getMessage();
+    $theNotificationToInsert = array(
+      'source' => 'Verifications and comments',
+      'source_type' => $notification->source_type,
+      'data' => json_encode(array(
+          'username'=>$notification->username,'occurrence_id'=>$notification->id,'comment'=>$comment,
+          'taxon'=>$notification->taxon,'date'=>$date,'entered_sref'=>$notification->public_entered_sref,
+          'auto_generated'=>$notification->auto_generated, 'record_status'=>$notification->record_status, 'updated_on'=>$notification->updated_on
+      )),
+      'linked_id' => $notification->id,
+      'user_id' => $notification->notify_user_id,
+      // use digest mode the user selected for this notification, or their default if not specific
+      'digest_mode' => 'N',
+      'source_detail' => $notification->source_detail
+    );
+    $db->insert('notifications', $theNotificationToInsert);
   }
+  echo count($notifications) . ' notifications generated<br/>';
 }
 
 ?>
