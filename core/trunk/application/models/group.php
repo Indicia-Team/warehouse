481dce8e9af1e8a046f9f54bf06b8ecb88143b6a
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-11-23 17:28:38 +0000

    Server side support for logos attached to groups.

--- core/trunk/application/models/group.php
+++ core/trunk/application/models/group.php
@@ -47,7 +47,7 @@ class Group_Model extends ORM {
     $array->add_rules('group_type_id', 'required');
     $array->add_rules('website_id', 'required');
     $this->unvalidatedFields = array('code', 'description', 'from_date','to_date','private_records',
-        'filter_id', 'joining_method', 'deleted', 'implicit_record_inclusion', 'view_full_precision');
+        'filter_id', 'joining_method', 'deleted', 'implicit_record_inclusion', 'view_full_precision', 'logo_path');
     // has the private records flag changed?
     $this->wantToUpdateReleaseStatus = isset($this->submission['fields']['private_records']) && 
         $this->submission['fields']['private_records']!==$this->private_records;
