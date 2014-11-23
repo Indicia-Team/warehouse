52ced90722057b7de6050fa2056402810622a9f9
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-11-23 17:21:15 +0000

    Corrected an out of date ORM relationship

--- core/trunk/application/models/location.php
+++ core/trunk/application/models/location.php
@@ -34,7 +34,7 @@ class Location_Model extends ORM_Tree {
 
   protected $ORM_Tree_children = "locations";
   protected $has_and_belongs_to_many = array('websites', 'groups');
-  protected $has_many = array('samples', 'location_attribute_values', 'location_images');
+  protected $has_many = array('samples', 'location_attribute_values', 'location_media');
   protected $belongs_to = array('created_by'=>'user', 'updated_by'=>'user', 'location_type'=>'termlists_term');
 
   // Declare that this model has child attributes, and the name of the node in the submission which contains them
