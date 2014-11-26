03ba1a4beba3ece098705b34941fd26d4868ef1a
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-11-26 10:53:04 +0000

    Safety check in script.

--- modules/notification_emails/db/version_0_1_0/201410131519_drop_fk_last_max_notification_id.sql
+++ modules/notification_emails/db/version_0_1_0/201410131519_drop_fk_last_max_notification_id.sql
@@ -1,2 +1,23 @@
--- this constraint is unnecessary, because notifications can get deleted (e.g. when clearing notifications from previous automated check runs).
-ALTER TABLE user_email_notification_frequency_last_runs DROP CONSTRAINT fk_last_max_notification_id;
+CREATE OR REPLACE function f_alter_ddl (OUT success bool)
+    LANGUAGE plpgsql AS
+$func$
+BEGIN 
+  
+success := TRUE;
+
+BEGIN
+	-- this constraint is unnecessary, because notifications can get deleted (e.g. when clearing notifications from previous automated check runs).
+  ALTER TABLE user_email_notification_frequency_last_runs DROP CONSTRAINT fk_last_max_notification_id;
+EXCEPTION
+    WHEN undefined_object THEN 
+      RAISE NOTICE 'constraint does not exist.';
+      success := FALSE;
+END;
+
+END
+$func$;
+
+SELECT f_alter_ddl();
+
+DROP FUNCTION f_alter_ddl();
+
