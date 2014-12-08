9d97075139a2ac06f272add57d103f252ef6f40d
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2014-12-08 13:21:36 +0000

    jQuery 1.9+ support.

--- core/trunk/media/js/jquery.autocomplete.js
+++ core/trunk/media/js/jquery.autocomplete.js
@@ -134,7 +134,7 @@ $.Autocompleter = function(input, options) {
     });
   }
   // prevent form submit in opera when selecting with return key
-  $.browser.opera && $(input.form).bind("submit.autocomplete", function() {
+  /opera/.test(navigator.userAgent.toLowerCase()) && $(input.form).bind("submit.autocomplete", function() {
     if (blockSubmit) {
       blockSubmit = false;
       return false;
@@ -142,7 +142,7 @@ $.Autocompleter = function(input, options) {
   });
   
   // only opera doesn't trigger keydown multiple times while pressed, others don't work with keypress at all
-  $input.bind(($.browser.opera ? "keypress" : "keydown") + ".autocomplete", function(event) {
+  $input.bind((/opera/.test(navigator.userAgent.toLowerCase()) ? "keypress" : "keydown") + ".autocomplete", function(event) {
     // track last key pressed
     lastKeyPressCode = event.keyCode;
     switch(event.keyCode) {
@@ -842,7 +842,7 @@ $.Autocompleter.Select = function (options, input, select, config) {
           overflow: 'auto'
         });
         
-        if($.browser.msie && typeof document.body.style.maxHeight === "undefined") {
+        if(/msie/.test(navigator.userAgent.toLowerCase()) && typeof document.body.style.maxHeight === "undefined") {
           var listHeight = 0;
           listItems.each(function() {
             listHeight += this.offsetHeight;
