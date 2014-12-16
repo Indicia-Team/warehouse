5334379cf522904aef8b2d68f27cb2d9c60cb426
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2014-12-16 15:05:12 +0000

    Lots of code tidying and comments.

--- modules/rest_api/i18n/en_GB/rest_api.php
+++ modules/rest_api/i18n/en_GB/rest_api.php
@@ -6,9 +6,9 @@
       'projects' => 'Retrieve a list of projects available to this client ID.',
       'projects/{project ID}' => 'Retrieve the details of a single project where {project id} is ' .
           'replaced by the project ID as retreived from an earlier request to /projects.',
-      'taxon_observations' => 'Retrieve a list of taxon-observations available to this client ID for a ' .
+      'taxon-observations' => 'Retrieve a list of taxon-observations available to this client ID for a ' .
           'project indicated by a supplied proj_id parameter.',
-      'taxon_observations/{taxon-observation ID}' => 'Retrieve the details of a single taxon-observation where ' .
+      'taxon-observations/{taxon-observation ID}' => 'Retrieve the details of a single taxon-observation where ' .
           '{taxon-observation ID} is replaced by the observation ID. A proj_id parameter must be provided and the ' .
           'observation should be available within that project\'s records.',
       'annotations' => 'Retrieve a list of annotations available to this client ID.',
@@ -18,15 +18,26 @@
     'projects' => array(
       'client_id' => 'Your unique client identifier'
     ),
-    'taxon_observations' => array(
+    'taxon-observations' => array(
       'client_id' => 'Your unique client identifier',
       'proj_id' => 'Identifier for the project that contains the observations the client is requesting.',
-      'page' => 'The page of records to retrieve when the records available is greater than page_size. The first '.
+      'page' => 'The page of records to retrieve when there are more records available than page_size. The first '.
           'page is page 1. Defaults to 1 if not provided.',
       'page_size' => 'The maximum number of records to retrieve. Defaults to 100 if not provided.',
       'edited_date_from' => 'Restricts the records to those created or edited on or after the date provided. ' .
           'Format yyyy-mm-dd.',
       'edited_date_to' => 'Restricts the records to those created or edited on or before the date provided. ' .
+          'Format yyyy-mm-dd.'
+    ),
+    'annotations' => array(
+      'client_id' => 'Your unique client identifier',
+      'proj_id' => 'Identifier for the project that contains the annotations the client is requesting.',
+      'page' => 'The page of records to retrieve when there are more records available than page_size. The first '.
+          'page is page 1. Defaults to 1 if not provided.',
+      'page_size' => 'The maximum number of records to retrieve. Defaults to 100 if not provided.',
+      'edited_date_from' => 'Restricts the annotations to those created or edited on or after the date provided. ' .
+          'Format yyyy-mm-dd.',
+      'edited_date_to' => 'Restricts the annotations to those created or edited on or before the date provided. ' .
           'Format yyyy-mm-dd.'
     )
   );
