765d37efbab6e1a927871a093d2cd703c6d0e51c
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2015-01-07 13:53:59 +0000

    Fixed script so does not error if the script this was moved from was already run.

--- modules/cache_builder/db/version_0_1_2/201412161538_cache_occurrences_taxon_group_index.sql
+++ modules/cache_builder/db/version_0_1_2/201412161538_cache_occurrences_taxon_group_index.sql
@@ -1,4 +1,28 @@
+CREATE OR REPLACE function f_add_ddl (OUT success bool)
+    LANGUAGE plpgsql AS
+$func$
+BEGIN 
+  
+success := TRUE;
+
+BEGIN
 CREATE INDEX ix_cache_occurrences_taxon_group_id
   ON cache_occurrences
   USING btree
   (taxon_group_id);
+EXCEPTION
+    WHEN OTHERS THEN 
+      RAISE NOTICE 'index exists.';
+      success := FALSE;
+END;
+
+END
+$func$;
+
+SELECT f_add_ddl();
+
+DROP FUNCTION f_add_ddl();
+
+
+  
+  
