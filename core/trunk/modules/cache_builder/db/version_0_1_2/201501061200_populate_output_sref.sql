e394a80fec0c4624f883e4de6a895d13641b2779
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2015-01-07 15:26:32 +0000

    Performance improvement by removing unnecessary joins or making inner joins in the population of output_sref.

--- core/trunk/modules/cache_builder/db/version_0_1_2/201501061200_populate_output_sref.sql
+++ core/trunk/modules/cache_builder/db/version_0_1_2/201501061200_populate_output_sref.sql
@@ -7,18 +7,10 @@ set output_sref=get_output_sref(
     greatest(
       co.sensitivity_precision,
       co.privacy_precision,
-      -- work out best square size to reflect a lat long's true precision
-      case
-        when sav.int_value>=501 then 10000
-        when sav.int_value between 51 and 500 then 1000
-        when sav.int_value between 6 and 50 then 100
-        else 10
-      end,
+      -- no need to consider the sref_precision sample attribute at this stage as it has only just been added
       10 -- default minimum square size
     ),
     co.public_geom
 )
 from samples s
-left join sample_attribute_values sav on sav.sample_id=s.id and sav.deleted=false
-left join sample_attributes sa on sa.id=sav.sample_attribute_id and sa.system_function='sref_precision' and sa.deleted=false
 where s.id=co.sample_id and s.deleted=false;
