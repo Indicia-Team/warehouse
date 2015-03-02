a4d52ff33b9652bfbf0bba0b17b05efc36ff4b01
johnvanbreda72 <johnvanbreda72@gmail.com>
2015-03-02 13:17:04 +0000

    Custom cache tables module allowing interim tables for reporting optimisation to be periodically rebuilt.

---
+++ modules/custom_cache_tables/helpers/custom_cache_tables.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package	Modules
+ * @subpackage Summary_builder
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL
+ * @link 	http://code.google.com/p/indicia/
+ */
+
+/**
+ * Helper class for custom_cache_tables functionality
+ *
+ * @package	Modules
+ * @subpackage Custom_cache_tables
+ */
+class custom_cache_tables {
+
+  public static function populate_tables($db, $last_run_date) {
+    $lastDoneInfo = (array)variable::get('custom_cache_tables', array(), false);
+    foreach (glob(MODPATH . "custom_cache_tables/definitions/*.php") as $filename) {
+      require_once $filename;
+      $defname = preg_replace('/\.php$/', '', basename($filename));
+      if (!function_exists("get_{$defname}_query") || !function_exists("get_{$defname}_update_frequency") ) {
+        kohana::log('warning', "Skipping incomplete custom_cache_tables definition $filename");
+        continue; // foreach
+      }
+      $frequency = call_user_func("get_{$defname}_update_frequency");
+      if (empty($lastDoneInfo[$defname]) || strtotime($lastDoneInfo[$defname]) < strtotime("-$frequency")) {
+        // if the table already exists, delete it
+        if (!empty($lastDoneInfo[$defname]))
+          $db->query("DROP TABLE custom_cache_tables.$defname");
+        echo "building cache table $defname<br/>";
+        self::build_table($db, $defname);
+        $lastDoneInfo[$defname]=date(DATE_ISO8601);
+      }
+    }
+    variable::set('custom_cache_tables', $lastDoneInfo);
+  }
+
+  private static function build_table($db, $defname) {
+    if (function_exists("get_{$defname}_query")) {
+      $qry = call_user_func("get_{$defname}_query");
+      $db->query($qry);
+    }
+  }
+
+
+
+}
