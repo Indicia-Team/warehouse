afddfb0e045ec8578016fa3fca1effe0c64e00f5
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2015-04-07 11:35:19 +0000

    Copied to 0.9.1 - added external_key to cache_taxon_searchterms

---
+++ modules/cache_builder/db/version_0_1_2/201504071219_cache_taxon_searchterms_external_key.sql
@@ -0,0 +1,9 @@
+﻿ALTER TABLE indicia.cache_taxon_searchterms
+   ADD COLUMN external_key character varying;
+COMMENT ON COLUMN indicia.cache_taxon_searchterms.external_key
+  IS 'External identifier for the taxon.';
+
+UPDATE cache_taxon_searchterms cts
+SET external_key = cttl.external_key
+FROM cache_taxa_taxon_lists cttl
+WHERE cttl.id=cts.taxa_taxon_list_id;
