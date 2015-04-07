b12620e3fb9ac94ee08b7b79b23080690a3dbcd2
john.vanbreda <john.vanbreda@biodiverseit.co.uk>
2015-04-07 11:46:03 +0000

    Small script performance improvement.

--- modules/cache_builder/db/version_0_1_2/201504071219_cache_taxon_searchterms_external_key.sql
+++ modules/cache_builder/db/version_0_1_2/201504071219_cache_taxon_searchterms_external_key.sql
@@ -6,4 +6,5 @@ COMMENT ON COLUMN indicia.cache_taxon_searchterms.external_key
 UPDATE cache_taxon_searchterms cts
 SET external_key = cttl.external_key
 FROM cache_taxa_taxon_lists cttl
-WHERE cttl.id=cts.taxa_taxon_list_id;
+WHERE cttl.id=cts.taxa_taxon_list_id
+AND cttl.external_key IS NOT NULL;
