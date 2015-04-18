13d821672819ca850d88ee3552e1f5e218c3b4a0
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2015-04-18 19:38:05 +0000

    Copied to 0.9.1 - URL safe version of group title.

---
+++ core/branches/version 0.9.1/modules/indicia_setup/db/version_0_9_1/201504171128_detail_groups_url_safe_title.sql
@@ -0,0 +1,27 @@
+﻿CREATE OR REPLACE VIEW detail_groups AS 
+ SELECT g.id,
+    g.title,
+    g.code,
+    g.group_type_id,
+    g.description,
+    g.from_date,
+    g.to_date,
+    g.private_records,
+    g.website_id,
+    g.joining_method,
+    g.filter_id,
+    f.definition AS filter_definition,
+    g.created_by_id,
+    c.username AS created_by,
+    g.updated_by_id,
+    u.username AS updated_by,
+    g.logo_path,
+    case g.joining_method 
+      when 'P' then trim(both '-' from regexp_replace(regexp_replace(lower(g.title), '[ ]', '-', 'g'), '[^a-z0-9\-]', '', 'g')) 
+      else null 
+    end as url_safe_title
+   FROM groups g
+     LEFT JOIN filters f ON f.id = g.filter_id AND f.deleted = false
+     JOIN users c ON c.id = g.created_by_id
+     JOIN users u ON u.id = g.updated_by_id
+  WHERE g.deleted = false;
