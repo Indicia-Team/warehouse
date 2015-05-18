be1db727dc0a28f4a2bc68aab336811a16b68388
johnvanbreda72@gmail.com <johnvanbreda72@gmail.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2015-05-18 20:30:35 +0000

    Fixed logic in script where clause

--- core/trunk/modules/cache_builder/db/version_0_1_2/201504211103_cache_occurrences_record_substatus_data.sql
+++ core/trunk/modules/cache_builder/db/version_0_1_2/201504211103_cache_occurrences_record_substatus_data.sql
@@ -2,4 +2,4 @@
 UPDATE cache_occurrences co set record_status=o.record_status, record_substatus=o.record_substatus
 FROM occurrences o
 WHERE o.id=co.id
-AND co.record_status<>o.record_status OR o.record_substatus IS NOT NULL;
+AND (co.record_status<>o.record_status OR o.record_substatus IS NOT NULL);
