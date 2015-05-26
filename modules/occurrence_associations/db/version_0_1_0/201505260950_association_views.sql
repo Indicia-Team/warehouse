72df820b00066088a50f6090e23e3bffa4a3173a
johnvanbreda72 <johnvanbreda72@gmail.com>
2015-05-26 09:23:30 +0000

    Added website_id for view permissions.

--- modules/occurrence_associations/db/version_0_1_0/201505260950_association_views.sql
+++ modules/occurrence_associations/db/version_0_1_0/201505260950_association_views.sql
@@ -3,7 +3,7 @@ select
   oa.id, oa.from_occurrence_id, oa.to_occurrence_id, cofrom.taxon as from_taxon, coto.taxon as to_taxon, cofrom.sample_id,
   oa.association_type_id, atype.term as association_type, oa.part_id, part.term as part, 
   oa.condition_id, cond.term as condition, oa.position_id, pos.term as position,
-  oa.impact_id, impact.term as impact
+  oa.impact_id, impact.term as impact, cofrom.website_id
 from occurrence_associations oa
 join cache_occurrences cofrom on cofrom.id=oa.from_occurrence_id
 join cache_occurrences coto on coto.id=oa.to_occurrence_id
@@ -20,7 +20,7 @@ select
   oa.association_type_id, atype.term as association_type, oa.part_id, part.term as part, 
   oa.condition_id, cond.term as condition, oa.position_id, pos.term as position,
   oa.impact_id, impact.term as impact, oa.comment,
-  oa.created_by_id, c.username AS created_by, oa.updated_by_id, u.username AS updated_by
+  oa.created_by_id, c.username AS created_by, oa.updated_by_id, u.username AS updated_by, cofrom.website_id
 from occurrence_associations oa
 join cache_occurrences cofrom on cofrom.id=oa.from_occurrence_id
 join cache_occurrences coto on coto.id=oa.to_occurrence_id
