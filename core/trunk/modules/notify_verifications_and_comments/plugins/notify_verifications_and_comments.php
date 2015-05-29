38dc4e707acf0a2e309cd7478caa30ca70b24419
johnvanbreda72@gmail.com <johnvanbreda72@gmail.com@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2015-05-29 08:16:05 +0000

    Improved wording of NBN record cleaner messages. Slight tidy up.

--- core/trunk/modules/notify_verifications_and_comments/plugins/notify_verifications_and_comments.php
+++ core/trunk/modules/notify_verifications_and_comments/plugins/notify_verifications_and_comments.php
@@ -23,7 +23,9 @@
 // @todo Update to use occdelta
 /**
  * Hook into the task scheduler. Runs a query to find all comments and verification status updates that need
- * to be notified back to the recorder of a record. 
+ * to be notified back to the recorder of a record.
+ * @param string $last_run_date Date & time that this module was last run.
+ * @throws \Kohana_Database_Exception
  */
 function notify_verifications_and_comments_scheduled_task($last_run_date) { 
   if (!$last_run_date)
@@ -37,44 +39,36 @@ function notify_verifications_and_comments_scheduled_task($last_run_date) {
     if (empty($notification->comment)) {
       switch ($notification->record_status) {
         case 'V': 
-          $action='verified'; 
+          $action='accepted';
           break;
         case 'R': 
-          $action='rejected'; 
+          $action='not accepted';
           break;
         case 'D': 
-          $action='marked dubious'; 
+          $action='queried';
           break;
-        case 'S': 
-          $action='emailed for checking'; 
-          break;
+        default:
+          $action='amended';
       }
-      $comment = 'The record of '.$notification->taxon.' at '.$notification->public_entered_sref." on $date was $action.";
+      $comment = "The record of $notification->taxon at $notification->public_entered_sref on $date was $action.";
     } else {
       if ($notification->auto_generated==='t' && substr($notification->generated_by, 0, 12)==='data_cleaner' && $notification->record_owner==='t') {          
-        $comment = 'An automated check using the <a target="_blank" href="http://www.nbn.org.uk/Tools-Resources/Recording-Resources/NBN-Record-Cleaner.aspx" target="_blank">'.
-            'NBN Record Cleaner</a> rules has highlighted your record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date;
-        $comment .= ($notification->generated_by==='data_cleaner_identification_difficulty') 
-          ? ' as being of a species for which identification is not always trivial. <br/><em>'
-          : '. The following information was given: <br/><em>';
+        $comment = "The following message was attached to your record of $notification->generated_by at $notification->public_entered_sref on $date ".
+            "when it was checked using the <a target=\"_blank\" href=\"http://www.nbn.org.uk/Tools-Resources/Recording-Resources/NBN-Record-Cleaner.aspx\" target=\"_blank\">".
+            "NBN Record Cleaner</a>. This does not mean the record is incorrect or is being disputed; the information below is merely a flag against the record that ".
+            "might provide useful information for recording and verification purposes.";
       }
       elseif ($notification->verified_on>$last_run_date && $notification->record_status!=='I' && $notification->record_status!=='T' && $notification->record_status!=='C') {
         if ($notification->record_owner==='t')
-          $comment = 'Your record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.' was examined by an expert.<br/>"';
+          $comment = "Your record of $notification->taxon at $notification->public_entered_sref on $date was examined by an expert.";
         else
-          $comment = 'A record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.' which you\'d previously commented on was examined by an expert.<br/>"';
+          $comment = "A record of $notification->taxon at $notification->public_entered_sref on $date which you'd previously commented on was examined by an expert.";
       }
       elseif ($notification->record_owner==='t')
-        $comment = 'A comment was added to your record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.'.<br/>"';          
+        $comment = "A comment was added to your record of $notification->taxon at $notification->public_entered_sref on $date.";
       else
-        $comment = 'A reply was added to the record of '.$notification->taxon.' at '.$notification->public_entered_sref.' on '.$date.' which you\'ve previously commented on.<br/>"';          
-      $comment .= $notification->comment;
-      if ($notification->auto_generated==='t') {
-        // a difficult ID record is not necessarily important...
-        $thing = ($notification->generated_by==='data_cleaner_identification_difficulty') ? 'identification' : 'important record';
-        $comment .= "</em><br/>You may be contacted by an expert to confirm this $thing so if you can supply any more information or photographs it would be useful.";
-      } else 
-        $comment .= '"<br/>';
+        $comment = "A reply was added to the record of $notification->taxon at $notification->public_entered_sref on $date which you've previously commented on.";
+      $comment .= "<br/><em>$notification->comment</em>";
     }
     $theNotificationToInsert = array(
       'source' => 'Verifications and comments',
@@ -94,5 +88,3 @@ function notify_verifications_and_comments_scheduled_task($last_run_date) {
   }
   echo count($notifications) . ' notifications generated<br/>';
 }
-
-?>
