1c4867fe27a5d8eba420268bd8a617634a156df5
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2015-06-11 11:14:17 +0000

    Scripts correctly copied to 0.9.1.

--- core/branches/version 0.9.1/modules/occurrence_associations/db/version_0_1_0/201506111018_association_views.sql
+++ core/branches/version 0.9.1/modules/occurrence_associations/db/version_0_1_0/201506111018_association_views.sql
@@ -1,32 +1,32 @@
-﻿CREATE OR REPLACE VIEW list_occurrence_associations AS 
+﻿DROP VIEW IF EXISTS list_occurrence_associations;
+
+CREATE OR REPLACE VIEW list_occurrence_associations AS 
 select 
   oa.id, oa.from_occurrence_id, oa.to_occurrence_id, cofrom.taxon as from_taxon, coto.taxon as to_taxon, cofrom.sample_id,
   oa.association_type_id, atype.term as association_type, oa.part_id, part.term as part, 
-  oa.condition_id, cond.term as condition, oa.position_id, pos.term as position,
-  oa.impact_id, impact.term as impact, cofrom.website_id
+  oa.position_id, pos.term as position, oa.impact_id, impact.term as impact, cofrom.website_id
 from occurrence_associations oa
 join cache_occurrences cofrom on cofrom.id=oa.from_occurrence_id
 join cache_occurrences coto on coto.id=oa.to_occurrence_id
 join cache_termlists_terms atype on atype.id=oa.association_type_id
 left join cache_termlists_terms part on part.id=oa.part_id
-left join cache_termlists_terms cond on cond.id=oa.condition_id
 left join cache_termlists_terms pos on pos.id=oa.position_id
 left join cache_termlists_terms impact on impact.id=oa.impact_id
 where oa.deleted=false;
+
+DROP VIEW IF EXISTS detail_occurrence_associations;
 
 CREATE OR REPLACE VIEW detail_occurrence_associations AS 
 select 
   oa.id, oa.from_occurrence_id, oa.to_occurrence_id, cofrom.taxon as from_taxon, coto.taxon as to_taxon, cofrom.sample_id,
   oa.association_type_id, atype.term as association_type, oa.part_id, part.term as part, 
-  oa.condition_id, cond.term as condition, oa.position_id, pos.term as position,
-  oa.impact_id, impact.term as impact, oa.comment,
+  oa.position_id, pos.term as position, oa.impact_id, impact.term as impact, oa.comment,
   oa.created_by_id, c.username AS created_by, oa.updated_by_id, u.username AS updated_by, cofrom.website_id
 from occurrence_associations oa
 join cache_occurrences cofrom on cofrom.id=oa.from_occurrence_id
 join cache_occurrences coto on coto.id=oa.to_occurrence_id
 join cache_termlists_terms atype on atype.id=oa.association_type_id
 left join cache_termlists_terms part on part.id=oa.part_id
-left join cache_termlists_terms cond on cond.id=oa.condition_id
 left join cache_termlists_terms pos on pos.id=oa.position_id
 left join cache_termlists_terms impact on impact.id=oa.impact_id
 JOIN users c ON c.id = oa.created_by_id
