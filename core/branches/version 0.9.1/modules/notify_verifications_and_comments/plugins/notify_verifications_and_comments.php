d575f35506dad52e6808dd0ac6a8fba7d11e83d3
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2015-06-19 11:14:56 +0000

    Copied to 0.9.1 - Updates to notification emails - HTML structure improved and reflects new record substatus.

--- core/branches/version 0.9.1/modules/notify_verifications_and_comments/plugins/notify_verifications_and_comments.php
+++ core/branches/version 0.9.1/modules/notify_verifications_and_comments/plugins/notify_verifications_and_comments.php
@@ -27,7 +27,7 @@
  * @param string $last_run_date Date & time that this module was last run.
  * @throws \Kohana_Database_Exception
  */
-function notify_verifications_and_comments_scheduled_task($last_run_date) { 
+function notify_verifications_and_comments_scheduled_task($last_run_date) {
   if (!$last_run_date)
     // first run, so get all records changed in last day. Query will automatically gradually pick up the rest.
     $last_run_date=date('Y-m-d', time()-60*60*24*50);
@@ -37,22 +37,37 @@ function notify_verifications_and_comments_scheduled_task($last_run_date) {
     $vd = array($notification->date_start, $notification->date_end, $notification->date_type);
     $date = vague_date::vague_date_to_string($vd);
     if (empty($notification->comment)) {
-      switch ($notification->record_status) {
-        case 'V': 
+      switch ($notification->record_status . (empty($notification->record_substatus) ? '' : $notification->record_substatus)) {
+        case 'V':
           $action='accepted';
           break;
-        case 'R': 
+        case 'V1':
+          $action='accepted as correct';
+          break;
+        case 'V2':
+          $action='accepted as correct';
+          break;
+        case 'C3':
+          $action='plausible';
+          break;
+        case 'D':
+          $action='queried';
+          break;
+        case 'R':
           $action='not accepted';
           break;
-        case 'D': 
-          $action='queried';
+        case 'R4':
+          $action='not accepted as unable to verify';
+          break;
+        case 'R5':
+          $action='not accepted as incorrect';
           break;
         default:
           $action='amended';
       }
       $comment = "The record of $notification->taxon at $notification->public_entered_sref on $date was $action.";
     } else {
-      if ($notification->auto_generated==='t' && substr($notification->generated_by, 0, 12)==='data_cleaner' && $notification->record_owner==='t') {          
+      if ($notification->auto_generated==='t' && substr($notification->generated_by, 0, 12)==='data_cleaner' && $notification->record_owner==='t') {
         $comment = "The following message was attached to your record of $notification->taxon at $notification->public_entered_sref on $date ".
             "when it was checked using the <a target=\"_blank\" href=\"http://www.nbn.org.uk/Tools-Resources/Recording-Resources/NBN-Record-Cleaner.aspx\" target=\"_blank\">".
             "NBN Record Cleaner</a>. This does not mean the record is incorrect or is being disputed; the information below is merely a flag against the record that ".
@@ -76,7 +91,8 @@ function notify_verifications_and_comments_scheduled_task($last_run_date) {
       'data' => json_encode(array(
           'username'=>$notification->username,'occurrence_id'=>$notification->id,'comment'=>$comment,
           'taxon'=>$notification->taxon,'date'=>$date,'entered_sref'=>$notification->public_entered_sref,
-          'auto_generated'=>$notification->auto_generated, 'record_status'=>$notification->record_status, 'updated_on'=>$notification->updated_on
+          'auto_generated'=>$notification->auto_generated, 'record_status'=>$notification->record_status, 
+          'record_substatus'=>$notification->record_substatus, 'updated_on'=>$notification->updated_on
       )),
       'linked_id' => $notification->id,
       'user_id' => $notification->notify_user_id,
