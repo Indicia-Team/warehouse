f5395a14eaac9dfff441e419fd1dae48d343797f
john.vanbreda@biodiverseit.co.uk <john.vanbreda@biodiverseit.co.uk@642a62a4-ac1b-11dd-9a07-0f3041a8e97c>
2015-06-25 09:57:26 +0000

    Left join fix

--- core/trunk/modules/indicia_setup/db/version_0_9_1/201506251054_occurrence_comments_person.sql
+++ core/trunk/modules/indicia_setup/db/version_0_9_1/201506251054_occurrence_comments_person.sql
@@ -2,8 +2,8 @@ CREATE OR REPLACE VIEW list_occurrence_comments AS
  SELECT oc.id, oc.comment, oc.occurrence_id, oc.email_address, oc.updated_on, COALESCE(oc.person_name, ((p.surname::text || ', '::text) || p.first_name::text)::character varying) AS person_name, u.username, o.website_id, oc.record_status, oc.record_substatus, oc.query
    FROM occurrence_comments oc
    JOIN occurrences o ON o.id = oc.occurrence_id AND o.deleted = false
-   JOIN users u ON oc.created_by_id = u.id and u.id<>1
-   JOIN people p ON p.id = u.person_id AND p.deleted = false
+   LEFT JOIN users u ON oc.created_by_id = u.id and u.id<>1
+   LEFT JOIN people p ON p.id = u.person_id AND p.deleted = false
   WHERE oc.deleted = false;
 
 CREATE OR REPLACE VIEW detail_occurrence_comments AS 
@@ -12,5 +12,5 @@ CREATE OR REPLACE VIEW detail_occurrence_comments AS
    JOIN occurrences o ON o.id = oc.occurrence_id AND o.deleted = false
    JOIN users c ON oc.created_by_id = c.id
    JOIN users u ON oc.updated_by_id = u.id
-   JOIN people p ON p.id = c.person_id AND p.deleted = false and c.created_by_id<>1
+   LEFT JOIN people p ON p.id = c.person_id AND p.deleted = false and c.created_by_id<>1
   WHERE oc.deleted = false;
