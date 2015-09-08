05be51e2b84a83f1c05c96bc14a8b899d0f4f2ce
John van Breda <john.vanbreda@biodiverseit.co.uk>
2015-09-08 11:33:33 +0100

    Basic tests on the taxon-observations endpoint
    PHPUnit for REST api

--- modules/rest_api/tests/Rest_ControllerTest.php
+++ modules/rest_api/tests/Rest_ControllerTest.php
@@ -69,17 +69,87 @@ class Rest_ControllerTest extends PHPUnit_Framework_TestCase {
         "$response[curlErrno] ($response[errorMessage]).");
       $fromApi = json_decode($response['response']);
       $this->assertEquals($fromApi->Title, $projDef['Title'],
-        "Unexpected title $fromApi->Title returned for project $projDef[id] by /projects/$projDef[id].");
+          "Unexpected title $fromApi->Title returned for project $projDef[id] by /projects/$projDef[id].");
       $this->assertEquals($fromApi->Title, $projDef['Title'],
-        "Unexpected title $fromApi->Title returned for project $projDef[id] by /projects/$projDef[id].");
+          "Unexpected title $fromApi->Title returned for project $projDef[id] by /projects/$projDef[id].");
       $this->assertEquals($fromApi->Description, $projDef['Description'],
-        "Unexpected description $fromApi->Description returned for project $projDef[id] by /projects/$projDef[id].");
+          "Unexpected description $fromApi->Description returned for project $projDef[id] by /projects/$projDef[id].");
     }
-
   }
 
-  private function callService($method, $userId, $sharedSecret) {
+  public function testTaxon_observations_get_incorrect_params() {
+    $response = $this->callService('taxon-observations', self::$userId, '---');
+    $this->assertTrue($response['httpCode']===401,
+      'Incorrect shared secret passed to /taxon-observations but request authorised. ' .
+      "Http response $response[httpCode].");
+    $this->assertTrue($response['response']==='Unauthorized',
+      'Incorrect shared secret passed to /taxon-observations but data still returned. '.
+      var_export($response, true));
+    $response = $this->callService('taxon-observations', '---', self::$config['shared_secret']);
+    $this->assertTrue($response['httpCode']===401,
+      'Incorrect userId passed to /taxon-observations but request authorised. ' .
+      "Http response $response[httpCode].");
+    $this->assertTrue($response['response']==='Unauthorized',
+      'Incorrect userId passed to /taxon-observations but data still returned. '.
+      var_export($response, true));
+    $response = $this->callService("taxon-observations", self::$userId, self::$config['shared_secret']);
+    $this->assertEquals($response['httpCode'], 400,
+        'Requesting taxon observations without params should be a bad request');
+    foreach (self::$config['projects'] as $projDef) {
+      $response = $this->callService("taxon-observations", self::$userId, self::$config['shared_secret'],
+          array('proj_id' => $projDef['id']));
+      $this->assertEquals($response['httpCode'], 400,
+          'Requesting taxon observations without edited_date_from should be a bad request');
+      $response = $this->callService("taxon-observations", self::$userId, self::$config['shared_secret'],
+        array('edited_date_from' => '2015-01-01'));
+      $this->assertEquals($response['httpCode'], 400,
+        'Requesting taxon observations without proj_id should be a bad request');
+      // only test a single project
+      break;
+    }
+  }
+
+  /**
+   * Test the /taxon-observations endpoint in valid use.
+   * @todo Test the pagination responses
+   */
+  public function testTaxon_observations_get() {
+    foreach (self::$config['projects'] as $projDef) {
+      $response = $this->callService("taxon-observations", self::$userId, self::$config['shared_secret'],
+        array('proj_id' => $projDef['id'], 'edited_date_from' => '2015-01-01'));
+      $this->assertTrue($response['curlErrno']===0 && $response['httpCode']===200,
+        "Invalid response from call to /taxon-observations. HTTP Response $response[httpCode]. Curl error " .
+        "$response[curlErrno] ($response[errorMessage]).");
+      $apiResponse = json_decode($response['response'], true);
+      $this->assertArrayHasKey('paging', $apiResponse, 'Paging missing from response to call to taxon-observations');
+      $this->assertArrayHasKey('data', $apiResponse, 'Data missing from response to call to taxon-observations');
+      $data = $apiResponse['data'];
+      foreach ($data as $occurrence)
+        $this->checkValidTaxonOccurrence($occurrence);
+      // only test a single project
+      break;
+    }
+  }
+
+  /**
+   * Checks that an array retrieved from the API is a valid taxon-occurrence resource.
+   * @param $data Array to be tested as a taxon occurrence resource
+   */
+  private function checkValidTaxonOccurrence($data) {
+    $mustHave = array('id', 'href', 'TaxonVersionKey', 'TaxonName', 'StartDate', 'EndDate', 'DateType',
+        'Projection', 'Precision', 'Recorder', 'lasteditdate');
+    foreach ($mustHave as $key) {
+      $this->assertArrayHasKey($key, $data,
+          "Missing $key from taxon-observation resource. " . var_export($data, true));
+      $this->assertNotEmpty($data[$key],
+          "Empty $key in taxon-observation resource" . var_export($data, true));
+    }
+  }
+
+  private function callService($method, $userId, $sharedSecret, $query=false) {
     $url = url::base(true) . "/services/rest/$method";
+    if ($query)
+      $url .= '?' . http_build_query($query);
     $session = curl_init();
     // Set the POST options.
     curl_setopt ($session, CURLOPT_URL, $url);
