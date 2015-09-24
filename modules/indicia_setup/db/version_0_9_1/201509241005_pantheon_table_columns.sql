453c05d024634ca2b36c9e44070c176dda29bf3a
Andrew van Breda <andrew_van_breda@hotmail.com>
2015-09-24 16:22:00 +0100

    New columns for termlists_terms table and the 6 attributes tables for initial use with the Pantheon project, although will be available for general use.

---
+++ modules/indicia_setup/db/version_0_9_1/201509241005_pantheon_table_columns.sql
@@ -0,0 +1,73 @@
+ALTER TABLE taxa_taxon_list_attributes 
+ADD COLUMN description text,
+ADD COLUMN source_id integer,
+ADD CONSTRAINT fk_source_id FOREIGN KEY (source_id) 
+REFERENCES termlists_terms (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+COMMENT ON COLUMN taxa_taxon_list_attributes.description IS
+  'Holds a description for the attribute.';
+COMMENT ON COLUMN taxa_taxon_list_attributes.source_id IS
+  'Points to a termlists_term which describes where the attribute originated.';
+
+ALTER TABLE survey_attributes
+ADD COLUMN description text,
+ADD COLUMN source_id integer,
+ADD CONSTRAINT fk_source_id FOREIGN KEY (source_id) 
+REFERENCES termlists_terms (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+COMMENT ON COLUMN survey_attributes.description IS
+  'Holds a description for the attribute.';
+COMMENT ON COLUMN survey_attributes.source_id IS
+  'Points to a termlists_term which describes where the attribute originated.';
+
+ALTER TABLE sample_attributes
+ADD COLUMN description text,
+ADD COLUMN source_id integer,
+ADD CONSTRAINT fk_source_id FOREIGN KEY (source_id) 
+REFERENCES termlists_terms (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+COMMENT ON COLUMN sample_attributes.description IS
+  'Holds a description for the attribute.';
+COMMENT ON COLUMN sample_attributes.source_id IS
+  'Points to a termlists_term which describes where the attribute originated.';
+
+ALTER TABLE person_attributes
+ADD COLUMN description text,
+ADD COLUMN source_id integer,
+ADD CONSTRAINT fk_source_id FOREIGN KEY (source_id) 
+REFERENCES termlists_terms (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+COMMENT ON COLUMN person_attributes.description IS
+  'Holds a description for the attribute.';
+COMMENT ON COLUMN person_attributes.source_id IS
+  'Points to a termlists_term which describes where the attribute originated.';
+
+ALTER TABLE occurrence_attributes
+ADD COLUMN description text,
+ADD COLUMN source_id integer,
+ADD CONSTRAINT fk_source_id FOREIGN KEY (source_id) 
+REFERENCES termlists_terms (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+COMMENT ON COLUMN occurrence_attributes.description IS
+  'Holds a description for the attribute.';
+COMMENT ON COLUMN occurrence_attributes.source_id IS
+  'Points to a termlists_term which describes where the attribute originated.';
+
+ALTER TABLE location_attributes
+ADD COLUMN description text,
+ADD COLUMN source_id integer,
+ADD CONSTRAINT fk_source_id FOREIGN KEY (source_id) 
+REFERENCES termlists_terms (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+COMMENT ON COLUMN location_attributes.description IS
+  'Holds a description for the attribute.';
+COMMENT ON COLUMN location_attributes.source_id IS
+  'Points to a termlists_term which describes where the attribute originated.';
+
+ALTER TABLE termlists_terms 
+ADD COLUMN source_id integer,
+ADD CONSTRAINT fk_source_id FOREIGN KEY (source_id)
+REFERENCES termlists_terms (id) MATCH SIMPLE
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+COMMENT ON COLUMN termlists_terms.source_id IS
+  'Points to a termlists_term which describes where the term originated.';
