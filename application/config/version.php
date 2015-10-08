6cafb5bec243c977599f897f38efcf45e27af9a3
John <john.vanbreda@biodiverseit.co.uk>
2015-10-08 14:37:39 +0100

    Fixes #22. Diacritics allowed in record cleaner rule files.

--- application/config/version.php
+++ application/config/version.php
@@ -26,7 +26,7 @@ defined('SYSPATH') or die('No direct script access.');
 /**
  * @var string The application files version number.
  */
-$config['version'] = '0.9.1.6';
+$config['version'] = '0.9.1.7';
 $config['release_date']  = '2015-10-08';
 $config['repository'] = 'https://github.com/Indicia-Team/warehouse/releases';
 
