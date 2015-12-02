f5c4ef9157e0592a774343f2014d8b43de82fb3a
John van Breda <john.vanbreda@biodiverseit.co.uk>
2015-12-02 20:18:25 +0000

    Users views include licence_id

--- modules/indicia_setup/db/version_0_9_1/201512021339_licences_fk_views.sql
+++ modules/indicia_setup/db/version_0_9_1/201512021339_licences_fk_views.sql
@@ -110,3 +110,39 @@ CREATE OR REPLACE VIEW detail_samples AS
      JOIN users c ON c.id = s.created_by_id
      JOIN users u ON u.id = s.updated_by_id
   WHERE s.deleted = false;
+  
+
+CREATE OR REPLACE VIEW list_users AS
+ SELECT u.id,
+    u.username,
+    uw.website_id,
+    u.person_id,
+    uw.licence_id
+   FROM users u
+     JOIN users_websites uw ON u.id = uw.user_id AND uw.site_role_id IS NOT NULL
+  WHERE u.deleted = false;
+
+
+CREATE OR REPLACE VIEW detail_users AS
+ SELECT u.id,
+    u.username,
+    uw.website_id,
+    u.person_id,
+    u.created_by_id,
+    cu.username AS created_by,
+    u.updated_by_id,
+    u.created_on,
+    u.updated_on,
+    uu.username AS updated_by,
+    p.surname,
+    p.first_name,
+    p.surname::text || COALESCE(', '::text || p.first_name::text, ''::text) AS person_name,
+    p.email_address,
+    (((p.surname::text || COALESCE(', '::text || p.first_name::text, ''::text)) || ' ('::text) || p.email_address::text) || ')'::text AS name_and_email,
+    uw.licence_id
+   FROM users u
+     JOIN users cu ON cu.id = u.created_by_id
+     JOIN users uu ON uu.id = u.updated_by_id
+     JOIN users_websites uw ON u.id = uw.user_id AND uw.site_role_id IS NOT NULL
+     JOIN people p ON p.id = u.person_id AND p.deleted = false
+  WHERE u.deleted = false;
