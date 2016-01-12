0df2a74ba6e03cc9fc8dc9daee875190774fc1d7
Gary van Breda <gary.h@vbitservices.com>
2016-01-12 10:52:00 +0000

    version bump

--- application/config/version.php
+++ application/config/version.php
@@ -26,8 +26,8 @@ defined('SYSPATH') or die('No direct script access.');
 /**
  * @var string The application files version number.
  */
-$config['version'] = '0.9.1.19';
-$config['release_date']  = '2015-12-21';
+$config['version'] = '0.9.1.20';
+$config['release_date']  = '2016-01-12';
 $config['repository'] = 'https://github.com/Indicia-Team/warehouse/releases';
 
 ?>
