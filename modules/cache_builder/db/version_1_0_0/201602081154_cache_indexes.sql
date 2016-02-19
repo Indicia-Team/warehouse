2032eabe17019360208fbb91de6a6ca6e8530d2b
John <john.vanbreda@biodiverseit.co.uk>
2016-02-19 15:32:27 +0000

    Added some more indexes to the new cache structure.

--- modules/cache_builder/db/version_1_0_0/201602081154_cache_indexes.sql
+++ modules/cache_builder/db/version_1_0_0/201602081154_cache_indexes.sql
@@ -1,16 +1,7 @@
-
--- Index: ix_cache_occurrences_functional_family_taxa_taxon_list_id
-
--- DROP INDEX ix_cache_occurrences_functional_family_taxa_taxon_list_id;
-
 CREATE INDEX ix_cache_occurrences_functional_family_taxa_taxon_list_id
   ON cache_occurrences_functional
   USING btree
   (family_taxa_taxon_list_id);
-
--- Index: ix_cache_occurrences_functional_submission
-
--- DROP INDEX ix_cache_occurrences_functional_submission;
 
 CREATE INDEX ix_cache_occurrences_functional_submission
   ON cache_occurrences_functional
@@ -18,18 +9,10 @@ CREATE INDEX ix_cache_occurrences_functional_submission
   (website_id, survey_id, sample_id);
 ALTER TABLE cache_occurrences_functional CLUSTER ON ix_cache_occurrences_functional_submission;
 
--- Index: ix_cache_occurrences_functional_date_start
-
--- DROP INDEX ix_cache_occurrences_functional_date_start;
-
 CREATE INDEX ix_cache_occurrences_functional_date_start
   ON cache_occurrences_functional
   USING btree
   (date_start);
-
--- Index: ix_cache_samples_functional_survey
-
--- DROP INDEX ix_cache_samples_functional_survey;
 
 CREATE INDEX ix_cache_samples_functional_survey
   ON cache_samples_functional
@@ -37,11 +20,57 @@ CREATE INDEX ix_cache_samples_functional_survey
   (website_id, survey_id);
 ALTER TABLE cache_samples_functional CLUSTER ON ix_cache_samples_functional_survey;
 
--- Index: ix_cache_occurrences_functional_taxon_group_id
-
--- DROP INDEX ix_cache_occurrences_functional_taxon_group_id;
-
 CREATE INDEX ix_cache_occurrences_functional_taxon_group_id
   ON cache_occurrences_functional
   USING btree
   (taxon_group_id);
+
+CREATE INDEX ix_cache_occurrences_functional_created_by_id
+  ON cache_occurrences_functional
+  USING btree
+  (created_by_id);
+
+CREATE INDEX ix_cache_occurrences_functional_map_sq_10km_id
+  ON cache_occurrences_functional
+  USING btree
+  (map_sq_1km_id);
+  
+CREATE INDEX ix_cache_occurrences_functional_map_sq_2km_id
+  ON cache_occurrences_functional
+  USING btree
+  (map_sq_1km_id);
+  
+CREATE INDEX ix_cache_occurrences_functional_map_sq_1km_id
+  ON cache_occurrences_functional
+  USING btree
+  (map_sq_1km_id);
+  
+CREATE INDEX ix_cache_occurrences_functional_record_status
+  ON cache_occurrences_functional
+  USING btree
+  (record_status);
+  
+CREATE INDEX ix_cache_occurrences_functional_public_geom
+  ON cache_occurrences_functional
+  USING gist
+  (public_geom);
+  
+CREATE INDEX ix_cache_samples_functional_created_by_id
+  ON cache_samples_functional
+  USING btree
+  (created_by_id);
+  
+CREATE INDEX ix_cache_samples_functional_map_sq_10km_id
+  ON cache_samples_functional
+  USING btree
+  (map_sq_1km_id);
+  
+CREATE INDEX ix_cache_samples_functional_map_sq_2km_id
+  ON cache_samples_functional
+  USING btree
+  (map_sq_1km_id);
+  
+CREATE INDEX ix_cache_samples_functional_map_sq_1km_id
+  ON cache_samples_functional
+  USING btree
+  (map_sq_1km_id);
