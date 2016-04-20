21cf128d90a4c6a28e390e039257c350260dffa4
John van Breda <john.vanbreda@biodiverseit.co.uk>
2016-04-20 13:44:09 +0100

    Auto verify tidy up
    * $processOldData not read from config at file level, only read inside
    a function when required. This prevents it loading config every time
    this file is touched.
    * Added comment header for file.
    * Added todos for suggestions about the config value data types.
    * Removed extend_data_services plugin which was incorrect. No
    auto_verify entity needs to be exposed, probably a copy and paste error.
    * Fixed to use new occdelta structure (based on
    cache_occurrences_functional)
    * Autoverify outputs applied to the cache_occurrences_* tables, not to
    the view (which caused an exception as the cache_occurrences view is
    read only)

--- modules/auto_verify/plugins/auto_verify.php
+++ modules/auto_verify/plugins/auto_verify.php
@@ -1,45 +1,60 @@
 <?php
-$processOldData=kohana::config('auto_verify.process_old_data');
-function auto_verify_extend_data_services() {
-  return array(
-    'auto_verify'=>array('allow_full_access'=>true)
-  );
-}
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @package	Modules
+ * @subpackage Auto verify
+ * @author	Indicia Team
+ * @license	http://www.gnu.org/licenses/gpl.html GPL
+ * @link 	https://github.com/Indicia-Team/warehouse
+ */
 
 /**
- * Hook into the task scheduler. When run, the system checks the cache_occurrences table for records where the data cleaner has marked
+ * Hook into the task scheduler. When run, the system checks the cache_occurrences_functional table for records where the data cleaner has marked
  * the record as data_cleaner_info "pass", record_status="C", the system then sets the record to verified automatically.
  * @param string $last_run_date Date last run, or null if never run
  * @param object $db Database object.
+ * @todo Config for $autoVerifyNullIdDiff should be a boolean, not a string
+ * @todo Config for $processOldData should be a boolean, not a string
  */
 function auto_verify_scheduled_task($last_run_date, $db) {
   $autoVerifyNullIdDiff=kohana::config('auto_verify.auto_accept_occurrences_with_null_id_difficulty');
-  global $processOldData;
   $processOldData=kohana::config('auto_verify.process_old_data');
   if (empty($autoVerifyNullIdDiff)) {
-    print_r("Unable to automatically verify occurrences when the auto_accept_occurrences_with_null_id_difficulty entry is empty.<br>");
+    echo "Unable to automatically verify occurrences when the auto_accept_occurrences_with_null_id_difficulty entry is empty.<br>";
     kohana::log('error', 'Unable to automatically verify occurrences when the auto_accept_occurrences_with_null_id_difficulty configuration entry is empty.');
     return false;
   }
   //Do we need to consider old data (probably as a one-off run) or just newly changed data.
   $subQuery="
-    SELECT co.id";
+    SELECT delta.id";
   if (!empty($processOldData)&&$processOldData==='true') { 
     $subQuery.="  
-      FROM cache_occurrences co";
+      FROM cache_occurrences_functional delta";
   } else {
     $subQuery.="  
-      FROM occdelta od
-      JOIN cache_occurrences co on co.id=od.id";
+      FROM occdelta delta";
   }
   $subQuery.="
-    JOIN surveys s on s.id = co.survey_id AND s.auto_accept=true AND s.deleted=false
-    LEFT JOIN cache_taxon_searchterms cts on cts.taxon_meaning_id = co.taxon_meaning_id 
-    WHERE co.data_cleaner_info='pass' AND co.record_status='C' AND co.record_substatus IS NULL
+    JOIN surveys s on s.id = delta.survey_id AND s.auto_accept=true AND s.deleted=false
+    LEFT JOIN cache_taxon_searchterms cts on cts.taxon_meaning_id = delta.taxon_meaning_id
+    WHERE delta.data_cleaner_result=true
+    AND delta.record_status='C' AND delta.record_substatus IS NULL
         AND ((".$autoVerifyNullIdDiff."=false AND cts.identification_difficulty IS NOT NULL AND cts.identification_difficulty<=s.auto_accept_max_difficulty) 
         OR (".$autoVerifyNullIdDiff."=true AND (cts.identification_difficulty IS NULL OR cts.identification_difficulty<=s.auto_accept_max_difficulty)))";
   $verificationTime=gmdate("Y\/m\/d H:i:s");
-  //Need to update cache_occurrences, as this table has already been built at this point. 
+  //Need to update cache_occurrences_*, as these tables have already been built at this point.
   $query = "
     INSERT INTO occurrence_comments (comment, generated_by, occurrence_id,record_status,record_substatus,created_by_id,updated_by_id,created_on,updated_on,auto_generated)
     SELECT 'Accepted based on automatic checks', 'system', id,'V','2',1,1,'".$verificationTime."','".$verificationTime."',true
@@ -58,13 +73,17 @@ function auto_verify_scheduled_task($last_run_date, $db) {
     WHERE id in
     (".$subQuery.");
       
-    UPDATE cache_occurrences
+    UPDATE cache_occurrences_functional
     SET 
     record_status='V',
     record_substatus='2',
     release_status='R',
-    verified_on='".$verificationTime."',
-    verifier='admin, core'
+    verified_on='".$verificationTime."'
+    WHERE id in
+    (".$subQuery.");
+
+    UPDATE cache_occurrences_nonfunctional
+    SET verifier='admin, core'
     WHERE id in
     (".$subQuery.");";
   $results=$db->query($query)->result_array(false);
@@ -72,7 +91,7 @@ function auto_verify_scheduled_task($last_run_date, $db) {
   //records correctly.
   $query = "
     SELECT count(id)
-    FROM cache_occurrences co
+    FROM cache_occurrences_functional co
     WHERE co.verified_on='".$verificationTime."';";
   $results=$db->query($query)->result_array(false);
   if (!empty($results[0]['count']) && $results[0]['count']>1)
@@ -86,12 +105,8 @@ function auto_verify_scheduled_task($last_run_date, $db) {
 /*
  * Tell the system that we need the occdelta table to find out which occurrences have been created/changed recently.
  */
-if (empty($processOldData)||$processOldData==='false') { 
-  function auto_verify_metadata() {
-    return array(
-      'requires_occurrences_delta'=>TRUE
-    );
-  }
+function auto_verify_metadata() {
+  return array(
+    'requires_occurrences_delta'=>TRUE
+  );
 }
-?> 
- 
