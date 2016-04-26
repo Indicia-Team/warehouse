845ce7758ce53e7ce43327bb308ae21b22a54ba7
John van Breda <john.vanbreda@biodiverseit.co.uk>
2016-04-26 10:09:31 +0100

    Bump version/release date 0.9.1.23

--- application/config/version.php
+++ application/config/version.php
@@ -27,7 +27,7 @@ defined('SYSPATH') or die('No direct script access.');
  * @var string The application files version number.
  */
 $config['version'] = '0.9.1.23';
-$config['release_date']  = '2016-03-11';
+$config['release_date']  = '2016-04-26';
 $config['repository'] = 'https://github.com/Indicia-Team/warehouse/releases';
 
 ?>
