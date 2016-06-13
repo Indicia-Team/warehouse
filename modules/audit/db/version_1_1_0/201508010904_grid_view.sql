584b4919a0ddc87f8e06a45be3c7f840fc884f63
John van Breda <john.vanbreda@biodiverseit.co.uk>
2016-06-13 09:43:47 +0100

    Added action to gv view for audit trail

--- modules/audit/db/version_1_1_0/201508010904_grid_view.sql
+++ modules/audit/db/version_1_1_0/201508010904_grid_view.sql
@@ -11,6 +11,7 @@ CREATE VIEW gv_logged_actions AS
 SELECT min(la.id) as id,
     la.search_table_name,
     la.search_key,
+    CASE la.action WHEN 'I' THEN 'insert' WHEN 'U' THEN 'update' WHEN 'D' THEN 'delete' WHEN 'T' THEN 'truncate' ELSE la.action END as action,
     la.action_tstamp_tx,
     u.username AS updated_by,
     la.transaction_id,
@@ -20,4 +21,4 @@ FROM audit.logged_actions la
 LEFT JOIN users u ON la.updated_by_id = u.id
 LEFT JOIN audit.logged_actions_websites law ON law.logged_action_id = la.id
 LEFT JOIN websites w ON law.website_id = w.id
-GROUP BY la.search_table_name, la.search_key, la.action_tstamp_tx, u.username, la.transaction_id, law.website_id, w.title;
+GROUP BY la.search_table_name, la.search_key, la.action, la.action_tstamp_tx, u.username, la.transaction_id, law.website_id, w.title;
