0281c1f14b96f3178616585f4889c2e7518cea03
Gary van Breda <gary.h@vbitservices.com>
2016-06-16 09:44:18 +0100

    Further changes to CSV upload process following review of changes.

--- application/models/sample.php
+++ application/models/sample.php
@@ -268,7 +268,7 @@ class Sample_Model extends ORM_Tree
   /**
    * Define a form that is used to capture a set of predetermined values that apply to every record during an import.
    */
-  public function fixed_values_form() {
+  public function fixed_values_form($options=array()) {
     $srefs = array();
     $systems = spatial_ref::system_list();
     foreach ($systems as $code=>$title) 
@@ -277,7 +277,7 @@ class Sample_Model extends ORM_Tree
     				str_replace(array(',',':'), array('&#44', '&#56'), $title);
     
     $sample_methods = array(":Defined in file");
-    $parent_sample_methods = array();
+    $parent_sample_methods = array(":No filter");
     $terms = $this->db->select('id, term')->from('list_termlists_terms')->where('termlist_external_key', 'indicia:sample_methods')->orderby('term', 'asc')->get()->result();
     foreach ($terms as $term) {
     	$sample_method = str_replace(array(',',':'), array('&#44', '&#56'), $term->id) .
@@ -294,7 +294,7 @@ class Sample_Model extends ORM_Tree
     						":".
     						str_replace(array(',',':'), array('&#44', '&#56'), $term->term);
     
-    return array(
+    $retval = array(
       'website_id' => array( 
         'display'=>'Website', 
         'description'=>'Select the website to import records into.', 
@@ -323,13 +323,6 @@ class Sample_Model extends ORM_Tree
         'datatype'=>'lookup',
         'lookup_values'=>implode(',', $sample_methods)
       ),
-      'fkFilter:sample:sample_method_id' => array(
-        'display'=>'Parent Sample Method', 
-        'description'=>'If this import file includes samples which reference parent sample records, you can restrict the type of samples looked '.
-      		'up by setting this sample method type. It is not currently possible to use a column in the file to do this on a sample by sample basis.',
-        'datatype'=>'lookup',
-      	'lookup_values'=>implode(',', $parent_sample_methods)
-      ),
       'fkFilter:location:location_type_id' => array(
         'display'=>'Location Type', 
         'description'=>'If this import file includes samples which reference locations records, you can restrict the type of locations looked '.
@@ -338,6 +331,16 @@ class Sample_Model extends ORM_Tree
         'lookup_values'=>implode(',', $location_types)
       )
     );
+    if(!empty($options['activate_parent_sample_method_filter']) && $options['activate_parent_sample_method_filter']==='t')
+    	$retval['fkFilter:sample:sample_method_id'] = array(
+    			'display'=>'Parent Sample Method',
+    			'description'=>'If this import file includes samples which reference parent sample records, you can restrict the type of samples looked '.
+    			'up by setting this sample method type. It is not currently possible to use a column in the file to do this on a sample by sample basis.',
+    			'datatype'=>'lookup',
+    			'lookup_values'=>implode(',', $parent_sample_methods)
+    	);
+    	 
+    return $retval;
   }
   
   /**
