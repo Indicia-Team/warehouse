9268976e79ec1edceb6d9d56f528125af0808d55
Jim Bacon <jame2@ceh.ac.uk>
2016-09-02 15:44:01 +0100

    Update Kohana unit tests for v2.3
    The supplied tests are clearly for v3 and references to new functions
    etc. need to be stripped out.

--- modules/phpUnit/tests/Helper_Date_Test.php
+++ modules/phpUnit/tests/Helper_Date_Test.php
@@ -13,12 +13,7 @@ class Helper_Date_Test extends PHPUnit_Framework_TestCase
 	public function offset_provider()
 	{
 		return array(
-			array('Europe/Berlin', 'America/Chicago', '2009-01-15', 25200),
-			array('Europe/Berlin', 'America/Chicago', '2009-06-15', 25200),
-			array('Europe/Berlin', 'Asia/Riyadh', '2009-01-15', -7200),
-			array('Europe/Berlin', 'Asia/Riyadh', '2009-06-15', -3600),
-			array('Europe/London', 'GMT', '2009-01-15', 0),
-			array('Europe/London', 'GMT', '2009-06-15', 3600),
+			array('Europe/Berlin', 'America/Chicago', 25200),
 		);
 	}
 
@@ -26,9 +21,9 @@ class Helper_Date_Test extends PHPUnit_Framework_TestCase
 	 * @dataProvider offset_provider
 	 * @test
 	 */
-	public function offset($local, $remote, $when, $expected)
+	public function offset($local, $remote, $expected)
 	{
-		$result = date::offset($local, $remote, $when, $expected);
+		$result = date::offset($local, $remote);
 		$this->assertEquals($expected, $result);
 	}
 }
