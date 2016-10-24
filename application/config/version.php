73dcad8c5f365b49515f990a22b1efdbe8806598
Jim Bacon <jame2@ceh.ac.uk>
2016-10-24 14:36:36 +0100

    Bump version

--- application/config/version.php
+++ application/config/version.php
@@ -26,8 +26,8 @@ defined('SYSPATH') or die('No direct script access.');
 /**
  * @var string The application files version number.
  */
-$config['version'] = '1.11.0';
-$config['release_date']  = '2016-10-20';
+$config['version'] = '1.11.1';
+$config['release_date']  = '2016-10-24';
 $config['repository'] = 'https://github.com/Indicia-Team/warehouse/releases';
 
 ?>
