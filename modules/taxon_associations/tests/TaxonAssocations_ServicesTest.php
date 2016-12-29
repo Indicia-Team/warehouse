890ecffde4c4afc7a2676f0684a1ee18e23d2894
John van Breda <john.vanbreda@biodiverseit.co.uk>
2016-12-29 10:56:28 +0000

    Taxon associations module for warehouse

---
+++ modules/taxon_associations/tests/TaxonAssocations_ServicesTest.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'client_helpers/data_entry_helper.php';
+require_once 'client_helpers/submission_builder.php';
+
+class TaxonAssociations_ServicesTest extends Indicia_DatabaseTestCase {
+  
+}
