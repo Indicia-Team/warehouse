5361c85d88e321a1079ee83a66fed61b944e57b2
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-07-26 13:07:20 +0100

    Fixes a cross join in the attr_sample_method update query

--- modules/cache_builder/db/version_1_39_2/201707240950_sample_method_sysfunc_data.sql
+++ modules/cache_builder/db/version_1_39_2/201707240950_sample_method_sysfunc_data.sql
@@ -3,4 +3,5 @@
 UPDATE cache_samples_nonfunctional
 SET attr_sample_method = t.term
 FROM samples s
-JOIN cache_termlists_terms t on t.id=s.sample_method_id;
+JOIN cache_termlists_terms t on t.id=s.sample_method_id
+WHERE s.id=cache_samples_nonfunctional.id;
