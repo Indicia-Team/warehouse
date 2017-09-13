2c85403b9784723fcf35f4d902fbd90e113dbabf
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-09-13 10:27:11 +0100

    PG db and extensions set up in .travis.yml rather than sh file
    Better matches documentation

--- .travis/postgres_setup.sh
+++ .travis/postgres_setup.sh
@@ -1,12 +1,6 @@
 psql -U postgres <<EOF
 
--- Create a blank Indicia database.
-CREATE DATABASE indicia;
 \connect indicia;
-
--- Activate PostGIS (version 2.1).
-CREATE EXTENSION postgis;
-CREATE EXTENSION postgis_topology;
 
 -- Create indicia_user and allocate permissions.
 CREATE USER indicia_user WITH PASSWORD 'indicia_user_pass';
@@ -21,5 +15,5 @@ ALTER USER indicia_user SET search_path = indicia, public, pg_catalog;
 -- Create indicia_report_user and allocate permissions.
 CREATE USER indicia_report_user WITH PASSWORD 'indicia_report_user_pass';
 ALTER USER indicia_report_user SET search_path = indicia, public, pg_catalog;
- 
+
 EOF
