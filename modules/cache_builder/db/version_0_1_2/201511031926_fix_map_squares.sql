3f1d4acda0cd55886d3e43e7ef3d0cab281c6bf3
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-09-13 16:16:15 +0100

    Dropping temp table to ensure scripts run from beginning

--- modules/cache_builder/db/version_0_1_2/201511031926_fix_map_squares.sql
+++ modules/cache_builder/db/version_0_1_2/201511031926_fix_map_squares.sql
@@ -29,27 +29,27 @@ OR msq10.y <> round(st_y(st_centroid(reduce_precision(coalesce(s.geom, l.centroi
 -- fill in the map square IDs for any where the squares already exist
 update tofix
 set msq_1k_id=msq.id
-from map_squares msq 
+from map_squares msq
 where msq.x=tofix.x1000 and msq.y=tofix.y1000 and msq.size=tofix.size1000;
 
 update tofix
 set msq_2k_id=msq.id
-from map_squares msq 
+from map_squares msq
 where msq.x=tofix.x2000 and msq.y=tofix.y2000 and msq.size=tofix.size2000;
 
 update tofix
 set msq_10k_id=msq.id
-from map_squares msq 
+from map_squares msq
 where msq.x=tofix.x10000 and msq.y=tofix.y10000 and msq.size=tofix.size10000;
 
 -- THE FOLLOWING VIOLATES UNIQUE INDEXES BUT IT SHOULDN'T Probably because the square size is set to 1000 in one case, but GREATES(...) in another?
 /*
-Thw square size for msq_1km_id might point to a map square for 10000 size if it is sensitive etc. 
+Thw square size for msq_1km_id might point to a map square for 10000 size if it is sensitive etc.
 */
 
 -- create the missing squares
 INSERT INTO map_squares (geom, x, y, size)
-SELECT distinct reduce_precision(s.geom, o.confidential, GREATEST(round(sqrt(st_area(st_transform(s.geom, sref_system_to_srid(s.entered_sref_system)))))::integer, o.sensitivity_precision, s.privacy_precision, 1000), s.entered_sref_system), 
+SELECT distinct reduce_precision(s.geom, o.confidential, GREATEST(round(sqrt(st_area(st_transform(s.geom, sref_system_to_srid(s.entered_sref_system)))))::integer, o.sensitivity_precision, s.privacy_precision, 1000), s.entered_sref_system),
     tofix.x1000, tofix.y1000, GREATEST(round(sqrt(st_area(st_transform(s.geom, sref_system_to_srid(s.entered_sref_system)))))::integer, o.sensitivity_precision, s.privacy_precision, 1000)
 FROM tofix
 join occurrences o on o.id=tofix.id
@@ -57,7 +57,7 @@ join samples s on s.id=o.sample_id
 where tofix.msq_1k_id is null;
 
 INSERT INTO map_squares (geom, x, y, size)
-SELECT distinct reduce_precision(s.geom, o.confidential, GREATEST(round(sqrt(st_area(st_transform(s.geom, sref_system_to_srid(s.entered_sref_system)))))::integer, o.sensitivity_precision, s.privacy_precision, 2000), s.entered_sref_system), 
+SELECT distinct reduce_precision(s.geom, o.confidential, GREATEST(round(sqrt(st_area(st_transform(s.geom, sref_system_to_srid(s.entered_sref_system)))))::integer, o.sensitivity_precision, s.privacy_precision, 2000), s.entered_sref_system),
     tofix.x2000, tofix.y2000, GREATEST(round(sqrt(st_area(st_transform(s.geom, sref_system_to_srid(s.entered_sref_system)))))::integer, o.sensitivity_precision, s.privacy_precision, 2000)
 FROM tofix
 join occurrences o on o.id=tofix.id
@@ -66,7 +66,7 @@ left join map_squares msq on msq.x=tofix.x2000 and msq.y=tofix.y2000 and msq.siz
 where tofix.msq_2k_id is null and msq.id is null and s.geom is not null;
 
 INSERT INTO map_squares (geom, x, y, size)
-SELECT distinct reduce_precision(s.geom, o.confidential, GREATEST(round(sqrt(st_area(st_transform(s.geom, sref_system_to_srid(s.entered_sref_system)))))::integer, o.sensitivity_precision, s.privacy_precision, 10000), s.entered_sref_system), 
+SELECT distinct reduce_precision(s.geom, o.confidential, GREATEST(round(sqrt(st_area(st_transform(s.geom, sref_system_to_srid(s.entered_sref_system)))))::integer, o.sensitivity_precision, s.privacy_precision, 10000), s.entered_sref_system),
     tofix.x10000, tofix.y10000, GREATEST(round(sqrt(st_area(st_transform(s.geom, sref_system_to_srid(s.entered_sref_system)))))::integer, o.sensitivity_precision, s.privacy_precision, 10000)
 FROM tofix
 join occurrences o on o.id=tofix.id
@@ -77,19 +77,19 @@ where tofix.msq_10k_id is null and msq.id is null and s.geom is not null;
 -- now link up the missing squares
 update tofix
 set msq_1k_id=msq.id
-from map_squares msq 
+from map_squares msq
 where msq.x=tofix.x1000 and msq.y=tofix.y1000 and msq.size=tofix.size1000
 and msq_1k_id is null;
 
 update tofix
 set msq_2k_id=msq.id
-from map_squares msq 
+from map_squares msq
 where msq.x=tofix.x2000 and msq.y=tofix.y2000 and msq.size=tofix.size2000
 and msq_2k_id is null;
 
 update tofix
 set msq_10k_id=msq.id
-from map_squares msq 
+from map_squares msq
 where msq.x=tofix.x10000 and msq.y=tofix.y10000 and msq.size=tofix.size10000
 and msq_10k_id is null;
 
@@ -100,7 +100,7 @@ from tofix
 where tofix.id=o.id;
 
 -- remove any unwanted map square records
-delete from map_squares 
+delete from map_squares
 where id in (
 select msq.id from map_squares msq
 left join cache_occurrences o on o.map_sq_1km_id=msq.id
@@ -114,3 +114,5 @@ select msq.id from map_squares msq
 left join cache_occurrences o on o.map_sq_10km_id=msq.id
 where o.id is null
 );
+
+drop table tofix;
