d74526114b157d01df80117cb907b21e1e868b3c
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-11-06 14:52:50 +0000

    Linting

--- modules/workflow/controllers/workflow_metadata.php
+++ modules/workflow/controllers/workflow_metadata.php
@@ -28,29 +28,35 @@ class Workflow_metadata_Controller extends Gridview_Base_Controller {
 
   public function __construct() {
     parent::__construct('workflow_metadata');
-    $this->columns = array('id'=>'ID',
-                           'entity'=>'Entity',
-                           'key'=>'Key',
-                           'key_value'=>'Key value');
+    $this->columns = array(
+      'id' => 'ID',
+      'entity' => 'Entity',
+      'key' => 'Key',
+      'key_value' => 'Key value'
+    );
     $this->pagetitle = 'Workflow module metadata specification';
   }
 
   protected function get_action_columns() {
     return array(
-      array('caption'=>'Edit',
-            'url'=>'workflow_metadata/edit/{id}'));
+      array(
+        'caption' => 'Edit',
+        'url' => 'workflow_metadata/edit/{id}'
+      )
+    );
   }
 
-  protected function prepareOtherViewData($values)
-  {
+  protected function prepareOtherViewData($values) {
     $config = kohana::config('workflow');
     $entitySelectItems = array();
 
-    foreach($config['entities'] as $entity => $entityDef){
+    foreach ($config['entities'] as $entity => $entityDef) {
       $entitySelectItems[$entity] = $entityDef['title'];
     }
-    return array('entities' => $config['entities'],
-                 'entitySelectItems' => $entitySelectItems);
+    return array(
+      'entities' => $config['entities'],
+      'entitySelectItems' => $entitySelectItems
+    );
   }
 
   /**
@@ -60,17 +66,17 @@ class Workflow_metadata_Controller extends Gridview_Base_Controller {
     return $this->auth->logged_in('CoreAdmin');
   }
 
-  protected function show_submit_fail()
-  {
-      $all_errors=$this->model->getAllErrors();
-      if (count($all_errors)!=0) {
-          $this->session->set_flash('flash_error', implode('<br/>',$all_errors));
-      } else {
-          $this->session->set_flash('flash_error', 'The record could not be saved.');
-      }
-      $values = $this->getDefaults();
-      $values = array_merge($values, $_POST);
-      $this->showEditPage($values);
+  protected function show_submit_fail() {
+    $all_errors = $this->model->getAllErrors();
+    if (count($all_errors) !== 0) {
+      $this->session->set_flash('flash_error', implode('<br/>', $all_errors));
+    }
+    else {
+      $this->session->set_flash('flash_error', 'The record could not be saved.');
+    }
+    $values = $this->getDefaults();
+    $values = array_merge($values, $_POST);
+    $this->showEditPage($values);
   }
-  
+
 }
