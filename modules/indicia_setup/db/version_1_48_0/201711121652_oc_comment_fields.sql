bd827f95e31ed7aab5d15acedd0806ec7afbc722
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-11-20 14:55:11 +0000

    Typo fixed

--- modules/indicia_setup/db/version_1_48_0/201711121652_oc_comment_fields.sql
+++ modules/indicia_setup/db/version_1_48_0/201711121652_oc_comment_fields.sql
@@ -1,6 +1,6 @@
-ALTER TABLE occurrence_comments ADD COLUMN correspondance_data text;
-COMMENT ON COLUMN occurrence_comments.correspondance_data IS
-  'Stores correspondance data related to the comment in JSON format. Typically this will be the sender, recipient and body of an email or the URL of a social media post.';
+ALTER TABLE occurrence_comments ADD COLUMN correspondence_data text;
+COMMENT ON COLUMN occurrence_comments.correspondence_data IS
+  'Stores correspondence data related to the comment in JSON format. Typically this will be the sender, recipient and body of an email or the URL of a social media post.';
 
 ALTER TABLE occurrence_comments ADD COLUMN reference text;
 COMMENT ON COLUMN occurrence_comments.reference IS
