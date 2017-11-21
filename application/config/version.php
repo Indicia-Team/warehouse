b0126e9af25830a428dec593158bf8a89506ab37
Andrew van Breda <support@avb-it.co.uk>
2017-11-21 12:07:28 +0000

    Increment version

--- application/config/version.php
+++ application/config/version.php
@@ -31,13 +31,13 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '1.47.4';
+$config['version'] = '1.48.0’;
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2017-11-10';
+$config['release_date'] = '2017-11-21’;
 /**
  * Link to the code repository downloads page.
  *
