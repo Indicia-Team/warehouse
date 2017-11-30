09a3a06de552465d19c1d0df0c71ff3ae97afbc9
Andrew van Breda <support@avb-it.co.uk>
2017-11-30 17:28:53 +0000

    NPMS user details report now has new data access column

--- application/config/version.php
+++ application/config/version.php
@@ -31,13 +31,13 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '1.51.0';
+$config['version'] = '1.52.0';
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2017-11-29';
+$config['release_date'] = '2017-11-30';
 
 /**
  * Link to the code repository downloads page.
