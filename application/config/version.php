6088e828bd09bb82c2a50c3781afc440e43883ee
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-12-01 16:04:40 +0000

    Workflow reports understand that queried records are not overdue a check.

--- application/config/version.php
+++ application/config/version.php
@@ -37,7 +37,7 @@ $config['version'] = '1.52.1';
  *
  * @var string
  */
-$config['release_date'] = '2017-11-30';
+$config['release_date'] = '2017-12-01';
 
 /**
  * Link to the code repository downloads page.
