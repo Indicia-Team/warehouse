0de6355126a8206f8eb0deb8e2fb3b1f44e3e9f0
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-12-22 14:20:02 +0000

    Linting/tidying

--- modules/survey_structure_export/views/survey_structure_export/index.php
+++ modules/survey_structure_export/views/survey_structure_export/index.php
@@ -1,36 +1,34 @@
-<p>This tab allows you to generate a piece of text which describes the structure of the custom attributes associated with 
+<p>This tool allows you to generate a piece of text which describes the structure of the custom attributes associated with
 a survey. The text generated can be used to recreate the same attribute structure on another survey on this warehouse
 or on another warehouse. It is therefore ideal for migrating development or test versions of your surveys to the live
 warehouse server.</p>
 <?php
-require_once(DOCROOT.'client_helpers/data_entry_helper.php');
-echo data_entry_helper::textarea(array(
-  'label'=>'Exported survey structure',
-  'fieldname'=>'export survey structure',
-  'cols'=>100,
-  'rows'=>8,
-  'helpText' => 'Copy this text to the clipboard. You can then paste it into another survey on this or another warehouse to clone the attributes.',
-  'default' => $export
-));
+warehouse::loadHelpers(['data_entry_helper']);
+echo data_entry_helper::textarea([
+  'label' => 'Exported survey structure',
+  'fieldname' => 'export survey structure',
+  'helpText' => 'Copy this text to the clipboard. You can then paste it into another survey on this or another ' .
+    'warehouse to clone the attributes.',
+  'default' => $export,
+]);
 ?>
-<form class="iform" action="<?php echo url::site(); ?>survey_structure_export/save" method="post" id="entry-form"">
-<fieldset>
-<?php
-echo data_entry_helper::textarea(array(
-  'label'=>'Import survey structure',
-  'fieldname'=>'import survey structure',
-  'cols'=>100,
-  'rows'=>8,
-  'helpText' => 'Paste in the export of another survey to import its attributes.'
-));
-echo data_entry_helper::hidden_text(array(
-  'fieldname'=>'survey_id',
-  'default'=>$surveyId
-));
+<form id="survey-structure-import" action="<?php echo url::site(); ?>survey_structure_export/save" method="post" id="entry-form"">
+  <fieldset>
+    <?php
+    echo data_entry_helper::textarea(array(
+      'label' => 'Import survey structure',
+      'fieldname' => 'import survey structure',
+      'helpText' => 'Paste in the export of another survey to import its attributes.',
+      'validation' => ['required'],
+    ));
+    echo data_entry_helper::hidden_text(array(
+      'fieldname' => 'survey_id',
+      'default' => $surveyId,
+    ));
 
-echo '<input type="submit" name="submit" value="Import" class="ui-corner-all ui-state-default button ui-priority-primary" />'."\n"; 
-data_entry_helper::link_default_stylesheet();
-echo data_entry_helper::dump_javascript();
-?>
-</fieldset>
+    echo '<input type="submit" name="submit" value="Import" class="ui-corner-all ui-state-default button ui-priority-primary" />' . "\n";
+    data_entry_helper::enable_validation('survey-structure-import');
+    echo data_entry_helper::dump_javascript();
+    ?>
+  </fieldset>
 </form>
--- modules/survey_structure_export/views/termlist_export/index.php
+++ modules/survey_structure_export/views/termlist_export/index.php
@@ -1,35 +1,32 @@
-<p>This tab allows you to generate a piece of text which describes the terms in a termlist. The text generated can
+<p>This tool allows you to generate a piece of text which describes the terms in a termlist. The text generated can
   be used to recreate the same termlist on another warehouse. It is therefore ideal for migrating development
   or test versions of your surveys to the live warehouse server.</p>
 <?php
-require_once(DOCROOT.'client_helpers/data_entry_helper.php');
+warehouse::loadHelpers(['data_entry_helper']);
 echo data_entry_helper::textarea(array(
-  'label'=>'Exported termlist contents',
-  'fieldname'=>'export_termlist_contents',
-  'cols'=>100,
-  'rows'=>8,
+  'label' => 'Exported termlist contents',
+  'fieldname' => 'export_termlist_contents',
   'helpText' => 'Copy this text to the clipboard. You can then paste it into another termlist on another warehouse to clone the content.',
-  'default' => $export
+  'default' => $export,
 ));
 ?>
-<form class="iform" action="<?php echo url::site(); ?>termlist_export/save" method="post" id="entry-form"">
-<fieldset>
-<?php
-echo data_entry_helper::textarea(array(
-  'label'=>'Import termlist contents',
-  'fieldname'=>'import_termlist_contents',
-  'cols'=>100,
-  'rows'=>8,
-  'helpText' => 'Paste in the export of another termlist to import its terms.'
-));
-echo data_entry_helper::hidden_text(array(
-  'fieldname'=>'termlist_id',
-  'default'=>$termlistId
-));
+<form id="termlist_structure_export" action="<?php echo url::site(); ?>termlist_export/save" method="post" id="entry-form"">
+  <fieldset>
+    <?php
+    echo data_entry_helper::textarea(array(
+      'label' => 'Import termlist contents',
+      'fieldname' => 'import_termlist_contents',
+      'helpText' => 'Paste in the export of another termlist to import its terms.',
+      'validation' => ['required'],
+    ));
+    echo data_entry_helper::hidden_text(array(
+      'fieldname' => 'termlist_id',
+      'default' => $termlistId,
+    ));
 
-echo '<input type="submit" name="submit" value="Import" class="ui-corner-all ui-state-default button ui-priority-primary" />'."\n"; 
-data_entry_helper::link_default_stylesheet();
-echo data_entry_helper::dump_javascript();
-?>
-</fieldset>
+    echo '<input type="submit" name="submit" value="Import" class="ui-corner-all ui-state-default button ui-priority-primary" />' . "\n";
+    data_entry_helper::link_default_stylesheet();
+    echo data_entry_helper::dump_javascript();
+    ?>
+  </fieldset>
 </form>
