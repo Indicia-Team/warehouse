e462fd1399f6db1190c3992598b5bb11a98887ba
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-01-31 15:56:06 +0000

    Linting

--- modules/data_cleaner_identification_difficulty/plugins/data_cleaner_identification_difficulty.php
+++ modules/data_cleaner_identification_difficulty/plugins/data_cleaner_identification_difficulty.php
@@ -1,5 +1,9 @@
 <?php
+
 /**
+ * @file
+ * Plugin methods for the idenfication difficulty rule.
+ *
  * Indicia, the OPAL Online Recording Toolkit.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -13,60 +17,68 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
  *
- * @package	Data Cleaner
- * @subpackage Plugins
- * @author	Indicia Team
- * @license	http://www.gnu.org/licenses/gpl.html GPL
- * @link 	http://code.google.com/p/indicia/
+ * @author Indicia Team
+ * @license http://www.gnu.org/licenses/gpl.html GPL
+ * @link http://code.google.com/p/indicia/
  */
 
 /**
- * Hook into the data cleaner to declare checks for the difficulty of identification
- * of a species.
- * @return type array of rules.
+ * Returns definitions of the rule queries.
+ *
+ * Hook into the data cleaner to declare checks for the difficulty of
+ * identification of a species.
+ *
+ * @return array
+ *   array of rules.
  */
 function data_cleaner_identification_difficulty_data_cleaner_rules() {
   return array(
     'testType' => 'IdentificationDifficulty',
-    'optional' => array('Data'=>array('*'), 'Taxa'=>array('*'), 'INI'=>array('*')),
+    'optional' => array(
+      'Data' => array('*'),
+      'Taxa' => array('*'),
+      'INI' => array('*'),
+    ),
     'errorMsgField' => 'vrdini.value',
     'queries' => array(
-      // test where the rule is keyed by TVK
-      array(
-        'joins' => 
-          "join cache_taxa_taxon_lists cttl on cttl.id=co.taxa_taxon_list_id ".
-          "join verification_rule_data vrd on vrd.header_name='Data' and vrd.key = co.taxa_taxon_list_external_key and vrd.deleted=false ".
-          "join verification_rules vr on vr.id=vrd.verification_rule_id and vr.test_type='IdentificationDifficulty' and vr.deleted=false ".
-          "join verification_rule_data vrdini on vrdini.verification_rule_id=vr.id and vrdini.header_name='INI' and vrdini.key=vrd.value and cast(vrdini.key as int)>1 and vrdini.deleted=false",
-        'subtypeField' => 'vrdini.key'
-      ),
-      // repeat test where the rule is keyed by species name
+      // Test where the rule is keyed by TVK.
       array(
         'joins' =>
-          "join cache_taxa_taxon_lists cttl on cttl.id=co.taxa_taxon_list_id ".
-          "join verification_rule_data vrd on vrd.header_name='Taxa' and vrd.key = cttl.preferred_taxon and vrd.deleted=false ".
-          "join verification_rules vr on vr.id=vrd.verification_rule_id and vr.test_type='IdentificationDifficulty' and vr.deleted=false ".
+          "join cache_taxa_taxon_lists cttl on cttl.id=co.taxa_taxon_list_id " .
+          "join verification_rule_data vrd on vrd.header_name='Data' and vrd.key = co.taxa_taxon_list_external_key and vrd.deleted=false " .
+          "join verification_rules vr on vr.id=vrd.verification_rule_id and vr.test_type='IdentificationDifficulty' and vr.deleted=false " .
           "join verification_rule_data vrdini on vrdini.verification_rule_id=vr.id and vrdini.header_name='INI' and vrdini.key=vrd.value and cast(vrdini.key as int)>1 and vrdini.deleted=false",
-        'subtypeField' => 'vrdini.key'
+        'subtypeField' => 'vrdini.key',
+      ),
+      // Repeat test where the rule is keyed by species name.
+      array(
+        'joins' =>
+          "join cache_taxa_taxon_lists cttl on cttl.id=co.taxa_taxon_list_id " .
+          "join verification_rule_data vrd on vrd.header_name='Taxa' and vrd.key = cttl.preferred_taxon and vrd.deleted=false " .
+          "join verification_rules vr on vr.id=vrd.verification_rule_id and vr.test_type='IdentificationDifficulty' and vr.deleted=false " .
+          "join verification_rule_data vrdini on vrdini.verification_rule_id=vr.id and vrdini.header_name='INI' and vrdini.key=vrd.value and cast(vrdini.key as int)>1 and vrdini.deleted=false",
+        'subtypeField' => 'vrdini.key',
       )
     )
   );
 }
 
-/** 
- * Taxon version keys should really be uppercase, so enforce this. Otherwise the query needs to be case insensitive which makes it slow.
- * Also, we need to store the identification difficulty results into cache_taxon_searchterms so they are available when searching
- * for taxa.
+/**
+ * Postprocess saved rules.
+ *
+ * Taxon version keys should really be uppercase, so enforce this. Otherwise
+ * the query needs to be case insensitive which makes it slow. Also, we need to
+ * store the identification difficulty results into cache_taxon_searchterms so
+ * they are available when searching for taxa.
  */
 function data_cleaner_identification_difficulty_data_cleaner_postprocess($id, $db) {
   $db->query("update verification_rule_data set key=upper(key) where header_name='Data' and key<>upper(key) and verification_rule_id=$id");
   $db->query("update cache_taxon_searchterms set identification_difficulty=null, id_diff_verification_rule_id=null " .
-      "where id_diff_verification_rule_id=$id"); 
+      "where id_diff_verification_rule_id=$id");
   $db->query("update cache_taxon_searchterms cts " .
-      "set identification_difficulty=vrd.value::integer, id_diff_verification_rule_id=vrd.verification_rule_id ".
-      "from cache_taxa_taxon_lists cttl ".
-      "join verification_rule_data vrd on vrd.header_name='Data' and upper(vrd.key)=cttl.external_key and vrd.deleted=false ".
-      "join verification_rules vr on vr.id=vrd.verification_rule_id and vr.deleted=false ".
-      "where cttl.id=cts.preferred_taxa_taxon_list_id ".
-      "and vr.id=$id");
+      "set identification_difficulty=vrd.value::integer, id_diff_verification_rule_id=vrd.verification_rule_id " .
+      "from cache_taxa_taxon_lists cttl " .
+      "join verification_rule_data vrd on vrd.header_name='Data' and upper(vrd.key)=cttl.external_key and vrd.deleted=false " .
+      "join verification_rules vr on vr.id=vrd.verification_rule_id and vr.deleted=false " .
+      "where cttl.id=cts.preferred_taxa_taxon_list_id and vr.id=$id");
 }
