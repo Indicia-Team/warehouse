9ba9a5baa1425457975d8ef87c6001dc404191f0
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-02-13 16:44:21 +0000

    Add Jersey reference systems

--- application/config/sref_notations.php
+++ application/config/sref_notations.php
@@ -27,25 +27,26 @@ defined('SYSPATH') or die('No direct script access.');
  * List of supported spatial reference notations which are just straight translations to an x,y
  * or lat long format. Any other notations (e.g. grids) need a module to handle the grid notation.
  */
-$config['sref_notations'] = array
-(
-  '4326' =>'Latitude and Longitude (WGS84)',
-  '4277' =>'Latitude and Longitude (OSGB36)',
-  '27700' =>'British National Grid Easting/Northing',
-  '2169' =>'Gauss Luxembourg',
-  '27572'=>'NTF (Paris) / Lambert zone II',
-  '31370' => 'Belgian Lambert 72'
+$config['sref_notations'] = array(
+  '4326' => 'Latitude and Longitude (WGS84)',
+  '4277' => 'Latitude and Longitude (OSGB36)',
+  '27700' => 'British National Grid Easting/Northing',
+  '2169' => 'Gauss Luxembourg',
+  '27572' => 'NTF (Paris) / Lambert zone II',
+  '31370' => 'Belgian Lambert 72',
+  '3109' => 'ETRS89 / Jersey Transverse Mercator',
+  '23030' => 'ED50 / UTM zone 30N',
 );
 
 // Set the internally stored geoms to use spherical mercator projection
-$config['internal_srid']=900913;
+$config['internal_srid'] = 900913;
 
 // For each known SRID, specify a rounding value to set the number of digits usually given after the decimal place.
 $config['roundings'] = array
 (
-  '4326'=>5,
-  '4277'=>5,
-  '2169'=>0
+  '4326' => 5,
+  '4277' => 5,
+  '2169' => 0
 );
 
 // provide a list of systems which translate x,y format into a proper Lat/Long format, and the default ouput format
