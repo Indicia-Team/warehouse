01b0a24461e34c3a6dcdb015ff7ac3a0c599f89f
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-02-16 15:58:03 +0000

    Script for localisation of attribute caption db field

---
+++ modules/indicia_setup/db/version_1_56_0/201820161544_attr_caption_i18n.sql
@@ -0,0 +1,24 @@
+ALTER TABLE sample_attributes
+   ADD COLUMN caption_i18n jsonb;
+COMMENT ON COLUMN sample_attributes.caption_i18n
+  IS 'Stores a list of localised versions of the caption keyed by language code.';
+
+ALTER TABLE occurrence_attributes
+   ADD COLUMN caption_i18n jsonb;
+COMMENT ON COLUMN occurrence_attributes.caption_i18n
+  IS 'Stores a list of localised versions of the caption keyed by language code.';
+
+ALTER TABLE location_attributes
+   ADD COLUMN caption_i18n jsonb;
+COMMENT ON COLUMN location_attributes.caption_i18n
+  IS 'Stores a list of localised versions of the caption keyed by language code.';
+
+ALTER TABLE taxa_taxon_list_attributes
+   ADD COLUMN caption_i18n jsonb;
+COMMENT ON COLUMN taxa_taxon_list_attributes.caption_i18n
+  IS 'Stores a list of localised versions of the caption keyed by language code.';
+
+ALTER TABLE termlists_term_attributes
+   ADD COLUMN caption_i18n jsonb;
+COMMENT ON COLUMN termlists_term_attributes.caption_i18n
+  IS 'Stores a list of localised versions of the caption keyed by language code.';
