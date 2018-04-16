91c8ed36d39c6367f85c83bd45a267538daae015
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-04-16 14:33:43 +0100

    Can't fetch a record count if need parameters
    Prevents a potential error when query runs to count without parameters.

--- modules/indicia_svc_data/controllers/data_service_base.php
+++ modules/indicia_svc_data/controllers/data_service_base.php
@@ -197,7 +197,7 @@ META;
       $array['columns'] = $this->view_columns;
     if ($wantParameters && isset($data['parameterRequest']))
       $array['parameterRequest'] = $data['parameterRequest'];
-    if ($wantCount) {
+    if ($wantCount && !isset($data['parameterRequest'])) {
       $count = $this->record_count();
       if ($count !== FALSE)
         $array['count'] = $count;
