fb7bf5a17e15d1fca91f0e90e8abd4ad5cc60c8b
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-05-02 12:37:56 +0100

    Survey attribute table missed out from allow_ranges updates.

--- modules/indicia_setup/db/version_1_60_0/201804251042_attr_value_ranges.sql
+++ modules/indicia_setup/db/version_1_60_0/201804251042_attr_value_ranges.sql
@@ -1,3 +1,5 @@
+ALTER TABLE survey_attributes
+  ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
 ALTER TABLE sample_attributes
   ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
 ALTER TABLE occurrence_attributes
@@ -11,6 +13,7 @@ ALTER TABLE termlists_term_attributes
 ALTER TABLE person_attributes
   ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
 
+COMMENT ON COLUMN survey_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
 COMMENT ON COLUMN sample_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
 COMMENT ON COLUMN occurrence_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
 COMMENT ON COLUMN location_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
@@ -18,6 +21,8 @@ COMMENT ON COLUMN taxa_taxon_list_attributes.allow_ranges IS 'Set to true if num
 COMMENT ON COLUMN termlists_term_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
 COMMENT ON COLUMN person_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
 
+ALTER TABLE survey_attribute_values
+  ADD COLUMN upper_value float;
 ALTER TABLE sample_attribute_values
   ADD COLUMN upper_value float;
 ALTER TABLE occurrence_attribute_values
@@ -31,6 +36,7 @@ ALTER TABLE termlists_term_attribute_values
 ALTER TABLE person_attribute_values
   ADD COLUMN upper_value float;
 
+COMMENT ON COLUMN survey_attribute_values.upper_value IS 'If the attribute allows value ranges, then provides the upper value of the range.';
 COMMENT ON COLUMN sample_attribute_values.upper_value IS 'If the attribute allows value ranges, then provides the upper value of the range.';
 COMMENT ON COLUMN occurrence_attribute_values.upper_value IS 'If the attribute allows value ranges, then provides the upper value of the range.';
 COMMENT ON COLUMN location_attribute_values.upper_value IS 'If the attribute allows value ranges, then provides the upper value of the range.';
