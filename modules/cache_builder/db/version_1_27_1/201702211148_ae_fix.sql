28ad511f78c9853086f740bd42be3c66109b760c
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-05-18 12:03:31 +0100

    Fixed type of quotes preventing install from running

--- modules/cache_builder/db/version_1_27_1/201702211148_ae_fix.sql
+++ modules/cache_builder/db/version_1_27_1/201702211148_ae_fix.sql
@@ -10,4 +10,4 @@ SET searchterm = regexp_replace(regexp_replace(
 WHERE simplified=true
 AND searchterm <> regexp_replace(regexp_replace(
           lower( regexp_replace(original, E'\\(.+\\)', '', 'g') || coalesce(authority, '') ), 'ae', 'e', 'g'
-        ), E'[^a-z0-9\\?\\+]', '', ‘g’);
+        ), E'[^a-z0-9\\?\\+]', '', 'g');
