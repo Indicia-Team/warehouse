be84706834601eac36bb367f555c095f436b68c9
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-05-29 10:37:33 +0100

    Remove 1.60 folders

--- modules/indicia_setup/db/version_1_60_0/201805181100_grid_views.sql
+++
@@ -1,7 +0,0 @@
-DROP VIEW gv_taxon_lists;
-
-CREATE VIEW gv_taxon_lists AS
- SELECT t.id, t.title, t.website_id, w.title as website_title, t.parent_id, t.description
-   FROM taxon_lists t
-   LEFT JOIN websites w on w.id=t.website_id and w.deleted=false
-  WHERE t.deleted = false;
