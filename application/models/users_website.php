9bc55afdfa92ed9ddd4c865be0798dbd5270c5a2
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-05-29 10:58:10 +0100

    Tidy code layout.

--- application/models/users_website.php
+++ application/models/users_website.php
@@ -47,39 +47,52 @@ class Users_website_Model extends ORM
     // uses PHP trim() to remove whitespace from beginning and end of all fields before validation
     $array->pre_filter('trim');
 
-    $this->unvalidatedFields = array('user_id', 'website_id', 'site_role_id', 'licence_id');
+    $this->unvalidatedFields = array(
+      'user_id',
+      'website_id',
+      'site_role_id',
+      'licence_id',
+      'media_licence_id',
+    );
     return parent::validate($array, $save);
   }
 
   public function applyLicence($new) {
     // Are we applying a first time licence for records belonging to this user?
     if (!empty($new['licence_id']) && empty($this->licence_id)) {
-      $this->db->query("update samples s" .
-        " set licence_id=$new[licence_id]" .
-        " from surveys su" .
-        " where su.website_id=$new[website_id]" .
-        " and s.created_by_id=$new[user_id]" .
-        " and su.id=s.survey_id" .
-        " and licence_id is null"
-      );
-      $this->db->query("update cache_occurrences_functional o" .
-        " set licence_id=l.id" .
-        " from licences l" .
-        " where o.website_id=$new[website_id]" .
-        " and o.created_by_id=$new[user_id]" .
-        " and o.licence_id is null" .
-        " and l.id=$new[licence_id]"
-      );
-      $this->db->query("update cache_occurrences_nonfunctional onf" .
-        " set licence_code=l.code" .
-        " from licences l, cache_occurrences_functional o " .
-        " where o.id=onf.id" .
-        " and o.website_id=$new[website_id]" .
-        " and o.created_by_id=$new[user_id]" .
-        " and coalesce(onf.licence_code, '')<>l.code" .
-        " and o.licence_id=l.id" .
-        " and l.id=$new[licence_id]"
-      );
+      $sql = <<<SQL
+update samples s
+set licence_id=$new[licence_id]
+from surveys su
+where su.website_id=$new[website_id]
+and s.created_by_id=$new[user_id]
+and su.id=s.survey_id
+and licence_id is null;
+SQL;
+      $this->db->query($sql);
+      $sql = <<<SQL
+update cache_occurrences_functional o
+set licence_id=l.id
+from licences l
+where o.website_id=$new[website_id]
+and o.created_by_id=$new[user_id]
+and o.licence_id is null
+and l.id=$new[licence_id]
+SQL;
+      $this->db->query($sql);
+      $sql = <<<SQL
+update cache_occurrences_nonfunctional onf
+set licence_code=l.code
+from licences l, cache_occurrences_functional o
+where o.id=onf.id
+and o.website_id=$new[website_id]
+and o.created_by_id=$new[user_id]
+and coalesce(onf.licence_code, '')<>l.code
+and o.licence_id=l.id
+and l.id=$new[licence_id]
+SQL;
+      $this->db->query($sql);
     }
   }
+
 }
