91b787d6af1e73e4e5ac2be9110a0d28aff1e630
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-05-29 10:36:26 +0100

    Switched feature version to 1.61 since code not tested yet
    Need to use 1.60 version for licence/GDPR related changes

---
+++ modules/indicia_setup/db/version_1_61_0/201804251042_attr_value_ranges.sql
@@ -0,0 +1,82 @@
+ALTER TABLE survey_attributes
+  ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
+ALTER TABLE sample_attributes
+  ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
+ALTER TABLE occurrence_attributes
+  ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
+ALTER TABLE location_attributes
+  ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
+ALTER TABLE taxa_taxon_list_attributes
+  ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
+ALTER TABLE termlists_term_attributes
+  ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
+ALTER TABLE person_attributes
+  ADD COLUMN allow_ranges boolean NOT NULL DEFAULT false;
+
+COMMENT ON COLUMN survey_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
+COMMENT ON COLUMN sample_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
+COMMENT ON COLUMN occurrence_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
+COMMENT ON COLUMN location_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
+COMMENT ON COLUMN taxa_taxon_list_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
+COMMENT ON COLUMN termlists_term_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
+COMMENT ON COLUMN person_attributes.allow_ranges IS 'Set to true if numeric attributes allow a value range to be entered.';
+
+ALTER TABLE survey_attribute_values
+  ADD COLUMN upper_value float;
+ALTER TABLE sample_attribute_values
+  ADD COLUMN upper_value float;
+ALTER TABLE occurrence_attribute_values
+  ADD COLUMN upper_value float;
+ALTER TABLE location_attribute_values
+  ADD COLUMN upper_value float;
+ALTER TABLE taxa_taxon_list_attribute_values
+  ADD COLUMN upper_value float;
+ALTER TABLE termlists_term_attribute_values
+  ADD COLUMN upper_value float;
+ALTER TABLE person_attribute_values
+  ADD COLUMN upper_value float;
+
+COMMENT ON COLUMN survey_attribute_values.upper_value IS
+  'If the attribute allows value ranges, then provides the upper value of the range.';
+COMMENT ON COLUMN sample_attribute_values.upper_value IS
+  'If the attribute allows value ranges, then provides the upper value of the range.';
+COMMENT ON COLUMN occurrence_attribute_values.upper_value IS
+  'If the attribute allows value ranges, then provides the upper value of the range.';
+COMMENT ON COLUMN location_attribute_values.upper_value IS
+  'If the attribute allows value ranges, then provides the upper value of the range.';
+COMMENT ON COLUMN taxa_taxon_list_attribute_values.upper_value IS
+  'If the attribute allows value ranges, then provides the upper value of the range.';
+COMMENT ON COLUMN termlists_term_attribute_values.upper_value IS
+  'If the attribute allows value ranges, then provides the upper value of the range.';
+COMMENT ON COLUMN person_attribute_values.upper_value IS
+  'If the attribute allows value ranges, then provides the upper value of the range.';
+
+ALTER TABLE survey_attributes_websites
+  ADD COLUMN default_upper_value float;
+ALTER TABLE sample_attributes_websites
+  ADD COLUMN default_upper_value float;
+ALTER TABLE occurrence_attributes_websites
+  ADD COLUMN default_upper_value float;
+ALTER TABLE location_attributes_websites
+  ADD COLUMN default_upper_value float;
+ALTER TABLE taxon_lists_taxa_taxon_list_attributes
+  ADD COLUMN default_upper_value float;
+ALTER TABLE termlists_termlists_term_attributes
+  ADD COLUMN default_upper_value float;
+ALTER TABLE person_attributes_websites
+  ADD COLUMN default_upper_value float;
+
+COMMENT ON COLUMN survey_attributes_websites.default_upper_value IS
+  'If the attribute allows value ranges, then provides the default upper value of the range.';
+COMMENT ON COLUMN sample_attributes_websites.default_upper_value IS
+  'If the attribute allows value ranges, then provides the default upper value of the range.';
+COMMENT ON COLUMN occurrence_attributes_websites.default_upper_value IS
+  'If the attribute allows value ranges, then provides the default upper value of the range.';
+COMMENT ON COLUMN location_attributes_websites.default_upper_value IS
+  'If the attribute allows value ranges, then provides the default upper value of the range.';
+COMMENT ON COLUMN taxon_lists_taxa_taxon_list_attributes.default_upper_value IS
+  'If the attribute allows value ranges, then provides the default upper value of the range.';
+COMMENT ON COLUMN termlists_termlists_term_attributes.default_upper_value IS
+  'If the attribute allows value ranges, then provides the default upper value of the range.';
+COMMENT ON COLUMN person_attributes_websites.default_upper_value IS
+  'If the attribute allows value ranges, then provides the default upper value of the range.';
