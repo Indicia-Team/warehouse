77c4840924db7d20680500eccbb7251d86185968
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-05-30 13:33:38 +0100

    Switched to v2
    V2 because client code will not be 100% compatible, due to jQuery 3
    switch.

--- application/config/version.php
+++ application/config/version.php
@@ -29,14 +29,14 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '1.61.0';
+$config['version'] = '2.0.0';
 
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2018-05-29';
+$config['release_date'] = '2018-05-30';
 
 /**
  * Link to the code repository downloads page.
