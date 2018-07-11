584381e0a1005265426f0850e55f84c015934872
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-07-11 09:19:08 +0100

    Field name corrected

--- modules/cache_builder/db/version_2_0_0/201806220938_term_image_paths.sql
+++ modules/cache_builder/db/version_2_0_0/201806220938_term_image_paths.sql
@@ -1,5 +1,5 @@
 
 ALTER TABLE cache_termlists_terms
-  ADD COLUMN image_path character varying;
-COMMENT ON COLUMN cache_termlists_terms.image_path IS
+  ADD COLUMN preferred_image_path character varying;
+COMMENT ON COLUMN cache_termlists_terms.preferred_image_path IS
   'Path to an image file representing the term, e.g. an explanatory diagram.';
