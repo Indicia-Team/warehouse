b0c7e4ba61ac38ac4a8d54c6fcff0d79b7e0f737
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-07-30 13:51:53 +0100

    Fix fn so that editing flag set correctly

---
+++ modules/indicia_setup/db/version_2_0_0/201807301435_build_sharing_index_fn.sql
@@ -0,0 +1,49 @@
+CREATE OR REPLACE FUNCTION refresh_index_websites_website_agreements() RETURNS boolean AS $$
+BEGIN
+DELETE FROM index_websites_website_agreements i
+WHERE NOT EXISTS(SELECT 1 FROM build_index_websites_website_agreements bi
+WHERE bi.from_website_id=i.from_website_id AND bi.to_website_id=i.to_website_id);
+
+UPDATE index_websites_website_agreements i
+SET provide_for_reporting=bi.provide_for_reporting,
+    receive_for_reporting=bi.receive_for_reporting,
+    provide_for_peer_review=bi.provide_for_peer_review,
+    receive_for_peer_review=bi.receive_for_peer_review,
+    provide_for_verification=bi.provide_for_verification,
+    receive_for_verification=bi.receive_for_verification,
+    provide_for_data_flow=bi.provide_for_data_flow,
+    receive_for_data_flow=bi.receive_for_data_flow,
+    provide_for_moderation=bi.provide_for_moderation,
+    receive_for_moderation=bi.receive_for_moderation,
+    provide_for_editing=bi.provide_for_editing,
+    receive_for_editing=bi.receive_for_editing
+FROM build_index_websites_website_agreements bi
+WHERE i.from_website_id=bi.from_website_id AND i.to_website_id=bi.to_website_id
+AND (i.provide_for_reporting<>bi.provide_for_reporting
+ OR i.receive_for_reporting<>bi.receive_for_reporting
+ OR i.provide_for_peer_review<>bi.provide_for_peer_review
+ OR i.receive_for_peer_review<>bi.receive_for_peer_review
+ OR i.provide_for_verification<>bi.provide_for_verification
+ OR i.receive_for_verification<>bi.receive_for_verification
+ OR i.provide_for_data_flow<>bi.provide_for_data_flow
+ OR i.receive_for_data_flow<>bi.receive_for_data_flow
+ OR i.provide_for_moderation<>bi.provide_for_moderation
+ OR i.receive_for_moderation<>bi.receive_for_moderation
+ OR i.provide_for_editing<>bi.provide_for_editing
+ OR i.receive_for_editing<>bi.receive_for_editing);
+
+INSERT INTO index_websites_website_agreements
+SELECT nextval('index_websites_website_agreements_id_seq'::regclass), bi.from_website_id, bi.to_website_id,
+    bi.provide_for_reporting, bi.receive_for_reporting, bi.provide_for_peer_review, bi.receive_for_peer_review,
+    bi.provide_for_verification, bi.receive_for_verification, bi.provide_for_data_flow, bi.receive_for_data_flow,
+    bi.provide_for_moderation, bi.receive_for_moderation, bi.provide_for_editing, bi.receive_for_editing
+
+FROM build_index_websites_website_agreements bi
+LEFT JOIN index_websites_website_agreements i ON i.from_website_id=bi.from_website_id AND i.to_website_id=bi.to_website_id
+WHERE i.from_website_id IS NULL;
+
+RETURN true;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT refresh_index_websites_website_agreements();
