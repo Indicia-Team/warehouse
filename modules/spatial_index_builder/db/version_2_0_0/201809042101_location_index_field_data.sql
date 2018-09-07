06255aec7a0e7719423c731c19d6db251ca4e8f4
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-09-07 21:07:55 +0100

    A script to copy old indexed_location_samples data to new location_ids field.

--- modules/spatial_index_builder/db/version_2_0_0/201809042101_location_index_field_data.sql
+++ modules/spatial_index_builder/db/version_2_0_0/201809042101_location_index_field_data.sql
@@ -6,14 +6,9 @@ SET location_ids=(
   WHERE ils.sample_id=o.sample_id
 );
 
-UPDATE cache_samples_functional o
+UPDATE cache_samples_functional s
 SET location_ids=(
   SELECT array_agg(distinct ils.location_id)
   FROM index_locations_samples ils
   WHERE ils.sample_id=s.id
 );
-
-CREATE INDEX ix_cache_occurrences_functional_location_ids ON cache_occurrences_functional USING GIN(location_ids);
-CREATE INDEX ix_cache_samples_functional_location_ids ON cache_samples_functional USING GIN(location_ids);
-
-DROP TABLE index_locations_samples;
