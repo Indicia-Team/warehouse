e5eeb5764cbc224212480e2441dad24d663cde1e
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-09-11 12:50:29 +0100

    Version bump

--- application/config/version.php
+++ application/config/version.php
@@ -31,13 +31,13 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '1.60.17';
+$config['version'] = '1.60.18';
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2018-09-03';
+$config['release_date'] = '2018-09-11';
 
 /**
  * Link to the code repository downloads page.
