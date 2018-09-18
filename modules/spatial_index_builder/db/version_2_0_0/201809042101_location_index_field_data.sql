0068d4560966b0b78efc2299aae2c88656e4efff
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-09-18 08:45:57 +0100

    Improves performance on update script
    By disabling indexes, the update operation is much faster.

--- modules/spatial_index_builder/db/version_2_0_0/201809042101_location_index_field_data.sql
+++ modules/spatial_index_builder/db/version_2_0_0/201809042101_location_index_field_data.sql
@@ -1,14 +1,43 @@
 -- #slow script#
-UPDATE cache_occurrences_functional o
-SET location_ids=(
-  SELECT array_agg(distinct ils.location_id)
-  FROM index_locations_samples ils
-  WHERE ils.sample_id=o.sample_id
+
+-- Build a temp table with sample IDs mapped to their array of indexed locations.
+SELECT sample_id, array_agg(distinct location_id) as location_ids
+INTO temporary ils_temp
+FROM index_locations_samples
+GROUP BY sample_id;
+
+CREATE INDEX ix_ils_temp ON ils_temp(sample_id);
+
+-- Temporarily disable indexes for faster updates.
+UPDATE pg_index
+SET indisready=false
+WHERE indrelid in (
+    SELECT oid
+    FROM pg_class
+    WHERE relname in ('cache_occurrences_functional', 'cache_samples_functional')
 );
 
+UPDATE cache_occurrences_functional o
+SET location_ids=t.location_ids
+FROM ils_temp t
+WHERE t.sample_id=o.sample_id;
+
 UPDATE cache_samples_functional s
-SET location_ids=(
-  SELECT array_agg(distinct ils.location_id)
-  FROM index_locations_samples ils
-  WHERE ils.sample_id=s.id
+SET location_ids=t.location_ids
+FROM ils_temp t
+WHERE t.sample_id=s.id;
+
+-- Re-enable the indexes then rebuild them.
+UPDATE pg_index
+SET indisready=true
+WHERE indrelid in (
+    SELECT oid
+    FROM pg_class
+    WHERE relname in ('cache_occurrences_functional', 'cache_samples_functional')
 );
+
+REINDEX cache_occurrences_functional;
+REINDEX cache_samples_functional;
+
+-- Table no longer required.
+DROP TABLE index_locations_samples;
--- modules/spatial_index_builder/db/version_2_0_0/201809042102_drop_index_locations_samples.sql
+++
@@ -1,3 +0,0 @@
--- Table not required in v2 indexing strategy
---DROP TABLE index_locations_samples;
-select 1;
