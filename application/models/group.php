cb488c7d1a9a1fd3fb3c158c5364cb64bd2ad064
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-10-02 11:41:49 +0100

    Fixes a merge mistake

--- application/models/group.php
+++ application/models/group.php
@@ -73,86 +73,11 @@ where s.deleted=false and s.id=o.sample_id and s.group_id=$this->id";
    * suggest groups based on geographic region.
    */
   private function processIndexGroupsLocations() {
-    $filter = json_decode($this->filter->definition, true);
-    $exist = $this->db->select('id', 'location_id')
-        ->from('index_groups_locations')
-        ->where('group_id', $this->id)
-        ->get();
-
-    $location_ids = array();
-    // backwards compatibility
-    if (!empty($filter['indexed_location_id']) && empty($filter['indexed_location_list']))
-      $filter['indexed_location_list'] = $filter['location_id']; // backwards compatibility
-    if (!empty($filter['location_id']) && empty($filter['location_list']))
-      $filter['location_list'] = $filter['location_id']; // backwards compatibility
-
-    if (!empty($filter['indexed_location_list'])) {
-      // Got an indexed location as the filter boundary definition, so we can use that as it is.
-      $location_ids = explode(',', $filter['indexed_location_list']);
-    } elseif (!empty($filter['location_list']) || !empty($filter['searchArea'])) {
-      // got either an unindexed location, or a freehand boundary, so need to intersect to find the indexed locations
-      // Without a configuration for the indexed location type layers we can't go any further.
-      $config=kohana::config_load('spatial_index_builder', false);
-      if (array_key_exists('location_types', $config)) {
-        $types = "'".implode("','", $config['location_types'])."'";
-        if (!empty($filter['location_list'])) {
-          $rows = $this->db->query('select l.id from locations l ' .
-            'join locations search on ' .
-            '  st_intersects(st_buffer(coalesce(search.boundary_geom, search.centroid_geom), 0), st_buffer(coalesce(l.boundary_geom, l.centroid_geom), 0)) ' .
-            '  and not st_touches(st_buffer(coalesce(search.boundary_geom, search.centroid_geom), 0), st_buffer(coalesce(l.boundary_geom, l.centroid_geom), 0)) ' .
-            'join cache_termlists_terms t on t.id=l.location_type_id ' .
-            "where search.id in ($filter[location_list]) and t.preferred_term in ($types)")->result();
-        } else {
-          $srid = kohana::config('sref_notations.internal_srid');
-          $rows = $this->db->query('select l.id from locations l ' .
-            "join cache_termlists_terms t on t.id=l.location_type_id and t.preferred_term in ($types) " .
-            "where st_intersects(st_buffer(st_geomfromtext('$filter[searchArea]', $srid), 0), st_buffer(coalesce(l.boundary_geom, l.centroid_geom), 0)) " .
-            "and not st_touches(st_buffer(st_geomfromtext('$filter[searchArea]', $srid), 0), st_buffer(coalesce(l.boundary_geom, l.centroid_geom), 0)) "
-            )->result();
-        }
-        foreach ($rows as $row)
-          $location_ids[] = $row->id;
-      }
-    }
-    $foundExistingLocationIds = array();
-    // go through the existing index entries for this group. Remove any that are not needed now.
-    foreach($exist as $record) {
-      if (in_array($record->location_id, $location_ids)) {
-        // Got a correct one already. Remove the location ID from the list we want to add later
-        $key = array_search($record->location_id, $location_ids);
-        if ($key !== false)
-          unset($location_ids[$key]);
-        if (in_array($record->location_id, $foundExistingLocationIds)) {
-          // this one must exist twice in the index so clean it up.
-          $this->db->delete('index_groups_locations', array('id'=>$record->id));
-        } else {
-          $foundExistingLocationIds[] = $record->location_id;
-        }
-      } else {
-        // Got one we didn't actually want.
-        $this->db->delete('index_groups_locations', array('id'=>$record->id));
-      }
-    }
-    // Any remaining in our list now need to be added.
-    foreach ($location_ids as $location_id) {
-      $this->db->insert('index_groups_locations', array(
-          'group_id'=>$this->id,
-          'location_id'=>$location_id
-      ));
-    }
-  }
-
-  /**
-   * Method to populate the indexed taxon groups that this group intersects with. Makes it easy to do things like
-   * suggest groups based on species being recorded.
-   */
-  private function processIndexGroupsLocations() {
     $filter = json_decode($this->filter->definition, TRUE);
     $exist = $this->db->select('id', 'location_id')
       ->from('index_groups_locations')
       ->where('group_id', $this->id)
       ->get();
-
     $location_ids = array();
     // Backwards compatibility checks.
     if (!empty($filter['indexed_location_id']) && empty($filter['indexed_location_list'])) {
@@ -161,7 +86,6 @@ where s.deleted=false and s.id=o.sample_id and s.group_id=$this->id";
     if (!empty($filter['location_id']) && empty($filter['location_list'])) {
       $filter['location_list'] = $filter['location_id'];
     }
-
     if (!empty($filter['indexed_location_list'])) {
       // Got an indexed location as the filter boundary definition, so we can
       // use that as it is.
