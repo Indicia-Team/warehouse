173da8a860652f5e4bbd3ba3fe481f0682b659f5
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-11-08 11:25:46 +0000

    Moving the master taxon list ID setting into core.

--- application/helpers/warehouse.php
+++ application/helpers/warehouse.php
@@ -52,4 +52,24 @@ class warehouse {
     helper_base::$base_url = preg_replace('/^https?/', $protocol, helper_base::$base_url);
   }
 
+  /**
+   * Find the master taxon list's ID from config.
+   *
+   * This identifies the taxon list which provides an overall taxonomic
+   * hierarchy. Zero returned if not set to prevent errors in SQL.
+   *
+   * @return int
+   *   Taxon list ID or zero.
+   */
+  public static function getMasterTaxonListId() {
+    // Preferred location in indicia config file.
+    $masterTaxonListId = kohana::config('indicia.master_list_id', FALSE, FALSE);
+    // Legacy support - in v1.x the setting was in the cache builder module.
+    if (!$masterTaxonListId) {
+      $masterTaxonListId = kohana::config('cache_builder_variables.master_list_id', FALSE, FALSE);
+    }
+    // If not set, default to zero for safety.
+    return empty($masterTaxonListId) ? 0 : $masterTaxonListId;
+  }
+
 }
