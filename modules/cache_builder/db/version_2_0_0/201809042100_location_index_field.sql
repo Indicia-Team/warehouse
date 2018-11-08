7bcaf9971c7d924cd1a040b9745eaecf76b9607c
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-11-08 12:17:22 +0000

    Removed index scripts
    Will be added in later in the list, after a major update of the data
    (since indexes slow updates)

--- modules/cache_builder/db/version_2_0_0/201809042100_location_index_field.sql
+++ modules/cache_builder/db/version_2_0_0/201809042100_location_index_field.sql
@@ -3,6 +3,3 @@ ADD COLUMN location_ids integer[];
 
 ALTER TABLE cache_samples_functional
 ADD COLUMN location_ids integer[];
-
-CREATE INDEX ix_cache_occurrences_functional_location_ids ON cache_occurrences_functional USING GIN(location_ids);
-CREATE INDEX ix_cache_samples_functional_location_ids ON cache_samples_functional USING GIN(location_ids);
