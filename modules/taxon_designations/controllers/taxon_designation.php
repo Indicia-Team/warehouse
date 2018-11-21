525a309347ac3186c9e90f0ec0a023910e2e8460
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-11-21 09:45:08 +0000

    Linting

--- modules/taxon_designations/controllers/taxon_designation.php
+++ modules/taxon_designations/controllers/taxon_designation.php
@@ -1,5 +1,9 @@
 <?php
+
 /**
+ * @file
+ * Controller for the list of taxon designations.
+ *
  * Indicia, the OPAL Online Recording Toolkit.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -13,11 +17,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
  *
- * @package	Taxon Designations
- * @subpackage Controllers
- * @author	Indicia Team
- * @license	http://www.gnu.org/licenses/gpl.html GPL
- * @link 	https://github.com/indicia-team/warehouse/
+ * @author Indicia Team
+ * @license http://www.gnu.org/licenses/gpl.html GPL
+ * @link https://github.com/indicia-team/warehouse/
  */
 
 /**
@@ -25,13 +27,12 @@
  */
 class Taxon_designation_Controller extends Gridview_Base_Controller {
 
-  public function __construct()
-  {
+  public function __construct() {
     parent::__construct('taxon_designation', 'taxon_designation/index');
     $this->columns = array(
       'id'          => '',
       'title'       => '',
-      'category'    => ''
+      'category'    => '',
     );
     $this->pagetitle = "Taxon Designations";
     $this->model = ORM::factory('taxon_designation');
@@ -41,9 +42,9 @@ class Taxon_designation_Controller extends Gridview_Base_Controller {
    * Get the list of terms ready for the location types list.
    */
   protected function prepareOtherViewData(array $values) {
-    return array(
-      'category_terms' => $this->get_termlist_terms('indicia:taxon_designation_categories')
-    );
+    return [
+      'category_terms' => $this->get_termlist_terms('indicia:taxon_designation_categories'),
+    ];
   }
 
   /**
@@ -58,191 +59,217 @@ class Taxon_designation_Controller extends Gridview_Base_Controller {
    */
   public function upload_csv() {
     try {
-      // We will be using a POST array to send data, and presumably a FILES array for the
-      // media.
-      // Upload size
+      // Validate the uploaded CSV.
       $ups = Kohana::config('indicia.maxUploadSize');
       $_FILES = Validation::factory($_FILES)->add_rules(
         'csv_upload', 'upload::valid', 'upload::required',
         'upload::type[csv]', "upload::size[$ups]"
       );
-      if (count($_FILES)===0) {
+      if (count($_FILES) === 0) {
         echo "No file was uploaded.";
       }
-      elseif ($_FILES->validate())
-      {
-        if (array_key_exists('name_is_guid', $_POST) && $_POST['name_is_guid']=='true')
+      elseif ($_FILES->validate()) {
+        if (array_key_exists('name_is_guid', $_POST) && $_POST['name_is_guid'] === 'true') {
           $finalName = strtolower($_FILES['csv_upload']['name']);
-        else
-          $finalName = time().strtolower($_FILES['csv_upload']['name']);
+        }
+        else {
+          $finalName = time() . strtolower($_FILES['csv_upload']['name']);
+        }
         $fTmp = upload::save('csv_upload', $finalName);
-        url::redirect('taxon_designation/import_progress?file='.urlencode(basename($fTmp)));
+        url::redirect('taxon_designation/import_progress?file=' . urlencode(basename($fTmp)));
       }
-      else
-      {
-        kohana::log('error', 'Validation errors uploading file '. $_FILES['csv_upload']['name']);
-        kohana::log('error', print_r($_FILES->errors('form_error_messages'), true));
-        Throw new ValidationError('Validation error', 2004, $_FILES->errors('form_error_messages'));
+      else {
+        kohana::log('error', 'Validation errors uploading file ' . $_FILES['csv_upload']['name']);
+        kohana::log('error', print_r($_FILES->errors('form_error_messages'), TRUE));
+        throw new ValidationError('Validation error', 2004, $_FILES->errors('form_error_messages'));
       }
     }
-    catch (Exception $e)
-    {
+    catch (Exception $e) {
       $this->handle_error($e);
     }
   }
 
   /**
-   * Controller method for the import_progress path. Displays the upload template with
-   * progress bar and status message, which then initiates the actual import.
+   * Controller method for the import_progress path.
+   *
+   * Displays the upload template with progress bar and status message, which
+   * then initiates the actual import.
    */
   public function import_progress() {
-    if (file_exists(kohana::config('upload.directory').'/'.$_GET['file'])) {
+    if (file_exists(kohana::config('upload.directory') . "/$_GET[file]")) {
       $this->template->content = new View('taxon_designation/upload');
       $this->template->title = 'Uploading designations';
     }
   }
 
   /**
-   * AJAX callback to handle upload of a single chunk of designations spreadsheet.
+   * AJAX callback to handle upload of a chunk of designations spreadsheet.
    */
   public function upload() {
-    $this->auto_render=false;
+    $this->auto_render = FALSE;
     $csvTempFile = DOCROOT . "upload/" . $_GET['uploaded_csv'];
-    $cache= Cache::instance();
+    $cache = Cache::instance();
     kohana::log('debug', 'in upload method');
-    if (file_exists($csvTempFile))
-    {
-      // Following helps for files from Macs
-      ini_set('auto_detect_line_endings',1);
-      // create the file pointer, plus one for errors
-      $handle = fopen ($csvTempFile, "r");
-      $count=0;
-      $limit = (isset($_GET['limit']) ? $_GET['limit'] : false);
+    if (file_exists($csvTempFile)) {
+      // Following helps for files from Macs.
+      ini_set('auto_detect_line_endings', 1);
+      // Create the file pointer, plus one for errors.
+      $handle = fopen($csvTempFile, "r");
+      $count = 0;
+      $limit = (isset($_GET['limit']) ? $_GET['limit'] : FALSE);
       $filepos = (isset($_GET['filepos']) ? $_GET['filepos'] : 0);
       $offset = (isset($_GET['offset']) ? $_GET['offset'] : 0);
-      // skip rows to allow for the last file position
+      // Skip rows to allow for the last file position.
       fseek($handle, $filepos);
-      if ($filepos==0) {
-        // first row, so load the column headings. Force lowercase so we can case insensitive search later.
-        $headings = array_map('strtolower',fgetcsv($handle, 1000, ","));
-        // Also work out the termlist_id for the cateogories
+      if ($filepos == 0) {
+        // First row, so load the column headings. Force lowercase so we can
+        // case insensitive search later.
+        $headings = array_map('strtolower', fgetcsv($handle, 1000, ","));
+        // Also work out the termlist_id for the cateogories.
         $r = $this->db
-            ->select('id')
-            ->from('termlists')
-            ->where(array('external_key'=>"indicia:taxon_designation_categories"))
-            ->get()->result_array(false);
-        $obj = array('termlist_id'=>$r[0]['id'], 'headings'=>$headings);
+          ->select('id')
+          ->from('termlists')
+          ->where(array('external_key' => "indicia:taxon_designation_categories"))
+          ->get()->result_array(FALSE);
+        $obj = array('termlist_id' => $r[0]['id'], 'headings' => $headings);
         $filepos = ftell($handle);
-        $cache->set(basename($_GET['uploaded_csv']).'metadata', $obj);
-      } else {
-        $obj = $cache->get(basename($_GET['uploaded_csv']).'metadata');
+        $cache->set(basename($_GET['uploaded_csv']) . 'metadata', $obj);
       }
-      while (($data = fgetcsv($handle, 1000, ",")) !== FALSE && ($limit===false || $count<$limit)) {
+      else {
+        $obj = $cache->get(basename($_GET['uploaded_csv']) . 'metadata');
+      }
+      while (($data = fgetcsv($handle, 1000, ",")) !== FALSE && ($limit === FALSE || $count < $limit)) {
         $count++;
         $filepos = ftell($handle);
-        $designationTitle = $this->findValue($data, array('designation title', 'designation'), $obj);
-        $designationCode = $this->findValue($data, array('designation code'), $obj);
-        $designationAbbr = $this->findValue($data, array('designation abbr', 'designation abbreviation'), $obj);
-        $designationDescription = $this->findValue($data, array('designation description'), $obj);
-        $designationCategory = $this->findValue($data, array('designation category','reporting category'), $obj);
-        $taxonExternalKey = $this->findValue(
-            $data, array('taxon external key', 'taxon version key', 'recommended taxon version key'), $obj);
-        $taxon = $this->findValue($data, array('taxon', 'current taxon name', 'recommended taxon'), $obj);
-        $startDate = $this->findValue($data, array('start date', 'year'), $obj);
-        $source = $this->findValue($data, array('source'), $obj);
-        $geographicConstraint = $this->findValue($data, array('geographic constraint'), $obj);
-        // First step - ensure a designation category exists
+        $designationTitle = $this->findValue($data, ['designation title', 'designation'], $obj);
+        $designationCode = $this->findValue($data, ['designation code'], $obj);
+        $designationAbbr = $this->findValue($data, ['designation abbr', 'designation abbreviation'], $obj);
+        $designationDescription = $this->findValue($data, ['designation description'], $obj);
+        $designationCategory = $this->findValue($data, ['designation category', 'reporting category'], $obj);
+        $taxonExternalKey = $this->findValue($data, [
+          'taxon external key',
+          'taxon version key',
+          'recommended taxon version key',
+        ], $obj);
+        $taxon = $this->findValue($data, [
+          'taxon',
+          'current taxon name',
+          'recommended taxon',
+        ], $obj);
+        $startDate = $this->findValue($data, ['start date', 'year'], $obj);
+        $source = $this->findValue($data, ['source'], $obj);
+        $geographicConstraint = $this->findValue($data, ['geographic constraint'], $obj);
+        // First step - ensure a designation category exists.
         $r = $this->db
-            ->select('id')
-            ->from('list_termlists_terms')
-            ->where(array('termlist_id'=>$obj['termlist_id'], 'term'=>$designationCategory))
-            ->get()->result_array(false);
-        if (count($r)==0) {
-          kohana::log('debug', 'inserting category '.$designationCategory);
-          $this->db->query("SELECT insert_term('$designationCategory', 'eng', null, null, 'indicia:taxon_designation_categories');");
+          ->select('id')
+          ->from('list_termlists_terms')
+          ->where(array('termlist_id' => $obj['termlist_id'], 'term' => $designationCategory))
+          ->get()->result_array(FALSE);
+        if (count($r) === 0) {
+          kohana::log('debug', "Inserting category $designationCategory");
+          $this->db->query("SELECT insert_term('$designationCategory', 'eng', NULL, NULL, 'indicia:taxon_designation_categories');");
           $r = $this->db
             ->select('id')
             ->from('list_termlists_terms')
-            ->where(array('termlist_id'=>$obj['termlist_id'], 'term'=>$designationCategory))
-            ->get()->result_array(false);
+            ->where([
+              'termlist_id' => $obj['termlist_id'],
+              'term' => $designationCategory,
+            ])
+            ->get()->result_array(FALSE);
         }
         $catId = $r[0]['id'];
         kohana::log('debug', "got category $catId");
-        // Second step - ensure the designation exists
+        // Second step - ensure the designation exists.
         $r = $this->db
-            ->select('id')
-            ->from('taxon_designations')
-            ->where(array('category_id'=>$catId, 'title'=>$designationTitle, 'deleted'=>'f'))
-            ->get()->result_array(false);
-        if (count($r)==0) {
-          kohana::log('debug', 'inserting designation '.$designationTitle);
+          ->select('id')
+          ->from('taxon_designations')
+          ->where([
+            'category_id' => $catId,
+            'title' => $designationTitle,
+            'deleted' => 'f',
+          ])
+          ->get()->result_array(FALSE);
+        if (count($r) === 0) {
+          kohana::log('debug', "Inserting designation $designationTitle");
           $this->db->insert('taxon_designations', array(
-            'title'=>$designationTitle,
-            'code'=>$designationCode,
-            'abbreviation'=>$designationAbbr,
-            'description'=>$designationDescription,
-            'category_id'=>$catId,
-            'created_on'=>date("Ymd H:i:s"),
-            'created_by_id'=>$_SESSION['auth_user']->id,
-            'updated_on'=>date("Ymd H:i:s"),
-            'updated_by_id'=>$_SESSION['auth_user']->id
+            'title' => $designationTitle,
+            'code' => $designationCode,
+            'abbreviation' => $designationAbbr,
+            'description' => $designationDescription,
+            'category_id' => $catId,
+            'created_on' => date("Ymd H:i:s"),
+            'created_by_id' => $_SESSION['auth_user']->id,
+            'updated_on' => date("Ymd H:i:s"),
+            'updated_by_id' => $_SESSION['auth_user']->id,
           ));
           $r = $this->db
             ->select('id')
             ->from('taxon_designations')
-            ->where(array('category_id'=>$catId, 'title'=>$designationTitle))
-            ->get()->result_array(false);
+            ->where([
+              'category_id' => $catId,
+              'title' => $designationTitle,
+            ])
+            ->get()->result_array(FALSE);
         }
         $desId = $r[0]['id'];
-        // Third step - find the pre-existing taxon/taxa
+        // Third step - find the pre-existing taxon/taxa.
         $where = array();
-        if (!empty($taxon))
-          $where['taxon']=trim($taxon);
-        if (!empty($taxonExternalKey))
-          $where['external_key']=trim($taxonExternalKey);
-        if (count($where)===0)
+        if (!empty($taxon)) {
+          $where['taxon'] = trim($taxon);
+        }
+        if (!empty($taxonExternalKey)) {
+          $where['external_key'] = trim($taxonExternalKey);
+        }
+        if (count($where) === 0) {
           throw new exception('Missing taxon or external key - cannot link to a taxon');
+        }
         $r = $this->db
-            ->select('id')
-            ->from('taxa')
-            ->where($where)
-            ->get()->result_array(false);
-        // convert years to a date
-        if (preg_match('/\d\d\d\d/', $startDate))
-          $startDate = $startDate.'-01-01';
-        if (empty($startDate))
-          $startDate=null;
+          ->select('id')
+          ->from('taxa')
+          ->where($where)
+          ->get()->result_array(FALSE);
+        // Convert years to a date.
+        if (preg_match('/\d\d\d\d/', $startDate)) {
+          $startDate = $startDate . '-01-01';
+        }
+        if (empty($startDate)) {
+          $startDate = NULL;
+        }
         foreach ($r as $taxon) {
-          // Insert a link from each matched taxon to the designation, if not already present.
+          // Insert a link from each matched taxon to the designation, if not
+          // already present.
           $r = $this->db
             ->select('id')
             ->from('taxa_taxon_designations')
-            ->where(array('taxon_designation_id'=>$desId, 'taxon_id'=>$taxon['id'], 'deleted'=>'f'))
-            ->get()->result_array(false);
-          if (count($r)===0) {
+            ->where(array(
+              'taxon_designation_id' => $desId,
+              'taxon_id' => $taxon['id'],
+              'deleted' => 'f',
+            ))
+            ->get()->result_array(FALSE);
+          if (count($r) === 0) {
             $this->db->insert('taxa_taxon_designations', array(
-              'taxon_id'=>$taxon['id'],
-              'taxon_designation_id'=>$desId,
-              'start_date'=>$startDate,
-              'source'=>$source,
-              'geographical_constraint'=>$geographicConstraint,
-              'created_on'=>date("Ymd H:i:s"),
-              'created_by_id'=>$_SESSION['auth_user']->id,
-              'updated_on'=>date("Ymd H:i:s"),
-              'updated_by_id'=>$_SESSION['auth_user']->id
+              'taxon_id' => $taxon['id'],
+              'taxon_designation_id' => $desId,
+              'start_date' => $startDate,
+              'source' => $source,
+              'geographical_constraint' => $geographicConstraint,
+              'created_on' => date("Ymd H:i:s"),
+              'created_by_id' => $_SESSION['auth_user']->id,
+              'updated_on' => date("Ymd H:i:s"),
+              'updated_by_id' => $_SESSION['auth_user']->id,
             ));
-          } else {
+          }
+          else {
             $this->db->update('taxa_taxon_designations', array(
-                'taxon_id'=>$taxon['id'],
-                'taxon_designation_id'=>$desId,
-                'start_date'=>$startDate,
-                'source'=>$source,
-                'geographical_constraint'=>$geographicConstraint,
-                'updated_on'=>date("Ymd H:i:s"),
-                'updated_by_id'=>$_SESSION['auth_user']->id
-              ),
-              array('id'=>$r[0]['id'])
-            );
+              'taxon_id' => $taxon['id'],
+              'taxon_designation_id' => $desId,
+              'start_date' => $startDate,
+              'source' => $source,
+              'geographical_constraint' => $geographicConstraint,
+              'updated_on' => date("Ymd H:i:s"),
+              'updated_by_id' => $_SESSION['auth_user']->id,
+            ), array('id' => $r[0]['id']));
             kohana::log('debug', 'updated');
           }
         }
@@ -255,14 +282,16 @@ class Taxon_designation_Controller extends Gridview_Base_Controller {
   }
 
   /**
-   * Controller method for the upload_complate path, called at the end of upload.
-   * Displays a message about the number of designations uploaded, cleans the cache
-   * and upload file, then navigaes to the taxon designation index page.
+   * Controller method for the upload_complate path.
+   *
+   * Called at the end of upload. Displays a message about the number of
+   * designations uploaded, cleans the cache and upload file, then navigates to
+   * the taxon designation index page.
    */
   public function upload_complete() {
-    $this->session->set_flash('flash_info', $_GET['total']." designations were uploaded.");
-    $cache= Cache::instance();
-    $cache->delete(basename($_GET['uploaded_csv']).'metadata');
+    $this->session->set_flash('flash_info', $_GET['total'] . " designations were uploaded.");
+    $cache = Cache::instance();
+    $cache->delete(basename($_GET['uploaded_csv']) . 'metadata');
     $csvTempFile = DOCROOT . "upload/" . $_GET['uploaded_csv'];
     unlink($csvTempFile);
     url::redirect('taxon_designation/index');
@@ -270,19 +299,21 @@ class Taxon_designation_Controller extends Gridview_Base_Controller {
 
   /**
    * Finds a field value if it exists in the data for a CSV row.
+   *
    * @param type $data
-   * @param array $names List of the possible column titles that this value can match against.
-   * @param type $obj
+   *   Data array to search in.
+   * @param array $names
+   *   List of the possible column titles that this value can match against.
+   * @param array $obj
    */
-  private function findValue($data, $names, $obj) {
+  private function findValue($data, array $names, array $obj) {
     foreach ($names as $name) {
       $idx = array_search($name, $obj['headings']);
-      if ($idx!==false) break;
+      if ($idx !== FALSE) {
+        break;
+      }
     }
-    if ($idx===false)
-      return null;
-    else
-      return $data[$idx];
+    return $idx === FALSE ? NULL : $data[$idx];
   }
 
 }
--- modules/taxon_designations/views/taxon_designation/index.php
+++ modules/taxon_designations/views/taxon_designation/index.php
@@ -1,6 +1,9 @@
 <?php
 
 /**
+ * @file
+ * View template for the list of taxon designations.
+ *
  * Indicia, the OPAL Online Recording Toolkit.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -14,39 +17,35 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
  *
- * @package	Taxon Designations
- * @subpackage Views
- * @author	Indicia Team
- * @license	http://www.gnu.org/licenses/gpl.html GPL
- * @link 	https://github.com/indicia-team/warehouse/
+ * @author Indicia Team
+ * @license http://www.gnu.org/licenses/gpl.html GPL
+ * @link https://github.com/indicia-team/warehouse/
  */
-
 
 echo $grid;
 ?>
-<form action="<?php echo url::site().'taxon_designation/create'; ?>">
+<form action="<?php echo url::site() . 'taxon_designation/create'; ?>">
 <input type="submit" value="New taxon designation" class="ui-corner-all ui-state-default button" />
 </form>
 <?php echo $upload_csv_form ?>
-<form enctype="multipart/form-data" class="linear-form" method="post" action="<?php echo url::site().'taxon_designation/upload_csv'; ?>">
-<fieldset>
-<label for="csv_upload" class="auto">Upload a Designations Spreadsheet (CSV) file into this list:</label>
-<input type="file" name="csv_upload" id="csv_upload" size="40" />
-<input type="submit" value="Upload Designations File" />
-<p>This lets you import designations including links for any existing taxon, identified by the external key.
-  To use this facility, create a spreadsheet with the following columns, or columns matching the JNCC Conservation Designations spreadsheet:</p>
-<ol>
-  <li>designation title</li>
-  <li>designation code</li>
-  <li>designation abbr</li>
-  <li>designation description</li>
-  <li>designation category</li>
-  <li>taxon</li>
-  <li>taxon external key</li>
-  <li>start date</li>
-  <li>source</li>
-  <li>geographic constraint</li>
-</ol>
-
-</fieldset>
+<form enctype="multipart/form-data" class="linear-form" method="post" action="<?php echo url::site() . 'taxon_designation/upload_csv'; ?>">
+  <fieldset>
+    <label for="csv_upload" class="auto">Upload a Designations Spreadsheet (CSV) file into this list:</label>
+    <input type="file" name="csv_upload" id="csv_upload" size="40" />
+    <input type="submit" value="Upload Designations File" />
+    <p>This lets you import designations including links for any existing taxon, identified by the external key.
+      To use this facility, create a spreadsheet with the following columns, or columns matching the JNCC Conservation Designations spreadsheet:</p>
+    <ol>
+      <li>designation title</li>
+      <li>designation code</li>
+      <li>designation abbr</li>
+      <li>designation description</li>
+      <li>designation category</li>
+      <li>taxon</li>
+      <li>taxon external key</li>
+      <li>start date</li>
+      <li>source</li>
+      <li>geographic constraint</li>
+    </ol>
+  </fieldset>
 </form>
