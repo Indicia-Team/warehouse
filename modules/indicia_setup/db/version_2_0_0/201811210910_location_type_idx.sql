6777783c678f08f45bab0ce9ee30b18888862ad9
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-11-23 15:44:12 +0000

    Index tweaks
    Index on taxon meaning ID
    Other index script made simpler

--- modules/indicia_setup/db/version_2_0_0/201811210910_location_type_idx.sql
+++ modules/indicia_setup/db/version_2_0_0/201811210910_location_type_idx.sql
@@ -1,19 +1,5 @@
-
-DO $$
-BEGIN
-
-IF NOT EXISTS (
-    SELECT 1
-    FROM   pg_class c
-    JOIN   pg_namespace n ON n.oid = c.relnamespace
-    WHERE  c.relname = 'fki_location_type'
-    ) THEN
-
-    CREATE INDEX fki_location_type
+DROP INDEX IF EXISTS fki_location_type;
+CREATE INDEX fki_location_type
     ON locations
     USING btree
     (location_type_id);
-END IF;
-
-END$$;
-
