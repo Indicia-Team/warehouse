daa202b76a46099b8f9b8ab74a9bbcb4d71fa8e9
John van Breda <john.vanbreda@biodiverseit.co.uk>
2018-12-19 15:11:01 +0000

    Lower priority of upgrade related queue items
    So that new record changes are prioritised (as more likely to be
    noticeable).

--- modules/cache_builder/db/version_2_0_2/201812171348_attrs_json_queue.sql
+++ modules/cache_builder/db/version_2_0_2/201812171348_attrs_json_queue.sql
@@ -8,17 +8,17 @@ DELETE FROM work_queue WHERE task IN (
 );
 
 INSERT INTO work_queue (task, entity, record_id, priority, cost_estimate, created_on)
-SELECT DISTINCT 'task_cache_builder_attrs_occurrence', 'occurrence', id, 2, 60, now()
+SELECT DISTINCT 'task_cache_builder_attrs_occurrence', 'occurrence', id, 3, 60, now()
 FROM occurrences
 WHERE deleted=false;
 
 INSERT INTO work_queue (task, entity, record_id, priority, cost_estimate, created_on)
-SELECT DISTINCT 'task_cache_builder_attrs_sample', 'sample', id, 2, 60, now()
+SELECT DISTINCT 'task_cache_builder_attrs_sample', 'sample', id, 3, 60, now()
 FROM samples
 WHERE deleted=false;
 
 INSERT INTO work_queue (task, entity, record_id, priority, cost_estimate, created_on)
-SELECT DISTINCT 'task_cache_builder_attrs_taxa_taxon_list', 'taxa_taxon_list', ttl.id, 2, 60, now()
+SELECT DISTINCT 'task_cache_builder_attrs_taxa_taxon_list', 'taxa_taxon_list', ttl.id, 3, 60, now()
 FROM taxa_taxon_lists ttl
 --May as well skip any without an attribute.
 JOIN taxa_taxon_list_attribute_values av on av.taxa_taxon_list_id=ttl.id and av.deleted=false
