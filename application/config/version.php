1767255231b368f1874938eb3b7cb67803820bae
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-02-18 10:57:10 +0000

    Version date

--- application/config/version.php
+++ application/config/version.php
@@ -36,7 +36,7 @@ $config['version'] = '2.8.1';
  *
  * @var string
  */
-$config['release_date'] = '2018-02-15';
+$config['release_date'] = '2018-02-18';
 
 /**
  * Link to the code repository downloads page.
