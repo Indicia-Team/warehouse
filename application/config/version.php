77c2ddb9f6d93bbbed3419d7d6a0a47d3b62ce80
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-05-01 10:13:50 +0100

    Version update

--- application/config/version.php
+++ application/config/version.php
@@ -29,14 +29,14 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '2.24.1';
+$config['version'] = '2.24.2';
 
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2019-04-30';
+$config['release_date'] = '2019-05-01';
 
 /**
  * Link to the code repository downloads page.
