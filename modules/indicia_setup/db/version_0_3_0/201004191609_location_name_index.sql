da306470266023a23c8e95bc5fb3578961ec5cd9
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-05-28 14:07:09 +0100

    Adds full text indexing to location name lookups
    Required due to performance hit of location lookups during periods of
    high load.

--- modules/indicia_setup/db/version_0_3_0/201004191609_location_name_index.sql
+++ modules/indicia_setup/db/version_0_3_0/201004191609_location_name_index.sql
@@ -1,2 +1,4 @@
-CREATE INDEX ix_location_name ON locations ("name");
-
+-- #slow script#
+CREATE INDEX ix_location_name_fulltext ON locations USING gin
+    (to_tsvector('simple'::regconfig, quote_literal(quote_literal(name::text))))
+    WHERE deleted=false;
