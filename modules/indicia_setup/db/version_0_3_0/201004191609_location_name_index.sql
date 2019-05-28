df4458d5a6435c1c91a71579bf1c4670e96ef373
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-05-28 14:18:10 +0100

    Undid an unnecessary change

--- modules/indicia_setup/db/version_0_3_0/201004191609_location_name_index.sql
+++ modules/indicia_setup/db/version_0_3_0/201004191609_location_name_index.sql
@@ -1,4 +1,3 @@
 -- #slow script#
 CREATE INDEX ix_location_name_fulltext ON locations USING gin
-    (to_tsvector('simple'::regconfig, quote_literal(quote_literal(name::text))))
-    WHERE deleted=false;
+    (to_tsvector('simple'::regconfig, quote_literal(quote_literal(name::text))));
