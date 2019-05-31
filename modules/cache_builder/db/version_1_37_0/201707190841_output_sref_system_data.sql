4b13759ee11bcd846a638142f150430bb73a2670
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-05-31 16:36:47 +0100

    Fixes call to incorrect function prototype
    Since tidy-up of spatial functions.

--- modules/cache_builder/db/version_1_37_0/201707190841_output_sref_system_data.sql
+++ modules/cache_builder/db/version_1_37_0/201707190841_output_sref_system_data.sql
@@ -3,9 +3,7 @@
 UPDATE cache_occurrences_nonfunctional onf
 SET output_sref_system=get_output_system(
   reduce_precision(coalesce(s.geom, l.centroid_geom), o.confidential, greatest(o.sensitivity_precision, s.privacy_precision),
-    case when s.entered_sref_system is null then l.centroid_sref_system else s.entered_sref_system end),
-  case when s.entered_sref_system is null then l.centroid_sref_system else s.entered_sref_system end,
-  '4326'
+    case when s.entered_sref_system is null then l.centroid_sref_system else s.entered_sref_system end)
 )
 FROM occurrences o
 JOIN samples s ON s.id=o.sample_id AND s.deleted=false
