a2f35152c78b8705d63d06d8b7bc91ed6b4d6b02
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-06-07 14:46:20 +0100

    Media type term for impending imgex image support.

---
+++ modules/indicia_setup/db/version_2_27_3/201804171130_imgex_photos.sql
@@ -0,0 +1,5 @@
+SELECT insert_term('Image:imgex', 'eng', null, 'indicia:media_types');
+
+UPDATE termlists_terms
+SET parent_id=(SELECT id FROM list_termlists_terms WHERE term IN ('Image') AND termlist_external_key='indicia:media_types')
+WHERE id IN (SELECT id FROM list_termlists_terms WHERE term IN ('Image:iNaturalist', 'Image:imgex') AND termlist_external_key='indicia:media_types');
