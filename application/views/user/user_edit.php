fa273c8310f6d6b11faf97683e432ff467c07737
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-11-29 16:55:09 +0000

    Tidies up the user table, removing unused fields.

--- application/views/user/user_edit.php
+++ application/views/user/user_edit.php
@@ -27,87 +27,66 @@ $id = html::initial_value($values, 'user:id');
 ?>
 <p>This page allows you to specify a users details.</p>
 <form id="user-edit" action="<?php echo url::site() . 'user/save'; ?>" method="post">
-  <fieldset>
-    <legend>User's Details<?php echo $metadata; ?></legend>
-    <input type="hidden" name="id" id="id" value="<?php echo html::specialchars($model->id); ?>" />
-    <input type="hidden" name="person_id" id="person_id" value="<?php echo html::specialchars($model->person_id); ?>" />
-    <?php
-    echo data_entry_helper::text_input([
-      'label' => 'Username',
-      'fieldname' => 'user:username',
-      'default' => html::initial_value($values, 'user:username'),
-      'validation' => ['required'],
-    ]);
-    echo data_entry_helper::textarea([
-      'label' => 'Interests',
-      'fieldname' => 'user:interests',
-      'default' => html::initial_value($values, 'user:interests'),
-    ]);
-    echo data_entry_helper::textarea([
-      'label' => 'Location name',
-      'fieldname' => 'user:location_name',
-      'default' => html::initial_value($values, 'user:location_name'),
-    ]);
-    echo data_entry_helper::checkbox([
-      'label' => 'Email visible',
-      'fieldname' => 'user:email_visible',
-      'default' => html::initial_value($values, 'user:email_visible'),
-    ]);
-    echo data_entry_helper::checkbox([
-      'label' => 'View common names',
-      'fieldname' => 'user:view_common_names',
-      'default' => html::initial_value($values, 'user:view_common_names'),
-    ]);
-    echo data_entry_helper::checkbox([
-      'label' => 'This user allows records to be shared for reporting',
-      'fieldname' => 'user:allow_share_for_reporting',
-      'default' => html::initial_value($values, 'user:allow_share_for_reporting'),
-    ]);
-    echo data_entry_helper::checkbox([
-      'label' => 'This user allows records to be shared for peer review',
-      'fieldname' => 'user:allow_share_for_peer_review',
-      'default' => html::initial_value($values, 'user:allow_share_for_peer_review'),
-    ]);
-    echo data_entry_helper::checkbox([
-      'label' => 'This user allows records to be shared for verification',
-      'fieldname' => 'user:allow_share_for_verification',
-      'default' => html::initial_value($values, 'user:allow_share_for_verification'),
-    ]);
-    echo data_entry_helper::checkbox([
-      'label' => 'This user allows records to be shared for data flow',
-      'fieldname' => 'user:allow_share_for_data_flow',
-      'default' => html::initial_value($values, 'user:allow_share_for_data_flow'),
-    ]);
-    echo data_entry_helper::checkbox([
-      'label' => 'This user allows records to be shared for moderation',
-      'fieldname' => 'user:allow_share_for_moderation',
-      'default' => html::initial_value($values, 'user:allow_share_for_moderation'),
-    ]);
-    echo data_entry_helper::checkbox([
-      'label' => 'This user allows records to be shared for editing',
-      'fieldname' => 'user:allow_share_for_editing',
-      'default' => html::initial_value($values, 'user:allow_share_for_editing'),
-    ]);
-    if ($this->auth->logged_in('CoreAdmin')) {
-      $roles = ORM::factory('core_role')->orderby('title', 'asc')->find_all();
-      $lookupValues = [];
-      foreach ($roles as $role) {
-        $lookupValues[$role->id] = $role->title;
-      }
-      echo data_entry_helper::select([
-        'label' => 'Role within Warehouse',
-        'fieldname' => 'user:core_role_id',
-        'default' => html::initial_value($values, 'user:core_role_id'),
-        'lookupValues' => $lookupValues,
-        'blankText' => '<none>',
-      ]);
+  <input type="hidden" name="id" id="id" value="<?php echo html::specialchars($model->id); ?>" />
+  <input type="hidden" name="person_id" id="person_id" value="<?php echo html::specialchars($model->person_id); ?>" />
+  <?php
+  echo data_entry_helper::text_input([
+    'label' => 'Username',
+    'fieldname' => 'user:username',
+    'default' => html::initial_value($values, 'user:username'),
+    'validation' => ['required'],
+  ]);
+  echo '<fieldset><legend>User data sharing options</legend>';
+  echo data_entry_helper::checkbox([
+    'label' => 'This user allows records to be shared for reporting',
+    'fieldname' => 'user:allow_share_for_reporting',
+    'default' => html::initial_value($values, 'user:allow_share_for_reporting'),
+  ]);
+  echo data_entry_helper::checkbox([
+    'label' => 'This user allows records to be shared for peer review',
+    'fieldname' => 'user:allow_share_for_peer_review',
+    'default' => html::initial_value($values, 'user:allow_share_for_peer_review'),
+  ]);
+  echo data_entry_helper::checkbox([
+    'label' => 'This user allows records to be shared for verification',
+    'fieldname' => 'user:allow_share_for_verification',
+    'default' => html::initial_value($values, 'user:allow_share_for_verification'),
+  ]);
+  echo data_entry_helper::checkbox([
+    'label' => 'This user allows records to be shared for data flow',
+    'fieldname' => 'user:allow_share_for_data_flow',
+    'default' => html::initial_value($values, 'user:allow_share_for_data_flow'),
+  ]);
+  echo data_entry_helper::checkbox([
+    'label' => 'This user allows records to be shared for moderation',
+    'fieldname' => 'user:allow_share_for_moderation',
+    'default' => html::initial_value($values, 'user:allow_share_for_moderation'),
+  ]);
+  echo data_entry_helper::checkbox([
+    'label' => 'This user allows records to be shared for editing',
+    'fieldname' => 'user:allow_share_for_editing',
+    'default' => html::initial_value($values, 'user:allow_share_for_editing'),
+  ]);
+  echo '</fieldset>';
+  if ($this->auth->logged_in('CoreAdmin')) {
+    $roles = ORM::factory('core_role')->orderby('title', 'asc')->find_all();
+    $lookupValues = [];
+    foreach ($roles as $role) {
+      $lookupValues[$role->id] = $role->title;
     }
+    echo data_entry_helper::select([
+      'label' => 'Role within Warehouse',
+      'fieldname' => 'user:core_role_id',
+      'default' => html::initial_value($values, 'user:core_role_id'),
+      'lookupValues' => $lookupValues,
+      'blankText' => '<none>',
+    ]);
+  }
 
-    if (isset($password_field) and $password_field != '') {
-      echo $password_field;
-      echo html::error_message($model->getError('user:password'));
-    } ?>
-  </fieldset>
+  if (isset($password_field) and $password_field != '') {
+    echo $password_field;
+    echo html::error_message($model->getError('user:password'));
+  } ?>
   <fieldset>
     <legend>Website roles</legend>
       <?php
@@ -120,14 +99,16 @@ $id = html::initial_value($values, 'user:id');
         }
         $otherOptions = implode("\n        ", $otherOptionList);
         echo <<<HTML
-<div class="form-group">
-  <label for="$website[name]" class="col-sm-3 control-label">$website[title]:</label>
-  <div class="col-sm-9">
-      <select class="form-control" name="$website[name]" id="$website[name]">
-        <option>None</option>
-        $otherOptions
-      </select>
-    </div>
+<div class="row">
+  <div class="form-group">
+    <label for="$website[name]" class="col-sm-2 control-label">$website[title]:</label>
+    <div class="col-sm-3">
+        <select class="form-control" name="$website[name]" id="$website[name]">
+          <option>None</option>
+          $otherOptions
+        </select>
+      </div>
+  </div>
 </div>
 
 HTML;
