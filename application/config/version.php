dc75810ba3e59ecc1a775a1e5f3eaccdbcf1be2e
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-01-15 10:39:56 +0000

    Version bump

--- application/config/version.php
+++ application/config/version.php
@@ -29,14 +29,14 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '3.0.1';
+$config['version'] = '3.1.0';
 
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2019-12-10';
+$config['release_date'] = '2020-01-15';
 
 /**
  * Link to the code repository downloads page.
