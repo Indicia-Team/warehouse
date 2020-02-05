2ffb732cac5fa75b752a748ded4f568e6f6181b1
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-02-05 14:17:08 +0000

    Separate data from schema changes
    Prevents script running order issues.

---
+++ modules/cache_builder/db/version_0_1_2/201504071220_cache_taxon_searchterms_external_key_data.sql
@@ -0,0 +1,7 @@
+﻿-- #slow script#
+
+UPDATE cache_taxon_searchterms cts
+SET external_key = cttl.external_key
+FROM cache_taxa_taxon_lists cttl
+WHERE cttl.id=cts.taxa_taxon_list_id
+AND cttl.external_key IS NOT NULL;
