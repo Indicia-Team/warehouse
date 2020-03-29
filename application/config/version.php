38b8232f4d6be0c06da5e62af5074b975c8d5c64
Andrew van Breda <support@avb-it.co.uk>
2020-03-29 11:37:23 +0100

    Simply change really improves the report's usefulness. Uses "in" to allow for multiple location types. Can now be used in the Tracking The Impact project.

--- application/config/version.php
+++ application/config/version.php
@@ -29,14 +29,14 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '3.1.21';
+$config['version'] = '3.2.0';
 
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2020-03-25';
+$config['release_date'] = '2020-03-29';
 
 /**
  * Link to the code repository downloads page.
