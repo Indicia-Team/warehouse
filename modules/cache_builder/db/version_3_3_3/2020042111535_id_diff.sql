4805e11aa380bb32f768a828a8ea278063330723
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-04-22 15:41:56 +0100

    Ensure ID diff populated for existing cache_occurrences
    Fixes https://github.com/BiologicalRecordsCentre/iRecord/issues/833

---
+++ modules/cache_builder/db/version_3_3_3/2020042111535_id_diff.sql
@@ -0,0 +1,6 @@
+-- #slow script#
+UPDATE cache_occurrences_functional o
+SET identification_difficulty=extkey.value::integer
+FROM verification_rule_data extkey
+JOIN verification_rules vr ON vr.id=extkey.verification_rule_id AND vr.test_type='IdentificationDifficulty' AND vr.deleted=false
+WHERE extkey.key=UPPER(o.taxa_taxon_list_external_key) AND extkey.header_name='Data' AND extkey.deleted=false;
