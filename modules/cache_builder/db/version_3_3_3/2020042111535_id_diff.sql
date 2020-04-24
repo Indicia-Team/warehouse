4d9c7e86185ddf4111b87c4290540bbc14f56c0a
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-04-24 11:42:15 +0100

    Changes trigger to allow skip of tracking updates
    This means id_diff changes don’t trigger a complete ES refresh.

--- modules/cache_builder/db/version_3_3_3/2020042111535_id_diff.sql
+++ modules/cache_builder/db/version_3_3_3/2020042111535_id_diff.sql
@@ -1,4 +1,14 @@
 -- #slow script#
+DROP TRIGGER trigger_cache_occurrences_functional_changed ON cache_occurrences_functional;
+CREATE TRIGGER trigger_cache_occurrences_functional_changed
+    BEFORE INSERT OR UPDATE
+    ON indicia.cache_occurrences_functional
+    FOR EACH ROW WHEN (current_setting('session_replication_role') <> 'local')
+    EXECUTE PROCEDURE indicia.cache_functional_changed();
+
+-- Disable tracking increments, so doesn't force a complete ES refresh.
+SET session_replication_role = 'local';
+
 UPDATE cache_occurrences_functional o
 SET identification_difficulty=extkey.value::integer
 FROM verification_rule_data extkey
