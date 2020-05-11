eab5474af751288fa420e6e9b3ea7dde7bbe3d9a
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-05-11 16:52:44 +0100

    Version bump
    V4 contains Elasticsearch API changes

--- application/config/version.php
+++ application/config/version.php
@@ -29,14 +29,14 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '3.3.5';
+$config['version'] = '4.0.0';
 
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2020-04-28';
+$config['release_date'] = '2020-05-11';
 
 /**
  * Link to the code repository downloads page.
