efa4973530617da4bedc1764e7b77c2bc248a9c6
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-06-23 12:53:39 +0100

    Ensure install scripts in correct sequence.
    See https://github.com/Indicia-Team/warehouse/issues/347

--- modules/data_cleaner/db/version_3_3_3/2020042111535_id_diff.sql
+++ modules/data_cleaner/db/version_3_3_3/2020042111535_id_diff.sql
@@ -1,10 +1,4 @@
 -- #slow script#
-DROP TRIGGER trigger_cache_occurrences_functional_changed ON cache_occurrences_functional;
-CREATE TRIGGER trigger_cache_occurrences_functional_changed
-    BEFORE INSERT OR UPDATE
-    ON indicia.cache_occurrences_functional
-    FOR EACH ROW WHEN (current_setting('application_name') <> 'skiptrigger')
-    EXECUTE PROCEDURE indicia.cache_functional_changed();
 
 -- Disable tracking increments, so doesn't force a complete ES refresh.
 SET application_name = 'skiptrigger';
