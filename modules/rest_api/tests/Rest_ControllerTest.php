ef2a23552a176129d7de04fe4c6bc9e00375570a
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-07-20 09:53:43 +0100

    Require path corrected.

--- modules/rest_api/tests/Rest_ControllerTest.php
+++ modules/rest_api/tests/Rest_ControllerTest.php
@@ -158,34 +158,34 @@ class Rest_ControllerTest extends Indicia_DatabaseTestCase {
     $this->assertEquals(401, $response['httpCode'], 'Invalid token oAuth2 request to taxon-observations should fail.');
   }
 
-  private function getJwt() {
-    require_once '../../../vendor/autoload.php';
-    $privateKey = <<<KEY
------BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
-vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
-5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
-AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
-bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
-Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
-cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
-5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
-ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
-k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
-qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
-eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
-B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
------END RSA PRIVATE KEY-----
-KEY;
+  private function getJwt($privateKey, $iss, $userId, $exp) {
+    require_once 'vendor/autoload.php';
     $payload = [
-      'iss' => 'http://localhost/d8-dev/web',
-      'indicia_user_id' => 1,
-      'exp' => time() + 120,
+      'iss' => $iss,
+      'indicia_user_id' => $userId,
+      'exp' => $exp,
     ];
     return \Firebase\JWT\JWT::encode($payload, $privateKey, 'RS256');
   }
 
   public function testJwt() {
+    $privateKey = <<<KEY
+  -----BEGIN RSA PRIVATE KEY-----
+  MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
+  vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
+  5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
+  AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
+  bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
+  Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
+  cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
+  5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
+  ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
+  k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
+  qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
+  eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
+  B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
+  -----END RSA PRIVATE KEY-----
+  KEY;
     $publicKey = <<<KEY
 -----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8kGa1pSjbSYZVebtTRBLxBz5H
@@ -194,6 +194,18 @@ MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8kGa1pSjbSYZVebtTRBLxBz5H
 ehde/zUxo6UvS7UrBQIDAQAB
 -----END PUBLIC KEY-----
 KEY;
+    $this->authMethod = 'jwtUser';
+    // Make sure there is no public key stored.
+    $db = new Database();
+    $db->update(
+      'websites',
+      array('public_key' => NULL),
+      array('id' => 1)
+    );
+    // Make an otherwise valid call - should be unauthorised.
+    $this->jwt = $this->getJwt($privateKey, 'http://www.indicia.org.uk', 1, time() + 120);
+    $response = $this->callService('reports/library/months/filterable_species_counts.xml');
+    $this->assertTrue($response['httpCode'] === 401);
     // Store the public key so Indicia can check signed requests.
     $db = new Database();
     $db->update(
@@ -201,9 +213,10 @@ KEY;
       array('public_key' => $publicKey),
       array('id' => 1)
     );
-    $this->authMethod = 'jwtUser';
+    // Make a valid call - should be authorised.
+    $this->jwt = $this->getJwt($privateKey, 'http://www.indicia.org.uk', 1, time() + 120);
     $response = $this->callService('reports/library/months/filterable_species_counts.xml');
-    $this->assertTrue($response['httpCode']===200);
+    $this->assertTrue($response['httpCode'] === 200);
   }
 
   public function testProjects_authentication() {
