8b238038f9347ef2ee03197038a7e6a3872365b4
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-09-03 13:59:51 +0100

    Code syntax fix

--- application/models/occurrence.php
+++ application/models/occurrence.php
@@ -467,7 +467,7 @@ SQL;
    *   * **occurrence_associations** - Set to 't' to enable occurrence associations options. The
    *     relevant warehouse module must also be enabled.
    */
-  public function fixedValuesForm($options = []]) {
+  public function fixedValuesForm($options = []) {
     $srefs = [];
     $systems = spatial_ref::system_list();
     foreach ($systems as $code => $title) {
