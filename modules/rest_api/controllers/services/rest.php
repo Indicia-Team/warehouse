2105d6a3138ccb31a3c30c3cce5df236014aaa9f
Richard Burkmar <rburkmar@ceh.ac.uk>
2020-09-16 13:30:07 +0100

    Correct syntax error

--- modules/rest_api/controllers/services/rest.php
+++ modules/rest_api/controllers/services/rest.php
@@ -1582,7 +1582,7 @@ class Rest_Controller extends Controller {
     }
     $dtvalue = $this->getRawEsFieldValue($doc, $params[0]);
     $dt = DateTime::createFromFormat("M dS Y, G:i:s.u", $dtvalue);
-    return  $dt.format($params[1]);
+    return  $dt->format($params[1]);
   }
 
   /**
