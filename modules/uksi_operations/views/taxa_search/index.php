670a6ce04ae3ca4ca861d66efcdea46d2374aa27
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-12-29 20:40:06 +0000

    A search tool to support the UKSI operations module

---
+++ modules/uksi_operations/views/taxa_search/index.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ * View template for the list of taxon designations.
+ *
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @author Indicia Team
+ * @license http://www.gnu.org/licenses/gpl.html GPL
+ * @link https://github.com/indicia-team/warehouse/
+ */
+
+?>
+<form method="post" action="<?php echo url::site(); ?>taxa_search">
+  <input type="text" name="filter-param_organism_key" />
+  <input type="submit" value="Search" />
+</form>
+<?php
+warehouse::loadHelpers(['report_helper']);
+$readAuth = report_helper::get_read_auth(0 - $_SESSION['auth_user']->id, kohana::config('indicia.private_key'));
+$template = <<<HTML
+<div class="panel panel-info">
+  <div class="panel-heading"><span class="{name_class}">{taxon}</span> {attribute} {authority}</div>
+  <div class="panel-body">
+    <dl class="dl-horizontal">
+      <dt>Name type</dt>
+      <dd>{name_type}</dd>
+
+      <dt>Rank</dt>
+      <dd>{rank}</dd>
+
+      <dt>TVK</dt>
+      <dd>{search_code}</dd>
+
+      <dt>Accepted name TVK</dt>
+      <dd>{external_key}</dd>
+
+      <dt>Organism Key</dt>
+      <dd>{organism_key}</dd>
+
+      <dt title="ID for the taxon concept given by the Indicia warehouse.">Taxon meaning ID</dt>
+      <dd>{taxon_meaning_id}</dd>
+
+      <dt>Redundant</dt>
+      <dd>{redundant}</dd>
+    </dl>
+  </div>
+</div>
+HTML;
+echo report_helper::freeform_report([
+  'readAuth' => $readAuth,
+  'dataSource' => 'library/taxa/uksi_taxa_search',
+  'autoParamsForm' => FALSE,
+  'reportGroup' => 'filter',
+  'extraParams' => ['param_taxon_list_id' => $listId],
+  'bands' => [
+    [
+      'content' => $template,
+    ],
+  ],
+]);
