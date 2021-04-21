a809845da470e4dc8e4cdb6f516684d9c00adf6e
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-04-21 10:37:25 +0100

    Warning message if id mapped incorrectly on import

--- modules/uksi_operations/views/uksi_operation/importer.php
+++ modules/uksi_operations/views/uksi_operation/importer.php
@@ -1,9 +1,15 @@
-<?php
+<?php if (!empty($_FILES)) : ?>
+  <div class="alert alert-warning">
+    Please remove the mapping for the import file's <strong>id</strong> column before proceeding.
+  </div>
+<?php endif; ?>
 
+<?php
 warehouse::loadHelpers(['import_helper']);
 $auth = import_helper::get_read_write_auth(0 - $_SESSION['auth_user']->id, kohana::config('indicia.private_key'));
 
 $fieldMappings = <<<TXT
+uksi_operation:sequence=row_id
 uksi_operation:parent_organism_key=parent_orgkey
 uksi_operation:organism_key=org_key
 uksi_operation:taxon_version_key=new_tv_key
@@ -11,7 +17,7 @@ uksi_operation:batch_processed_on=processed_date
 uksi_operation:current_organism_key=current_orgkey
 TXT;
 
-echo import_helper::importer(array(
+echo import_helper::importer([
   'model' => 'uksi_operation',
   'auth' => $auth,
   'fieldMap' => [
@@ -19,6 +25,6 @@ echo import_helper::importer(array(
       'fields' => $fieldMappings,
     ],
   ],
-));
+]);
 
 echo import_helper::dump_javascript();
