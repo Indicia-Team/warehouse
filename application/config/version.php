38df414843514060fdac5c980a79c1291fd20605
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-04-22 15:44:44 +0100

    Develop branch version bump

--- application/config/version.php
+++ application/config/version.php
@@ -29,14 +29,14 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '5.1.4';
+$config['version'] = '5.2.0';
 
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2021-04-01';
+$config['release_date'] = '2021-04-21';
 
 /**
  * Link to the code repository downloads page.
