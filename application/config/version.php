2c357c973066bb2511e1f7841c0228d043a4ef4d
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-05-14 10:26:26 +0100

    Release version changes

--- application/config/version.php
+++ application/config/version.php
@@ -36,7 +36,7 @@ $config['version'] = '6.0.0';
  *
  * @var string
  */
-$config['release_date'] = '2021-05-12';
+$config['release_date'] = '2021-05-14';
 
 /**
  * Link to the code repository downloads page.
