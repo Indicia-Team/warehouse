8e08ae50be6a3238b1ddbcd5bc0899fc1ed2cc83
Jim Bacon <jame2@ceh.ac.uk>
2021-07-20 15:53:50 +0100

    Remove schema from setup query

--- modules/indicia_setup/db/version_6_1_0/202107151745_list_text_value_of_float_attribute.sql
+++ modules/indicia_setup/db/version_6_1_0/202107151745_list_text_value_of_float_attribute.sql
@@ -3,7 +3,6 @@
 -- The text value is the entered value, including trailing zeroes,
 -- which we need when a decimal validation rule is in place checking
 -- the number of digits after the decimal place.
--- Decimal validation and ranges do not play nicely together.
 
 -- I.e.
 -- 
@@ -25,8 +24,9 @@
 --     END
 -- END
 
+-- This allows for legacy float values where there is no text_value.
 
-CREATE OR REPLACE VIEW indicia.list_location_attribute_values
+CREATE OR REPLACE VIEW list_location_attribute_values
  AS
  SELECT av.id,
     l.id AS location_id,
@@ -90,18 +90,18 @@ CREATE OR REPLACE VIEW indicia.list_location_attribute_values
     l.location_type_id,
     rc.term AS reporting_category,
     rc.id AS reporting_category_id
-   FROM indicia.locations l
-     JOIN indicia.locations_websites lw ON lw.location_id = l.id AND lw.deleted = false
-     JOIN indicia.location_attribute_values av ON av.location_id = l.id AND av.deleted = false
-     JOIN indicia.location_attributes a ON a.id = av.location_attribute_id AND a.deleted = false
-     LEFT JOIN (indicia.termlists_terms tt
-     JOIN indicia.terms t ON t.id = tt.term_id
-     JOIN indicia.languages lg ON lg.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
-     LEFT JOIN indicia.cache_termlists_terms rc ON rc.id = a.reporting_category_id
+   FROM locations l
+     JOIN locations_websites lw ON lw.location_id = l.id AND lw.deleted = false
+     JOIN location_attribute_values av ON av.location_id = l.id AND av.deleted = false
+     JOIN location_attributes a ON a.id = av.location_attribute_id AND a.deleted = false
+     LEFT JOIN (termlists_terms tt
+     JOIN terms t ON t.id = tt.term_id
+     JOIN languages lg ON lg.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
+     LEFT JOIN cache_termlists_terms rc ON rc.id = a.reporting_category_id
   WHERE l.deleted = false
   ORDER BY a.id;
 
-CREATE OR REPLACE VIEW indicia.list_occurrence_attribute_values
+CREATE OR REPLACE VIEW list_occurrence_attribute_values
  AS
  SELECT av.id,
     o.id AS occurrence_id,
@@ -142,7 +142,7 @@ CREATE OR REPLACE VIEW indicia.list_occurrence_attribute_values
                 END
             END
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS value,
         CASE a.data_type
@@ -152,7 +152,7 @@ CREATE OR REPLACE VIEW indicia.list_occurrence_attribute_values
             WHEN 'B'::bpchar THEN av.int_value::text
             WHEN 'F'::bpchar THEN av.float_value::text
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS raw_value,
         CASE
@@ -164,20 +164,20 @@ CREATE OR REPLACE VIEW indicia.list_occurrence_attribute_values
     aw.website_id,
     rc.term AS reporting_category,
     rc.id AS reporting_category_id
-   FROM indicia.occurrences o
-     JOIN indicia.samples s ON s.id = o.sample_id AND s.deleted = false
-     JOIN indicia.surveys su ON su.id = s.survey_id AND su.deleted = false
-     JOIN indicia.occurrence_attributes_websites aw ON aw.website_id = su.website_id AND (aw.restrict_to_survey_id = su.id OR aw.restrict_to_survey_id IS NULL) AND aw.deleted = false
-     JOIN indicia.occurrence_attributes a ON a.id = aw.occurrence_attribute_id AND a.deleted = false
-     LEFT JOIN indicia.occurrence_attribute_values av ON av.occurrence_attribute_id = a.id AND av.occurrence_id = o.id AND av.deleted = false
-     LEFT JOIN (indicia.termlists_terms tt
-     JOIN indicia.terms t ON t.id = tt.term_id
-     JOIN indicia.languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
-     LEFT JOIN indicia.cache_termlists_terms rc ON rc.id = a.reporting_category_id
+   FROM occurrences o
+     JOIN samples s ON s.id = o.sample_id AND s.deleted = false
+     JOIN surveys su ON su.id = s.survey_id AND su.deleted = false
+     JOIN occurrence_attributes_websites aw ON aw.website_id = su.website_id AND (aw.restrict_to_survey_id = su.id OR aw.restrict_to_survey_id IS NULL) AND aw.deleted = false
+     JOIN occurrence_attributes a ON a.id = aw.occurrence_attribute_id AND a.deleted = false
+     LEFT JOIN occurrence_attribute_values av ON av.occurrence_attribute_id = a.id AND av.occurrence_id = o.id AND av.deleted = false
+     LEFT JOIN (termlists_terms tt
+     JOIN terms t ON t.id = tt.term_id
+     JOIN languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
+     LEFT JOIN cache_termlists_terms rc ON rc.id = a.reporting_category_id
   WHERE o.deleted = false
   ORDER BY a.id;
 
-CREATE OR REPLACE VIEW indicia.list_person_attribute_values
+CREATE OR REPLACE VIEW list_person_attribute_values
  AS
  SELECT av.id,
     p.id AS person_id,
@@ -218,7 +218,7 @@ CREATE OR REPLACE VIEW indicia.list_person_attribute_values
                 END
             END
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS value,
         CASE a.data_type
@@ -228,7 +228,7 @@ CREATE OR REPLACE VIEW indicia.list_person_attribute_values
             WHEN 'B'::bpchar THEN av.int_value::text
             WHEN 'F'::bpchar THEN av.float_value::text
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS raw_value,
         CASE
@@ -240,20 +240,20 @@ CREATE OR REPLACE VIEW indicia.list_person_attribute_values
     aw.website_id,
     rc.term AS reporting_category,
     rc.id AS reporting_category_id
-   FROM indicia.people p
-     LEFT JOIN indicia.person_attribute_values av ON av.person_id = p.id AND av.deleted = false
-     LEFT JOIN (indicia.users u
-     JOIN indicia.users_websites uw ON uw.user_id = u.id AND uw.site_role_id IS NOT NULL
-     JOIN indicia.person_attributes_websites aw ON aw.website_id = uw.website_id AND aw.deleted = false) ON u.person_id = p.id
-     JOIN indicia.person_attributes a ON (a.id = COALESCE(av.person_attribute_id, aw.person_attribute_id) OR a.public = true) AND (a.id = av.person_attribute_id OR av.id IS NULL) AND a.deleted = false
-     LEFT JOIN (indicia.termlists_terms tt
-     JOIN indicia.terms t ON t.id = tt.term_id
-     JOIN indicia.languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
-     LEFT JOIN indicia.cache_termlists_terms rc ON rc.id = a.reporting_category_id
+   FROM people p
+     LEFT JOIN person_attribute_values av ON av.person_id = p.id AND av.deleted = false
+     LEFT JOIN (users u
+     JOIN users_websites uw ON uw.user_id = u.id AND uw.site_role_id IS NOT NULL
+     JOIN person_attributes_websites aw ON aw.website_id = uw.website_id AND aw.deleted = false) ON u.person_id = p.id
+     JOIN person_attributes a ON (a.id = COALESCE(av.person_attribute_id, aw.person_attribute_id) OR a.public = true) AND (a.id = av.person_attribute_id OR av.id IS NULL) AND a.deleted = false
+     LEFT JOIN (termlists_terms tt
+     JOIN terms t ON t.id = tt.term_id
+     JOIN languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
+     LEFT JOIN cache_termlists_terms rc ON rc.id = a.reporting_category_id
   WHERE p.deleted = false
   ORDER BY a.id;
 
-CREATE OR REPLACE VIEW indicia.list_sample_attribute_values
+CREATE OR REPLACE VIEW list_sample_attribute_values
  AS
  SELECT av.id,
     s.id AS sample_id,
@@ -294,7 +294,7 @@ CREATE OR REPLACE VIEW indicia.list_sample_attribute_values
                 END
             END
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS value,
         CASE a.data_type
@@ -304,7 +304,7 @@ CREATE OR REPLACE VIEW indicia.list_sample_attribute_values
             WHEN 'B'::bpchar THEN av.int_value::text
             WHEN 'F'::bpchar THEN av.float_value::text
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS raw_value,
         CASE
@@ -316,19 +316,19 @@ CREATE OR REPLACE VIEW indicia.list_sample_attribute_values
     aw.website_id,
     rc.term AS reporting_category,
     rc.id AS reporting_category_id
-   FROM indicia.samples s
-     JOIN indicia.surveys su ON su.id = s.survey_id AND su.deleted = false
-     JOIN indicia.sample_attributes_websites aw ON aw.website_id = su.website_id AND (aw.restrict_to_survey_id = su.id OR aw.restrict_to_survey_id IS NULL) AND aw.deleted = false
-     JOIN indicia.sample_attributes a ON a.id = aw.sample_attribute_id AND a.deleted = false
-     LEFT JOIN indicia.sample_attribute_values av ON av.sample_attribute_id = a.id AND av.sample_id = s.id AND av.deleted = false
-     LEFT JOIN (indicia.termlists_terms tt
-     JOIN indicia.terms t ON t.id = tt.term_id
-     JOIN indicia.languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
-     LEFT JOIN indicia.cache_termlists_terms rc ON rc.id = a.reporting_category_id
+   FROM samples s
+     JOIN surveys su ON su.id = s.survey_id AND su.deleted = false
+     JOIN sample_attributes_websites aw ON aw.website_id = su.website_id AND (aw.restrict_to_survey_id = su.id OR aw.restrict_to_survey_id IS NULL) AND aw.deleted = false
+     JOIN sample_attributes a ON a.id = aw.sample_attribute_id AND a.deleted = false
+     LEFT JOIN sample_attribute_values av ON av.sample_attribute_id = a.id AND av.sample_id = s.id AND av.deleted = false
+     LEFT JOIN (termlists_terms tt
+     JOIN terms t ON t.id = tt.term_id
+     JOIN languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
+     LEFT JOIN cache_termlists_terms rc ON rc.id = a.reporting_category_id
   WHERE s.deleted = false
   ORDER BY a.id;
 
-CREATE OR REPLACE VIEW indicia.list_survey_attribute_values
+CREATE OR REPLACE VIEW list_survey_attribute_values
  AS
  SELECT av.id,
     s.id AS survey_id,
@@ -369,7 +369,7 @@ CREATE OR REPLACE VIEW indicia.list_survey_attribute_values
                 END
             END
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS value,
         CASE a.data_type
@@ -379,7 +379,7 @@ CREATE OR REPLACE VIEW indicia.list_survey_attribute_values
             WHEN 'B'::bpchar THEN av.int_value::text
             WHEN 'F'::bpchar THEN av.float_value::text
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS raw_value,
         CASE
@@ -391,17 +391,17 @@ CREATE OR REPLACE VIEW indicia.list_survey_attribute_values
     s.website_id,
     rc.term AS reporting_category,
     rc.id AS reporting_category_id
-   FROM indicia.surveys s
-     JOIN indicia.survey_attribute_values av ON av.survey_id = s.id AND av.deleted = false
-     JOIN indicia.survey_attributes a ON a.id = av.survey_attribute_id AND a.deleted = false
-     LEFT JOIN (indicia.termlists_terms tt
-     JOIN indicia.terms t ON t.id = tt.term_id
-     JOIN indicia.languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
-     LEFT JOIN indicia.cache_termlists_terms rc ON rc.id = a.reporting_category_id
+   FROM surveys s
+     JOIN survey_attribute_values av ON av.survey_id = s.id AND av.deleted = false
+     JOIN survey_attributes a ON a.id = av.survey_attribute_id AND a.deleted = false
+     LEFT JOIN (termlists_terms tt
+     JOIN terms t ON t.id = tt.term_id
+     JOIN languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
+     LEFT JOIN cache_termlists_terms rc ON rc.id = a.reporting_category_id
   WHERE s.deleted = false
   ORDER BY a.id;
 
-CREATE OR REPLACE VIEW indicia.list_taxa_taxon_list_attribute_values
+CREATE OR REPLACE VIEW list_taxa_taxon_list_attribute_values
  AS
  SELECT NULL::integer AS id,
     NULL::integer AS taxa_taxon_list_id,
@@ -433,10 +433,10 @@ CREATE OR REPLACE VIEW indicia.list_taxa_taxon_list_attribute_values
     tl.website_id,
     rc.term AS reporting_category,
     rc.id AS reporting_category_id
-   FROM indicia.taxa_taxon_list_attributes a
-     JOIN indicia.taxon_lists_taxa_taxon_list_attributes tlttla ON tlttla.taxa_taxon_list_attribute_id = a.id AND tlttla.deleted = false
-     JOIN indicia.taxon_lists tl ON tl.id = tlttla.taxon_list_id AND tl.deleted = false
-     LEFT JOIN indicia.cache_termlists_terms rc ON rc.id = a.reporting_category_id
+   FROM taxa_taxon_list_attributes a
+     JOIN taxon_lists_taxa_taxon_list_attributes tlttla ON tlttla.taxa_taxon_list_attribute_id = a.id AND tlttla.deleted = false
+     JOIN taxon_lists tl ON tl.id = tlttla.taxon_list_id AND tl.deleted = false
+     LEFT JOIN cache_termlists_terms rc ON rc.id = a.reporting_category_id
   WHERE a.deleted = false
 UNION
  SELECT av.id,
@@ -504,18 +504,18 @@ UNION
     tl.website_id,
     rc.term AS reporting_category,
     rc.id AS reporting_category_id
-   FROM indicia.taxa_taxon_lists ttl
-     JOIN indicia.taxon_lists_taxa_taxon_list_attributes tlttla ON tlttla.taxon_list_id = ttl.taxon_list_id AND tlttla.deleted = false
-     JOIN indicia.taxon_lists tl ON tl.id = tlttla.taxon_list_id AND tl.deleted = false
-     JOIN indicia.taxa_taxon_list_attributes a ON a.id = tlttla.taxa_taxon_list_attribute_id AND a.deleted = false
-     LEFT JOIN indicia.taxa_taxon_list_attribute_values av ON av.taxa_taxon_list_attribute_id = a.id AND av.taxa_taxon_list_id = ttl.id AND av.deleted = false
-     LEFT JOIN (indicia.termlists_terms tt
-     JOIN indicia.terms t ON t.id = tt.term_id AND t.deleted = false
-     JOIN indicia.languages l ON l.id = t.language_id AND l.deleted = false) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar AND tt.deleted = false
-     LEFT JOIN indicia.cache_termlists_terms rc ON rc.id = a.reporting_category_id
+   FROM taxa_taxon_lists ttl
+     JOIN taxon_lists_taxa_taxon_list_attributes tlttla ON tlttla.taxon_list_id = ttl.taxon_list_id AND tlttla.deleted = false
+     JOIN taxon_lists tl ON tl.id = tlttla.taxon_list_id AND tl.deleted = false
+     JOIN taxa_taxon_list_attributes a ON a.id = tlttla.taxa_taxon_list_attribute_id AND a.deleted = false
+     LEFT JOIN taxa_taxon_list_attribute_values av ON av.taxa_taxon_list_attribute_id = a.id AND av.taxa_taxon_list_id = ttl.id AND av.deleted = false
+     LEFT JOIN (termlists_terms tt
+     JOIN terms t ON t.id = tt.term_id AND t.deleted = false
+     JOIN languages l ON l.id = t.language_id AND l.deleted = false) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar AND tt.deleted = false
+     LEFT JOIN cache_termlists_terms rc ON rc.id = a.reporting_category_id
   WHERE ttl.deleted = false;
 
-CREATE OR REPLACE VIEW indicia.list_termlists_term_attribute_values
+CREATE OR REPLACE VIEW list_termlists_term_attribute_values
  AS
  SELECT av.id,
     tlt.id AS termlists_term_id,
@@ -556,7 +556,7 @@ CREATE OR REPLACE VIEW indicia.list_termlists_term_attribute_values
                 END
             END
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS value,
         CASE a.data_type
@@ -566,7 +566,7 @@ CREATE OR REPLACE VIEW indicia.list_termlists_term_attribute_values
             WHEN 'B'::bpchar THEN av.int_value::text
             WHEN 'F'::bpchar THEN av.float_value::text
             WHEN 'D'::bpchar THEN av.date_start_value::text
-            WHEN 'V'::bpchar THEN indicia.vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
+            WHEN 'V'::bpchar THEN vague_date_to_raw_string(av.date_start_value, av.date_end_value, av.date_type_value)::text
             ELSE NULL::text
         END AS raw_value,
         CASE
@@ -578,13 +578,13 @@ CREATE OR REPLACE VIEW indicia.list_termlists_term_attribute_values
     tl.website_id,
     rc.term AS reporting_category,
     rc.id AS reporting_category_id
-   FROM indicia.termlists_terms tlt
-     JOIN indicia.termlists_term_attribute_values av ON av.termlists_term_id = tlt.id AND av.deleted = false
-     JOIN indicia.termlists_term_attributes a ON a.id = av.termlists_term_attribute_id AND a.deleted = false
-     JOIN indicia.termlists tl ON tl.id = tlt.termlist_id AND tl.deleted = false
-     LEFT JOIN (indicia.termlists_terms tt
-     JOIN indicia.terms t ON t.id = tt.term_id
-     JOIN indicia.languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
-     LEFT JOIN indicia.cache_termlists_terms rc ON rc.id = a.reporting_category_id
+   FROM termlists_terms tlt
+     JOIN termlists_term_attribute_values av ON av.termlists_term_id = tlt.id AND av.deleted = false
+     JOIN termlists_term_attributes a ON a.id = av.termlists_term_attribute_id AND a.deleted = false
+     JOIN termlists tl ON tl.id = tlt.termlist_id AND tl.deleted = false
+     LEFT JOIN (termlists_terms tt
+     JOIN terms t ON t.id = tt.term_id
+     JOIN languages l ON l.id = t.language_id) ON tt.id = av.int_value AND a.data_type = 'L'::bpchar
+     LEFT JOIN cache_termlists_terms rc ON rc.id = a.reporting_category_id
   WHERE tlt.deleted = false
   ORDER BY a.id;
