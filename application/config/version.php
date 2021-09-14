65df51d9f41f237e5a9380fe4529a3812f295075
Jim Bacon <jame2@ceh.ac.uk>
2021-09-14 17:39:24 +0100

    Bump version

--- application/config/version.php
+++ application/config/version.php
@@ -29,14 +29,14 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '6.2.14';
+$config['version'] = '6.2.15';
 
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2021-09-13';
+$config['release_date'] = '2021-09-14';
 
 /**
  * Link to the code repository downloads page.
