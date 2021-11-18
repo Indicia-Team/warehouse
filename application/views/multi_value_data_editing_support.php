fb1940ef82a7a59b6092f406f9864039421a8dbb
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-11-18 11:41:49 +0000

    Linting

--- application/views/multi_value_data_editing_support.php
+++ application/views/multi_value_data_editing_support.php
@@ -1,44 +1,48 @@
 <?php
+
 /**
- * Organise attributes
- * 
- * The $values['attributes'] array has multi-value attributes on separate rows, so organise these into a sub array
+ * Organise attributes.
+ *
+ * The $values['attributes'] array has multi-value attributes on separate rows,
+ * so organise these into a sub array.
  */
 function organise_values_attribute_array($attributeModel, $valuesAttributes) {
-  $readAuth = data_entry_helper::get_read_auth(0 - $_SESSION['auth_user']->id, kohana::config('indicia.private_key'));
-  $attrsWithMulti = array();
+  $attrsWithMulti = [];
   foreach ($valuesAttributes as $key => $attr) {
     if (!empty($attr['multi_value']) && $attr['multi_value'] === 't') {
-      // The $valuesAttributes array actually has multi-value attributes on separate rows
-      // This means we need to gather all the rows into sub array.
-      $attrsWithMulti[$attr[$attributeModel.'_id']][] = $attr;
-    } else {
-      // Single value attributes have just one row, so can save that directly
-      $attrsWithMulti[$attr[$attributeModel.'_id']] = $attr;
+      // The $valuesAttributes array actually has multi-value attributes on
+      // separate rows. This means we need to gather all the rows into sub
+      // array.
+      $attrsWithMulti[$attr[$attributeModel . '_id']][] = $attr;
+    }
+    else {
+      // Single value attributes have just one row, so can save that directly.
+      $attrsWithMulti[$attr[$attributeModel . '_id']] = $attr;
     }
   }
   return $attrsWithMulti;
 }
 
 /**
- * Handle multi value attributes
- * 
- * Draw multi value attributes to the screen
+ * Handle multi-value attributes.
+ *
+ * Draw multi value attributes to the screen.
  */
 function handle_multi_value_attributes($fieldPrefix, $attributeId, $multiAttr, &$values) {
   $readAuth = data_entry_helper::get_read_auth(0 - $_SESSION['auth_user']->id, kohana::config('indicia.private_key'));
-  $valueToInsert = array();
+  $valueToInsert = [];
   $attrCaption = '';
-  $attrTermlistId = null;
-  $default = array();
-  // Multi-attributes are over multiple rows so cycle subarray
+  $attrTermlistId = NULL;
+  $default = [];
+  // Multi-attributes are over multiple rows so cycle subarray.
   foreach ($multiAttr as $multiAttrElementIdx => $multiAttrElement) {
     $attrCaption = $multiAttrElement['caption'];
-    $name = $fieldPrefix . ':' . $attributeId.'[]';
+    $name = "$fieldPrefix:$attributeId" . '[]';
     if (!empty($multiAttrElement['termlist_id'])) {
       $attrTermlistId = $multiAttrElement['termlist_id'];
       foreach ($values['terms_' . $multiAttrElement['termlist_id']] as $number => $word) {
-        // Remove this element from the drop-downs as we don't need it and it causes problems
+        // Remove this element from the drop-downs as we don't need it and it
+        // causes problems.
         if ($word === '-no value-') {
           unset($values['terms_' . $multiAttrElement['termlist_id']][$number]);
         }
@@ -49,36 +53,43 @@ function handle_multi_value_attributes($fieldPrefix, $attributeId, $multiAttr, &
       case 'L':
         $datatype = 'lookup';
         break;
+
       case 'T':
         $datatype = 'text';
         break;
+
       default:
         $datatype = 'unsupported';
     }
-    // Set up default value for existing data
+    // Set up default value for existing data.
     if (!empty($multiAttrElement['raw_value'])) {
       $valueToInsert = json_decode($multiAttrElement['raw_value']);
-      $valueToInsert = (array)$valueToInsert;
+      $valueToInsert = (array) $valueToInsert;
       $valueToInsert = $valueToInsert[0];
-      $default[]=array(
-        'fieldname' => $fieldPrefix . ':' .$attributeId.':'.$multiAttrElement['id'],
-        'default' => json_encode(array($valueToInsert)),
-        'defaultUpper' => null,
-      );
+      $default[] = [
+        'fieldname' => "$fieldPrefix:$attributeId:$multiAttrElement[id]",
+        'default' => json_encode([$valueToInsert]),
+        'defaultUpper' => NULL,
+      ];
     }
   }
   if ($datatype !== 'unsupported') {
     if ($datatype === 'lookup') {
       $columns = [
-        '0' => array('label' => $attrCaption,
-        'datatype' => $datatype,
-        'lookupValues' => $values['terms_' . $attrTermlistId],
-        'termlist_id' => $attrTermlistId)
+        '0' => [
+          'label' => $attrCaption,
+          'datatype' => $datatype,
+          'lookupValues' => $values['terms_' . $attrTermlistId],
+          'termlist_id' => $attrTermlistId,
+        ],
       ];
-    } elseif ($datatype === 'text') {
+    }
+    elseif ($datatype === 'text') {
       $columns = [
-        '0' => array('label' => $attrCaption,
-        'datatype' => $datatype)
+        '0' => [
+          'label' => $attrCaption,
+          'datatype' => $datatype,
+        ],
       ];
     }
     echo data_entry_helper::complex_attr_grid([
@@ -86,11 +97,11 @@ function handle_multi_value_attributes($fieldPrefix, $attributeId, $multiAttr, &
       'columns' => $columns,
       'default' => $default,
       'defaultRows' => 1,
-      'extraParams' => $readAuth
-    ])."<br>";
-  } else {
-    echo "The multi-value attribute \"".$multiAttrElement['caption']."\" (of type \"".$multiAttrElement['data_type']."\") has been specified. 
+      'extraParams' => $readAuth,
+    ]) . "<br>";
+  }
+  else {
+    echo "The multi-value attribute \"$multiAttrElement[caption]\" (of type \"$multiAttrElement[data_type]\") has been specified.
     Please note that the Warehouse UI currently only supports text or lookup multi-value attribute data editing.<br>";
   }
 }
-?>
