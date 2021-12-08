2ae7ba2f835275f701cc49da068f7940718207a8
John van Breda <john.vanbreda@biodiverseit.co.uk>
2021-12-08 11:21:28 +0000

    Created_by_id useful for filtering locations

---
+++ modules/indicia_setup/db/version_6_13_0/201212081112_locations_views.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE VIEW list_locations AS
+ SELECT l.id,
+    l.name,
+    l.code,
+    l.centroid_sref,
+    lw.website_id,
+    l.location_type_id,
+    l.parent_id,
+    l.created_by_id
+   FROM locations l
+     LEFT JOIN locations_websites lw ON l.id = lw.location_id AND lw.deleted = false
+  WHERE l.deleted = false;
