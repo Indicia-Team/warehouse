30366ecab6203d9c8ac429332c51eaafa6818835
Jim Bacon <jame2@ceh.ac.uk>
2022-02-07 16:51:29 +0000

    Bump version

--- application/config/version.php
+++ application/config/version.php
@@ -29,7 +29,7 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '6.12.9';
+$config['version'] = '6.12.10';
 
 
 /**
@@ -37,7 +37,7 @@ $config['version'] = '6.12.9';
  *
  * @var string
  */
-$config['release_date'] = '2022-02-03';
+$config['release_date'] = '2022-02-07';
 
 
 /**
