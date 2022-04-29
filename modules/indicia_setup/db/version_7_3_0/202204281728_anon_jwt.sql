e52bbb0e583df9de98dbe30ae448a4035aebcb37
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-04-29 10:01:14 +0100

    Website option to allow anonymous JWT post
    https://github.com/NERC-CEH/asian-hornet-watch-app/issues/57

---
+++ modules/indicia_setup/db/version_7_3_0/202204281728_anon_jwt.sql
@@ -0,0 +1,4 @@
+ALTER TABLE websites
+  ADD COLUMN allow_anon_jwt_post boolean not null default false;
+
+COMMENT ON COLUMN websites.allow_anon_jwt_post IS 'Set to TRUE if anonymous JWT tokens which don''t claim a user ID allow new records to be POSTed.';
