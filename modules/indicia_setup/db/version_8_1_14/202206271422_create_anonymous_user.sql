2b777b98aa3c1e518e3f0cc0096cd7d13d4cc9dd
Andrew van Breda <support@avb-it.co.uk>
2022-06-28 23:18:08 +0100

    Fix accident removal of code before commit

--- modules/indicia_setup/db/version_8_1_14/202206271422_create_anonymous_user.sql
+++ modules/indicia_setup/db/version_8_1_14/202206271422_create_anonymous_user.sql
@@ -19,3 +19,20 @@ VALUES (
   1,
   'indicia:anonymous'
 );
+
+INSERT INTO users (
+  person_id,
+  created_on,
+  created_by_id,
+  updated_on,
+  updated_by_id,
+  username
+)
+VALUES (
+  (select id from people where external_key = 'indicia:anonymous' AND deleted = false),
+  now(),
+  1,
+  now(),
+  1,
+  'anonymous'
+);
