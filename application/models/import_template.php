4e46d363fedcd583af233640d2e86d4403f7780b
John van Breda <john.vanbreda@biodiverseit.co.uk>
2022-07-28 08:22:40 +0100

    Table for storing import configuration templates

---
+++ application/models/import_template.php
@@ -0,0 +1,39 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Indicia, the OPAL Online Recording Toolkit.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
+ *
+ * @author Indicia Team
+ * @license http://www.gnu.org/licenses/gpl.html GPL
+ * @link https://github.com/indicia-team/warehouse
+ */
+
+/**
+ * Model class for the import_templates table.
+ */
+class Import_template_Model extends ORM {
+
+  public function validate(Validation $array, $save = FALSE) {
+    // Cleanup leading/trailing whitespace.
+    $array->pre_filter('trim');
+    $array->add_rules('entity', 'required');
+    $array->add_rules('mappings', 'required');
+    $array->add_rules('global_values', 'required');
+    $array->add_rules('group_id', 'integer');
+
+    $this->unvalidatedFields = [];
+    return parent::validate($array, $save);
+  }
+
+}
