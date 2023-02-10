2ff4e93e5b3c36be78907bc88ccf2c7845f26d01
John van Breda <john.vanbreda@biodiverseit.co.uk>
2023-02-10 14:31:55 +0000

    Add redetermination info to comment data
    See https://github.com/BiologicalRecordsCentre/iRecord/issues/1362

---
+++ modules/indicia_setup/db/version_8_16_0/202302101029_notifications_redet.sql
@@ -0,0 +1,8 @@
+-- #slow script#
+
+ALTER TABLE notifications
+  ADD COLUMN redet_taxa_taxon_list_id integer;
+
+COMMENT ON COLUMN notifications.redet_taxa_taxon_list_id IS
+  'If a comment is for a redetermination, provides the ID of the taxon it was redetermined to. '
+  'No constraint added as it is just a snapshot to indicate this was a redet, so performance overhead not worthwhile.';
