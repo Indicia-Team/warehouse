ea5a4e12023ac2086371f31954a7fa6f77dd13e2
John van Breda <john.vanbreda@biodiverseit.co.uk>
2023-03-09 13:05:58 +0000

    Import v2 uses importTools helper
    To share code with other importer classes.

--- modules/indicia_svc_import/controllers/services/import_2.php
+++ modules/indicia_svc_import/controllers/services/import_2.php
@@ -32,10 +32,6 @@ define('SYSTEM_FIELD_NAMES', [
 
 require 'vendor/autoload.php';
 
-use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
-use PhpOffice\PhpSpreadsheet\Reader\Xls;
-use PhpOffice\PhpSpreadsheet\Reader\Csv;
-use PhpOffice\PhpSpreadsheet\Reader\IReadFilter;
 use PhpOffice\PhpSpreadsheet\Shared\Date as ImportDate;
 
 /**
@@ -47,70 +43,6 @@ class RequestAbort extends Exception {}
  * Exception class for failure to find item in a list.
  */
 class NotFoundException extends Exception {}
-
-/**
- * PHPSpreadsheet filter for reading the header row.
- */
-class FirstRowReadFilter implements IReadFilter {
-
-  /**
-   * Enable reading of row 1 only.
-   *
-   * @param int $column
-   *   Column number - ignored.
-   * @param int $row
-   *   Row number.
-   * @param string $worksheetName
-   *   Worksheet name - ignored.
-   */
-  public function readCell($column, $row, $worksheetName = '') {
-    return $row == 1;
-  }
-
-}
-
-/**
- * PHPSpreadsheet filter for reading a range of data rows.
- */
-class RangeReadFilter implements IReadFilter {
-
-  /**
-   * Start row to read from.
-   *
-   * @var int
-   */
-  private $offset;
-
-  /**
-   * Number of rows to read.
-   *
-   * @var int
-   */
-  private $limit;
-
-  /**
-   * Object constructor, sets offset and limit.
-   */
-  public function __construct($offset, $limit) {
-    $this->offset = $offset;
-    $this->limit = $limit;
-  }
-
-  /**
-   * Enable reading of only the rows that are in range.
-   *
-   * @param int $column
-   *   Column number - ignored.
-   * @param int $row
-   *   Row number.
-   * @param string $worksheetName
-   *   Worksheet name - ignored.
-   */
-  public function readCell($column, $row, $worksheetName = '') {
-    return $row >= $this->offset && $row < $this->offset + $this->limit;
-  }
-
-}
 
 /**
  * Controller class for import web services.
@@ -160,6 +92,7 @@ class Import_2_Controller extends Service_Base_Controller {
     $this->authenticate('read');
     switch ($entity) {
       case 'sample':
+      case 'occurrence':
         $attrTypeFilter = empty($_GET['sample_method_id']) ? NULL : $_GET['sample_method_id'];
         break;
 
@@ -212,49 +145,12 @@ class Import_2_Controller extends Service_Base_Controller {
     try {
       // Ensure we have write permissions.
       $this->authenticate();
-      // We will be using a POST array to send data, and presumably a FILES
-      // array for the media.
-      // Upload size.
-      $ups = Kohana::config('indicia.maxUploadSize');
-      $validatedFiles = Validation::factory($_FILES)->add_rules(
-        'media_upload',
-        'upload::valid',
-        'upload::required',
-        'upload::type[csv,xls,xlsx,zip]',
-        "upload::size[$ups]"
-      );
-      if (count($validatedFiles) === 0) {
-        echo json_encode([
-          'error' => 'No file was uploaded.',
-        ]);
-      }
-      elseif ($validatedFiles->validate()) {
-        $safeFileName = str_replace(' ', '_', strtolower($validatedFiles['media_upload']['name']));
-        if (array_key_exists('name_is_guid', $_POST) && $_POST['name_is_guid'] == 'true') {
-          $finalName = $safeFileName;
-        }
-        else {
-          $finalName = uniqid() . '_' . $safeFileName;
-        }
-        $fTmp = upload::save('media_upload', $finalName, DOCROOT . 'import');
-        echo json_encode([
-          'status' => 'ok',
-          'uploadedFile' => basename($fTmp),
-        ]);
-      }
-      else {
-        kohana::log('error', 'Validation errors uploading file ' . $validatedFiles['media_upload']['name']);
-        kohana::log('error', print_r($validatedFiles->errors('form_error_messages'), TRUE));
-        foreach ($validatedFiles as $file) {
-          if (!empty($file['error'])) {
-            kohana::log('error', 'PHP reports file upload error: ' . $this->fileValidationcodeToMessage($file['error']));
-          }
-        }
-        http_response_code(400);
-        echo json_encode([
-          'error' => $validatedFiles->errors('form_error_messages'),
-        ]);
-      }
+      $importTools = new ImportTools();
+      $uploadedFile = $importTools->uploadFile();
+      echo json_encode([
+        'status' => 'ok',
+        'uploadedFile' => basename($uploadedFile),
+      ]);
     }
     catch (Exception $e) {
       kohana::log('debug', 'Error in upload_file: ' . $e->getMessage());
@@ -282,29 +178,8 @@ class Import_2_Controller extends Service_Base_Controller {
       if (empty($_POST['uploaded-file'])) {
         throw new exception('Parameter uploaded-file is required for file extraction');
       }
-      if (!file_exists(DOCROOT . 'import/' . $_POST['uploaded-file'])) {
-        throw new exception('Parameter uploaded-file refers to a missing file');
-      }
-      $zip = new ZipArchive();
-      $res = $zip->open(DOCROOT . 'import/' . $_POST['uploaded-file']);
-      if ($res !== TRUE) {
-        throw new exception('The Zip archive could not be opened.');
-      }
-      if ($zip->count() !== 1) {
-        throw new exception('The Zip archive must contain only one file.');
-      }
-      $ext = pathinfo($zip->getNameIndex(0), PATHINFO_EXTENSION);
-      if (!in_array($ext, ['csv', 'xls', 'xlsx'])) {
-        throw new exception('The Zip archive contains a file type that cannot be imported.');
-      }
-      $fileName = uniqid(TRUE) . ".$ext";
-      if (!$zip->renameIndex(0, $fileName)) {
-        throw new exception('Unable to rename the file in the Zip archive.');
-      }
-      if (!$zip->extractTo(DOCROOT . "import", $fileName)) {
-        throw new exception('Unable to unzip the Zip archive.');
-      };
-      $zip->close();
+      $importTools = new ImportTools();
+      $fileName = $importTools->extractFile($_POST['uploaded-file']);
       echo json_encode([
         'status' => 'ok',
         'dataFile' => $fileName,
@@ -1295,7 +1170,7 @@ SQL;
       'termlists_term' => 'trmAttr',
       'person' => 'psnAttr',
     ];
-    return isset($entityPrefixes[$entity]) ? $entityPrefixes[$entity] : NULL;
+    return $entityPrefixes[$entity] ?? NULL;
   }
 
   /**
@@ -1317,7 +1192,7 @@ SQL;
       'trmAttr' => 'termlists_term',
       'psnAttr' => 'person',
     ];
-    return isset($entitiesByPrefix[$prefix]) ? $entitiesByPrefix[$prefix] : NULL;
+    return $entitiesByPrefix[$prefix] ?? NULL;
   }
 
   /**
@@ -1913,7 +1788,8 @@ SQL;
    *   File upload config.
    */
   private function loadNextRecordsBatch($fileName, array &$config) {
-    $file = $this->openSpreadsheet($fileName, $config);
+    $importTools = new ImportTools();
+    $file = $importTools->openSpreadsheet($fileName, $config, BATCH_ROW_LIMIT);
     $rowPos = 0;
     $count = 0;
     $rows = [];
@@ -1921,7 +1797,7 @@ SQL;
     while (($rowPos < BATCH_ROW_LIMIT) && ($data = $this->getNextRow($file, $rowPos + $config['rowsRead'] + 1, $config))) {
       // Nulls need to be empty strings for trim() to work.
       $data = array_map(function ($value) {
-        return $value === NULL ? '' : $value;
+        return $value ?? '';
       }, $data);
       // Skip empty rows.
       if (!empty(implode('', $data))) {
@@ -2011,6 +1887,7 @@ SQL;
       $parentEntity = 'sample';
       $model = ORM::Factory($parentEntity);
       $parentSupportsImportGuid = in_array('import_guid', array_keys($model->as_array()));
+      $importTools = new ImportTools();
       // Create a new config object.
       return [
         'fileName' => $fileName,
@@ -2018,7 +1895,7 @@ SQL;
         'isExcel' => in_array($ext, ['xls', 'xlsx']),
         'entity' => $entity,
         'parentEntity' => $parentEntity,
-        'columns' => $this->loadColumnNamesFromFile($fileName),
+        'columns' => $this->tidyUpColumnsList($importTools->loadColumnNamesFromFile($fileName)),
         'systemAddedColumns' => [],
         'state' => 'initial',
         // Rows loaded into the temp table (excludes blanks).
@@ -2026,7 +1903,7 @@ SQL;
         // Rows read from the import file (includes blanks).
         'rowsRead' => 0,
         'progress' => 0,
-        'totalRows' => $this->getTotalRows($fileName),
+        'totalRows' => $importTools->getTotalRows($fileName),
         'rowsInserted' => 0,
         'rowsUpdated' => 0,
         'rowsProcessed' => 0,
@@ -2068,59 +1945,6 @@ SQL;
     unlink(DOCROOT . "import/$baseName.json");
     unlink(DOCROOT . "import/$config[fileName]");
     $db->query("DROP TABLE IF EXISTS import_temp.$config[tableName]");
-  }
-
-  /**
-   * Opens a PHPSpreadsheet Reader for the selected file.
-   *
-   * @param string $fileName
-   *   Name of the import file.
-   *
-   * @return PhpOffice\PhpSpreadsheet\Reader
-   *   Reader object.
-   */
-  private function getReader($fileName) {
-    $ext = pathinfo($fileName, PATHINFO_EXTENSION);
-    if ($ext === 'csv') {
-      $reader = new Csv();
-      $reader->setInputEncoding(Csv::GUESS_ENCODING);
-    }
-    elseif ($ext === 'xlsx') {
-      $reader = new Xlsx();
-    }
-    elseif ($ext === 'xls') {
-      $reader = new Xls();
-    }
-    else {
-      error_logger::log_trace(debug_backtrace());
-      throw new exception("Unsupported file type \"$ext\" for file \"$fileName\".");
-    }
-    // Don't read document formatting.
-    $reader->setReadDataOnly(TRUE);
-    return $reader;
-  }
-
-  /**
-   * Return number of rows to import in the file.
-   *
-   * For CSV, the file size in bytes which can be compared with filepos to get
-   * progress info. For PHPSpreadsheet files, the worksheet's size.
-   *
-   * @param string $fileName
-   *   Name of the import file.
-   *
-   * @return int
-   *   A representation of the file size.
-   */
-  private function getTotalRows($fileName) {
-    $reader = $this->getReader($fileName);
-    // Minimise data read from spreadsheet - first sheet only.
-    $worksheetData = $reader->listWorksheetInfo(DOCROOT . "import/$fileName");
-    if (count($worksheetData) === 0) {
-      throw new exception('Spreadsheet contains no worksheets');
-    }
-    // Subtract 1 to exclude header.
-    return $worksheetData[0]['totalRows'] - 1;
   }
 
   /**
@@ -2171,57 +1995,6 @@ SQL;
       ];
     }
     return $colsAndFieldInfo;
-  }
-
-  /**
-   * Uses the first row of data to build a list of columns in the import file.
-   *
-   * @param string $fileName
-   *   The data file to load.
-   *
-   * @return array
-   *   List of columns in the file as array keys, with values being the
-   *   suggested field names.
-   */
-  private function loadColumnNamesFromFile($fileName) {
-    $reader = $this->getReader($fileName);
-    // Minimise data read from spreadsheet - first sheet only.
-    $worksheetData = $reader->listWorksheetInfo(DOCROOT . "import/$fileName");
-    if (count($worksheetData) === 0) {
-      throw new exception('Spreadsheet contains no worksheets');
-    }
-    $reader->setLoadSheetsOnly($worksheetData[0]['worksheetName']);
-    // Only read first row.
-    $reader->setReadFilter(new FirstRowReadFilter());
-    $file = $reader->load(DOCROOT . "import/$fileName");
-    $data = $file->getActiveSheet()->toArray();
-    if (count($data) === 0) {
-      throw new exception('The spreadsheet file is empty');
-    }
-    return $this->tidyUpColumnsList($data[0]);
-  }
-
-  /**
-   * Opens a CSV or spreadsheet file and winds to current rowsRead offset.
-   *
-   * @param string $fileName
-   *   The data file to open.
-   * @param array $config
-   *   Import config information.
-   */
-  private function openSpreadsheet($fileName, array &$config) {
-    $reader = $this->getReader($fileName);
-    // Minimise data read from spreadsheet - first sheet only.
-    $worksheetData = $reader->listWorksheetInfo(DOCROOT . "import/$fileName");
-    if (count($worksheetData) === 0) {
-      throw new exception('Spreadsheet contains no worksheets');
-    }
-    $reader->setLoadSheetsOnly($worksheetData[0]['worksheetName']);
-    // Add two to the range start, as it is indexed from one not zero unlike
-    // the data array read out and we skip the header row.
-    $reader->setReadFilter(new RangeReadFilter($config['rowsRead'] + 2, BATCH_ROW_LIMIT));
-    $file = $reader->load(DOCROOT . "import/$fileName");
-    return $file->getActiveSheet()->toArray();
   }
 
   /**
