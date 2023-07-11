829a82c42a28e0af0bf12701c23e01453cb92cba
Andrew van Breda <support@avb-it.co.uk>
2023-07-11 16:33:43 +0100

    Code improved so that when a new occurrences is inserted and its parent sample is training, then the occurrence is training to. This is in addition to previous logic where is a sample is updated to training, then all the occurrences of that sample become training.

--- modules/indicia_setup/db/version_8_21_3/202307111300_set_occurrences_to_training_trigger.sql
+++ modules/indicia_setup/db/version_8_21_3/202307111300_set_occurrences_to_training_trigger.sql
@@ -1,25 +1,50 @@
-DROP TRIGGER IF EXISTS set_occurrences_to_training_trigger ON samples;
+DROP TRIGGER IF EXISTS set_sample_occurrences_to_training_trigger ON samples;
 
-DROP FUNCTION IF EXISTS set_occurrences_to_training();
+DROP FUNCTION IF EXISTS set_sample_occurrences_to_training();
 
-CREATE OR REPLACE FUNCTION set_occurrences_to_training()
+CREATE OR REPLACE FUNCTION set_sample_occurrences_to_training()
   RETURNS trigger AS
   $BODY$
     BEGIN
-	IF (OLD.training = false AND NEW.training = true) THEN
-		UPDATE occurrences
-		SET training = true, updated_on=now() 
-		FROM samples s
-		WHERE sample_id = s.id and s.id=OLD.id;
-	END IF;
-	RETURN OLD;
+      IF (OLD.training = false AND NEW.training = true) THEN
+        UPDATE occurrences
+        SET training = true, updated_on=now()
+        WHERE sample_id = NEW.id;
+      END IF;
+      RETURN OLD;
     END;
     $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 
-CREATE TRIGGER set_occurrences_to_training_trigger
+
+CREATE TRIGGER set_sample_occurrences_to_training_trigger
   AFTER UPDATE
   ON samples
   FOR EACH ROW
-  EXECUTE PROCEDURE set_occurrences_to_training();
+  EXECUTE PROCEDURE set_sample_occurrences_to_training();
+
+DROP TRIGGER IF EXISTS set_occurrence_to_training_from_sample_trigger ON occurrences;
+
+DROP FUNCTION IF EXISTS set_occurrence_to_training_from_sample();
+
+CREATE OR REPLACE FUNCTION set_occurrence_to_training_from_sample()
+  RETURNS trigger AS
+  $BODY$
+    BEGIN
+      UPDATE occurrences o
+      SET training = true, updated_on=now()
+      FROM samples s
+      WHERE o.sample_id = s.id AND o.id=NEW.id AND s.training = true;
+      RETURN NEW;
+    END;
+    $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+
+CREATE TRIGGER set_occurrence_to_training_from_sample_trigger
+  AFTER INSERT
+  ON occurrences
+  FOR EACH ROW
+  EXECUTE PROCEDURE set_occurrence_to_training_from_sample();
