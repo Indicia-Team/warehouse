4f3eb2241f22cd0295d0ccd5a778de0bc9bd111b
John van Breda <john.vanbreda@biodiverseit.co.uk>
2023-10-31 13:58:03 +0000

    Fix install error on PHP 8.1
    Fixes https://github.com/Indicia-Team/warehouse/issues/431
    
    (cherry picked from commit 3121d22c4f44680b58d7427e73231a0d1ee06a66)

--- system/vendor/swift/Swift/Message/Encoder.php
+++ system/vendor/swift/Swift/Message/Encoder.php
@@ -381,7 +381,7 @@ class Swift_Message_Encoder
     |\xF0[\x90-\xBF][\x80-\xBF]{2}		# planes 1-3
     |[\xF1-\xF3][\x80-\xBF]{3}			# planes 4-15
     |\xF4[\x80-\x8F][\x80-\xBF]{2}		# plane 16
-    )+%xs', $data);
+    )+%xs', $data ?? '');
   }
   /**
    * This function checks for 7bit *printable* characters
