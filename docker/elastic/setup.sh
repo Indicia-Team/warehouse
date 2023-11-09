fd1f24c7d3854842f35f2dcd59217f36a88783b2
Jim Bacon <jame2@ceh.ac.uk>
2023-11-09 19:20:03 +0000

    docker: Disable all Elasticsearch security

--- docker/elastic/setup.sh
+++ docker/elastic/setup.sh
@@ -11,42 +11,43 @@ response=$(curl \
 if [ $response = 404 ]; then
   echo "Setting up ElasticSearch"
 
-  echo "Setting kibana_system password."
-  # (But, you will log in to the Kibana UI as 'elastic')
-  curl localhost:9200/_security/user/kibana_system/_password \
-    --silent \
-    --request POST \
-    --user elastic:password \
-    --header 'Content-Type: application/json' \
-    --data '{"password":"password"}' \
-    --output outputfile
+  # ELASTIC SECURITY
+  # echo "Setting kibana_system password."
+  # # (But, you will log in to the Kibana UI as 'elastic')
+  # curl localhost:9200/_security/user/kibana_system/_password \
+  #   --silent \
+  #   --request POST \
+  #   --user elastic:password \
+  #   --header 'Content-Type: application/json' \
+  #   --data '{"password":"password"}' \
+  #   --output outputfile
 
-  echo "Setting logstash_system password."
-  curl localhost:9200/_security/user/logstash_system/_password \
-    --silent \
-    --request POST \
-    --user elastic:password \
-    --header 'Content-Type: application/json' \
-    --data '{"password":"password"}' \
-    --output outputfile
+  # echo "Setting logstash_system password."
+  # curl localhost:9200/_security/user/logstash_system/_password \
+  #   --silent \
+  #   --request POST \
+  #   --user elastic:password \
+  #   --header 'Content-Type: application/json' \
+  #   --data '{"password":"password"}' \
+  #   --output outputfile
 
-  echo "Creating logstash_writer role."
-  curl localhost:9200/_security/role/logstash_writer \
-    --silent \
-    --request POST \
-    --user elastic:password \
-    --header 'Content-Type: application/json' \
-    --data @elastic/setup/role_logstash_writer.json \
-    --output outputfile
+  # echo "Creating logstash_writer role."
+  # curl localhost:9200/_security/role/logstash_writer \
+  #   --silent \
+  #   --request POST \
+  #   --user elastic:password \
+  #   --header 'Content-Type: application/json' \
+  #   --data @elastic/setup/role_logstash_writer.json \
+  #   --output outputfile
 
-  echo "Creating logstash_pipeline user."
-  curl localhost:9200/_security/user/logstash_pipeline \
-    --silent \
-    --request POST \
-    --user elastic:password \
-    --header 'Content-Type: application/json' \
-    --data @elastic/setup/user_logstash_pipeline.json \
-    --output outputfile
+  # echo "Creating logstash_pipeline user."
+  # curl localhost:9200/_security/user/logstash_pipeline \
+  #   --silent \
+  #   --request POST \
+  #   --user elastic:password \
+  #   --header 'Content-Type: application/json' \
+  #   --data @elastic/setup/user_logstash_pipeline.json \
+  #   --output outputfile
 
   echo "Creating occurrence index."
   curl localhost:9200/occurrence_brc1_v1 \
