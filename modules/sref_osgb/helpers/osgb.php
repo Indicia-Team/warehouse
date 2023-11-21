b7d8f25974acf2a868a2473d7f05e98b0f24642d
John van Breda <john.vanbreda@biodiverseit.co.uk>
2023-11-21 09:49:23 +0000

    Linting

--- modules/sref_osgb/helpers/osgb.php
+++ modules/sref_osgb/helpers/osgb.php
@@ -13,39 +13,40 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/gpl.html.
  *
- * @package Modules
- * @subpackage OSGB Grid References
- * @author  Indicia Team
+ * @author Indicia Team
  * @license http://www.gnu.org/licenses/gpl.html GPL 3.0
- * @link  https://github.com/indicia-team/warehouse/
+ * @link https://github.com/indicia-team/warehouse/
  */
 
 /**
  * Conversion class for OSGB grid references.
- * @package Modules
- * @subpackage OSGB Grid References
- * @author  Indicia Team
  */
 class osgb {
 
   /**
-   * Returns true if the spatial reference is a recognised British National Grid square.
+   * Is the string a valid grid reference?
    *
-   * @param $sref string Spatial reference to validate
+   * Returns true if the spatial reference is a recognised British National
+   * Grid square.
+   *
+   * @param string $sref
+   *   Spatial reference to validate.
    */
-  public static function is_valid($sref)
-  {
-    // ignore any spaces in the grid ref
-    $sref = str_replace(' ','',$sref);
+  public static function is_valid($sref) {
+    // Ignore any spaces in the grid ref.
+    $sref = str_replace(' ', '', $sref);
     $sq100 = strtoupper(substr($sref, 0, 2));
-    if (!preg_match('(H[L-Z]|J[LMQR]|N[A-HJ-Z]|O[ABFGLMQRVW]|S[A-HJ-Z]|T[ABFGLMQRVW])', $sq100))
+    if (!preg_match('(H[L-Z]|J[LMQR]|N[A-HJ-Z]|O[ABFGLMQRVW]|S[A-HJ-Z]|T[ABFGLMQRVW])', $sq100)) {
       return FALSE;
-    $eastnorth=substr($sref, 2);
-    // 2 cases - either remaining chars must be all numeric and an equal number, up to 10 digits
-    // OR for DINTY Tetrads, 2 numbers followed by a letter (Excluding O, including I)
-    if ((!preg_match('/^[0-9]*$/', $eastnorth) || strlen($eastnorth) % 2 != 0 || strlen($eastnorth)>10) AND
-                    (!preg_match('/^[0-9][0-9][A-NP-Z]$/', $eastnorth)))
+    }
+    $eastnorth = substr($sref, 2);
+    // 2 cases - either remaining chars must be all numeric and an equal
+    // number, up to 10 digits OR for DINTY Tetrads, 2 numbers followed by a
+    // letter (Excluding O, including I).
+    if ((!preg_match('/^[0-9]*$/', $eastnorth) || strlen($eastnorth) % 2 != 0 || strlen($eastnorth) > 10) &&
+                    (!preg_match('/^[0-9][0-9][A-NP-Z]$/', $eastnorth))) {
       return FALSE;
+    }
     return TRUE;
   }
 
@@ -53,26 +54,28 @@ class osgb {
    * Converts a spatial reference in OSGB notation into the WKT text for the polygon, in
    * OSGB easting northings.
    */
-  public static function sref_to_wkt($sref)
-  {
-    // ignore any spaces in the grid ref
-    $sref = str_replace(' ','',$sref);
-    if (!self::is_valid($sref))
+  public static function sref_to_wkt($sref) {
+    // Ignore any spaces in the grid ref.
+    $sref = str_replace(' ', '', $sref);
+    if (!self::is_valid($sref)) {
       throw new InvalidArgumentException("Spatial reference $sref is not a recognisable grid square.", 4001);
+    }
     $sq_100 = self::get_100k_square($sref);
-    if (strlen($sref)==5) {
+    if (strlen($sref) == 5) {
       // Assume DINTY Tetrad format 2km squares
-      // extract the easting and northing
+      // extract the easting and northing.
       $east = substr($sref, 2, 1);
       $north = substr($sref, 3, 1);
       $sq_code_letter_ord = ord(substr($sref, 4, 1));
       if ($sq_code_letter_ord > 79) {
-        $sq_code_letter_ord--; // Adjust for no O
+        // Adjust for no O.
+        $sq_code_letter_ord--;
       }
       $sq_size = 2000;
       $east = $east * 10000 + floor(($sq_code_letter_ord - 65) / 5) * 2000;
       $north = $north * 10000 + (($sq_code_letter_ord - 65) % 5) * 2000;
-    } else {
+    }
+    else {
       // Normal Numeric Format.
       $coordLen = (strlen($sref) - 2) / 2;
       // Extract the easting and northing.
@@ -95,7 +98,7 @@ class osgb {
    * Converts a WKT polygon for a grid square (easting northing OSGB) into the
    * spatial reference notation. Only accepts POINT & POLYGON WKT at the moment.
    */
-  public static function wkt_to_sref($wkt, $precision = null) {
+  public static function wkt_to_sref($wkt, $precision = NULL) {
     if (substr($wkt, 0, 7) == 'POLYGON') {
       $points = substr($wkt, 9, -2);
     }
@@ -105,27 +108,31 @@ class osgb {
         throw new Exception('wkt_to_sref translation for POINTs requires an accuracy.');
       }
     }
-    else
+    else {
       throw new Exception('wkt_to_sref translation only works for POINT or POLYGON wkt.');
-
-    $points = explode(',',$points);
-    // use the first point to do the conversion
-    $point = explode(' ',$points[0]);
+    }
+    $points = explode(',', $points);
+    // Use the first point to do the conversion.
+    $point = explode(' ', $points[0]);
     $easting = $point[0];
     $northing = $point[1];
-    if ($easting < 0 || $easting > 700000 || $northing < 0 || $northing > 1300000)
+    if ($easting < 0 || $easting > 700000 || $northing < 0 || $northing > 1300000) {
       throw new Exception('wkt_to_sref translation is outside range of grid.');
-    if ($precision===null) {
-      // find the distance in metres from point 2 to point 1 (assuming a square is passed).
-      // This is the accuracy of the polygon.
-      $point_2 = explode(' ',$points[1]);
-      $accuracy = abs(($point_2[0]-$point[0]) + ($point_2[1]-$point[1]));
-      $precision = 12 - strlen($accuracy)*2;
-    } else if ($precision==3) {
+    }
+    if ($precision === NULL) {
+      // Find the distance in metres from point 2 to point 1 (assuming a square
+      // is passed). This is the accuracy of the polygon.
+      $point_2 = explode(' ', $points[1]);
+      $accuracy = abs(($point_2[0] - $point[0]) + ($point_2[1] - $point[1]));
+      $precision = 12 - strlen($accuracy) * 2;
+    }
+    elseif ($precision == 3) {
       // DINTY TETRADS
       // no action as all fixed.
-    } else
-      $accuracy = pow(10, (10-$precision)/2);
+    }
+    else {
+      $accurcy = pow(10, (10 - $precision) / 2);
+    }
 
     $hundredKmE = floor($easting / 100000);
     $hundredKmN = floor($northing / 100000);
@@ -133,79 +140,103 @@ class osgb {
     if ($hundredKmN < 5) {
       if ($hundredKmE < 5) {
         $firstLetter = "S";
-      } else {
+      }
+      else {
         $firstLetter = "T";
       }
-    } else if ($hundredKmN < 10) {
+    }
+    elseif ($hundredKmN < 10) {
       if ($hundredKmE < 5) {
         $firstLetter = "N";
-      } else {
+      }
+      else {
         $firstLetter = "O";
       }
-    } else {
+    }
+    else {
       $firstLetter = "H";
     }
     $secondLetter = "";
     $index = 65 + ((4 - ($hundredKmN % 5)) * 5) + ($hundredKmE % 5);
-    // Shift index along if letter is greater than I, since I is skipped
-    if ($index >= 73) $index++;
+    // Shift index along if letter is greater than I, since I is skipped.
+    if ($index >= 73) {
+      $index++;
+    }
     $secondLetter = chr($index);
     if ($precision == 3) {
       // DINTY TETRADS
-      // 2 numbers at start equivalent to precision = 2
+      // 2 numbers at start equivalent to precision = 2.
       $e = floor(($easting - (100000 * $hundredKmE)) / 10000);
       $n = floor(($northing - (100000 * $hundredKmN)) / 10000);
       $letter = 65 + floor(($northing - (100000 * $hundredKmN) - ($n * 10000)) / 2000) + 5 * floor(($easting - (100000 * $hundredKmE) - ($e * 10000)) / 2000);
-      if ($letter >= 79) $letter++; // Adjust for no O
-      return $firstLetter.$secondLetter.str_pad($e, 1, '0', STR_PAD_LEFT).str_pad($n, 1, '0', STR_PAD_LEFT).chr($letter);
+      if ($letter >= 79) {
+        $letter++; // Adjust for no O
+      }
+      return $firstLetter . $secondLetter . str_pad($e, 1, '0', STR_PAD_LEFT) . str_pad($n, 1, '0', STR_PAD_LEFT) . chr($letter);
     }
     $e = floor(($easting - (100000 * $hundredKmE)) / $accuracy);
     $n = floor(($northing - (100000 * $hundredKmN)) / $accuracy);
-    return $firstLetter.$secondLetter.str_pad($e, $precision/2, '0', STR_PAD_LEFT).str_pad($n, $precision/2, '0', STR_PAD_LEFT);
+    return $firstLetter . $secondLetter . str_pad($e, $precision / 2, '0', STR_PAD_LEFT) . str_pad($n, $precision / 2, '0', STR_PAD_LEFT);
   }
 
   /**
    * Tidying function for input grid refs.
+   *
    * Forces uppercase with no spaces for consistency.
-   * @param type $sref
-   * @return type
+   *
+   * @param string $sref
+   *   Spatial ref string.
+   *
+   * @return string
+   *   Tidied spatial reference.
    */
   public static function sref_format_tidy($sref) {
     return str_replace(' ', '', strtoupper($sref));
   }
 
-  /** Retrieve the easting and northing of the sw corner of a
+  /**
+   * Get's a squares 100km Easting/Northing.
+   *
+   * Retrieve the easting and northing of the sw corner of a
    * 100km square, indicated by the first 2 chars of the grid ref.
    *
-   * @param string $sref Spatial reference string to parse (OSGB)
-   * @return array Array containing (x, y)
+   * @param string $sref
+   *   Spatial reference string to parse (OSGB).
+   *
+   * @return array
+   *   Array containing (x, y).
    */
-  protected static function get_100k_square($sref)
-  {
+  protected static function get_100k_square($sref) {
     $north = 0;
     $east = 0;
-    $char1 =substr($sref, 0, 1);
-    switch ($char1){
+    $char1 = substr($sref, 0, 1);
+    switch ($char1) {
       case 'H':
         $north += 1000000;
         break;
+
       case 'N':
         $north += 500000;
         break;
+
       case 'O':
         $north += 500000;
         $east  += 500000;
         break;
+
       case 'T':
         $east += 500000;
         break;
     }
     $char2ord = ord(substr($sref, 1, 1));
-    if ($char2ord > 73) $char2ord--; // Adjust for no I
+    if ($char2ord > 73) {
+      // Adjust for no I.
+      $char2ord--;
+    }
     $east += (($char2ord - 65) % 5) * 100000;
     $north += (4 - floor(($char2ord - 65) / 5)) * 100000;
-    $output['x']=$east;
-    $output['y']=$north;
+    $output['x'] = $east;
+    $output['y'] = $north;
     return $output;
   }
 
