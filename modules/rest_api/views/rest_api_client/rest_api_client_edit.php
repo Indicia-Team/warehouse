c8c846cdb5bdee19bc982f2911502468a01ae113
John van Breda <john.vanbreda@biodiverseit.co.uk>
2023-12-29 11:29:41 +0000

    Form adapts according to enabled auth methods

--- modules/rest_api/views/rest_api_client/rest_api_client_edit.php
+++ modules/rest_api/views/rest_api_client/rest_api_client_edit.php
@@ -45,53 +45,83 @@ $id = html::initial_value($values, 'rest_api_client:id');
       'default' => html::initial_value($values, 'rest_api_client:description'),
     ]);
 
-    echo data_entry_helper::select([
-      'label' => 'Website',
-      'fieldname' => 'rest_api_client:website_id',
-      'default' => html::initial_value($values, 'rest_api_client:website_id'),
-      'lookupValues' => $other_data['websites'],
-      'helpText' => 'The website the REST API client is associated with.',
-      'validation' => ['required'],
-    ]);
+    ?>
+    <div class="row">
+      <div class="col-md-6">
+        <?php
 
-    echo data_entry_helper::text_input([
-      'label' => 'Username',
-      'fieldname' => 'rest_api_client:username',
-      'default' => html::initial_value($values, 'rest_api_client:username'),
-      'validation' => ['required'],
-    ]);
+        echo data_entry_helper::select([
+          'label' => 'Website',
+          'fieldname' => 'rest_api_client:website_id',
+          'default' => html::initial_value($values, 'rest_api_client:website_id'),
+          'lookupValues' => $other_data['websites'],
+          'helpText' => 'The website the REST API client is associated with.',
+          'validation' => ['required'],
+        ]);
 
-    echo data_entry_helper::password_input([
-      'label' => 'Secret',
-      'fieldname' => 'rest_api_client:secret',
-    ]);
+        echo data_entry_helper::text_input([
+          'label' => 'Username',
+          'fieldname' => 'rest_api_client:username',
+          'default' => html::initial_value($values, 'rest_api_client:username'),
+          'validation' => ['required'],
+        ]);
 
-    $secret2Config = [
-      'label' => 'Retype secret',
-      'fieldname' => 'secret2',
-    ];
-    if (empty(html::initial_value($values, 'rest_api_client:secret'))) {
-      $secret2Config['helpText'] = <<<TXT
-Enter a secret if you wish to enable authentication using the username and secret combination for
-this client's connections. Please ensure that you keep a copy of the secret you enter into the
-fields above as it cannot be retrieved.
-TXT;
+        $config = kohana::config('rest');
+        if (array_key_exists('directClient', $config['authentication_methods'])) {
+          echo data_entry_helper::password_input([
+            'label' => 'Secret',
+            'fieldname' => 'rest_api_client:secret',
+          ]);
+
+          $secret2Config = [
+            'label' => 'Retype secret',
+            'fieldname' => 'secret2',
+          ];
+          if (empty(html::initial_value($values, 'rest_api_client:secret'))) {
+            $secret2Config['helpText'] = <<<TXT
+      Enter a secret if you wish to enable authentication using the username and secret combination for
+      this client's connections. Please ensure that you keep a copy of the secret you enter into the
+      fields above as it cannot be retrieved.
+      TXT;
+          }
+          else {
+            $secret2Config['helpText'] = <<<TXT
+      Leave the above Secret fields blank to keep the existing secret. If you choose to enter a new
+      secret, then please ensure that you keep a copy of the secret you enter into the fields above as it
+      cannot be retrieved.
+      TXT;
+          }
+          echo data_entry_helper::password_input($secret2Config);
+        }
+        else {
+          echo '<p class="alert alert-info">The directClient authentication method is currently disabled in the modules/rest_api/config/rest.php configuration file. Enable it then set a secret here to allow directClient authentication.</p>';
+          echo data_entry_helper::text_input([
+            'label' => 'Secret',
+            'fieldname' => 'foo',
+            'attributes' => ['disabled' => 'disabled'],
+          ]);
+        }
+
+        ?>
+      </div>
+    </div>
+    <?php
+    if (array_key_exists('jwtClient', $config['authentication_methods'])) {
+      echo data_entry_helper::textarea([
+        'label' => 'Public key',
+        'fieldname' => 'rest_api_client:public_key',
+        'default' => html::initial_value($values, 'rest_api_client:public_key'),
+        'helpText' => "Enter a public key if you wish to enable authentication using JWT for this client's connections. Paste the public key from a private/public key pair here.",
+      ]);
     }
     else {
-      $secret2Config['helpText'] = <<<TXT
-Leave the above Secret fields blank to keep the existing secret. If you choose to enter a new
-secret, then please ensure that you keep a copy of the secret you enter into the fields above as it
-cannot be retrieved.
-TXT;
+      echo '<p class="alert alert-info">The jwyClient authentication method is currently disabled in the modules/rest_api/config/rest.php configuration file. Enable it then set a public key here to allow jwtClient authentication.</p>';
+      echo data_entry_helper::textarea([
+        'label' => 'Public key',
+        'fieldname' => 'foo',
+        'disabled' => 'disabled',
+      ]);
     }
-    echo data_entry_helper::password_input($secret2Config);
-
-    echo data_entry_helper::textarea([
-      'label' => 'Public key',
-      'fieldname' => 'rest_api_client:public_key',
-      'default' => html::initial_value($values, 'rest_api_client:public_key'),
-      'helpText' => "Enter a public key if you wish to enable authentication using JWT for this client's connections. Paste the public key from a private/public key pair here.",
-    ]);
 
     ?>
   </fieldset>
