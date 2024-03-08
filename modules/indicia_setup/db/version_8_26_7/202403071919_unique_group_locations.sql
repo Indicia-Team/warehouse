0f08307d4844612b456626e87c6ad3be94490197
John van Breda <john.vanbreda@biodiverseit.co.uk>
2024-03-08 12:10:47 +0000

    Syntax

--- modules/indicia_setup/db/version_8_26_7/202403071919_unique_group_locations.sql
+++ modules/indicia_setup/db/version_8_26_7/202403071919_unique_group_locations.sql
@@ -4,7 +4,7 @@ WHERE gl2.deleted=false
 AND gl1.deleted=false
 AND gl2.group_id=gl1.group_id
 AND gl2.location_id=gl1.location_id
-AND gl2.id>gl1.id
+AND gl2.id>gl1.id;
 
 CREATE UNIQUE INDEX IF NOT EXISTS ix_groups_location_unique ON groups_locations(group_id, location_id)
 WHERE deleted=false;
