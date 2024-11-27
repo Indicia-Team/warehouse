9b88e18a0b452cec24c7527ab21773b7655ea39b
John van Breda <john.vanbreda@biodiverseit.co.uk>
2024-11-27 11:25:29 +0000

    View might need drop if installing from scratch
    As original version depended on cache_taxon_searchterms

--- modules/cache_builder/db/version_9_1_21/202406141130_authority_type.sql
+++ modules/cache_builder/db/version_9_1_21/202406141130_authority_type.sql
@@ -1,4 +1,5 @@
 DROP VIEW IF EXISTS cache_occurrences;
+DROP VIEW IF EXISTS lookup_taxa_taxon_lists;
 
 ALTER TABLE cache_taxa_taxon_lists
   ALTER COLUMN authority TYPE VARCHAR,
@@ -87,3 +88,16 @@ SELECT o.id,
   JOIN cache_occurrences_nonfunctional onf on onf.id=o.id
   JOIN cache_samples_nonfunctional snf on snf.id=o.sample_id
   JOIN cache_taxa_taxon_lists cttl on cttl.id=o.taxa_taxon_list_id;
+
+CREATE OR REPLACE VIEW lookup_taxa_taxon_lists AS
+  SELECT ttl.id,
+      ttl.taxon_meaning_id,
+      ttl.taxon_list_id,
+      t.taxon || COALESCE(' ' || t.attribute, '') as taxon,
+      t.authority,
+      t.external_key,
+      t.search_code
+    FROM taxa_taxon_lists ttl
+    JOIN taxa t on t.id=ttl.taxon_id AND t.deleted=false
+    WHERE ttl.deleted=false
+    ORDER BY ttl.allow_data_entry DESC, ttl.preferred DESC;
