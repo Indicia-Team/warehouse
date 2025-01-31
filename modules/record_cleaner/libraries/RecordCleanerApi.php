e489c5d2737f94c3518828421bdf1b7e8e9c1a94
John van Breda <john.vanbreda@biodiverseit.co.uk>
2025-01-31 14:18:53 +0000

    Basic functionality for Record Cleaner API in place

--- modules/record_cleaner/libraries/RecordCleanerApi.php
+++ modules/record_cleaner/libraries/RecordCleanerApi.php
@@ -22,7 +22,18 @@
  */
 
 const BATCH_SIZE = 100;
-const PAGE_LIMIT = 10;
+
+/*
+ * @todo Handle Identification difficulty changes as no longer using verification rules locally. E.g. how to store in cache_taxon_searchterms?
+ * @todo The cache table update query needs to alter applicable_verification_rule_types to include the rules run by the Record Cleaner.
+ * @todo Should output_sref be the blurred or precise version in the submitted records?
+ * @todo Correct date format submitted in the records?
+ * @todo Check icons generated in PG reports. Are the standard icons even being generated?
+ * @todo Check icons generated in ES reports
+ * @todo Check filtering in PG reports
+ * @todo Check filtering in ES reports
+ *
+ */
 
 /**
  * Functionality for the Record Cleaner API interactions.
@@ -50,21 +61,37 @@ class RecordCleanerApi {
    */
   private $authToken;
 
+  /**
+   * Initialise the object instance.
+   *
+   * @param Database $db
+   *   Database connection.
+   */
   public function __construct($db) {
     $this->db = $db;
     $this->config = kohana::config('record_cleaner');
   }
 
+  /**
+   * Main entry point which processes all outstanding records.
+   *
+   * @param string $endtime
+   */
   public function processRecords($endtime) {
     // Process records here.
     if (!$this->authenticate()) {
       // Abort if authentication fails.
       return;
     }
-    $this->processAll();
-    $this->updateOccurrenceMetadata($this->db, $endtime);
+    $this->processAll($endtime);
   }
 
+  /**
+   * Obtain an API authentication token.
+   *
+   * @return bool
+   *   True if authentication successful, false otherwise.
+   */
   private function authenticate() {
     $response = $this->curlRequest('/token', [], [
       'grant_type' => 'password',
@@ -82,12 +109,13 @@ class RecordCleanerApi {
     return TRUE;
   }
 
-  private function processAll() {
+  private function processAll($endtime) {
     $query = <<<SQL
-      select id
-      from occdelta
-      where record_status not in ('I','V','R','D')
-      order by id
+      SELECT id
+      FROM occdelta
+      WHERE record_status not in ('I','V','R','D')
+      AND verification_checks_enabled=true
+      ORDER BY id
     SQL;
     $result = $this->db->query($query)->result_array(TRUE);
     $ids = array_column($result, 'id');
@@ -96,13 +124,40 @@ class RecordCleanerApi {
       $start = $page * BATCH_SIZE;
       $end = min(($page + 1) * BATCH_SIZE, count($ids));
       $batch = array_slice($ids, $start, $end);
+      $this->cleanoutOldMessages($batch);
       $this->processBatch($batch);
+      $this->updateOccurrenceMetadata($batch, $endtime);
+      $this->updateCacheTables($batch);
     }
   }
 
+
+  /**
+   * Cleans out old messages for the given batch of occurrence IDs.
+   *
+   * @param array $batch
+   *   Array of occurrence IDs to clean messages for.
+   */
+  private function cleanoutOldMessages(array $batch) {
+    $ids = implode(',', $batch);
+    $query = <<<SQL
+      UPDATE occurrence_comments
+      SET deleted=true, updated_on=now()
+      WHERE occurrence_id IN ($ids)
+      AND (
+        generated_by IN (
+          'data_cleaner_identification_difficulty',
+          'data_cleaner_period',
+          'data_cleaner_period_within_year',
+          'data_cleaner_without_polygon'
+        )
+        OR generated_by LIKE 'record_cleaner:%'
+      );
+    SQL;
+    $this->db->query($query);
+  }
+
   private function processBatch(array $batch) {
-    // @todo Should output_sref be the blurred or precise version?
-    // @todo Correct date format?
     $ids = implode(',', $batch);
     $query = <<<SQL
       select o.id, o.taxa_taxon_list_external_key as tvk, o.date_start, o.date_end, o.date_type,
@@ -126,9 +181,114 @@ class RecordCleanerApi {
         'stage' => $record->stage
       ];
     }
-    echo '<pre>';
-    var_export($this->curlRequest('/verify', [], json_encode(['records' => $recordsArray])));
-    echo '</pre>';
+    $response = $this->curlRequest('/verify', [], json_encode(['records' => $recordsArray]));
+    echo '<pre>'; var_export($response); echo '</pre>';
+    $batchWithRulesRun = [];
+    foreach ($response->records as $record) {
+      $this->saveRecordResponse($record);
+    }
+  }
+
+  /**
+   * Saves verification responses for a record.
+   *
+   * This function processes the messages in the provided record array. If a message
+   * matches the pattern `:(difficulty|period|phenology|10km):`, it extracts the relevant
+   * tokens and inserts a comment into the `occurrence_comments` table.
+   *
+   * @param object $record The record object containing messages and other details.
+   *   - 'id' (int): The ID of the occurrence record.
+   *   - 'messages' (array): An array of messages to be processed.
+   *
+   * The function expects the messages to be in a specific format, where the message
+   * contains tokens separated by colons. The expected format is:
+   * `:<sourceCode>:<ruleType>:<comment>` or `:<sourceCode>:<ruleType>:<subType>:<comment>`
+   */
+  private function saveRecordResponse($record) {
+    echo "Processing ID $record->id: " . var_export($record->messages, TRUE) . '<br/>';
+    foreach ($record->messages as $message) {
+      if (preg_match('/:(difficulty|period|phenology|tenkm):/', $message)) {
+        $tokens = explode(':', $message);
+        $sourceCode = $tokens[1];
+        $ruleType = $tokens[2];
+        $comment = trim($tokens[count($tokens) - 1]);
+        $subType = $ruleType === 'difficulty' ? $tokens[3] : NULL;
+        if ($ruleType === 'difficulty' && (int) $subType < 2) {
+          continue;
+        }
+        $query = <<<SQL
+          INSERT INTO occurrence_comments (occurrence_id, comment, auto_generated, generated_by, generated_by_subtype, implies_manual_check_required, created_by_id, created_on, updated_by_id, updated_on)
+          VALUES (?, ?, true, ?, ?, true, 1, now(), 1, now())
+        SQL;
+        $this->db->query($query, [
+          $record->id,
+          $comment,
+          "record_cleaner:$ruleType:$sourceCode",
+          $subType,
+        ]);
+      }
+    }
+  }
+
+  private function updateCacheTables(array $batch) {
+    $ids = implode(',', $batch);
+    $query = <<<SQL
+      SELECT o.id,
+      CASE WHEN o.last_verification_check_date IS NULL THEN NULL ELSE
+        COALESCE(string_agg(distinct '[' || replace(oc.generated_by, ':', ' ') || ']{' || oc.comment || '}', ' '), 'pass')
+      END AS data_cleaner_info,
+      CASE WHEN o.last_verification_check_date IS NULL THEN NULL ELSE COUNT(oc.id)=0 END AS data_cleaner_result,
+      ocdiff.generated_by_subtype::integer as difficulty
+      INTO TEMPORARY data_cleaner_results
+      FROM occurrences o
+      LEFT JOIN occurrence_comments oc ON oc.occurrence_id=o.id
+        AND oc.implies_manual_check_required=true
+        AND oc.deleted=false
+      LEFT JOIN occurrence_comments ocdiff ON ocdiff.occurrence_id=o.id
+        AND ocdiff.implies_manual_check_required=true
+        AND ocdiff.deleted=false
+        AND ocdiff.generated_by like 'record_cleaner:difficulty:%'
+      WHERE o.id in ($ids)
+      GROUP BY o.id, o.last_verification_check_date, ocdiff.generated_by_subtype;
+
+      UPDATE cache_occurrences_functional o
+      SET data_cleaner_result = dcr.data_cleaner_result,
+        applied_verification_rule_types=cttl.applicable_verification_rule_types,
+        identification_difficulty = dcr.difficulty
+      FROM data_cleaner_results dcr, cache_taxa_taxon_lists cttl
+      WHERE dcr.id=o.id
+      AND cttl.external_key = o.taxa_taxon_list_external_key
+      AND cttl.preferred=true;
+
+      UPDATE cache_occurrences_nonfunctional o
+      SET data_cleaner_info = dcr.data_cleaner_info
+      FROM data_cleaner_results dcr
+      WHERE dcr.id=o.id;
+
+      DROP TABLE data_cleaner_results;
+    SQL;
+    $this->db->query($query);
+  }
+
+  /**
+   * Update the metadata associated occurrences so we know rules have been run.
+   *
+   * @param object $db
+   *   Kohana database instance.
+   * @param string $endtime
+   *   Timestamp to save as the last verification check date.
+   */
+  function updateOccurrenceMetadata(array $batch, $endtime) {
+    $ids = implode(',', $batch);
+    // Note we use the information from the point when we started the process,
+    // in caseany changes have happened in the meanwhile which might otherwise be
+    // missed.
+    $query = <<<SQL
+      update occurrences o
+      set last_verification_check_date='$endtime'
+      where o.id in ($ids)
+    SQL;
+    $this->db->query($query);
   }
 
 
@@ -143,11 +303,9 @@ class RecordCleanerApi {
     curl_setopt($session, CURLOPT_RETURNTRANSFER, TRUE);
     if (!empty($post)) {
       curl_setopt ($session, CURLOPT_POST, TRUE);
-      echo '<pre>' . var_export($post); echo '</pre>';
       curl_setopt ($session, CURLOPT_POSTFIELDS, $post);
     }
     // @todo Why does attaching content-type on token request break it?
-
     // Attach authentication token.
     if (isset($this->authToken)) {
       curl_setopt($session, CURLOPT_HTTPHEADER, [
@@ -163,27 +321,6 @@ class RecordCleanerApi {
     }
     curl_close($session);
     return json_decode($response);
-  }
-
-  /**
-   * Update the metadata associated occurrences so we know rules have been run.
-   *
-   * @param object $db
-   *   Kohana database instance.
-   * @param string $endtime
-   *   Timestamp to save as the last verification check date.
-   */
-  function updateOccurrenceMetadata($db, $endtime) {
-    // Note we use the information from the point when we started the process,
-    // in caseany changes have happened in the meanwhile which might otherwise be
-    // missed.
-    $query = <<<SQL
-      update occurrences o
-      set last_verification_check_date='$endtime'
-      from occdelta
-      where occdelta.id=o.id and occdelta.record_status not in ('I','V','R','D')
-    SQL;
-    $db->query($query);
   }
 
 }
