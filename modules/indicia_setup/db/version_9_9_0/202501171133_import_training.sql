6bf57f954295574d5463d38907adb320a1a87dcd
John van Breda <john.vanbreda@biodiverseit.co.uk>
2025-02-25 15:49:02 +0000

    Script performance improved.

--- modules/indicia_setup/db/version_9_9_0/202501171133_import_training.sql
+++ modules/indicia_setup/db/version_9_9_0/202501171133_import_training.sql
@@ -2,8 +2,3 @@ ALTER TABLE imports
 ADD training boolean NOT NULL DEFAULT false;
 
 COMMENT ON COLUMN imports.training is 'Flag indicating if this import was performed in training mode.';
-
--- Set existing values.
-UPDATE imports i
-SET training=true
-WHERE (SELECT min(s.training::integer) FROM samples s WHERE s.import_guid=i.import_guid)=1;
