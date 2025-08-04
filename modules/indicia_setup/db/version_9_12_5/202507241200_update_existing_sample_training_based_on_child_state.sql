dbb5b6bd4a644be63a9e073e8c89a07a047a9ef2
Andrew van Breda <support@avb-it.co.uk>
2025-08-04 12:17:18 +0100

    New code that will hopefully work but not yet tested.

--- modules/indicia_setup/db/version_9_12_5/202507241200_update_existing_sample_training_based_on_child_state.sql
+++ modules/indicia_setup/db/version_9_12_5/202507241200_update_existing_sample_training_based_on_child_state.sql
@@ -2,29 +2,34 @@
 
 -- Switch training flag on sample if all of its occurrences are in the opposite training state
 
-UPDATE samples s
-SET training = NOT s.training
-FROM occurrences o
-LEFT JOIN occurrences o2 on o2.sample_id = o.sample_id and o2.training = s.training and o2.deleted = false
-WHERE o.sample_id = s.id and o.training = NOT s.training and o2.id is null and s.deleted = false and o.deleted = false;
+UPDATE samples s1
+SET training = NOT s1.training
+FROM samples s2
+JOIN occurrences o1 on o1.sample_id = s2.id and o1.training = NOT s2.training and o1.deleted = false
+LEFT JOIN occurrences o2 on o2.sample_id = s2.id and o2.training = s2.training and o2.deleted = false
+WHERE s2.id = s1.id AND o2.id IS NULL AND s2.deleted=false;
+
 -- Do same for cache table
-UPDATE cache_samples_functional csf
-SET training = NOT csf.training
-FROM occurrences o
-LEFT JOIN occurrences o2 on o2.sample_id = o.sample_id and o2.training = csf.training and o2.deleted = false
-WHERE o.sample_id = csf.id and o.training = NOT csf.training and o2.id is null and o.deleted = false;
+UPDATE cache_samples_functional s1
+SET training = NOT s1.training
+FROM samples s2
+JOIN occurrences o1 on o1.sample_id = s2.id and o1.training = NOT s2.training and o1.deleted = false
+LEFT JOIN occurrences o2 on o2.sample_id = s2.id and o2.training = s2.training and o2.deleted = false
+WHERE s2.id = s1.id AND o2.id IS NULL AND s2.deleted=false;
 
 -- Do similar for samples/subsamples, switch training flag on parent sample if all its child subsamples
 -- are in the opposite state.
 
-UPDATE samples s_parent
-SET training = NOT s_parent.training
-FROM samples s_child
-LEFT JOIN samples s_child2 on s_child2.parent_id = s_child.parent_id and s_child2.training = s_parent.training and s_child2.deleted = false
-WHERE s_child.parent_id = s_parent.id and s_child.training = NOT s_parent.training and s_child2.id is null and s_parent.deleted = false and s_child.deleted = false;
+UPDATE samples s_parent1
+SET training = NOT s_parent1.training
+FROM samples s_parent2
+JOIN samples s_child1 on s_child1.parent_id = s_parent2.id and s_child1.training = NOT s_parent2.training and s_child1.deleted = false
+LEFT JOIN samples s_child2 on s_child2.parent_id = s_parent2.id and s_child2.training = s_parent2.training and s_child2.deleted = false
+WHERE s_parent2.id = s_parent1.id AND s_child2.id IS NULL AND s_parent2.deleted=false;
 
-UPDATE cache_samples_functional csf_parent
-SET training = NOT csf_parent.training
-FROM samples s_child
-LEFT JOIN samples s_child2 on s_child2.parent_id = s_child.parent_id and s_child2.training = csf_parent.training and s_child2.deleted = false
-WHERE s_child.parent_id = csf_parent.id and s_child.training = NOT csf_parent.training and s_child2.id is null and s_child.deleted = false;
+UPDATE cache_samples_functional s_parent1
+SET training = NOT s_parent1.training
+FROM samples s_parent2
+JOIN samples s_child1 on s_child1.parent_id = s_parent2.id and s_child1.training = NOT s_parent2.training and s_child1.deleted = false
+LEFT JOIN samples s_child2 on s_child2.parent_id = s_parent2.id and s_child2.training = s_parent2.training and s_child2.deleted = false
+WHERE s_parent2.id = s_parent1.id AND s_child2.id IS NULL AND s_parent2.deleted=false;
