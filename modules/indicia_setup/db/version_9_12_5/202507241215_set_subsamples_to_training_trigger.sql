b0aef5774cf426a4bd709699254df08cc22422e1
Andrew van Breda <support@avb-it.co.uk>
2025-08-04 09:42:38 +0100

    Tidying code, names and comments

--- modules/indicia_setup/db/version_9_12_5/202507241215_set_subsamples_to_training_trigger.sql
+++ modules/indicia_setup/db/version_9_12_5/202507241215_set_subsamples_to_training_trigger.sql
@@ -2,41 +2,41 @@ DROP TRIGGER IF EXISTS set_sample_occurrences_to_training_trigger ON samples;
 
 DROP FUNCTION IF EXISTS set_sample_occurrences_to_training();
 
-DROP TRIGGER IF EXISTS set_sample_children_to_training_trigger ON samples;
+DROP TRIGGER IF EXISTS set_sample_children_training_flag_trigger ON samples;
 
-DROP FUNCTION IF EXISTS set_sample_children_to_training();
+DROP FUNCTION IF EXISTS set_sample_children_training_flag();
 
-CREATE OR REPLACE FUNCTION set_sample_children_to_training()
+CREATE OR REPLACE FUNCTION set_sample_children_training_flag()
   RETURNS trigger AS
   $BODY$
     BEGIN
       IF ((OLD.training = false AND NEW.training = true) OR
           (OLD.training = true AND NEW.training = false)) THEN
+          
         -- Set any subsample training flags if parent training flag changes
         UPDATE samples
         SET training = NEW.training, updated_on=now()
         WHERE parent_id = NEW.id;
-        -- Do same thing for cache table. Also update the cache table for the sample being changed.
+        -- Do same thing for the cache table, and also update the cache table for the sample being changed.
         UPDATE cache_samples_functional
         SET training = NEW.training, updated_on=now()
         WHERE 
-        (id = NEW.id) OR
-        (parent_sample_id = NEW.id);
-        -- Update occurrences for the sample training flag change.
-        -- Also update occurrences for any subsample training flag changes
+        id = NEW.id OR parent_sample_id = NEW.id;
+        -- Similarly update occurrences for the sample training flag change.
+        -- Also update occurrences for any subsamples changed
         -- as a result of parent sample training flag change.
         UPDATE occurrences
         SET training = NEW.training, updated_on=now()
         FROM samples s
         WHERE sample_id = s.id AND
-        (sample_id = NEW.id OR
+        (s.id = NEW.id OR
         s.parent_id = NEW.id);
         -- Exactly the same for cache table
         UPDATE cache_occurrences_functional
         SET training = NEW.training, updated_on=now()
         FROM samples s
         WHERE sample_id = s.id AND
-        (sample_id = NEW.id OR
+        (s.id = NEW.id OR
         s.parent_id = NEW.id);
 
       END IF;
@@ -47,17 +47,17 @@ CREATE OR REPLACE FUNCTION set_sample_children_to_training()
   COST 100;
 
 
-CREATE TRIGGER set_sample_children_to_training_trigger
+CREATE TRIGGER set_sample_children_training_flag_trigger
   AFTER UPDATE
   ON samples
   FOR EACH ROW
   EXECUTE PROCEDURE set_sample_children_to_training();
 
-DROP TRIGGER IF EXISTS set_subsample_to_training_from_sample_trigger ON samples;
+DROP TRIGGER IF EXISTS set_new_subsample_to_training_from_parent_sample_trigger ON samples;
 
-DROP FUNCTION IF EXISTS set_subsample_to_training_from_sample();
+DROP FUNCTION IF EXISTS  set_new_subsample_to_training_from_parent_sample();
 
-CREATE OR REPLACE FUNCTION set_subsample_to_training_from_sample()
+CREATE OR REPLACE FUNCTION set_new_subsample_to_training_from_parent_sample()
   RETURNS trigger AS
   $BODY$
     BEGIN
@@ -78,7 +78,7 @@ CREATE OR REPLACE FUNCTION set_subsample_to_training_from_sample()
   LANGUAGE plpgsql VOLATILE
   COST 100;
 
-CREATE TRIGGER set_subsample_to_training_from_sample_trigger
+CREATE TRIGGER set_new_subsample_to_training_from_parent_sample_trigger
   AFTER INSERT
   ON samples
   FOR EACH ROW
@@ -88,7 +88,11 @@ DROP TRIGGER IF EXISTS set_occurrence_to_training_from_sample_trigger ON occurre
 
 DROP FUNCTION IF EXISTS set_occurrence_to_training_from_sample(); 
 
-CREATE OR REPLACE FUNCTION set_occurrence_to_training_from_sample()
+DROP TRIGGER IF EXISTS set_new_occurrence_to_training_from_its_sample_trigger ON occurrences;
+
+DROP FUNCTION IF EXISTS set_new_occurrence_to_training_from_its_sample(); 
+
+CREATE OR REPLACE FUNCTION set_new_occurrence_to_training_from_its_sample()
   RETURNS trigger AS
   $BODY$
     BEGIN
@@ -110,7 +114,7 @@ CREATE OR REPLACE FUNCTION set_occurrence_to_training_from_sample()
   COST 100;
 
 
-CREATE TRIGGER set_occurrence_to_training_from_sample_trigger
+CREATE TRIGGER set_new_occurrence_to_training_from_its_sample_trigger
   AFTER INSERT
   ON occurrences
   FOR EACH ROW
