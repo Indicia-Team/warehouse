0929816aa9eebe62baf01aacbcd9de029f5dcff8
John van Breda <john.vanbreda@biodiverseit.co.uk>
2025-08-07 09:35:45 +0100

    Code indentation

--- modules/cache_builder/helpers/task_cache_builder_attr_value_occurrence.php
+++ modules/cache_builder/helpers/task_cache_builder_attr_value_occurrence.php
@@ -69,90 +69,89 @@ class task_cache_builder_attr_value_occurrence {
         }
         $langTermSql .= <<<SQL
 
-  UNION
+          UNION
 
-  SELECT av.occurrence_id, a.multi_value,
-    av.occurrence_attribute_id::text || ':$lang' as f,
-    array_agg(COALESCE(ti18n.term, t.term) ORDER BY COALESCE(tlti18n.sort_order, tlt.sort_order)) as v
-  FROM work_queue q
-  JOIN occurrence_attribute_values avfilt ON avfilt.id=q.record_id
-  JOIN occurrence_attribute_values av ON av.occurrence_id=avfilt.occurrence_id AND av.deleted=false
-    AND COALESCE(av.int_value::text, av.text_value::text, av.float_value::text, av.date_start_value::text) IS NOT NULL
-  JOIN occurrence_attributes a ON a.id=av.occurrence_attribute_id AND a.deleted=false
-  LEFT JOIN termlists_terms tlt ON tlt.id=av.int_value AND tlt.deleted=false
-  LEFT JOIN terms t ON t.id=tlt.term_id AND t.deleted=false
-  LEFT JOIN (termlists_terms tlti18n
-    JOIN terms ti18n ON ti18n.id=tlti18n.term_id AND ti18n.deleted=false
-    JOIN languages l on l.id=ti18n.language_id AND l.deleted=false AND l.iso='$lang'
-  ) ON tlti18n.meaning_id=tlt.meaning_id AND tlti18n.termlist_id=tlt.termlist_id and tlti18n.deleted=false
-  WHERE q.entity='occurrence' AND q.task='task_cache_builder_attr_value_occurrence' AND claimed_by=$procIdEsc
-  AND a.data_type='L'
-  GROUP BY occurrence_id, occurrence_attribute_id, a.multi_value
+          SELECT av.occurrence_id, a.multi_value,
+            av.occurrence_attribute_id::text || ':$lang' as f,
+            array_agg(COALESCE(ti18n.term, t.term) ORDER BY COALESCE(tlti18n.sort_order, tlt.sort_order)) as v
+          FROM work_queue q
+          JOIN occurrence_attribute_values avfilt ON avfilt.id=q.record_id
+          JOIN occurrence_attribute_values av ON av.occurrence_id=avfilt.occurrence_id AND av.deleted=false
+            AND COALESCE(av.int_value::text, av.text_value::text, av.float_value::text, av.date_start_value::text) IS NOT NULL
+          JOIN occurrence_attributes a ON a.id=av.occurrence_attribute_id AND a.deleted=false
+          LEFT JOIN termlists_terms tlt ON tlt.id=av.int_value AND tlt.deleted=false
+          LEFT JOIN terms t ON t.id=tlt.term_id AND t.deleted=false
+          LEFT JOIN (termlists_terms tlti18n
+            JOIN terms ti18n ON ti18n.id=tlti18n.term_id AND ti18n.deleted=false
+            JOIN languages l on l.id=ti18n.language_id AND l.deleted=false AND l.iso='$lang'
+          ) ON tlti18n.meaning_id=tlt.meaning_id AND tlti18n.termlist_id=tlt.termlist_id and tlti18n.deleted=false
+          WHERE q.entity='occurrence' AND q.task='task_cache_builder_attr_value_occurrence' AND claimed_by=$procIdEsc
+          AND a.data_type='L'
+          GROUP BY occurrence_id, occurrence_attribute_id, a.multi_value
 
-SQL;
+        SQL;
       }
     }
     $sql = <<<SQL
 
-SELECT occurrence_id, ('{' || string_agg(
-  to_json(f)::text || ':' ||
-  CASE multi_value WHEN true THEN to_json(v)::text ELSE to_json(v[1])::text END
-, ',') || '}')::json AS attrs
-INTO temporary attrs
-FROM (
-  SELECT avfilt.occurrence_id, a.multi_value,
-    av.occurrence_attribute_id::text as f,
-    array_agg(
-      CASE a.data_type
-        WHEN 'T' THEN av.text_value
-        WHEN 'L' THEN t.term
-        WHEN 'I' THEN av.int_value::text ||
-          CASE
-            WHEN a.allow_ranges = true THEN COALESCE(' - '::text || av.upper_value::text, ''::text)
-            ELSE ''::text
-          END
-        WHEN 'F' THEN av.float_value::text ||
-          CASE
-            WHEN a.allow_ranges = true THEN COALESCE(' - '::text || av.upper_value::text, ''::text)
-            ELSE ''::text
-          END
-        WHEN 'B'::bpchar THEN av.int_value::text
-        WHEN 'D'::bpchar THEN av.date_start_value::text
-        WHEN 'V'::bpchar THEN
-          av.date_start_value::text ||
-          CASE WHEN av.date_end_value > av.date_start_value THEN ' - '::text || av.date_end_value::text ELSE '' END
-        ELSE NULL::text
-      END ORDER BY tlt.sort_order, t.term
-    ) as v
-  FROM work_queue q
-  JOIN occurrence_attribute_values avfilt ON avfilt.id=q.record_id
-  JOIN occurrence_attribute_values av ON av.occurrence_id=avfilt.occurrence_id AND av.deleted=false
-    AND COALESCE(av.int_value::text, av.text_value::text, av.float_value::text, av.date_start_value::text) IS NOT NULL
-  LEFT JOIN occurrence_attributes a ON a.id=av.occurrence_attribute_id AND a.deleted=false
-  LEFT JOIN termlists_terms tlt ON tlt.id=av.int_value AND a.data_type='L' AND tlt.deleted=false
-  LEFT JOIN terms t ON t.id=tlt.term_id AND t.deleted=false
-  WHERE q.entity='occurrence_attribute_value' AND q.task='task_cache_builder_attr_value_occurrence' AND claimed_by=$procIdEsc
-  GROUP BY avfilt.occurrence_id, av.occurrence_attribute_id, a.multi_value
-  $langTermSql
-) AS subquery
-GROUP BY occurrence_id;
+      SELECT occurrence_id, ('{' || string_agg(
+        to_json(f)::text || ':' ||
+        CASE multi_value WHEN true THEN to_json(v)::text ELSE to_json(v[1])::text END
+      , ',') || '}')::json AS attrs
+      INTO temporary attrs
+      FROM (
+        SELECT avfilt.occurrence_id, a.multi_value,
+          av.occurrence_attribute_id::text as f,
+          array_agg(
+            CASE a.data_type
+              WHEN 'T' THEN av.text_value
+              WHEN 'L' THEN t.term
+              WHEN 'I' THEN av.int_value::text ||
+                CASE
+                  WHEN a.allow_ranges = true THEN COALESCE(' - '::text || av.upper_value::text, ''::text)
+                  ELSE ''::text
+                END
+              WHEN 'F' THEN av.float_value::text ||
+                CASE
+                  WHEN a.allow_ranges = true THEN COALESCE(' - '::text || av.upper_value::text, ''::text)
+                  ELSE ''::text
+                END
+              WHEN 'B'::bpchar THEN av.int_value::text
+              WHEN 'D'::bpchar THEN av.date_start_value::text
+              WHEN 'V'::bpchar THEN
+                av.date_start_value::text ||
+                CASE WHEN av.date_end_value > av.date_start_value THEN ' - '::text || av.date_end_value::text ELSE '' END
+              ELSE NULL::text
+            END ORDER BY tlt.sort_order, t.term
+          ) as v
+        FROM work_queue q
+        JOIN occurrence_attribute_values avfilt ON avfilt.id=q.record_id
+        JOIN occurrence_attribute_values av ON av.occurrence_id=avfilt.occurrence_id AND av.deleted=false
+          AND COALESCE(av.int_value::text, av.text_value::text, av.float_value::text, av.date_start_value::text) IS NOT NULL
+        LEFT JOIN occurrence_attributes a ON a.id=av.occurrence_attribute_id AND a.deleted=false
+        LEFT JOIN termlists_terms tlt ON tlt.id=av.int_value AND a.data_type='L' AND tlt.deleted=false
+        LEFT JOIN terms t ON t.id=tlt.term_id AND t.deleted=false
+        WHERE q.entity='occurrence_attribute_value' AND q.task='task_cache_builder_attr_value_occurrence' AND claimed_by=$procIdEsc
+        GROUP BY avfilt.occurrence_id, av.occurrence_attribute_id, a.multi_value
+        $langTermSql
+      ) AS subquery
+      GROUP BY occurrence_id;
 
-UPDATE cache_occurrences_nonfunctional u
-SET attrs_json=a.attrs
-FROM attrs a
-WHERE a.occurrence_id=u.id;
+      UPDATE cache_occurrences_nonfunctional u
+      SET attrs_json=a.attrs
+      FROM attrs a
+      WHERE a.occurrence_id=u.id;
 
--- Force tracking update.
-UPDATE cache_occurrences_functional u
-SET website_id=u.website_id
-FROM attrs a
-WHERE a.occurrence_id=u.id;
+      -- Force tracking update.
+      UPDATE cache_occurrences_functional u
+      SET website_id=u.website_id
+      FROM attrs a
+      WHERE a.occurrence_id=u.id;
 
-DROP TABLE attrs;
+      DROP TABLE attrs;
 
-SQL;
+    SQL;
     $db->query($sql);
-    kohana::log('debug', $sql);
   }
 
 }
