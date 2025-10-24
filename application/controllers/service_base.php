efb5e65862069a031c61a5769f4b29165c80e12e
John van Breda <john.vanbreda@biodiverseit.co.uk>
2025-10-24 09:57:13 +0100

    Thumbnail generation image failure message
    https://github.com/BiologicalRecordsCentre/iRecord/issues/1942

--- application/controllers/service_base.php
+++ application/controllers/service_base.php
@@ -310,8 +310,9 @@ class Service_Base_Controller extends Controller {
       $statusCode = 400;
     }
     elseif (strpos($message, 'imagecreatefrom') !== FALSE) {
+      // Special case for errors during image resizing.
       $statusCode = 400;
-      $message = 'Image processing error: ensure the image is a valid format and not corrupted.';
+      $message = 'Image processing error: ensure the image is a valid format and not corrupted. The image has been saved but thumbnail generation failed.';
       error_logger::log_error('Image processing error in data services', $e);
     }
     elseif ($e instanceof AuthenticationError || $e instanceof AuthorisationError) {
