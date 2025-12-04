df82f2664ed4934ec6d7c51252a8be5d9aef0f09
Andrew van Breda <support@avb-it.co.uk>
2025-12-04 11:32:30 +0000

    Fix problem where report errored due to json column in Group By.

--- application/config/version.php
+++ application/config/version.php
@@ -29,14 +29,14 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '9.15.15';
+$config['version'] = '9.15.16';
 
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2025-11-25';
+$config['release_date'] = '2025-12-04';
 
 /**
  * Link to the code repository downloads page.
