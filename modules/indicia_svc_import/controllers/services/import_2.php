f6fff95d2f231636c1e9c32437133d04f6120670
John van Breda <john.vanbreda@biodiverseit.co.uk>
2025-12-09 11:13:40 +0000

    Batch limit changes when loading table for large imports
    Otherwise too slow (compromise due to memory use).

--- modules/indicia_svc_import/controllers/services/import_2.php
+++ modules/indicia_svc_import/controllers/services/import_2.php
@@ -2041,7 +2041,8 @@ SQL;
    */
   private function loadNextRecordsBatch($fileName, array &$config) {
     $importTools = new ImportTools();
-    $batchLimit = 1000;
+    // Larger batch size for big imports is more efficient at expensive of progress granularity.
+    $batchLimit = max(min(round($config['totalRows'] / 20), 10000), 500);
     $file = $importTools->openSpreadsheet($fileName, $config, $batchLimit);
     $rows = [];
     $rowsDoneInBatch = 0;
