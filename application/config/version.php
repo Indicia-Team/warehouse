38e1b05468c4a3e52e3b10b432da66652f87f9ac
John van Breda <john.vanbreda@biodiverseit.co.uk>
2026-01-13 12:09:57 +0000

    Version corrected

--- application/config/version.php
+++ application/config/version.php
@@ -29,14 +29,14 @@ defined('SYSPATH') or die('No direct script access.');
  *
  * @var string
  */
-$config['version'] = '9.16.0';
+$config['version'] = '9.17.0';
 
 /**
  * Version release date.
  *
  * @var string
  */
-$config['release_date'] = '2026-01-05';
+$config['release_date'] = '2026-01-13';
 
 /**
  * Link to the code repository downloads page.
